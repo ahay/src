8497942efa37a95517740cf78cbdca663a0f3965
Sergey Fomel <sergey.fomel@gmail.com>
2003-06-03 01:19:07 +0000

    new prog subs for randrefl

---
+++ filt/proc/randn.c
@@ -0,0 +1,39 @@
+#include <math.h>
+
+#include "randn.h"
+#include "random.h"
+
+static const float s = 0.449871, t = -0.386595;
+static const float a = 0.19600, b = 0.25472;
+static const float r1 = 0.27597, r2 = 0.27846;
+  
+float randn_one (void)
+{
+    float u, v, x, y, q;
+    
+    do {
+	u = random0 ();
+	v = random0 ();
+	v = 1.7156 * (v - 0.5);
+	/*     Evaluate the quadratic form */
+	x = u - s;
+	y = fabsf(v) - t;
+	q = x*x + y*(a*y - b*x);
+	/*     Accept P if inside inner ellipse */
+	if (q < r1) break;
+	/*     Reject P if outside outer ellipse */
+	if (q > r2) continue;
+	/*     Reject P if outside acceptance region */
+    } while (v*v >= -4.0*logf(u)*u*u);
+    return (v/u);
+}
+
+void randn (int nr, float *r)
+{
+    int i;
+
+    for (i = 0; i < nr; i++) {
+	r[i] = randn_one ();
+    }
+}
+
---
+++ filt/proc/randn.h
@@ -0,0 +1,7 @@
+#ifndef _randn_h
+#define _randn_h
+
+float randn_one (void);
+void randn (int nr, float *r);
+
+#endif
