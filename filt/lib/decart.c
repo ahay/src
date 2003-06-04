9affb6b89170e8e8c4b1cd2e4f0432cd5d899bb3
Sergey Fomel <sergey.fomel@gmail.com>
2003-06-04 01:23:39 +0000

    shooting example shooting debugged randrefl completed decart bug

--- filt/lib/decart.c
+++ filt/lib/decart.c
@@ -13,6 +13,8 @@ void sf_line2cart( int dim, const int* nn, int i, int* ii) {
 int sf_cart2line( int dim, const int* nn, const int* ii) {
     int i, axis;
 
+    if (dim < 1) return 0;
+
     i = ii[dim-1];
     for (axis = dim-2; axis >= 0; axis--) {
 	i = i*nn[axis] + ii[axis];
