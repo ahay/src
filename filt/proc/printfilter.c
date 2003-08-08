4b45163d570509316b879e8380733f259b17832c
Sergey Fomel <sergey.fomel@gmail.com>
2003-08-08 06:09:49 +0000

    helix printing

---
+++ filt/proc/printfilter.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <alloca.h>
+#include <stdio.h>
+
+#include <rsf.h>
+
+#include "printfilter.h"
+#include "boxfilter.h"
+#include "helix.h"
+
+void print (int dim, const int *nd, const int *center, const int *na, 
+	    const filter aa) 
+{
+    float* filt; 
+    int ii[SF_MAX_DIM], ii0[SF_MAX_DIM], ia, i, j, k, nf, br; 
+
+    nf = 1;
+    for (j=0; j < dim; j++) {
+	nf *= na[j];
+    }
+    filt = (float*) alloca(nf*sizeof(float));
+
+    box (dim, nd, center, na, aa, nf, filt); 
+    fprintf(stderr,"-----------------------------------------\n");
+
+    for (j=0; j < dim; j++) {
+	ii0[j] = 1;
+    }
+
+    for (ia = 0; ia < nf - na[0]+1; ia += na[0]) {
+	sf_line2cart(dim, na, ia, ii);
+	br = 0;
+	for (j=0; j < dim; j++) {
+	    if (ii[j] != ii0[j]) br++;
+	    ii0[j] = ii[j];
+	}
+
+        for (i=0; i < br-1; i++) {
+	    fprintf (stderr,"+++++++++++++++++++++++++++++++++++++++++\n");
+	    for (k=ia; k < ia+na[0]-1; k++) {
+		fprintf(stderr,"%7.3f",filt[k]);
+	    }
+	    fprintf (stderr,"\n");
+	}
+    }
+    
+    fprintf (stderr,"-----------------------------------------\n");
+}
+
---
+++ filt/proc/printfilter.h
@@ -0,0 +1,9 @@
+#ifndef _printfilter_h
+#define _printfilter_h
+
+#include "helix.h"
+
+void print (int dim, const int *nd, const int *center, const int *na, 
+	    const filter aa);
+
+#endif
