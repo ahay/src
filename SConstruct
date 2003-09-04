b41d3b95f0dbf5fe4de510d086b282d96d80bea4
Sergey Fomel <sergey.fomel@gmail.com>
2003-09-04 02:50:10 +0000

    documentation

--- SConstruct
+++ SConstruct
@@ -3,3 +3,19 @@ SConscript(dirs=['seis/rsf','seis/main','seis/proc','seis/imag'],
 SConscript(dirs=['vplot/lib','vplot/main'],
            name='SConstruct')
 
+import os
+
+env = Environment()
+
+doc = """echo 'import sys, os' > $TARGET
+echo "sys.path.append(os.environ.get('RSFROOT'))" >> $TARGET
+echo "import rsfdoc" >> $TARGET
+echo " " >> $TARGET
+echo "rsfprog={}" >> $TARGET
+cat $SOURCES >> $TARGET"""
+
+mains = ['seis/main','seis/proc','seis/imag','vplot/main']
+
+env.Command('rsfprogs.py',map(lambda x: os.path.join(x,'RSFdoc'),mains),doc)
+
+
--- rsfdoc.py
+++ rsfdoc.py
@@ -1,32 +1,152 @@
 import pydoc
+import re, sys, os
 
 def bold(text):
     """Format a string in bold by overstriking."""
     return ''.join(map(lambda ch: ch + "\b" + ch, text))
 
-def show(head,body):
-    text = "\n".join(map(lambda line: "\t" + line, body.split("\n")))
-    pydoc.pager(bold(head.upper()) + "\n" + text)
+def underline(text):
+    """Format a string in underline by overstriking."""
+    return ''.join(map(lambda ch: ch + "\b_", text))
 
-class par:
-    def __init__(self,name,type,default=None,desc=None):
-        self.name = name
-        self.type = type
-        self.default = self.default
-        self.desc = self.desc
-    
+def section(head,body):
+    text = "\n".join(map(lambda line: "\t" + line, body.split("\n")))
+    return bold(head.upper()) + "\n" + text + "\n"
+
+class rsfpar:
+    def __init__(self,type,default='',range='',desc=''):
+        self.type = underline(type) + " "
+        self.default = "=" + str(default)
+        self.range = " " + range
+        self.desc = "\t" + desc + "\n"
+    def show(self,name):
+        return self.type + bold(name + self.default) + self.range + self.desc
+
 class rsfprog:
-    def __init__(self,name,file,desc=None):
+    def __init__(self,name,file,dir,desc=None):
         self.name = name
         self.file = file
+        self.prog = os.path.join(dir,name) 
         self.desc = desc
+        self.snps = None
+        self.cmts = None
+        self.also = None
         self.pars = {}
-    def add_par (par):
-        self.pars[par.name] = par
-    def doc(self):
-        show('name',self.name)
+    def synopsis (self,snps,cmts):
+        self.snps = snps
+        self.cmts = cmts
+    def par (self,name,par):
+        self.pars[name] = par
+    def document(self):
+        doc = section('name',self.name)
         if self.desc:
-            show('description',self.desc)
+            doc += section('description',self.desc)
+        if self.snps:
+            doc += section('synopsis',self.snps)
+        if self.cmts:
+            doc += section('comments',self.cmts)
+        pars =  self.pars.keys()
+        if pars:
+            pars.sort()
+            pardoc = ''
+            for par in pars:
+                pardoc += self.pars[par].show(par)
+            doc += section('parameters',pardoc.rstrip())
+        if self.also:
+            doc += section('see also',self.also)
+        doc += section('source',self.file)
+        pydoc.pager(doc)
 
-sfin = rsfprog('sfin','sfin.c')
-sfin.doc()
+comment = None
+param = None
+string = None
+synopsis = None
+
+rsfprefix = 'sf'
+
+def getprog(file,out):
+    global comment, param, synopsis, string
+    if not comment:
+        comment = re.compile(r'\/\*((?:[^*]|\*[^/])+)\*\/')
+        param = re.compile(r'if\s*\(\!sf_get(?P<type>bool|int|float)\s*'
+                           '\(\s*\"(?P<name>\w+)\"\s*\,\s*\&(?P<var>\w+)\s*'
+                           '\)\s*\)\s*[\{]?\s*'
+                           '(?:(?P=var)\s*\=\s*(?P<default>[^\;]+))?'
+                           '\;\s*(?:\/\*\s*(?P<desc>(?:[^*]|\*[^/])+)\*\/)?')
+        string = re.compile(r'sf_getstring\s*\(\s*\"(?P<name>\w+)\"[^\;]*\;'
+                            '\s*(?:\/\*\s*(?P<desc>(?:[^*]|\*[^/])+)\*\/)?')
+        synopsis = re.compile(r'\s*Takes\s*\:\s*((?:[^\n]|[\n][^\n])+)'
+                              '((?:.|\n)*)$')
+    dir = os.path.dirname(os.path.abspath(file))
+    print dir
+    name = rsfprefix + re.sub('^M','',os.path.basename(file))
+    name = re.sub('.c$','',name)
+    src = open(file,"r")   # open source
+    text = ''.join(src.readlines())
+    src.close()
+    first = comment.match(text)
+    if first:
+        tops = first.group(1).split("\n")
+        desc = tops.pop(0).lstrip()
+        first = "\n".join(tops)
+    else:
+        desc = None
+    prog = rsfprog(name,file,dir,desc)
+    out.write("%s = rsfdoc.rsfprog('%s','%s','%s','%s')\n" %
+              (name,name,file,dir,desc))
+    pars = param.findall(text)
+    parline = ''
+    for par in pars:
+        type = par[0]
+        parname = par[1]
+        default = par[3]
+        desc = par[4]
+        range = ''
+        if (type == 'bool'):
+            if (default == 'true'):
+                default = 'y'
+            else:
+                default = 'n'
+            type = 'bool  ' # to align with string
+            range = '[y/n]'
+        elif (type == 'int'):
+            type = 'int   ' # to align with string
+        elif (type == 'float'):
+            type = 'float ' # to align with string
+        prog.par(parname,rsfpar(type,default,range,desc))
+        out.write("%s.par('%s',rsfdoc.rsfpar('%s','%s','%s','''%s'''))\n" %
+                  (name,parname,type,default,range,desc))
+        parline += " %s=%s" % (parname,default)
+    pars = string.findall(text)
+    for par in pars:
+        type = 'string'
+        parname = par[0]
+        desc = par[1]
+        prog.par(parname,rsfpar("string",desc=desc))
+        out.write("%s.par('%s',rsfdoc.rsfpar('string',desc='''%s'''))\n" %
+                  (name,parname,desc))
+        parline += " %s=" % (parname)
+    if first:
+        info = synopsis.match(first)
+        if info:
+            snps = name + " " + info.group(1).lstrip() + parline
+            cmts = info.group(2).lstrip()
+            prog.synopsis(snps,cmts)
+            out.write("%s.synopsis('''%s''','''%s''')\n" % (name,snps,cmts))
+    out.write("rsfprog['%s']=%s\n\n" % (name,name))
+
+if __name__ == "__main__":
+    junk = open('junk.py',"w")
+    junk.write("import rsfdoc\n\n")
+    junk.write("rsfprog = {}\n")
+    getprog('seis/main/dd.c',junk)
+    junk.write("sfdd.document()\n\n")
+    junk.write("print sfdd.prog\n\n")
+    junk.close()
+    #
+    import junk
+    #
+    os.unlink("junk.py")
+    os.unlink("junk.pyc")
+
+
