abf37882e9743905f4d1737990c0118442c36cf3
Sergey Fomel <sergey.fomel@gmail.com>
2003-09-15 14:45:57 +0000

    plotting plotting plotting plotting plotting

--- filt/main/headermath.c
+++ filt/main/headermath.c
@@ -1,3 +1,8 @@
+/* Mathematical operations, possibly on header keys.
+
+Takes: < input.rsf > output.rsf
+*/
+
 #include <string.h>
 
 #include <rsf.h>
@@ -7,16 +12,17 @@ int main(int argc, char* argv[])
     int i, i1, i2, n1, n2, len, nkey;
     sf_file in, out;
     char *eq, *output, *key, *arg;
-    float **ftra, **fbuf, **fst;
+    float **ftra, **fbuf, **fst, d2, o2;
 
     sf_init (argc,argv);
     in = sf_input ("in");
     out = sf_output ("out");
 
     sf_putint(out,"N",0);
+    sf_putint(out,"T",1);
 
     for (i=0; i < SF_NKEYS; i++) {
-	sf_putint(out,sf_segykeyword(i),i+1);
+	sf_putint(out,sf_segykeyword(i),i+2);
     }
 
     for (i=1; i< argc; i++) { /* collect inputs */
@@ -27,11 +33,11 @@ int main(int argc, char* argv[])
 	
 	len = (size_t) (eq-arg);
 	key = sf_charalloc(len+1);
-	strncpy(key,arg,len);
+	memcpy(key,arg,len);
 	key[len]='\0';
 
 	if (sf_getint(key,&nkey))
-	    sf_putint(out,key,nkey+1);
+	    sf_putint(out,key,nkey+2);
 	free(key);
     }
 
@@ -39,31 +45,35 @@ int main(int argc, char* argv[])
 
     if (!sf_histint(in,"n1",&n1)) n1=1;
     if (!sf_histint(in,"n2",&n2)) n2=1;
-    if (n1 > 1) {
-	if (n2 > 1) {
+    if (n1 > 1) { 
+	if (n2 > 1) { /* input: many keys */
 	    sf_putint(out,"n1",1);
-	} else {
+	} else { /* input: one key, arranged in n1 */
 	    n2 = n1;
 	    n1 = 1;
 	}
     }
+
+    if (!sf_histfloat(in,n1>1? "d2":"d1",&d2)) d2=1.;
+    if (!sf_histfloat(in,n1>1? "o2":"o1",&o2)) o2=0.;
     
     if (NULL == (output = sf_getstring("output"))) sf_error("Need output=");
 
     len = sf_math_parse (output,out);
     
     ftra = sf_floatalloc2(n1,n2);
-    fbuf = sf_floatalloc2(n2,n1+1);
+    fbuf = sf_floatalloc2(n2,n1+2);
     fst  = sf_floatalloc2(n2,len+2);
     
     sf_read(ftra[0],sizeof(float),n1*n2,in);
 
     for (i2=0; i2 < n2; i2++) {
 	fbuf[0][i2]=(float) i2;
+	fbuf[1][i2]=o2+i2*d2;
     }
     for (i1=0; i1 < n1; i1++) {
 	for (i2=0; i2 < n2; i2++) {
-	    fbuf[i1+1][i2]=ftra[i2][i1];
+	    fbuf[i1+2][i2]=ftra[i2][i1];
 	}
     }
 
