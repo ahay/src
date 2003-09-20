7b9c7d4636e755bf0cb4a988f60820de79a89c60
Sergey Fomel <sergey.fomel@gmail.com>
2003-09-20 06:10:49 +0000

    annotate

--- filt/imag/Munif2.c
+++ filt/imag/Munif2.c
@@ -7,6 +7,8 @@ In each layer, velocity is a linear function of position.
 Inspired by SU's unif2.
 */
 
+/* 	$Id: Munif2.c,v 1.2 2003/09/20 06:10:49 fomels Exp $	 */
+
 #include <rsf.h>
 
 int main(int argc, char **argv)
