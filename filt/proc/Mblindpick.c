579add223bce51d65845cd876ddb1e764a6f8fa7
Sergey Fomel <sergey.fomel@gmail.com>
2004-05-07 03:40:04 +0000

    blindpick milo 3-D

--- filt/proc/Mblindpick.c
+++ filt/proc/Mblindpick.c
@@ -1,129 +1,115 @@
-/* Automatic picking from semblance-like panels.
+/* Automatic picking from semblance-like panels using shaping regularization.
 
-Takes: < semblance.rsf > pick.rsf
+Takes: < semblance.rsf [ampl=ampl.rsf] > pick.rsf
 */
+#include <math.h>
+#include <float.h>
 
 #include <rsf.h>
 
-#include "tridiagonal.h"
+#include "divn.h"
 
 int main(int argc, char* argv[])
 {
-    tris slv;
-    int nt, ns, nx, ix, is, it, iter, niter;
-    float **slice, *pick, *pick0, *ampl, *diag, *offd;
-    float s0, ds, eps, lam, t, asum;
+    int dim, n[SF_MAX_DIM], rect[SF_MAX_DIM], nd;
+    int ns, nt, nm, nx, i, ix, is, it, niter, imax;
+    float **slice, *pick0, *pick, *ampl;
+    float s0, ds, t, asum, ai, amax, ap, am, num, den, dx;
+    char key[6];
     sf_file in, out;
 
     sf_init (argc,argv);
     in = sf_input("in");
     out = sf_output("out");
 
-    if (!sf_histint(in,"n1",&nt)) sf_error("No n1= in input");
-    if (!sf_histint(in,"n2",&ns)) ns=1;
-    nx = sf_leftsize(in,2);
+    dim = sf_filedims (in,n);
+    if (dim < 2) sf_error("Need at least two dimensions");
+
+    nd = 1;
+    for (i=0; i < dim; i++) {
+	nd *= n[i];
+	if (n[i] > 1) {
+	    snprintf(key,6,"rect%d",i+1);
+	    if (!sf_getint(key,rect+i)) rect[i]=1;
+	} else {
+	    rect[i]=1;
+	}
+    }
+    nt = n[0];
+    ns = n[1];
+    nm = nd/ns;
+    nx = nm/nt;
 
     if (!sf_histfloat(in,"o2",&s0)) sf_error("No o2= in input");
     if (!sf_histfloat(in,"d2",&ds)) sf_error("No d2= in input");
     sf_putint(out,"n2",1);
 
-    if (!sf_getfloat("eps",&eps)) eps=0.01;
-    /* Vertical smoothness parameter */
-    if (!sf_getfloat("lam",&lam)) lam=0.01;
-    /* Horizontal smoothness parameter */
-    if (!sf_getint("niter",&niter)) niter=1;
-    /* Number of picking iterations */
+    if (!sf_getint("niter",&niter)) niter=100;
+    /* number of iterations */
+
+    divn_init(dim,nd,n,rect,niter);
 
     slice = sf_floatalloc2(nt,ns);
-    pick = sf_floatalloc(nt);
-    pick0 = sf_floatalloc(nt);
-    offd = sf_floatalloc(nt);
-    diag = sf_floatalloc(nt);
-    ampl = sf_floatalloc(nt);
 
-    slv = tridiagonal_init (nt);
-
-    for (it=0; it < nt; it++) {
-	offd[it] = -eps;
-    }
+    pick0 = sf_floatalloc(nm);
+    pick = sf_floatalloc(nm);
+    ampl = sf_floatalloc(nm);
 
     for (ix = 0; ix < nx; ix++) {
 	sf_floatread (slice[0],nt*ns,in);
 
-	if (ix > 0) {
-	    for (it = 0; it < nt; it++) { /* previous trace */
-		pick0[it] = pick[it];
+	/* pick blind maximum */
+	for (it = 0; it < nt; it++) {
+	    i = ix*nt+it;
+
+	    imax = 0;
+	    amax = 0.;
+	    for (is = 0; is < ns; is++) {
+		t = slice[is][it];
+		if (t > amax) {
+		    amax = t;
+		    imax = is;
+		}
+	    }
+
+	    /* quadratic interpolation for sub-pixel accuracy */
+	    if (imax > 0 && imax < ns-1) {
+		am = slice[imax-1][it];
+		ap = slice[imax+1][it];
+		num = 0.5*(am-ap);
+		den = am+ap-2.*amax;
+		dx = num*den/(den*den + FLT_EPSILON);
+		if (fabsf(dx) >= 1.) dx=0.;
+
+		ampl[i] = amax - 0.5*dx*dx*den;
+		pick0[i] = s0+(imax+dx)*ds;
+	    } else {
+		ampl[i] = amax;
+		pick0[i] = s0+imax*ds;
 	    }
 	}
+    }
+    
+    /* normalize amplitudes */
+    asum = 0.;
+    for (i = 0; i < nm; i++) {
+	ai = ampl[i];
+	asum += ai*ai;
+    }
+    asum = sqrtf (asum/nm);
 
-	for (iter=0; iter < niter; iter++) {
-	    if (0==iter) { /* pick blind maximum */
-		for (it = 0; it < nt; it++) {
-		    ampl[it] = 0.;
-		    pick[it] = s0+0.5*(ns-1)*ds;
-		}
-
-		for (is = 0; is < ns; is++) {
-		    for (it = 0; it < nt; it++) {
-			t = slice[is][it];
-			/* t *= t; */
-			if (t > ampl[it]) {
-			    ampl[it] = t;
-			    pick[it] = s0+is*ds;
-			}
-		    }
-		}
-	    } else {
-		for (it = 0; it < nt; it++) {
-		    /* nearest neighbor interpolation */
-		    is = 0.5+(pick[it]-s0)/ds;
-		    if (is < 0) {
-			is=0;
-			pick[it]=s0;
-		    } else if (is > ns-1) {
-			is=ns-1;
-			pick[it] = s0 + (ns-1)*ds;
-		    }
-		    /*
-		    t = slice[is][it];
-		    ampl[it] = t*t;
-		    */
-		    ampl[it] = slice[is][it];
-		}
-	    }
-
-	    /* normalize amplitudes */
-	    asum = 0.;
-	    for (it = 0; it < nt; it++) {
-		ampl[it] *= ampl[it];
-		asum += ampl[it];
-	    }
-	    for (it = 0; it < nt; it++) {
-		t = ampl[it]/asum;
-		diag[it] = 2.*eps + t;
-		pick[it] *= t;
-	    }
-	    /* boundary conditions */
-	    diag[0] -= eps;
-	    diag[nt-1] -= eps;
-	    
-	    if (ix > 0) { /* lateral smoothing */
-		for (it = 0; it < nt; it++) {
-		    diag[it] += lam;
-		    pick[it] += lam*pick0[it];
-		}
-	    }
-	    
-	    tridiagonal_define (slv,diag,offd);
-	    tridiagonal_solve (slv, pick);    
-	} /* iterations */
-
-	sf_floatwrite (pick,nt,out);	
+    for(i=0; i < nm; i++) {
+	ampl[i] /= asum;
+	pick0[i] *= ampl[i];
     }
 
+    divn(pick0,ampl,pick);
+
+    sf_floatwrite (pick,nm,out);	
+    
     sf_close();
     exit (0);
 }
 
-/* 	$Id: Mblindpick.c,v 1.7 2004/04/19 21:51:46 fomels Exp $	 */
+/* 	$Id: Mblindpick.c,v 1.8 2004/05/07 03:40:04 fomels Exp $	 */
 
