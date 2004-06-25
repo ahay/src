07bccfce4ee983501db8ce953b98d23c29591308
Sergey Fomel <sergey.fomel@gmail.com>
2004-06-25 18:09:01 +0000

    latex processing
    documentation
    
    GPL note
    
    artifact
    
    GPL note

--- plot/test/nmotraj.c
+++ plot/test/nmotraj.c
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2004 University of Texas at Austin
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #include <math.h>
 
 #include <rsfplot.h>
@@ -77,3 +95,4 @@ static void onebox(float x, float y, float dx, float dy )
     vp_udraw( vx[3], vy[3]);
 }
 
+/* 	$Id: nmotraj.c,v 1.5 2004/06/25 18:09:01 fomels Exp $	 */
