78a918cda02891e7398e3cad15d24fa186b63072
Sergey Fomel <sergey.fomel@gmail.com>
2004-09-24 07:55:36 +0000

    compress.h auto-generated

--- filt/proc/compress.c
+++ filt/proc/compress.c
@@ -1,11 +1,36 @@
+/* Compress a helical filter */
+/*
+  Copyright (C) 2004 University of Texas at Austin
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #include <math.h>
 
 #include <rsf.h>
+/*^*/
 
 #include "compress.h"
-#include "helix.h"
 
-filter compress( filter aa, float eps) {
+#include "helix.h"
+/*^*/
+
+filter compress( filter aa /* filter */, 
+		 float eps /* threshold */) 
+/*< return a new filter with coefficients less than eps removed >*/
+{
     filter bb;
     bool* keep;
     int i, k;
@@ -13,7 +38,7 @@ filter compress( filter aa, float eps) {
     keep = sf_boolalloc (aa->nh);
     k = 0;
     for(i=0; i < aa->nh; i++) {
-	if ( fabs( aa->flt[i]) > eps) {
+	if ( fabsf( aa->flt[i]) > eps) {
 	    keep[i] = true;
 	    k++;
 	} else {
@@ -35,4 +60,4 @@ filter compress( filter aa, float eps) {
     return bb;
 }
 
-/* 	$Id: compress.c,v 1.2 2003/10/01 22:45:56 fomels Exp $	 */
+/* 	$Id$	 */
