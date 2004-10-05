c6fd3c1919d88a3ea09cb58afd9af82634dd0687
Sergey Fomel <sergey.fomel@gmail.com>
2004-10-05 10:00:32 +0000

    expder2.h auto-generated

--- filt/proc/expder2.h
+++
@@ -1,9 +0,0 @@
-#ifndef _expder2_h
-#define _expder2_h
-
-#include <rsf.h>
-
-void expder2_init(int m1, int m2, float **aa);
-void expder2_lop (bool adj, bool add, int nx, int ny, float *xx, float *yy);
-
-#endif
