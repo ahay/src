b9510dff09914f3a1543d9d3f93b6a1890f61985
Sergey Fomel <sergey.fomel@gmail.com>
2004-12-18 01:57:58 +0000

    test

--- filt/main/cut.c
+++ filt/main/cut.c
@@ -1,21 +1,22 @@
-/* Zero a portion of the dataset.
-*/
+/* Zero a portion of the dataset. 
+
+Reverse of window. */
 /*
-Copyright (C) 2004 University of Texas at Austin
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  Copyright (C) 2004 University of Texas at Austin
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 #include <stdio.h>
@@ -184,4 +185,4 @@ static void seektable(int dim, int *n, int *m, int *f, int *j,
     table[n2] = (n3-t2)*n[0]-f[0]-n1;
 }
 
-/* 	$Id: window.c 691 2004-07-04 19:28:08Z fomels $	 */
+/* 	$Id: cut.c 691 2004-07-04 19:28:08Z fomels $	 */
