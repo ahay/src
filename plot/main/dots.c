2ca71d7b9c7ec244b1764702331af0bd0d830df2
Sergey Fomel <sergey.fomel@gmail.com>
2005-12-06 04:24:36 +0000

    update

--- plot/main/dots.c
+++ plot/main/dots.c
@@ -3,21 +3,21 @@
 Takes: > plot.vpl
 */
 /*
-Copyright (C) 2004 University of Texas at Austin
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  Copyright (C) 2004 University of Texas at Austin
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 
@@ -155,8 +155,8 @@ int main (int argc, char* argv[])
     /* dot radius */
     if(radius > dd2/15) radius = dd2/15;
 
-    tracehigh = overlap * (dots? dd2 - 3*radius: dd2);
-
+    tracehigh = overlap * (dots? dd2 - 3*radius: dd2); 
+    
     for (i3=0; i3 < n3; i3++) {
 	sf_floatread(data[0],n1*n2,in);
 	
@@ -370,4 +370,4 @@ static void circle(int corners,
     vp_area(vx,vy,corners,1,1,1);
 }
 
-/* 	$Id: dots.c,v 1.11 2004/07/02 11:54:57 fomels Exp $	 */
+/* 	$Id$	 */
