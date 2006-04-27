9b0c07ad9b7d9f9cfe87611b9a4775b5533c9b95
Sergey Fomel <sergey.fomel@gmail.com>
2006-04-27 01:05:30 +0000

    update

--- user/nobody/allp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _allp_h
-#define _allp_h
-
-#include <rsf.h>
-
-void allp_init(int nw1, int nj1, int m1, int m2, float **pp1);
-void allp_lop (bool adj, bool add, int nx, int ny, float* xx, float* yy);
-
-#endif
--- user/nobody/xcorr.h
+++
@@ -1,8 +0,0 @@
-#ifndef _xcorr_h
-#define _xcorr_h
-
-float xcorr (int n1, int n2, const float *x1, const float *x2, 
-	     int nc, float *xc);
-#endif
-
-/* 	$Id$	 */
