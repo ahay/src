d15497b56bc5b6a9e1228a6ddbd62c69b201b0a1
Sergey Fomel <sergey.fomel@gmail.com>
2007-02-14 13:59:23 +0000

    placeholders

--- pens/main/ppmpen.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-sys.stderr.write('''
-%s is not installed.
-Check $RSFROOT/lib/rsfconfig.py for PPM
-and reinstall if necessary.
-''' % sys.argv[0])
-sys.exit(1)
