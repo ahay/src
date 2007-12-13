39a457be2b6345428df78d6c847eadae6748613f
Ioan Vlad <i.vlad@yahoo.com>
2007-12-13 00:06:29 +0000

    Towards the Octave API
    Former-commit-id: 77c801a4f1a368569db4a3f5ce54ad91922ed14d

--- filt/lib/rsf_create.m
+++ filt/lib/rsf_create.m
@@ -1,22 +1,55 @@
-% Used by the Octave API
+## Copyright (C) 2007 Ioan Vlad
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{stat}=} rsf_create (@var{file}, @var{arg2})
+##
+## Writes RSF header with desired info to disk.
+## Returns @var{default} if @var{name} is not present in @var{file}
+## If @var{arg2} is a filename, it copies that header
+## If @var{arg2} is a vector with dimensions, creates a header with those 
+## dimensions. If success, @var{stat}.err = false.
+## Else, @var{stat}.err = true and @var{stat}.msg contains the error message.
+##
+## @example
+## @code{[n1, stat] = rsf_par('junk.rsf','n1',1);}
+## or simply
+## @code{n1 = rsf_par('junk.rsf','n1',1);}
+## @end example
+## @seealso{rsf_dim, rsf_par, rsf_read, rsf_write}
+## @end deftypefn
+
 
 function stat = rsf_create( out_filename, arg2 )
     % Writes RSF header with desired info to disk
     %
     % Usage: stat = rsf_create( out_filename, inp_filename | dims )
 
-    docstr = 'Usage: error = rsf_create( out_filename, inp_filename | dims )';
+    docstr = '\nUsage: stat = rsf_create( out_filename, inp_filename | dims )';
 
     stat.err = true;  % assume we will have an error
     stat.msg = [];
 
     if nargin<2
-        stat.msg = ['Too few arguments.\n' docstr];
+        stat.msg = ['Too few arguments.' docstr];
         return
     endif
 
     if nargin>2
-        stat.msg = ['Too many arguments.\n' docstr];
+        stat.msg = ['Too many arguments.' docstr];
         return
     endif
 
@@ -29,7 +62,7 @@ function stat = rsf_create( out_filename, arg2 )
 
         command = ['sffileflush <' arg2 ' >' ];
 
-        [dummy_var sys_stat] = system( command );
+        [dummy_var, sys_stat] = system( command );
 
         if sys_stat!=0
             stat.msg = ['sffileflush failed with code ' num2str(sys_stat)];
@@ -48,7 +81,7 @@ function stat = rsf_create( out_filename, arg2 )
         endfor
         command = [command '>' out_filename];
 
-        [dummy_var sys_stat] = system( command );
+        [dummy_var, sys_stat] = system( command );
 
         if sys_stat!=0
             stat.msg = ['sfcreate failed with code ' num2str(sys_stat)];
--- filt/lib/rsf_dim.m
+++ filt/lib/rsf_dim.m
@@ -1,24 +1,50 @@
-% Used by the Octave API
+## Copyright (C) 2007 Ioan Vlad
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-function [stat, dims] = rsf_dim(in)
-    % Returns a vector "dims" with the dimensions of a rsf file
-    % without trailing dimensions of length 1
-    % 
-    % Usage: [dims stat] = rsf_dim(filename)
+## -*- texinfo -*-
+## @deftypefn {Function File} {[@var{dims}, @var{stat}]=} rsf_dim (@var{in})
+##
+## Returns a vector @var{dims} with the dimensions of RSF header @var{in}
+## without trailing dimensions of length 1.
+## If no error was encountered, @var{stat}.err = false.
+## Else, @var{stat}.err = true and @var{stat}.msg contains the error message
+##
+## @example
+## @code{[n, stat] = rsf_par('junk.rsf');}
+## or simply
+## @code{n = rsf_par('junk.rsf');}
+## @end example
+## @seealso{rsf_create, rsf_par, rsf_read, rsf_write}
+## @end deftypefn
+
+function [dims, stat] = rsf_dim(in)
 
     dims = [];
     stat.err = true; % assume we will have an error
     stat.msg = [];
 
-    docstr = 'Usage: [dims stat] = rsf_dim(filename)';
+    docstr = '\nUsage: [dims, stat] = rsf_dim(filename)';
 
     if nargin==0
-        stat.msg = ['Please specify filename.\n' docstr];
+        stat.msg = ['Please specify filename.' docstr];
         return
     endif
 
     if nargin>1
-        stat.msg = ['Too many arguments.\n' docstr];
+        stat.msg = ['Too many arguments.' docstr];
         return
     endif
 
@@ -29,7 +55,7 @@ function [stat, dims] = rsf_dim(in)
 
     command = ['sffiledims <' in ' parform=n'];
 
-    [stdout, sys_stat] = system( command );
+    [sys_stat, stdout] = system( command );
 
     if sys_stat!=0
         stat.msg = ['sffiledims failed with code ' num2str(sys_stat)];
