6f355637b17d3f905ebc34aae6ad28414367386d
William Burnett <will.burnett@gmail.com>
2008-01-24 18:08:23 +0000

    inmo3 debug
    Former-commit-id: ac9c1274c644c661bbdc744fbd6e0f4d75b97fe1

--- filt/proc/Minmo3.c
+++ filt/proc/Minmo3.c
@@ -86,10 +86,10 @@ int main (int argc, char* argv[])
 	if (1 > CDPtype) CDPtype=1;
 	sf_warning("CDPtype=%d",CDPtype);
 
-	for (ih = 0; ih < nh; ih++) {
+	for (ih = 0; ih < nhx; ih++) {
 	  offx[ih] = h0x + ih*dhx;
 	}
-	for (ih = 0; ih < nh; ih++) {
+	for (ih = 0; ih < nhy; ih++) {
 	  offy[ih] = h0y + ih*dhy;
 	}
 
