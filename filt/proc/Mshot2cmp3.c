d81b1d272d5938705138750eda912f477369de08
William Burnett <will.burnett@gmail.com>
2008-02-06 17:14:27 +0000

    update
    Former-commit-id: 792f754b64e36e8215ca2037cb16c72005429038

--- filt/proc/Mshot2cmp3.c
+++ filt/proc/Mshot2cmp3.c
@@ -1,7 +1,7 @@
-/* Convert shots to CMPs for regular 2-D geometry. 
+/* Convert shots to CMPs for regular 3-D geometry. 
 
-The axes in the input are {time,offset,shot}
-The axes in the output are {time,offset,midpoint}
+The axes in the input are {time, offset_x, offset_y, shot_x, shoty}
+The axes in the output are {time, offset_x, offset_y, midpoint_x, midpoint_y}
 */
 /*
   Copyright (C) 2004 University of Texas at Austin
@@ -27,71 +27,134 @@ The axes in the output are {time,offset,midpoint}
 
 int main(int argc, char* argv[])
 {
-    int type;
+
+    int typex, typey;
     off_t pos;
     bool sign, half;
-    int   ns,    ny,    nh, nh2, nt;
-    int   is,    iy,    ih, ih2=0, *mask;
-    float os,ds, oy,dy, oh,dh;
+    int   nsx, nsy,   nmx, nmy,   nhx, nhy,   nmhx, nmhy,   nt;
+    int   isx, isy,   imx, imy,   ihx, ihy,   imhx=0, imhy=0;
+    float osx, osy, dsx, dsy, omx, omy, dmx, dmy, ohx, ohy, dhx, dhy, binx, biny;
+    float s_xmin, s_ymin, r_xmin, r_ymin, s_xmax, s_ymax, r_xmax, r_ymax, survey_xmin, survey_ymin, survey_xmax, survey_ymax;
     char *trace, *zero;
-    sf_file in, out, msk;
+    sf_file in, out;
+
 
     sf_init(argc,argv);
     in  = sf_input ( "in");
     out = sf_output("out");
+    sf_warning("WARNING: This 3-D CMP sorting code is not yet ready for use--this message will be removed when it is. ");
 
     if (!sf_histint  (in,"n1",&nt)) sf_error("No n1= in input");
+    /* Number of samples per trace */
 
-    if (!sf_histint  (in,"n2",&nh)) sf_error("No n2= in input");
-    if (!sf_histfloat(in,"o2",&oh)) sf_error("No o2= in input");
-    if (!sf_histfloat(in,"d2",&dh)) sf_error("No d2= in input");
-
-    if (!sf_histint  (in,"n3",&ns)) sf_error("No n3= in input");
-    if (!sf_histfloat(in,"d3",&ds)) sf_error("No d3= in input");
-    if (!sf_histfloat(in,"o3",&os)) sf_error("No o3= in input");
+    if (!sf_histint  (in,"n2",&nhx)) sf_error("No n2= in input");
+    /* Number of offsets per shot gather in x-direction*/
+    if (!sf_histfloat(in,"o2",&ohx)) sf_error("No o2= in input");
+    /* First offset x-component */
+    if (!sf_histfloat(in,"d2",&dhx)) sf_error("No d2= in input");
+    /* Offset increment in x-direction */
+    if (!sf_histint  (in,"n3",&nhy)) sf_error("No n3= in input");
+    /* Number of offsets per shot gather in y-direction*/
+    if (!sf_histfloat(in,"o3",&ohy)) sf_error("No o3= in input");
+    /* First offset y-component */
+    if (!sf_histfloat(in,"d3",&dhy)) sf_error("No d3= in input");
+    /* Offset increment in y-direction */
+    if (!sf_histint  (in,"n4",&nsx)) sf_error("No n4= in input");
+    /* Number of sources along x-direction*/
+    if (!sf_histfloat(in,"d4",&dsx)) sf_error("No d4= in input");
+    /* Source spacing in x-direction*/
+    if (!sf_histfloat(in,"o4",&osx)) sf_error("No o4= in input");
+    /* First source x-coordinate*/
+    if (!sf_histint  (in,"n5",&nsy)) sf_error("No n5= in input");
+    /* Number of sources along y-direction*/
+    if (!sf_histfloat(in,"d5",&dsy)) sf_error("No d5= in input");
+    /* Source spacing in y-direction*/
+    if (!sf_histfloat(in,"o5",&osy)) sf_error("No o5= in input");
+    /* First source y-coordinate*/
     
+    if (!sf_getfloat("binx",&binx)) sf_error("No x bin size specified. Please set binx=");
+    /*Number of bins along x-direction*/
+    if (!sf_getfloat("biny",&biny)) sf_error("No y bin size specified. Please set biny=");
+    /*Number of bins along y-direction*/
+
     if (!sf_getbool("positive",&sign)) sign=true;
-    /* initial offset orientation */
+    /* initial offset orientation:
+       yes is generally for off-end surveys, where the first offsets are positive.  
+       no is generally for split-spread surveys with first negative then positive offsets. */
 
     if (!sf_getbool("half",&half)) half=true;
     /* if y, the second axis is half-offset instead of full offset*/
 
     if (!half) {
-	dh /= 2;
-	oh /= 2;
+	dhx /= 2;
+	ohx /= 2;
+	dhy /= 2;
+	ohy /= 2;
     }
 
-    type = 0.5 + ds/dh;
+    s_xmin = osx;
+    s_ymin = osy;
+    s_xmaz = osx+(nsx*dsx);
+    s_ymax = osy+(nsy*dsy);
     
-    dy = dh;
-    oy = sign ? os + oh: os - oh - type*((nh-1)/type)*dh;
-    ny = ns*type + nh - 1;
+    r_xmin = s_xmin+2*ohx;
+    r_ymin = s_ymin+2*ohy;
+    r_xmax = s_xmax+2*(ohx+dhx*nhx);
+    r_ymax = s_ymax+2*(ohy+dhy*nhy);
 
-    nh2 = (nh+type-1)/type;
-    sf_putint  (out,"n2",nh2);
-    sf_putfloat(out,"d2",type*dh);
-
-    sf_putint  (out,"n3",ny);
-    sf_putfloat(out,"d3",dy);
-    sf_putfloat(out,"o3",oy);
-
-    sf_putstring(out,"label3","Midpoint");
-
-    nt *= sf_esize(in);
-
-    if (NULL != sf_getstring("mask")) {
-	msk = sf_output("mask");
-	sf_settype(msk,SF_INT);
-
-	sf_putint(msk,"n1",nh2);
-	sf_putint(msk,"n2",ny);
-	sf_putint(msk,"n3",1);
-
-	mask = sf_intalloc(nh2);
-    } else {
-	msk = NULL;
-	mask = NULL;
+    if (s_xmin <= r_xmin){
+      survey_xmin = s_xmin;
+    }else{
+      survey_xmin = r_xmin;
     }
+
+    if (s_ymin <= r_ymin){
+      survey_ymin = s_ymin;
+    }else{
+      survey_ymin = r_ymin;
+    }
+
+    if (s_xmax <= r_xmax){
+      survey_xmax = r_xmax;
+    }else{
+      survey_xmax = s_xmax;
+    }
+
+    if (s_ymax <= r_ymax){
+      survey_ymax = r_ymax;
+    }else{
+      survey_ymax = s_ymax;
+    }
+    
+    dmx = (survey_xmax - survey_xmin)/binx;
+    dmy = (survey_ymax - survey_ymin)/biny;
+    omx = survey_xmin + dmx/2.;
+    omy = survey_ymin + dmy/2.;
+    nmx = binx;
+    nmy = biny;
+    nmhx = (int)(nhx*nsx/nmx);
+    nmhy = (int)(nhy*nsy/nmy);
+    dmhx = dhx;
+    dmhy = dhy;
+
+    sf_putint  (out,"n2",nmhx);
+    sf_putint  (out,"n3",nmhy);
+    sf_putfloat(out,"d2",dmhx);
+    sf_putfloat(out,"d3",dmhy);
+
+    sf_putint  (out,"n4",nmx);
+    sf_putint  (out,"n5",nmy);
+    sf_putfloat(out,"d4",dmx);
+    sf_putfloat(out,"d5",dmy);
+    sf_putfloat(out,"o4",omx);
+    sf_putfloat(out,"o5",omy);
+
+    sf_putstring(out,"label3","hx");
+    sf_putstring(out,"label4","hy");
+    sf_putstring(out,"label5","mx");
+    sf_putstring(out,"label6","my");
+    
+    nt *= sf_esize(in);
 
     trace = sf_charalloc(nt);
     zero  = sf_charalloc(nt);
@@ -101,25 +164,30 @@ int main(int argc, char* argv[])
     sf_setform( in,SF_NATIVE);
     sf_setform(out,SF_NATIVE);
     
-    sf_unpipe(in,(off_t) ns*nh*nt);
+    sf_unpipe(in,(off_t) nsx*nsy*nhx*nhy*nt);
     pos = sf_tell(in);
 
-    for (iy=0; iy < ny; iy++) {
-	if (NULL != msk) ih2=0;
-	for (ih=iy%type; ih < nh+iy%type; ih += type) {
-	    is = sign? (iy - ih)/type : (iy + ih)/type - (nh - 1)/type;
+    for (isx=0; isx < nsx; isx++) {
+      for (isy=0; isy < nsy; isy++) {
 
-	    if (is >= 0 && is < ns && ih < nh) {
-		sf_seek(in,pos+(is*nh+ih)*nt,SEEK_SET);
+	for (ihx=0; ihx < nhx; ihx++) {
+	  for (ihy=0; ihy < nhy; ihy++) {
+
+	    imx = (int)((isx + ihx)/2.0);
+	    imy = (int)((isy + ihy)/2.0);
+
+	    if (isx >= 0 && isx < nsx && ihx < nhx) {
+	      if (isy >= 0 && isy < nsy && ihy < nhy) {
+		sf_seek(in,pos+((isx*nhx+ihx)+(isy*nhy+ihy))*nt,SEEK_SET);
 		sf_charread(trace,nt,in);
 		sf_charwrite(trace,nt,out);
-		if (NULL != msk) mask[ih2++] = 1;
+	      }
 	    } else {
 		sf_charwrite(zero,nt,out);
-		if (NULL != msk) mask[ih2++] = 0;
 	    }
+	  }
 	}
-	if (NULL != msk) sf_intwrite(mask,nh2,msk);
+      }
     }
 
     sf_close();
