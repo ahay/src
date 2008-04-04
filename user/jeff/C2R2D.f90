8ded4a9307f7c88dbe1771b053561d2130d7b549
Sergey Fomel <sergey.fomel@gmail.com>
2008-04-04 11:50:03 +0000

    update
    Former-commit-id: adfdcf2040ce500d21d435c183eac835b42cac44

--- user/jeff/C2R2D.f90
+++ user/jeff/C2R2D.f90
@@ -15,6 +15,7 @@
 !!$  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 module C2R2D
   use rsf
+! use nonexistant
   implicit none
 
   integer, private :: nsx,nsz,norm,cnx,cnz,rnx,rnz
