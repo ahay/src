c22b7bca3e43e5f612c79eee9c22eb1e31bef61f
Paul Sava <psava@mines.edu>
2008-04-06 20:42:02 +0000

    Former-commit-id: f1daf2eeef9c09954c1e0f3c2de739ab20948d32

--- user/psava/Mhdefd.c
+++ user/psava/Mhdefd.c
@@ -17,6 +17,10 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 #include <rsf.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 #include "fdutil.h"
 
 #define NOP 2 /* derivative operator half-size */
