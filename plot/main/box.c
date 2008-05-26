1b74065e1e1c01bb1703ac6b10c84cd3436b80f9
Ioan Vlad <i.vlad@yahoo.com>
2008-05-26 20:31:56 +0000

    proper copyright statement
    Former-commit-id: 251c0b1787d011db1f005193330c90e19c027543

--- plot/main/box.c
+++ plot/main/box.c
@@ -1,27 +1,22 @@
 /* Draw a balloon-style label.
 
-Takes: > out.vpl
-*/
+Takes: > out.vpl*/
 /*
-  Copyright (C) 2004 University of Texas at Austin
-  
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ Copyright 1987 the Board of Trustees of the Leland Stanford Junior
+ University. The official license for this software is included in the
+ Madagascar source distribution, in the "COPYRIGHT" section of 
+ pens/docs/vplot.mn , readable as such or formatted with "nroff -man vplot.mn" .
+
+ This file (box.c) has been heavily modified by Sergey Fomel in 2004. Subsequent
+ changes have been logged by the version control system and are visible at
+ http://rsf.svn.sourceforge.net/viewvc/rsf/trunk/plot/main/box.c . The previous
+ proprietary notices that must be preserved as requested by the license are:
+
+ ***************************************************************
+ * AUTHOR: Joe Dellinger (SEP), August 1989
+ ****************************************************************/
 
 #include <math.h>
-
 #include <rsf.h>
 #include <rsfplot.h>
 
@@ -214,5 +209,3 @@ static void rot (float ang, float xin, float yin, float *x, float *y)
     *x = xin * c - yin * s;
     *y = xin * s + yin * c;
 }
-
-/* 	$Id$	 */
