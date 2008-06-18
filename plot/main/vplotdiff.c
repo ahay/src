0a9c9cf46cd60f280e0cfd100e986e59c6b5bc1c
Sergey Fomel <sergey.fomel@gmail.com>
2008-06-18 21:29:59 +0000

    update
    Former-commit-id: 3e8c5e9538e569fa7770950a38c20b57396b3473

--- plot/main/vplotdiff.c
+++ plot/main/vplotdiff.c
@@ -62,7 +62,7 @@
 #define SLOP_FACTOR 1.25
 #define MAX_TXT 40
 
-#define RASTER_TOL 4 /* raster tolerance */
+#define RASTER_TOL 8 /* raster tolerance */
 #define VECTOR_TOL 2 /* vector tolerance */
 
 enum {NTC_NONEED, NTC_NEED, NTC_TEXT, NTC_EOF};
