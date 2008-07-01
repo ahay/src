e12f7ce007037777f744794dc877dfc0ada9a7b1
Yang Liu <yangliu1979@gmail.com>
2008-07-01 18:32:57 +0000

    update
    Former-commit-id: c265ee0edc9e61d2567b4de1c33116e1a75fef6e

--- user/fomels/Msin.c
+++ user/fomels/Msin.c
@@ -22,12 +22,13 @@
 
 int main(int argc, char* argv[])
 {
-    int n1, i2, n2;
-    bool adj;
+    int i1, n1, i2, n2, niter;
+    bool adj, verb, *known;
     char *oper;
     sf_complex *z0, *x, *y;
+    float *m=NULL;
     sf_coperator coper=NULL;
-    sf_file in, out, root;
+    sf_file in, out, root, mask=NULL;
 
     sf_init(argc,argv);
     in = sf_input("in");
@@ -46,6 +47,12 @@ int main(int argc, char* argv[])
     if (!sf_getbool("adj",&adj)) adj=false;
     /* adjoint flag */
 
+    if (!sf_getbool("verb",&verb)) verb=false;
+    /* verbosity flag */
+
+    if (!sf_getint("niter",&niter)) niter=0;
+    /* number of iterations */
+
     if (NULL == (oper = sf_getstring("oper"))) oper="destruct";
     /* operation to perform */
 
@@ -60,21 +67,51 @@ int main(int argc, char* argv[])
 	    sf_error("Unknown operator \"%s\"",oper);
     }
 
+
     x = sf_complexalloc(n1);
     y = sf_complexalloc(n1);
+    known = sf_boolalloc(n1);
+
+    if (niter > 0) { /* do inversion */
+	if (NULL != sf_getstring ("mask")) {
+	    mask = sf_input("mask");
+	    if (SF_FLOAT != sf_gettype(mask)) sf_error("Need float mask");
+	    m = sf_floatalloc(n1);
+	}
+
+	for (i1=0; i1 < n1; i1++) {
+	    y[i1] = 0.;
+	}
+    }
 
     for (i2=0; i2 < n2; i2++) {
 	sf_complexread(x,n1,in);
-
 	sin_init(z0[i2]);
 
-	if (adj) {
-	    coper(true,false,n1,n1,y,x);
-	} else {
-	    coper(false,false,n1,n1,x,y);
-	}
+	if (niter > 0) {
+	    if (NULL != mask) {
+		sf_floatread(m,n1,mask);
+		for (i1=0; i1 < n1; i1++) {
+		    known[i1] = (bool) (m[i1] != 0.);
+		}
+	    } else {
+		for (i1=0; i1 < n1; i1++) {
+		    known[i1] = (bool) (crealf(x[i1]) != 0. && cimagf(x[i1]) != 0.);
+		}
+	    }
 
-	sf_complexwrite(y,n1,out);
+	    sf_csolver(coper, sf_ccgstep, n1, n1, x, y, niter,
+		       "known", known, "x0", x, "verb", verb, "end");
+	    sf_complexwrite(x,n1,out);
+	} else {
+	    if (adj) {
+		coper(true,false,n1,n1,y,x);
+	    } else {
+		coper(false,false,n1,n1,x,y);
+	    }
+
+	    sf_complexwrite(y,n1,out);
+	}
     }
 
     exit(0);
