44ccab5c118813b79dbca7f0cba3fc852ae842ec
Sergey Fomel <sergey.fomel@gmail.com>
2008-10-06 22:49:00 +0000

    jpg2byte
    Former-commit-id: 185cd73b0a345f7a28d13edbcb550c16c45f0ae0

--- user/fomels/_jpeg.h
+++ user/fomels/_jpeg.h
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <jpeglib.h>
+#include <rsf.h>
 
 GLOBAL(void)
 write_JPEG_file (JSAMPLE * image_buffer, 
 		 int image_height, int image_width, int numcol);
+GLOBAL(void) read_JPEG_file (sf_file out);
 
 #endif
