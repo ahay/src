d1aa08c8bbc112eccc5c5bee071ebaa52a4ed087
Jules Browaeys <jules.browaeys@total.com>
2008-10-24 23:49:35 +0000

    Update
    Former-commit-id: f19a2d22cc709c656ffe9133c97b082e0fb99649

--- user/browaeys/Mbeamsteer.c
+++ user/browaeys/Mbeamsteer.c
@@ -1,9 +1,8 @@
-/* Beam steering. */
-/* Adapted from Steve Cole, Stanford University, 1995 */
+/* Beam steering for 2D surface seismic array. */
 /*
   Copyright (C) 2008 University of Texas at Austin
-  Original author: Steve Cole, Stanford University, 1995
-  
+  Adapted from Steve Cole, Stanford University, 1995
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
@@ -19,44 +18,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-
-
-/*sfbeamsteer sem=0 nslo=40 nazim=73 slomin=0. slomax=0.0033333 azmin=0. 
-              azmax=360. xref=0 yref=0 mode=0 d1= d2= d3= npad=500 
-              icoord=0 cord= < in.h > out.h */
-
-/* from aux:	cord integer n1:n1coord=2 */
-
-/*
-        integer infd,outfd
-	integer auxout,auxfd
-
-	if (n1out .eq. 1) then
-	to history:	integer n1:nslo, n2:nazim, n3:n1out
-	to history:	real o1:slomin, d1:dslo, o2:azmin, d2:dazim
-	else
-	to history:	integer n1:n1out, n2:nslo, n3:nazim
-	to history:	real d1:d1out
-	to history:	real o2:slomin, d2:dslo, o3:azmin, d3:dazim
-	endif
-	to history:	integer mode,sem
-	to aux:		live integer n1:n2, n2:n3 
-
-	infd = input()
-	outfd = output()
-	if (icoord .ne. 0) auxfd = auxin('cord')
-
-	call hclose()
-
-	auxfd = auxout('live')
-*/
-
-
-
-
 #include <rsf.h>
-#include <float.h>
-#include <math.h>
 
 #include "bsteer.h"
 
@@ -64,22 +26,28 @@
 
 int main(int argc, char* argv[])
 {
-    int i1,i2,i3,nlive,n1,n2,n3;
-    float d1,d2,d3,o2,o3,dazim,dslo,d1out;
-    float slomin,slomax,azmin,azmax,xref,yref,pmax,sum;
-    int nazim,nslo;
-    int npad,nrec,n1pad,lwind,n1out;
-    int mode,sem,icoord;
-    float ***data,***coord;
-    int **live;
-    float ***semb;
+    int n1,n2,n3,i1,i2,i3,nlive;
+    float o1,o2,o3,d1,d2,d3;
 
+    int nazim,nslo;
+    float dazim,dslo,d1out;
+    float slomin,slomax,azmin,azmax;
+
+    float xref,yref;
+
+    int npad,n1pad,lwind,n1out;
+    int mode,sem;
+    float pmax,sum;
+
+    float ***data, ***semb;
+    int **live;
+
+    sf_axis axout,axslo,axazi;
     sf_file in,out;
    
     sf_init (argc,argv);
     in = sf_input("in");
     out = sf_output("out");
-
 
     if (SF_FLOAT != sf_gettype(in)) sf_error("Need float input");
 
@@ -94,65 +62,72 @@ int main(int argc, char* argv[])
     if (!sf_histfloat(in,"d3",&d3)) d3=1.;
     /* spatial sampling interval in y */
 
+    if (!sf_histfloat(in,"o1",&o1)) o1=0.;
     if (!sf_histfloat(in,"o2",&o2)) o2=0.;
     if (!sf_histfloat(in,"o3",&o3)) o3=0.;
 
-
     if (!sf_getint("sem",&sem)) sem=0;
-    /* =0 result is stack along beam steering trajectory.
-       =1 result is semblance computed over beam steering trajectory */
+    /* =0 stack along beam steering trajectory.
+       =1 semblance computed over beam steering trajectory. */
 
     if (!sf_getint("mode",&mode)) mode=0;
     /* =0  beams are computed as a function of apparent slowness and azimuth angle. 
-       =1  changes to px and py. Nslo, slomin, slomax, nazim, azmin, azmax are 
-           interpreted as npx, pxmin, pxmax, npy, pymin, pymax. 
-           This is effectively a slant stack. In this case the default parameter 
-           values are not appropriate, so you need to supply the correct values */
+       =1  changes to px and py. nslo, slomin, slomax, nazim, azmin, azmax are 
+           interpreted as npx, pxmin, pxmax, npy, pymin, pymax. This is effectively
+           a slant stack. Default parameter not appropriate. */
+    if (mode == 1) pmax = MAX(slomax,azmax);
 
     if (!sf_getint("npad",&npad)) npad=500;
-    /* Program pads the input traces with this many samples at each end.
-       This is done to avoid having to check for beams going outside of 
-       available data. Does not affect the output. */
+    /* Pads input traces with this many samples at each end,
+       to avoid having to check for beams going outside of 
+       available data. */
 
     if (!sf_getint("lwind",&lwind)) lwind=1;
-    /* Semblance values computed over time windows lwind samples long */
-
-    if (!sf_getint("icoord",&icoord)) icoord=0;
-    /* =0 receiver coordinates are computed from o2,d2,o3,d3. */
+    /* Semblance values computed over time windows lwind samples long. */
       
-    if (!sf_getfloat("xref",&xref)) xref = 0.;
-    /* Coordinates of the point at which the beams are computed */
+    if (!sf_getfloat("xref",&xref)) xref=0.;
+    /* Coordinates of the point at which the beams are computed. */
 
-    if (!sf_getfloat("yref",&yref)) yref = 0.;
-    /* Default is center of the array if no coordinates are supplied.
-       If a coordinate file is supplied, need to specify this.*/
+    if (!sf_getfloat("yref",&yref)) yref=0.;
+    /* Default is center of the array. */
 
     if (!sf_getint("nazim",&nazim)) nazim=20;
-    /* Number of azimuth angles */
+    /* number of azimuth angles. */
     if (!sf_getfloat("azmax",&azmax)) azmax = 360.0;
     if (!sf_getfloat("azmin",&azmin)) azmin = 0.0;
-    if (!sf_getfloat("dazim",&dazim)) dazim = (azmax - azmin) / (nazim-1);
+    if (!sf_getfloat("dazim",&dazim)) dazim = (azmax-azmin)/(nazim-1);
 
     if (!sf_getint("nslo",&nslo)) nslo=20;
-    /* Number of slownesses in output */
+    /* number of slownesses in output. */
     if (!sf_getfloat("slomax",&slomax)) slomax = 0.003333;
     if (!sf_getfloat("slomin",&slomin)) slomin = 0.0;
-    if (!sf_getfloat("dslo",&dslo)) dslo = (slomax - slomin) / (nslo-1);
-
+    if (!sf_getfloat("dslo",&dslo)) dslo = (slomax-slomin)/(nslo-1);
 
     n1pad = n1 + 2*npad;
-    nrec = n2*n3;
-    if (mode == 1) pmax = MAX(slomax,azmax);
     n1out = n1/lwind;
     d1out = d1*lwind;
 
-    /* Input file and auxiliary */
-    data = sf_floatalloc3(n1pad,n2,n3);
-    live = sf_intalloc2(n2,n3);
-    coord = sf_floatalloc3(3,n2,n3);
+    /* output */ 
+    semb = sf_floatalloc3(nslo,nazim,n1out);
 
-    /* Output file */ 
-    semb = sf_floatalloc3(nazim,nslo,n1out);
+    axout = sf_maxa(n1out,d1out,o1);
+    axslo = sf_maxa(nslo,dslo,slomin);
+    axazi = sf_maxa(nazim,dazim,azmin);
+    if (n1out == 1){
+	sf_oaxa(out,axslo,1);
+	sf_oaxa(out,axazi,2);
+	sf_oaxa(out,axout,3);
+    } else {
+	sf_oaxa(out,axout,1);
+	sf_oaxa(out,axslo,2);
+	sf_oaxa(out,axazi,3);
+    }
+
+    /* input */
+    data = sf_floatalloc3(n1pad,n2,n3);
+
+    /* auxiliary */
+    live = sf_intalloc2(n2,n3);
 
     /* clear the array */
     for (i3 = 0; i3 < n3; i3++) {
@@ -163,30 +138,29 @@ int main(int argc, char* argv[])
 	}
     }
 
-    /* read in the data */
-    sf_floatread(data[0][0],n1pad*n2*n3,in);
+    /* read in the (paddded) data */
+    sf_floatread(data[0][0],n1*n2*n3,in);
 
     /* determine if this is a dead or live trace (saves time later) */
     nlive = 0;
     for (i3 = 0; i3 < n3; i3++) {
 	for (i2 = 0; i2 < n2; i2++) {
 	    sum = 0.;
-	    for (i1 = npad-1; i1 < npad+n1; i1++) sum = sum + data[i1][i2][i3];
-
+	    for (i1 = npad-1; i1 < npad+n1-1; i1++) sum += data[i1][i2][i3];
 	    if (sum == 0.) {
 		live[i2][i3] = 0;
 	    } else {
 		live[i2][i3] = 1;
-		nlive = nlive + 1;
+		nlive += 1;
 	    }
 	}
     }
 
     /* beam steering */
-    bsteer(data,n1pad,n2,n3,nslo,nazim,slomin,dslo,azmin,dazim,d1,d2,d3,live,mode,n1,npad,nlive,sem,pmax,lwind,n1out,icoord,coord,xref,yref,semb);
+    bsteer(data,n1pad,n2,n3,nslo,nazim,slomin,dslo,azmin,dazim,d1,d2,d3,o2,o3,live,mode,n1,npad,nlive,sem,pmax,lwind,n1out,xref,yref,semb);
 
     /* output beam */
-    sf_floatwrite (semb[0][0],nazim*nslo*n1out,out);
+    sf_floatwrite (semb[0][0],nslo*nazim*n1out,out);
 
     exit(0);
 }
--- user/browaeys/bsteer.c
+++ user/browaeys/bsteer.c
@@ -1,7 +1,7 @@
 /* Beam steering */
 /*
   Copyright (C) 2008 University of Texas at Austin
-  Original author: Steve Cole, Stanford University, 1995
+  Adapted from Steve Cole, Stanford University, 1995
   
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -18,8 +18,6 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include <float.h>
-#include <math.h>
 #include <rsf.h>
 
 #include "bsteer.h"
@@ -27,40 +25,41 @@
 #define MAX(a,b) (a > b ? a : b)
 #define MIN(a,b) (a < b ? a : b)
 
-
 void bsteer(float ***data,          
 	    int n1pad, int n2, int n3, 
 	    int nslo, int nazim,  
-	    float slomin, float dslo, 
-	    float azmin, float dazim, 
-	    float d1, float d2, float d3,
+	    float slomin, float dslo, float azmin, float dazim, 
+	    float d1, float d2, float d3, float o2, float o3,
 	    int **live,
 	    int mode, int n1, int npad, int nlive, int sem,
-	    float pmax, int lwind, int n1out, int icoord,
-            float ***coord,  
+	    float pmax, 
+            int lwind, int n1out,
             float xref, float yref,
             float ***semb)
-
-
+/*< beam steering >*/
 {
-    int iazim,islo,i1,i2,i3,j1,itshift,istart,istop;
-    float azim,slo,p,x,y,z,tshift;
-    float *beam,*pow;                                  /* array [n1pad] */
+    float x,y,slo,azim;
+    int istart,istop;
+    int i1,i2,i3,itshift,islo,iazim,j1;
+    float tshift,p;
+    float *beam,*pow;                                 
 
     /* loop over azimuth and apparent surface slowness */
 
-	for (iazim = 0; iazim < nazim; iazim++) {
+    beam = (float*)malloc(n1pad*sizeof(float));
+    pow = (float*)malloc(n1pad*sizeof(float));
+
+    for (iazim = 0; iazim < nazim; iazim++) {
 	azim = azmin + iazim*dazim;
 
 	for (islo = 0; islo < nslo; islo++) {
 	    slo = slomin + islo*dslo;
 
-            /* clear arrays */
-	    for (i1 = 0; i1 < n1pad; i1++) {
+	    for (i1 = 0; i1 < n1pad; i1++){
 		beam[i1] = 0.;
 		pow[i1] = 0.;
 	    }
-            for (i1 = 0; i1 < n1out; i1++) semb[iazim][islo][i1] = 0.;
+            for (i1 = 0; i1 < n1out; i1++) semb[islo][iazim][i1] = 0.;
 
             /* do not do this beam if it is for too big a p */
 	    p = sqrt(slo*slo + azim*azim);
@@ -72,52 +71,52 @@ void bsteer(float ***data,
 		    for (i2 = 0; i2 < n2; i2++) {
 
 			if (live[i2][i3] != 0) {
-			    if (icoord == 0) {
-				x = (i2+1-((n2+1)/2))*d2;
-				y = (i3+1-((n3+1)/2))*d3;
-			    } else {
-				x = coord[2][i2][i3] - xref;
-				y = coord[1][i2][i3] - yref;
-				z = coord[3][i2][i3];
-			    }
+
+			    x = i2*d2 + o2 - xref;
+			    y = i3*d3 + o3 - yref;
+
                             /* Given azimuth, slowness, and receiver location, compute */
 			    /* the time shift necessary to align the current trace */
-                            /* with the reference trace. */
-                            /* For mode=1, 'slo' is actually py, and 'azim' is px */
-
+                            /* with the reference trace */
+                            /* For mode=1, azim is px and slo is py */
 			    tshift = azim*x + slo*y;
+
                             /* Nearest integer */
 			    itshift = (tshift/d1);
 
                             /* loop over samples of beam */
                             /* sum shifted input into beam */
-
-			    istart = MAX(1+itshift,1);
+			    istart = MAX(itshift,0);
 			    istop = MIN(n1pad+itshift,n1pad);
 
-			    for (i1 = istart; i1 < istop+1; i1++){
-				beam[i1] = beam[i1] + data[i1-itshift][i2][i3];
-				pow[i1] = pow[i1] + data[i1-itshift][i2][i3]*data[i1-itshift][i2][i3];
+			    for (i1 = istart; i1 < istop; i1++){
+				beam[i1] += data[i1-itshift][i2][i3];
+				pow[i1] += data[i1-itshift][i2][i3]*data[i1-itshift][i2][i3];
 			    }
 			}
 		    }
 		}
 
-                /* normalize stack or compute semblance */
 		if (sem == 0) {
+                    /* normalize stack */
 		    for (i1 = 0; i1 < n1out; i1++){
 			for (j1 = 0; j1 < lwind; j1++){
-			    semb[iazim][islo][i1] = beam[npad+i1*lwind+j1]/nlive;
+			    semb[islo][iazim][i1] = beam[npad+i1*lwind+j1]/nlive;
   			}
 		    }
 		} else {
+                    /* compute semblance */
 		    for (i1 = 0; i1 < n1out; i1++){
-			semb[iazim][islo][i1] = beam[npad+i1-1]*beam[npad+i1-1] / (nlive * pow[npad+i1-1] + 0.000000001);
+			semb[islo][iazim][i1] = beam[npad+i1-1]*beam[npad+i1-1] / (nlive * pow[npad+i1-1] + 0.000000001);
 		    }
 		}
 	    }
 	}
     }
+
+    free(pow);
+    free(beam);
+
 }
  
 
