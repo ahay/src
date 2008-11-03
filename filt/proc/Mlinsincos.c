b6e94154a90b05f8e0f5ce92079aa07e858700dd
Jules Browaeys <jules.browaeys@total.com>
2008-11-03 00:52:11 +0000

    Update
    Former-commit-id: c757dc0f84273eede43b8804bc8bb8dc61a30c68

--- filt/proc/Mlinsincos.c
+++ filt/proc/Mlinsincos.c
@@ -1,4 +1,4 @@
-/* Convert (vx,vy) to angle d using equation vx*sin(d) + vy*cos(d) = 1/s0. */
+/* Convert F(vx,vy) to F(d) using equation vy*cos(d) + vx*sin(d) = 1/s0. */
 /*
   Copyright (C) 2008 University of Texas at Austin
 
@@ -21,42 +21,32 @@
 
 #include <rsf.h>
 
-#include "fint1.h"
-
 int main(int argc, char* argv[])
 {
-    fint1 sft;
-    int ext,fint;
-    float f;
-
-    int na;                        /* number of output angle values */
-    int nvx,nvy;                   /* number of velocity in x, number of velocity in y */
-    int npos;                      /* number of points where angles are computed */
+    int na;                        /* number of output angles */
     int nt;                        /* number of radial lines or polar angles */
-    int nr;                        /* number of radius samples on radial lines */
+    int npos;                      /* number of frames */
+    int nvx,nvy;                   /* number of velocity in x, number of velocity in y */
 
     float da,oa;                   /* increment in output angle, starting position */
     float dt,ot;                   /* increment in polar angle, starting position */
-    float dr,or;                   /* increment in radius, starting position */
     float dvx,ovx;                 /* increment of velocity in x, starting velocity */
     float dvy,ovy;                 /* increment of velocity in y, starting velocity */
     float s0;                      /* reference slowness */
 
-    float **v;                     /* data on 2D velocity squared grid vx-vy */
-    float **r;                     /* inverse polar radius in vx-vy plane */
-    float **s;                     /* polar angle in vx-vy plane */
-    float **a;                     /* output angle values */
-    float *tmp;                    /* inverse radius values on radial line */
+    float **v;                     /* data on squared grid vx-vy */
+    float **a;                     /* data on output angle grid */
 
-    int ivx,ivy;                   /* velocity counters */
     int ia;                        /* output angle counter */
     int it;                        /* polar angle counter */
-    int ir;                        /* radius counter */
+    int ivxm,ivym;                 /* bilinear interpolation cell indices */
 
-    float vx,vy;                   /* velocity vx and vy */
-    float ri;                      /* inverse radius on radial line */
-    float t;                       /* polar angle defining radial line */
     float d;                       /* output angle */
+    float t;                       /* polar angle defining radial line */
+    float vi;                      /* inverse velocity radius */
+    float dat;                     /* interpolated data value at vx,vy */
+    float vx,vy;                   /* velocity vx and vy */
+    float tx,ty;                   /* bilinear inside cell position */
 
     sf_axis aa,apos;
     sf_file in,out;
@@ -76,23 +66,12 @@ int main(int argc, char* argv[])
     if (!sf_getfloat("oa",&oa)) sf_error("Need oa=");
     /* output angle origin */
 
-    if (!sf_getint("nt",&nt)) nt=180;
-    /* number of polar angle for integration. */
-    if (!sf_getfloat("dt",&dt)) dt=2.;
+    if (!sf_getint("nt",&nt)) nt=360;
+    /* number of polar angles for integration. */
+    if (!sf_getfloat("dt",&dt)) dt=1.;
     /* polar angle sampling. */
     if (!sf_getfloat("ot",&ot)) ot=0.;
     /* polar angle origin */
-
-    if (!sf_getint("nr",&nr)) nr=nvx/2;
-    /* number of radii on radial lines */
-    if (!sf_getfloat("dr",&dr)) dr=dvx;
-    /* radii sampling. */
-
-    or = 1./s0 + 0.5*dr;
-   /* radius greater than or equal to 1/s0 */
-
-    if (!sf_getint("extend",&ext)) ext=4;       
-    /* tmp extension */
 
     /* read input file parameters */
     if (SF_FLOAT != sf_gettype(in)) sf_error("Need float input");
@@ -107,7 +86,7 @@ int main(int argc, char* argv[])
 
     if ((nvx != nvy) || (dvx != dvy)) sf_error("Need squared grid for vx-vy plane");
 
-    /* number of points where angles are computed. */
+    /* number of vx-vy frames for computation. */
     npos = 1;
 
     /* output file parameters */ 
@@ -122,59 +101,51 @@ int main(int argc, char* argv[])
 
     /* memory allocations */
     v = sf_floatalloc2(nvx,nvy);
-    r = sf_floatalloc2(nvx,nvy);
-    s = sf_floatalloc2(nvx,nvy);
     a = sf_floatalloc2(na,npos);
 
-    tmp = sf_floatalloc(nr);
-
-    /* scaled polar coordinates in 2D velocity grid */
-    s[0][0] = 0.;
-    r[0][0] = 1000000000.;
-    for (ivx = 1; ivx < nvx; ivx++) {
-	    vx = ovx + ivx*dvx;
-	    r[0][ivx] = 1./(s0*fabsf(vx));
-	    s[0][ivx] = atan2f(0,vx);
-    }
-    for (ivy = 1; ivy < nvy; ivy++) {
-	vy = ovy + ivy*dvy;
-	for (ivx = 0; ivx < nvx; ivx++) {
-	    vx = ovx + ivx*dvx;
-	    r[ivy][ivx] = 1./(s0*sqrtf(vx*vx + vy*vy));
-	    s[ivy][ivx] = atan2f(vy,vx); /* radians */
-	}
-    }
-    
     /* read data in velocity array */
     sf_floatread(v[0],nvx*nvy,in);
-
-    /* inverse radius values on radial line */
-    sft = fint1_init(ext,nr, 0);
-    for (ir = 0; ir < nr; ir++) {
-	tmp[ir] = 1./(or + ir*dr);
-    }
 
     /* initialize */
     for (ia = 0; ia < na; ia++) {
 	a[0][ia] = 0.;
     }
 
-    /* Loop on polar angle directions */
+    /* loop on polar angle directions */
     for (it = 0; it < nt; it++) {
-	t = ot + it*dt; /* degree */
+	t = (ot + it*dt)/180.*SF_PI;
 
+        /* loop on output angle */
 	for (ia = 0; ia < na; ia++) {
-	    d = oa + ia*da; /* degree */
+	    d = (oa + ia*da)/180.*SF_PI;
 
-            /* cos(d-t) for radial line equals inverse radius */
-	    ri = cosf((d-t)/180.*SF_PI);
+            /* inverse velocity radius equals scaled cos(d-t) on radial line */
+	    vi = s0*cosf(d-t);
 
-	    f = (ri - 1/or)*dr;
-	    fint = f;
+	    if (vi != 0.0) {
 
-            /* cumulative sum */
-	    if (fint >= 0 && fint < nr) {
-		a[0][ia] += fint1_apply(sft,fint,f-fint,false);
+		vx = sinf(t)/vi;
+		vy = cosf(t)/vi;
+ 
+                /* bilinear interpolation */
+		ivxm = floorf((vx-ovx)/dvx);
+		ivym = floorf((vy-ovy)/dvy);
+
+		if ( (ivxm >= 0) && (ivym >= 0) && (ivxm < (nvx-1)) && (ivym < (nvy-1)) ) {
+
+		    tx = (vx-ivxm*dvx-ovx)/dvx;
+		    ty = (vy-ivym*dvy-ovy)/dvy;
+
+                    /* calculate interpolated value at vx,vy */
+		    dat = 0.0;
+		    dat += v[ivym][ivxm]*(1.-tx)*(1.-ty);
+		    dat += v[ivym][ivxm+1]*tx*(1.-ty);
+		    dat += v[ivym+1][ivxm+1]*tx*ty;
+		    dat += v[ivym+1][ivxm]*(1.-tx)*ty;
+
+                    /* sum */
+		    a[0][ia] += dat;
+		}
 	    }
 	}
     }
