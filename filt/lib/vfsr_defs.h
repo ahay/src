cb7709d6bf027e61cdae12a07b7c49adae1605f7
Vladimir Bashkardin <bash@mail.utexas.edu>
2008-12-04 07:52:27 +0000

    TRUE/FALSE defines wrapped with #ifndef
    Former-commit-id: 8a8982086070b053a6cd6e45b2b2a57f54018776

--- filt/lib/vfsr_defs.h
+++ filt/lib/vfsr_defs.h
@@ -21,8 +21,12 @@
 #ifndef _VFSR_DEFS_H_
 #define _VFSR_DEFS_H_
 
+#ifndef TRUE
 #define TRUE        1
+#endif
+#ifndef FALSE
 #define FALSE       0
+#endif
 
 #define INTEGER_TYPE     1
 #define REAL_TYPE        0
