36f8ff4da85d8d8d1b5918e48d693eb018d13578
Jules Browaeys <jules.browaeys@total.com>
2009-01-20 20:28:35 +0000

    Update
    Former-commit-id: de3c0d27427ad5b383cffc73a05f43a98f42920a

--- user/browaeys/Mharmsyst.c
+++ user/browaeys/Mharmsyst.c
@@ -1,4 +1,4 @@
-/* Phase space evolution equation for time of 1D harmonic oscillator. */
+/* Phase space evolution equation for time function of 1D harmonic oscillator. */
 /*
    Equation is v.dt/dx - c.x.dt/dv = 1 with c > 0 
    Analytical solution is t = 1/sqrt(c).arctan[sqrt(c).x/v] 
@@ -30,25 +30,34 @@
 
 int main(int argc, char* argv[])
 {
-    int i,k,np;
+    int np;                        /* linear system matrix dimension */
+    int i,k;                       /* matrix row, colummn counters */
+
+    bool adj;                      /* adjoint */
 
     int nx,ix;                     /* number of grid points in x, counter */
     int nv,iv;                     /* number of grid points in v, counter */
 
     float dx,ox;                   /* increment in x, starting position */
-    float dv,ov;                   /* increment in v, starting position */
+    float dv,ov;                   /* increment in v, starting position */ 
 
+
+    float signadj;                 /* sign for adjoint implementation */
     float fx,fv;                   /* factors in the system matrix */
     float x;                       /* spatial position */
 
-    float **a,*b,c; 
+    float c;                       /* harmonic oscillator stiffness constant */
+    float **a;                     /* linear system matrix */
 
-    sf_axis arow,acol;
+    sf_axis row,col;
+
     sf_file out;
 
     sf_init(argc,argv);
-
     out = sf_output("out");
+
+    if (!sf_getbool("adj",&adj)) adj=false;
+    /* if y, perform adjoint operation */
 
     if (!sf_getfloat("c",&c)) sf_error("Need c=");
     if (c <= 0.0) sf_error("Need strictly positive c");
@@ -58,29 +67,38 @@ int main(int argc, char* argv[])
     /* number of x values. */
     if (!sf_getfloat("dx",&dx)) sf_error("Need dx=");
     /* x sampling. */
-    if (!sf_getfloat("ox",&ox)) ox = -0.5*(nx-1)*dx;
+    if (!sf_getfloat("ox",&ox)) ox = 0.0;
     /* x origin. */
 
     if (!sf_getint("nv",&nv)) sf_error("Need nv=");
     /* number of v values. */
     if (!sf_getfloat("dv",&dv)) sf_error("Need dv=");
     /* v sampling. */
-    if (!sf_getfloat("ov",&ov)) ov = -0.5*(nv-1)*dv;
+    if (!sf_getfloat("ov",&ov)) ov = dv;
     /* v origin. */
 
-    /* output file parameters */ 
-    arow = sf_maxa(np,0,1);
-    sf_oaxa(out,arow,1);
+    np = nv*nx;
 
-    acol = sf_maxa(np,0,1);
-    sf_oaxa(out,acol,2);
+    /* adjoint is a^t = -a */
+    if (adj) {
+	signadj = -1.;
+    } else { 
+	signadj = 1.;
+    }
+
+    /* output file parameters */ 
+    row = sf_maxa(np,0,1);
+    sf_oaxa(out,row,1);
+
+    col = sf_maxa(np,0,1);
+    sf_oaxa(out,col,2);
 
     sf_putstring(out,"label1","row");
     sf_putstring(out,"label2","column");
 
     /* memory allocations */
-    b = sf_floatalloc(np);
     a = sf_floatalloc2(np,np);
+
 
     /* ---------------------------- */
     /* LINEAR SYSTEM MATRIX A.T = b */
@@ -89,13 +107,6 @@ int main(int argc, char* argv[])
     /* Linear system depends on stencils and indexing of t(ix,iv) vector */
     /* t^T = [t(1,1), ..., t(nx,1), ...., t(1,nv), ..., t(nx,nv)] */
 
-    np = nv*nx;
-
-    /* Right hand side vector */
-    for (i = 0; i < np; i++) {
-	b[i] = 1.0;
-    }
-
     /* Initialize matrix A to zero */
     for (i = 0; i < np; i++) { /* matrix rows */
 	for (k = 0; k < np; k++) { /* matrix column */
@@ -103,8 +114,8 @@ int main(int argc, char* argv[])
 	}
     }
 
-    fv = 0.5*c/dv;
-    fx = 0.5*ov/dx;
+    fv = signadj*0.5*c/dv;
+    fx = signadj*0.5*ov/dx;
 
     /* First row */
     a[0][1]  =  fx;
@@ -114,12 +125,12 @@ int main(int argc, char* argv[])
     for (i = 1; i < nx; i++) {
 	a[i][i-1]  = -fx;
 	a[i][i+1]  =  fx;
-	a[i][i+nx] = -fv*(ox+ix*dx);
+	a[i][i+nx] = -fv*(ox + ix*dx);
     }
 
     /* Medium rows */
     for (iv = 1; iv < (nv-1); iv++) {
-	fx = 0.5*(ov+iv*dv)/dx;
+	fx = signadj*0.5*(ov + iv*dv)/dx;
 	for (ix = 0; ix < nx; ix++){
 	    i = iv*nx + ix;
 	    x = ox + ix*dx;
@@ -131,20 +142,17 @@ int main(int argc, char* argv[])
     }
 
     /* Lower rows */
-    fx = 0.5*(ov+(nv-1)*dv)/dx;
+    fx = signadj*0.5*(ov + (nv-1)*dv)/dx;
     for (ix = 0; ix < nx; ix++){
 	i = (nv-1)*nx + ix;
-	a[i][i-nx] =  fv*(ox+ix*dx);
+	a[i][i-nx] =  fv*(ox + ix*dx);
 	a[i][i-1]  = -fx;
 	a[i][i+1]  =  fx;
     }
 
     /* Last row */
-    a[np-1][np-1-nx] =  fv*(ox+(nx-1)*dx);
+    a[np-1][np-1-nx] =  fv*(ox + (nx-1)*dx);
     a[np-1][np-2]    = -fx;
-
-
-    /* Boundary conditions */
 
     /* output matrix */
     sf_floatwrite(a[0],np*np,out);
