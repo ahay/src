1533f8b85687dd65019207963a3026f460a60c27
Jules Browaeys <jules.browaeys@total.com>
2009-02-13 20:33:14 +0000

    Update
    Former-commit-id: 1c96b675ed37c6d7717a61413d6dcdb72dface23

--- user/browaeys/bmcgauss.c
+++ user/browaeys/bmcgauss.c
@@ -1,4 +1,4 @@
-/* Correlated Gaussian joint probability distribution histogram */
+/* Generation of correlated Gaussian distributed random deviates with modified Box Mulller algorithm */
 /*
   Copyright (C) 2009 University of Texas at Austin
   
@@ -17,8 +17,9 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include <math.h>
 #include <rsf.h>
+#include <math.h>
+#include "bmcgauss.h"
 
 #define IA 16807
 #define IM 2147483647 
@@ -29,7 +30,6 @@
 #define NDIV (1+(IM-1)/NTAB)
 #define EPS 1.2e-7
 #define RNMX (1.0-EPS)
-
 
 float rand1_gen(int *idum)
 {
@@ -98,102 +98,4 @@ void gauss_joint(int *iseed, float m1, float m2, float s1, float s2, float r, fl
  *y1 = s1*g1 + m1;
  *y2 = (g2+r*g1)*s2 + m2;
  return;
-}
-
-void histgauss(float **hist, int n, float r, float m1, float m2, float s1, float s2, int nbin, float dbin, float obin, int iseed) 
-{
-/* Joint Gaussian distribution histogram */
-
-    int i,i1,i2,isd;
-    float x1,x2,norm;   
-     
-    isd = iseed;
-    norm = 1.0/(dbin*dbin*n);
-
-    for (i = 0; i < n; i++) {
-
-	gauss_joint(&isd,m1,m2,s1,s2,r,&x1,&x2);
-
-	i1 = floorf((x1-obin)/dbin);
-	i2 = floorf((x2-obin)/dbin);
-
-	if ( (i1 >= 0) && (i1 < nbin) && (i2 >= 0) && (i2 < nbin) ) {
-	    hist[i1][i2] += norm;
-	}
-
-    }
-
-    return;
-}
-
-int main(int argc, char* argv[])
-{
-    int i,j;
-    int n,iseed;
-    int nbin;
-
-    float dbin,obin;
-    float m1,m2,s1,s2,r;
-    float **hist;
-
-    sf_axis axis_1,axis_2;
-    sf_file out;
-
-    sf_init (argc,argv);
-
-    out = sf_output("out");
-    sf_settype(out,SF_FLOAT);
-
-    if (!sf_getint("n",&n)) n=100;
-    /* number of random deviates pairs */
-
-    if (!sf_getfloat("m1",&m1)) m1=0.0;
-    /* mean for deviate 1 */
-
-    if (!sf_getfloat("m2",&m2)) m2=0.0;
-    /* mean for deviate 2 */
-
-    if (!sf_getfloat("s1",&s1)) s1=1.0;
-    /* standard deviation for deviate 1 */
-
-    if (!sf_getfloat("s2",&s2)) s2=1.0;
-    /* standard deviation for deviate 2 */
-
-    if (!sf_getfloat("r",&r)) r=0.0;
-    /* correlation coefficient */
-
-    if (!sf_getint("nbin",&nbin)) nbin=51;
-    /* number of bins for histogram */
-
-    if (!sf_getfloat("dbin",&dbin)) dbin=0.1;
-    /* histogram bin size */
-
-    if (!sf_getfloat("obin",&obin)) obin=0.0;
-    /* histogram origin */
-
-    if (!sf_getint("iseed",&iseed)) iseed=-33;
-    /* random generator seed */
-    if (iseed >= 0) sf_error("Need strictly negative iseed");
-
-    /* output file parameters */
-    axis_1 = sf_maxa(nbin,obin,dbin);
-    axis_2 = sf_maxa(nbin,obin,dbin);
-
-    sf_oaxa(out,axis_1,1);
-    sf_oaxa(out,axis_2,2);
-
-    /* Histogram */
-    hist = sf_floatalloc2(nbin,nbin);
-
-    for (j = 0; j < nbin; j++) {
-	for (i = 0; i < nbin; i++) {
-	    hist[j][i] = 0.0;
-	}
-    }
-    histgauss(hist,n,r,m1,m2,s1,s2,nbin,dbin,obin,iseed);
-
-    /* output */
-    sf_floatwrite(hist[0],nbin*nbin,out);
-
-    exit(0);
 }
