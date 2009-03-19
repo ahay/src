7ac90cb5a5dd390b75a83530516ce0cddc6249d7
Jules Browaeys <jules.browaeys@total.com>
2009-03-19 19:59:05 +0000

    Update
    Former-commit-id: 66f93c9faba638f855dde254cc1b38367326ee5f

--- user/browaeys/Mvelmap.c
+++ user/browaeys/Mvelmap.c
@@ -55,13 +55,13 @@ int main(int argc, char* argv[])
     /* input file parameters */
     if (SF_FLOAT != sf_gettype(in)) sf_error("Need float input");
 
-    if (!sf_histint(in,"nvx",&nvx)) sf_error("No nvx= in input");
-    if (!sf_histfloat(in,"dvx",&dvx)) sf_error("No dvx= in input");
-    if (!sf_histfloat(in,"ovx",&ovx)) sf_error("No ovx= in input");
+    if (!sf_histint(in,"n1",&nvx)) sf_error("No n1= in input");
+    if (!sf_histfloat(in,"d1",&dvx)) sf_error("No d1= in input");
+    if (!sf_histfloat(in,"o1",&ovx)) sf_error("No o1= in input");
 
-    if (!sf_histint(in,"nvy",&nvy)) sf_error("No nvy= in input");
-    if (!sf_histfloat(in,"dvy",&dvy)) sf_error("No dvy= in input");
-    if (!sf_histfloat(in,"ovy",&ovy)) sf_error("No ovy= in input");
+    if (!sf_histint(in,"n2",&nvy)) sf_error("No n2= in input");
+    if (!sf_histfloat(in,"d2",&dvy)) sf_error("No d2= in input");
+    if (!sf_histfloat(in,"o2",&ovy)) sf_error("No o2= in input");
 
     if ((nvx != nvy) || (dvx != dvy)) sf_error("Need squared grid for vx-vy plane");
 
