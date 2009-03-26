8bd73bbb2a3f2c6aee81a11a2f0b72df114034b2
Jim Jennings <jenningsjwj@mac.com>
2009-03-26 21:17:16 +0000

    add bunding box options to vplot2eps
    Former-commit-id: c2a17e14d52dab07a6cc0168a182163f730a8a3d

--- framework/vplot2eps.py
+++ framework/vplot2eps.py
@@ -24,26 +24,42 @@ pspen = os.path.join(bindir,'pspen')
 def convert(vplot,eps,
             options='color=n fat=1 fatmult=1.5 invras=y',
             psborder=0.1,
-            ppi=72 # points per inch resolution
+            ppi=72,                     # points per inch resolution
+            xbmin=None, xbmax=None,     # options to override default bounding box
+            ybmin=None, ybmax=None
             ):
     "Convert vplot to EPS"
+    
     space = float(os.environ.get('PSBORDER',psborder))
     opts = os.environ.get('PSTEXPENOPTS',options)
-#    print opts
-    # get bounding box
+
+    # Get bounding box info from vplot file
     getbb = vppen + ' big=n stat=l %s < %s | head -1' % (opts,vplot)
     
     out = os.popen(getbb)
     head = string.split(out.read())
     out.close() 
+    
+    # Use parameters for bounding box if supplied.
+    # Otherwise use bounding box from vplot file.
+    if xbmin==None: xbbm = head[7]
+    else:           xbbm = xbmin
+    if xbmax==None: xbbp = head[9]
+    else:           xbbp = xbmax
+
+    if ybmin==None: ybbm = head[12]
+    else:           ybbm = ybmin
+    if ybmax==None: ybbp = head[14]
+    else:           ybbp = ybmax
 
     # Compute bounding box
-    bbm = map(lambda x: (float(head[x])-space)*ppi,[7,12])
-    bbp = map(lambda x: (float(head[x])+space)*ppi,[9,14])
+    bbm = map(lambda x: (float(x)-space)*ppi,[xbbm,ybbm])
+    bbp = map(lambda x: (float(x)+space)*ppi,[xbbp,ybbp])
+
     # Round to integer
     ibbm = map(int,bbm)
     ibbp = map(lambda x: int(x)+1,bbp)
- 
+    
     out = open(eps,"w")
     out.write("%!PS-Adobe-2.0 EPSF-2.0\n")
     out.write("%%%%BoundingBox: %d %d %d %d\n" % tuple(ibbm+ibbp))
@@ -69,9 +85,12 @@ if __name__ == "__main__":
     
     if argc < 2:
         print '''
-        Usage: %s [options] file.vpl [file.eps]
-        Converts vplot to encapsulated postscript.
-        [options] are passed to pspen.
+Usage:
+%s [xbmin= xbmax= ybmin= ybmax=] [options] file.vpl [file.eps]
+
+Converts vplot to encapsulated postscript.
+[xbmin= xbmax= ybmin= ybmax=] overrides the default bounding box.
+[options] are passed to pspen.
         ''' % prog
         sys.exit(2)
 
@@ -82,11 +101,22 @@ if __name__ == "__main__":
         vpl = eps
         eps = re.sub('\.[^\.]+$','.eps',vpl)
 
+    options = []
+    xbmin=None; xbmax=None; ybmin=None; ybmax=None
+    for item in sys.argv:
+        if   item.split('=')[0] == 'xbmin': xbmin = item.split('=')[1] 
+        elif item.split('=')[0] == 'xbmax': xbmax = item.split('=')[1] 
+        elif item.split('=')[0] == 'ybmin': ybmin = item.split('=')[1] 
+        elif item.split('=')[0] == 'ybmax': ybmax = item.split('=')[1]
+        else:                               options.append(item)
+            
     try:
-        if sys.argv:
-            convert(vpl,eps,string.join(sys.argv,' '))
+        if options:
+            convert(vpl,eps,options=string.join(options,' '),
+                    xbmin=xbmin, xbmax=xbmax, ybmin=ybmin, ybmax=ybmax)
         else:
-            convert(vpl,eps)
+            convert(vpl,eps,
+                    xbmin=xbmin, xbmax=xbmax, ybmin=ybmin, ybmax=ybmax)
     except:
         print 'Failed to convert %s to %s' % (vpl,eps)
         sys.exit(1)
