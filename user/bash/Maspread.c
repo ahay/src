2dedb43548e7de1a26dcf805e2f7849dbab0210b
Vladimir Bashkardin <bash@mail.utexas.edu>
2009-04-21 03:38:12 +0000

    Analytic spread
    Former-commit-id: c83eb8002c0ab3f813386167b471ef9966227850

--- user/bash/Maspread.c
+++ user/bash/Maspread.c
@@ -1,4 +1,4 @@
-/* Spread surface(s) with analytic expressions in a multi-dimensional volume
+/* Spread surface(s) with analytic expressions in a data volume
 
 Example:
 sfaspread n1=501 n2=101 n3=101 o2=-0.5 o3=-0.5 d2=0.01 d3=0.01
@@ -10,7 +10,7 @@ for the #th surface, amp# defines an expression for the
 amplitude of the #th surface (it is optional and assumed
 to be "1" by default).
 
-See also sfmath. */
+See sfmath for the details about the syntax of expressions. */
 /*
   Copyright (C) 2009 University of Texas at Austin
   
@@ -46,7 +46,7 @@ int main (int argc, char* argv[]) {
     float **fbuf, ***p, ***a, d[SF_MAX_DIM], o[SF_MAX_DIM], *trace;
     float f, fpeak, d1, o1;
     char key[7], *label, *unit, **pos, **amp;
-    sf_bool conv, grad;
+    bool conv, grad;
     sf_file out;
 
     sf_init (argc, argv);
@@ -114,7 +114,7 @@ int main (int argc, char* argv[]) {
     /* Number of surfaces */
 
     pos = (char**)malloc (nsp * sizeof (char*));
-    amp = (char**)malloc ((nsp + 1) * sizeof (char*));
+    amp = (char**)malloc ((nsp + 2) * sizeof (char*));
 
     /* expressions for positions and amplitudes */
     amp[0] = NULL;
@@ -132,6 +132,11 @@ int main (int argc, char* argv[]) {
        snprintf (key, 5, "amp%d", 0);
        if (NULL == (amp[0] = sf_getstring (key))) amp[0] = unit_amplitude;
        /* If conv=false, then this describes values at the top. */
+       if (grad) {
+          snprintf (key, 5, "amp%d", nsp + 1);
+          if (NULL == (amp[nsp + 1] = sf_getstring (key))) amp[nsp + 1] = amp[nsp];
+          /* If grad=true, then this describes values at the bottom. */
+       }
     }
 
     for (i = 0; i < dim; i++) {
@@ -156,7 +161,7 @@ int main (int argc, char* argv[]) {
     sf_putfloat (out, "d1", 1);
 
     p = (float***)malloc (nsp * sizeof (float**));
-    a = (float***)malloc ((nsp + 1) * sizeof (float**));
+    a = (float***)malloc ((nsp + 2) * sizeof (float**));
 
     nbuf = max_buf / (sizeof (float) * 2 * nsp * dim);
     fbuf = sf_floatalloc2 (nbuf, dim);
@@ -187,13 +192,18 @@ int main (int argc, char* argv[]) {
             a[k + 1]  = sf_floatalloc2 (nbuf, len + 2);
 	    sf_math_evaluate (len, nbuf, fbuf, a[k + 1]);
         }
-        if (conv) {
+        if (!conv) {
             len = sf_math_parse (amp[0], out, SF_FLOAT);
             a[0]  = sf_floatalloc2 (nbuf, len + 2);
 	    sf_math_evaluate (len, nbuf, fbuf, a[0]);
+            if (grad) {
+                len = sf_math_parse (amp[nsp + 1], out, SF_FLOAT);
+                a[nsp + 1]  = sf_floatalloc2 (nbuf, len + 2);
+	        sf_math_evaluate (len, nbuf, fbuf, a[nsp + 1]);
+            }
         }
 
-        if (conv) {
+        if (conv) { /* Convolve with Ricker */
             /* Loop over traces */
 	    for (k = 0; k < nbuf; k++) {
                 /* Loop over time steps */
@@ -207,7 +217,7 @@ int main (int argc, char* argv[]) {
                 }
 	        sf_floatwrite (trace, n1, out);
             }
-        } else {
+        } else { /* Fill values below each surface */
             /* Loop over traces */
 	    for (k = 0; k < nbuf; k++) {
                 /* Loop over surfaces */
@@ -215,15 +225,15 @@ int main (int argc, char* argv[]) {
                 for (l = 0; l <= nsp; l++) {
                     f = 0;
                     t0 = t1;
-                    t1 = l < nsp ? (int)(p[l][1][k] + 0.5) : n1 - 1;
+                    t1 = l < nsp ? (int)((p[l][1][k] - o1) / d1 + 0.5) : n1;
                     if (t1 < 0)
                         t1 = 0;
-                    if (t1 >= n1)
-                        t1 = n1 - 1;
+                    if (t1 > n1)
+                        t1 = n1;
                     if (grad && t0 != t1)
                         f = (a[l + 1][1][k] - a[l][1][k]) / (t1 - t0);
                     /* Loop over time steps */
-                    for (i = t0; i < t1; i++) {
+                    for (i = t0; i != t1; t1 > t0 ? i++ : i--) {
                         trace[i] = a[l][1][k] + f * (i - t0);
                     }
                 }
@@ -234,8 +244,11 @@ int main (int argc, char* argv[]) {
             free (p[k]);
             free (a[k + 1]);
         }
-        if (conv)
+        if (!conv) {
             free (a[0]);
+            if (grad)
+                free (a[nsp + 1]);
+        }
     }
 
     exit (0);
