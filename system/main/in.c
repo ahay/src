8dabfea70d7b0a30c4cd655b83ce716e17e5aa58
Ioan Vlad <i.vlad@yahoo.com>
2009-06-20 15:15:37 +0000

    init pointer to NULL
    Former-commit-id: 231b861f62925b505a17fa3da763c6ffb32c055f

--- system/main/in.c
+++ system/main/in.c
@@ -10,17 +10,17 @@ unit1,unit2,... are axis units
 */
 /*
   Copyright (C) 2004 University of Texas at Austin
-  
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -38,7 +38,7 @@ int main (int argc, char* argv[])
     off_t nj, size, n[SF_MAX_DIM];
     float check, fj;
     char *label, *filename, *dataname, key[8], *val, buf[BUFSIZ], zero[BUFSIZ];
-    sf_file file;
+    sf_file file=NULL;
     bool info, trail;
     const char *type[] = {"uchar","char","int","float","complex","short"};
     const char *form[] = {"ascii","xdr","native"};
@@ -136,7 +136,7 @@ int main (int argc, char* argv[])
 	}
 
 	check_zeros (file, esize, size, ncheck, buf, zero);
-        sf_fileclose(file);
+        if (file!=NULL) sf_fileclose(file);
     }
     if (!info) printf("\n");
 
