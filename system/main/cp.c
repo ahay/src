afbf094f01bf6751dfc88d02d432304e1864d05c
Ioan Vlad <i.vlad@yahoo.com>
2009-06-23 17:22:22 +0000

    undid bug introduced in r4412
    Former-commit-id: fd471cad4cd8ef0a8976672f5aa2ee55669a4694

--- system/main/cp.c
+++ system/main/cp.c
@@ -36,7 +36,7 @@ int main(int argc, char* argv[])
     sf_init (argc,argv);
 
     /* the first two non-parameters are in and out files */
-    for (i=1; i< argc; i++) { 
+    for (i=1; i< argc; i++) {
 	if (NULL == strchr(argv[i],'=')) {
 	    if (NULL == in) {
 		infile = argv[i];
@@ -49,6 +49,8 @@ int main(int argc, char* argv[])
     }
     if (NULL == in || NULL == out)
 	sf_error ("not enough input");
+
+    sf_setformat(out,sf_histstring(in,"data_format"));
 
     sf_cp(in,out);
     if (NULL != strstr (sf_getprog(),"mv"))
