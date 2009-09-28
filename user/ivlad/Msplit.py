d10ffad41bfc612e204f0c8b270d3889849df622
Ioan Vlad <i.vlad@yahoo.com>
2009-09-28 21:08:41 +0000

    sfsplit
    Former-commit-id: 99693feb06d4ed0e9bbb0c987eb39e31300dc2fa

--- user/ivlad/Msplit.py
+++ user/ivlad/Msplit.py
@@ -9,6 +9,22 @@ sfsplit inp=file.rsf outdir=[file_split.rsf] nthick=[1]
 Parameter nthick gives the maximum thickness of a slice. The last slice may
 be thinner."""
 
+# Copyright (C) 2009 Ioan Vlad
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 import os, sys, rsfprog, commands, math
 from rsfuser.ivlad import unix_error, unix_success, ext, ndims, add_zeros, execute
 
