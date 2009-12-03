adf8361514a93e6445558365b54f07908f5add78
Vladimir Bashkardin <bash@mail.utexas.edu>
2009-12-03 08:13:57 +0000

    Comments about real()/imag()
    Former-commit-id: dfcbdedc186da46a09ce4767cc1ad9a855617e57

--- system/main/math.c
+++ system/main/math.c
@@ -6,6 +6,7 @@ cosh, sinh, tanh, acosh, asinh, atanh,
 exp,  log,  sqrt, abs,
 erf,  erfc (for float data),
 arg,  conj (for complex data).
+real, imag (complex data only)
 
 sfmath will work on float or complex data, but all the input and output
 files must be of the same data type.
