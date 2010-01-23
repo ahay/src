85fb5f212b9fef7e13f0dc2f520951d630563cc5
Jeff Godwin <godwin.jeffrey@gmail.com>
2010-01-23 10:11:51 +0000

    Former-commit-id: d66482ebfae1cce7f8c86979acadb67f5c466007

--- api/java/test/Clip.java
+++ api/java/test/Clip.java
@@ -5,6 +5,28 @@ import rsf.Header;
 
 public class Clip {
     public static void main(String[] args){
-                        
+         Par par = new Par(args);
+
+         float clip = par.getFloat("clip",0.0f);
+        
+         Header header = Reader.readHeader("in");
+
+         float[][][] data = Reader.readBinary3D(header);
+
+         int n3 = header.n[2];
+         int n2 = header.n[1];
+         int n1 = header.n[0];
+
+         for(int i = 0; i < n3; ++i){
+            for(int j = 0; j < n2; ++j){
+                for(int k = 0; k < n1; ++k){
+                    float trace = data[i][j][k];
+                    if (trace > clip) data[i][j][k] = clip;
+                    else if (trace < -clip) data[i][j][k] = -clip;
+                }
+            }
+         }
+
+         Writer.writeRSF(header,data,"out");
     }
 }
