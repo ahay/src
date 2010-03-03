faa00fe9d17fc82b6e4d6289d3c6652bf8aac4b6
Sergey Fomel <sergey.fomel@gmail.com>
2010-03-03 00:21:52 +0000

    sffft
    Former-commit-id: 0354364fa474a2cd499988348f8312c26b5040b3

---
+++ user/fomels/Mfft.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+'Fourier transform as a linear operator'
+
+##   Copyright (C) 2010 University of Texas at Austin
+##  
+##   This program is free software; you can redistribute it and/or modify
+##   it under the terms of the GNU General Public License as published by
+##   the Free Software Foundation; either version 2 of the License, or
+##   (at your option) any later version.
+##  
+##   This program is distributed in the hope that it will be useful,
+##   but WITHOUT ANY WARRANTY; without even the implied warranty of
+##   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##   GNU General Public License for more details.
+##  
+##   You should have received a copy of the GNU General Public License
+##   along with this program; if not, write to the Free Software
+##   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+import os, sys
+
+fft = os.path.join(os.environ.get('RSFROOT'),'bin/sffft3')
+
+def run(args):
+    adj = 'n'
+    for arg in args:
+        if arg[:4] == 'adj=':
+            adj = arg[4:]
+    if args and not os.isatty(sys.stdin.fileno()):
+        args.extend(['sym=y','inv='+adj])
+    os.spawnv(os.P_WAIT,fft,['sffft3',]+args)
+
+if __name__ == "__main__":
+    run(sys.argv[1:])
+    sys.exit(0)
