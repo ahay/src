b16babb890add7bfabfeda15311adca81c55cc6b
Ioan Vlad <i.vlad@yahoo.com>
2010-04-13 18:17:11 +0000

    reverting r5719 in rsfbak
    Former-commit-id: e06a36b181911b9946798829c41b595d45d6727e

--- api/python/rsfbak.py
+++ api/python/rsfbak.py
@@ -57,7 +57,7 @@ class Par:
         """Obtains value of argument from dictionary"""
         if self.__args.has_key(key):
             return self.__args[key]
-        elif default != None:
+        elif str(default):
             return default
         else:
             sys.stderr.write( '\n  Argument %s= not given to %s \n' %
