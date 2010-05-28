ed9c4337fd5915a9ee0f0f2b88098ea44ce74720
Sergey Fomel <sergey.fomel@gmail.com>
2010-05-28 18:46:25 +0000

    pgcc
    Former-commit-id: aef4596039403f01d5868462f5618c53fa4407d0

--- framework/configure.py
+++ framework/configure.py
@@ -1089,6 +1089,7 @@ def omp(context):
     icc = (string.rfind(CC,'icc') >= 0)
     if pgcc:
         CCFLAGS = flags + ' -mp'
+        LINKFLAGS = lflags
     elif gcc:
         LIBS.append('gomp')
         CCFLAGS = flags + ' -fopenmp'
