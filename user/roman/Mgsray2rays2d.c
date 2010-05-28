e5b12a2c4867ddf93f3c4d2eaf7f8d16913c8576
Roman Kazinnik <roman.only@gmail.com>
2010-05-28 21:20:22 +0000

    Former-commit-id: 6da338f359e041a60e5622f82ea03daf3c2dee1c

---
+++ user/roman/Mgsray2rays2d.c
@@ -0,0 +1,562 @@
+/* Oriented zero-offset migration. */
+/*
+  Copyright (C) 2009 University of Texas at Austin
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#include <rsf.h>
+#include <assert.h>
+/*------------------------------------------*/
+
+#define IMIN(X,Z) ( (X < Z) ? X : Z)
+#define IMAX(X,Z) ( (X > Z) ? X : Z)
+
+/* Phase space grid */
+static int nz,nx,na;
+static float oz,ox,oa;
+static float dz,dx,da;
+
+
+typedef enum {OUTSIDE=0, IZ_IX_UNINIT=0, IZ_IX_Z_N1=1, IZ_IX_Z_0=2, IZ_IX_N1_X=3, IZ_IX_0_X=4} colors;
+const float INIT_VALUE = 1e10;
+
+////////////////////////////////////////////////////
+float ISIGN(float X) 
+{
+    if (X + 1e-6f > 0.f)
+	return 1.f;
+	return -1.f;
+}
+int bc_padd(int ix, int nx)
+{
+    if (ix < 0)
+	return 0;
+    if (ix >= nx)
+	return nx-1;
+    return ix;
+}
+int bc_periodic(int ia1, int na)
+{
+	    while (ia1 < 0) 
+		ia1 += na; 
+	    while (ia1 >= na) 
+		ia1 -= na; 
+	    return ia1;
+}
+float bc_periodicf(float ray_a1)
+{
+    ray_a1 =  fmodf(ray_a1, 2.*SF_PI);
+
+    assert (fabs(ray_a1) < 2.0*SF_PI);
+
+    if (ray_a1 < 0.f) 
+	ray_a1 += 2.*SF_PI; 
+    
+    return ray_a1;
+}
+// float fx(float x, float z, float a)  {}
+////////////////////////////////////////////////////
+void step_z(float ray_z0, float DDz, float oz, float dz,
+	    float *p_ray_z1, int *p_iz1, int *p_iz2, float *p_dist_z1)
+{
+	// step in z
+        *p_ray_z1 = ray_z0 - DDz; // fz / fx * DDx;
+
+	
+	*p_iz1 = floor((*p_ray_z1 - oz)/dz);
+	assert(*p_ray_z1 +1e-6f > oz + (*p_iz1)*dz);
+
+	*p_iz2 = *p_iz1 + 1;
+	assert(*p_ray_z1 < 1e-6f + oz + (*p_iz2)*dz);
+
+	*p_dist_z1 = (*p_ray_z1 - oz)/dz - *p_iz1;
+	assert(*p_dist_z1 + 1e-6f > 0.f && *p_dist_z1 <= 1.0f);
+	
+	// merge two indices if dist==0 or ==1
+	if (*p_dist_z1 < 1e-4) {
+	    *p_dist_z1 = 0.f;
+	    *p_iz2 = *p_iz1;
+	    *p_ray_z1 = oz + (*p_iz2)*dz;
+	}
+	else  if (*p_dist_z1 + 1e-4 > 1.f) {
+	    *p_dist_z1 = 1.f;
+	    *p_iz1 = *p_iz2;
+	    *p_ray_z1 = oz + (*p_iz2)*dz;
+	}
+}
+
+float	propagate_x(int dir,
+		    float fx,  float dx, 
+		    float fz,  float dz, 
+		    float fa,  float da,
+		    float ox,
+		    float oz,
+		    float oa,
+		    int nx,
+		    int nz,
+		    int na,
+		    float ray_x0,  
+		    float ray_z0, 
+		    float ray_a0,
+		    int * p_ix1, 
+		    int * p_iz1, int *p_iz2,
+		    int * p_ia1, int *p_ia2,
+		    float * p_dist_z1,
+		    float * p_dist_a1,
+		    float * p_ray_x1, 
+		    float * p_ray_z1,
+		    float * p_ray_a1)
+{
+    assert (fabs(fx) > 0.1); // propagate in x
+
+    const int ixs = dir * ISIGN(fx);
+
+    //assert((ixs > 0 && ray_x0  > ox) || (ixs < 0 && ray_x0 < ox+(nx-1)*dx));
+
+    if (ixs > 0) {						
+	*p_ix1 = floor ( (ray_x0-ox) / dx );
+	if ((ray_x0 - (ox + (*p_ix1*dx)) < 0.01*dx) && *p_ix1 > 0) {
+	    (*p_ix1) = (*p_ix1) - 1;
+	}
+    }
+    else {
+	*p_ix1 = 1 + floor ((ray_x0-ox) / dx);
+	if (((ox + (*p_ix1*dx)) - ray_x0 < 0.01*dx) && *p_ix1 < nx - 1) {
+	    (*p_ix1) = (*p_ix1) + 1;
+	}
+    }
+        assert(dir < 0 || (*p_ix1 >= 0 && *p_ix1 < nx));
+
+	*p_ray_x1 = ox + (*p_ix1) * dx;
+	assert(dir < 0 || (fabs(*p_ray_x1 - ray_x0) > 1e-6f));
+
+	const float DDx = ray_x0 - *p_ray_x1;
+
+	step_z(ray_z0, fz / fx * DDx, oz, dz, p_ray_z1, p_iz1, p_iz2, p_dist_z1);
+
+	step_z(ray_a0, fa / fx * DDx, oa, da, p_ray_a1, p_ia1, p_ia2, p_dist_a1);
+
+	return fmax( fabs(DDx)/dx, fmax( fabs(fz/fx*DDx)/dz, fabs(fa / fx * DDx)/da));
+}
+
+
+//
+// propagate in x direction and in z direction the solution fx T_x dx + fz T_z dz = f
+//
+// here fx,fz > 0 but ixs, izs >< 0
+//
+// fx!=0 => propagate in x, 
+// fz!=0 => propagate in z
+// choose smallest value and smallest color (is someone was un-init => remain un-init)
+float propagate_x_z (int dir,
+                     int iq,
+		     float *** t, int *** t_color, float **s, float ** sx, float **sz,
+		     float dx, float dz, float da, 
+		     int iz0, int ix0, int ia0, 
+		     int * color,
+                     float **imagt_rays, int imagt_rays_col) 
+//int * is_short_ray)
+{
+    float 
+	ray_x0 = ox + ix0 * dx,
+        ray_z0 = oz + iz0 * dz,
+	ray_a0 = oa + ia0 * da,
+	accum_val = 0.f,
+	ff2 = s[ix0][iz0] * s[ix0][iz0],
+	ff1 = s[ix0][iz0],
+	sn = sinf(ray_a0),
+	cs = cosf(ray_a0),
+	fx = sn * s[ix0][iz0],
+	fz = cs * s[ix0][iz0],
+	fa = (cs*sx[ix0][iz0] - sn*sz[ix0][iz0]);
+    int
+	ix1 = ix0,
+	iz1 = iz0,
+	ia1 = ia0;
+   
+    int ik = 0;
+
+    while (ik < nx + nz + nz)
+    {
+	float  ss1, ss2, sx1, sx2, sz1, sz2, dist_x1z1, ray_x1 = ray_x0, ray_z1 = ray_z0, ray_a1 = ray_a0, dist_a1 = 0.f;
+	int ix2, iz2, ia2 = ia0;
+	ik++;
+	
+	if (fabs(fx) > fabs(fz)) {
+
+	    const float max_delta_x = 
+
+		propagate_x( dir,
+			     fx, dx, fz, dz, fa, da, 
+			     ox, oz, oa, nx, nz, na,
+			     ray_x0, ray_z0, ray_a0,
+			     &ix1, 
+			     &iz1, &iz2, &ia1, &ia2,
+			     &dist_x1z1, &dist_a1,
+			     &ray_x1, &ray_z1, &ray_a1);
+	    
+
+	    assert(dir < 0 || max_delta_x > 1e-6f);
+	    assert(dir < 0 || ISIGN(ray_x0 - ray_x1) == dir * ISIGN(fx));
+	    assert( 1e-6 > fabs(ox + ix1*dx - ray_x1) );
+	    assert(ray_z1 + 1e-6f > oz + iz1*dz && ray_z1 < 1e-6f + oz + iz2*dz);
+	    //assert(ix1 >= 0 && ix1 < nx && iz1 >= 0 && iz1 < nz && iz2 >= 0 && iz2 < nz);
+ 
+	     if (iq == 2) 
+		 accum_val += ff2 / fx * (ray_x0 - ray_x1);
+	     if (iq == 4) 
+		 accum_val += ff1 / fx * (ray_x0 - ray_x1);
+	     
+	     ray_a1 = bc_periodicf(ray_a1);
+
+		 ia1 = bc_periodic(ia1, na);
+		 ia2 = bc_periodic(ia2, na);
+
+		 ix1 = bc_padd(ix1, nx);
+
+		 iz1 = bc_padd(iz1, nz);
+		 iz2 = bc_padd(iz2, nz);
+
+
+	     if (ix1<=0 || ix1>=nx-1 || ((iz1 <= 0 || iz1 >= nz-1) && (iz2<=0 || iz2 >= nz-1)))		 
+	     {		
+		 if (t_color)
+		     *color = IMIN(IMIN(t_color[ia1][ix1][iz1], t_color[ia1][ix1][iz2]), IMIN(t_color[ia2][ix1][iz1], t_color[ia2][ix1][iz2]));
+		 else
+		     *color = 0;
+
+		 if (t)
+		     return  accum_val + 
+			 (1.0f - dist_x1z1) * (t[ia1][ix1][iz1] * (1.0f - dist_a1) +  t[ia2][ix1][iz1] * dist_a1) +
+			 dist_x1z1 * (t[ia1][ix1][iz2] * (1.0f - dist_a1) +  t[ia2][ix1][iz2] * dist_a1);
+		 return -1;
+	     }
+	     else {
+		
+		 ss1 = s[ix1][iz1];ss2 = s[ix1][iz2];
+		 
+		 sx1 = sx[ix1][iz1];sx2 = sx[ix1][iz2];
+
+		 sz1 = sz[ix1][iz1];sz2 = sz[ix1][iz2];
+	     }
+        }
+	else { // propagate Z
+
+	    const float max_delta_z = 
+
+		propagate_x( dir,
+			     fz, dz, fx, dx, fa, da, 
+			     oz, ox, oa, nz, nx, na,
+			     ray_z0, ray_x0, ray_a0,
+			     &iz1, 
+			     &ix1, &ix2, &ia1, &ia2,
+			     &dist_x1z1, &dist_a1,
+			     &ray_z1, &ray_x1, &ray_a1);
+	    
+
+	    assert(dir < 0 || max_delta_z > 1e-6f);
+	    assert(dir < 0 || ISIGN(ray_z0 - ray_z1) == dir * ISIGN(fz));
+	    assert(1e-6 > fabs(oz + iz1*dz - ray_z1));
+	    assert(ray_x1  + 1e-6f > ox + ix1*dx && ray_x1 < 1e-6f +  ox + ix2*dx);
+ 
+	     if (iq == 2) 
+		 accum_val += ff2 / fz * (ray_z0 - ray_z1);
+	     if (iq == 4) 
+		 accum_val += ff1 / fz * (ray_z0 - ray_z1);
+	     
+	     ray_a1 = bc_periodicf(ray_a1);
+
+		 ia1 = bc_periodic(ia1, na);
+		 ia2 = bc_periodic(ia2, na);
+
+		 iz1 = bc_padd(iz1, nz);
+
+		 ix1 = bc_padd(ix1, nx);
+		 ix2 = bc_padd(ix2, nx);
+
+	     if (iz1 <= 0 || iz1 >= nz-1  || ((ix1<=0 || ix1>=nx-1) && (ix2<=0 || ix2 >= nx-1)))		 
+	     {
+		 if (t_color)
+		     *color = IMIN(IMIN(t_color[ia1][ix1][iz1], t_color[ia1][ix2][iz1]), IMIN(t_color[ia2][ix1][iz1], t_color[ia2][ix2][iz1]));
+		 else
+		     *color = 0;
+		 
+		 if (t)
+		     return  accum_val + 
+			 (1.0f - dist_x1z1) * (t[ia1][ix1][iz1] * (1.0f - dist_a1) +  t[ia2][ix1][iz1] * dist_a1) +
+			 dist_x1z1 * (t[ia1][ix2][iz1] * (1.0f - dist_a1) +  t[ia2][ix2][iz1] * dist_a1);
+		 return -1;
+	    }	    
+	    else {
+
+		ss1 = s[ix1][iz1]; ss2 = s[ix2][iz1];
+
+		sx1 = sx[ix1][iz1];sx2 = sx[ix2][iz1];
+
+		sz1 = sz[ix1][iz1];sz2 = sz[ix2][iz1];
+		
+	    }
+	}
+	assert(ray_x1 >= ox && ray_x1 <= ox + (nx-1)*dx);
+	assert(ray_z1 >= oz && ray_z1 <= oz + (nz-1)*dz);
+
+	ray_x0 = ray_x1;
+	ray_z0 = ray_z1;
+	ray_a0 = ray_a1;
+
+	if (imagt_rays) {
+	    imagt_rays[iz1][ix1] = imagt_rays_col;
+	}
+
+	const float 
+	    ss = ss1 * (1.0f - dist_x1z1) + ss2 * dist_x1z1,
+	    ssx = sx1 * (1.0f - dist_x1z1) + sx2 * dist_x1z1,
+	    ssz = sz1 * (1.0f - dist_x1z1) + sz2 * dist_x1z1;
+
+	sn = sinf(ray_a0);
+	cs = cosf(ray_a0);
+
+	fx = sn * ss;
+	fz = cs * ss;
+
+	ff2 = ss*ss;
+	ff1 = ss;
+
+	fa = (cs*ssx - sn*ssz);
+    } // end while
+
+    assert(0);
+    //sf_warning(" ray tracing failed ");
+    *color = IZ_IX_UNINIT;
+    return 1e10f;
+}
+
+/*----------------------------------------*/
+void select_t_l_x(float l, float t, float src_dist, float dx, float *p_src, float *p_len_min, float *p_tr_time)
+{
+    /*
+	if ( (*p_src_neg > src_dist && src_dist > dx) ||
+	     (*p_src_neg > dx && src_dist < dx) ||
+	     (*p_src_neg < dx && src_dist < dx && *p_len_min_neg > l)) 
+    */
+    if ( (*p_src > src_dist + dx) || (fabs(*p_src-src_dist)<dx && *p_len_min > l))
+        {
+                *p_src = src_dist;
+		*p_len_min = l;
+		*p_tr_time = t;
+	}
+}
+/*************************88
+
+input: 
+1. escape x, a, t, z 
+2. xref, zref, aref
+outpur:
+image[ix x iz] == 1 at rays' points
+=0 otherwise
+****************************/
+int main(int argc, char* argv[])
+{
+    int i,ix;//, interpolate; iz, ia, 
+    //float  tola_da, tola, tolz_dz,tolz, tolx, tolx_dx;
+    //float xprev, lprev, tprev, eps;a, t, z, x,
+    //float *tim, *dis, *dep, *ang;
+    //int is_prev_sign, is_sign;
+    float **imagt_rays;//**len_minpath, **tr_time_minpath, **tr_time_pos, ***tr_time_z0;
+
+    /*float escx1 = 0.25, escz1=0.0, esca1=0.5;
+    float escx2 = 0.75, escz2=0.0, esca2=-0.5;
+    float refx1=0.5, refz1=0.75, refa1=0.5;*/
+    int ix0, iz0, ia0;
+
+    float **s, **sx, **sz;
+    float m[3][4];
+    sf_file time;
+    /* sf_file angl, time, dist, dept, timez0, minpath,lent, */
+    sf_file imagt, slow, slowz, slowx, fref1, fref2, fref3;
+    //char *timez0_name, *minpath_name;
+    
+
+    sf_init(argc,argv);
+
+    time/*dist*/ = sf_input("in");
+    /*time = sf_input("time");
+    dept = sf_input("dept");
+    angl = sf_input("ang");*/
+
+    if (!sf_histint(time,"n3",&nz)) sf_error("No n1= in input");
+    if (!sf_histint(time,"n2",&nx)) sf_error("No n2= in input");
+    if (!sf_histint(time,"n1",&na)) sf_error("No n3= in input");
+    if (!sf_histfloat(time,"d3",&dz)) sf_error("No d1= in input");
+    if (!sf_histfloat(time,"o3",&oz)) sf_error("No d1= in input");
+    if (!sf_histfloat(time,"d2",&dx)) sf_error("No d1= in input");
+    if (!sf_histfloat(time,"o2",&ox)) sf_error("No d1= in input");
+    if (!sf_histfloat(time,"d1",&da)) sf_error("No d1= in input");
+    if (!sf_histfloat(time,"o1",&oa)) sf_error("No d1= in input");
+    /*
+    if (!sf_getfloat("tolz",&tolz)) sf_error("No tolz= (float)");
+    if (!sf_getfloat("tolx",&tolx)) sf_error("No tolx= (float)");
+    if (!sf_getfloat("tola",&tola)) sf_error("No tola= (float)");
+    tolz_dz = tolz * dz;
+    tolx_dx = tolx * dx;
+    tola_da = tola * da;
+    */
+
+    /* read auxiliary slowness file */
+    slow = sf_input("slow");
+    s = sf_floatalloc2(nz,nx);
+    sf_floatread(s[0],nz*nx,slow);
+
+    /* read auxiliary slowness z-gradient file */
+    slowz = sf_input("slowz");
+    sz = sf_floatalloc2(nz,nx);
+    sf_floatread(sz[0],nz*nx,slowz);
+
+    /* read auxiliary slowness x-gradient file */
+    slowx = sf_input("slowx");
+    sx = sf_floatalloc2(nz,nx);
+    sf_floatread(sx[0],nz*nx,slowx);
+
+    /* read ref1 = {x, z, a1, a2} */
+    fref1 = sf_input("ref1");
+    sf_floatread(m[0],4,fref1);
+    fref2 = sf_input("ref2");
+    sf_floatread(m[1],4,fref2);
+    fref3 = sf_input("ref3");
+    sf_floatread(m[2],4,fref3);
+
+    /* convert to radians */
+    oa = oa*SF_PI/180.;
+    da = da*SF_PI/180.;
+
+
+    /* rays  */
+    imagt = sf_output("out");
+    imagt_rays = sf_floatalloc2(nx, nz); 
+
+    sf_putint(imagt,"n2",nz);
+    sf_putint(imagt,"n1",nx);
+    sf_putint(imagt,"n3",1);
+
+    sf_putfloat(imagt,"d2",dz);
+    sf_putfloat(imagt,"d1",dx);
+    sf_putfloat(imagt,"o2",oz);
+    sf_putfloat(imagt,"o1",ox);
+
+    sf_putstring(imagt,"label2","z depth");
+    sf_putstring(imagt,"label1","x dist");
+    sf_putstring(imagt,"unit2","km");	
+    sf_putstring(imagt,"unit1","km");	
+
+    /* min-path arrivals 
+    tim = sf_floatalloc(na);
+    dis = sf_floatalloc(na);
+    dep = sf_floatalloc(na);
+    ang = sf_floatalloc(na);
+   
+    timez0_name = sf_getstring ("timez0");    
+    assert (timez0_name);
+    timez0 = sf_output(timez0_name);   
+
+    minpath_name = sf_getstring ("minpath");
+    assert (minpath_name);
+    minpath = sf_output(minpath_name);
+    
+    sf_putint(minpath,"n2",nz);
+    sf_putint(minpath,"n1",nx);
+    sf_putint(minpath,"n3",1);
+
+    sf_putfloat(minpath,"d2",dz);
+    sf_putfloat(minpath,"d1",dx);
+    sf_putfloat(minpath,"o2",z0);
+    sf_putfloat(minpath,"o1",x0);
+
+    sf_putstring(minpath,"label2","z depth");
+    sf_putstring(minpath,"label1","x dist");
+    sf_putstring(minpath,"unit2","km");	
+    sf_putstring(minpath,"unit1","km");	
+
+    tr_time_z0 = sf_floatalloc3(na, nx, nz); 
+    tr_time_pos = sf_floatalloc2(nx, nz); 
+    tr_time_minpath = sf_floatalloc2(nx, nz); 
+    len_minpath = sf_floatalloc2(nx, nz); 
+    */
+
+
+    for (ix=0; ix < nx*nz; ix++) 
+	imagt_rays[0][ix] = 0.f;
+    
+/*
+    for (iz=0; iz < nz; iz++) {
+	
+	for (ix=0; ix < nx; ix++) {					
+
+	    sf_floatread(tim,na,time);
+	    sf_floatread(dis,na,dist);
+	    sf_floatread(dep,na,dept);
+	    sf_floatread(ang,na,angl);
+
+	    for (ia=0; ia < na; ia++) {
+
+		z = dep[ia];
+
+		x = dis[ia];
+
+		a = ang[ia];
+
+		t = tim[ia];
+		if (fabs(z-escz1)<tolz_dz && 
+		    fabs(x-escx1)<tolx_dx &&
+		    fabs(a-esca1)<tola_da) 
+		{		    
+		    //imagt_rays[iz][ix]=1.;
+		}
+		if (fabs(z-escz2)<tolz_dz && 
+		    fabs(x-escx2)<tolx_dx &&
+		    fabs(a-esca2)<tola_da) 
+		{		    
+		    //imagt_rays[iz][ix]=2.;
+		}		    
+	    }  
+	} 
+    } 
+*/
+	
+    int ts_color;
+
+    for (i=0; i<3;i++) {
+	ix0 = floor(0.5+(m[i][0]-ox)/dx);
+	iz0 = floor(0.5+(m[i][1]-oz)/dz);
+	ia0 = floor(0.5+(m[i][2]-oa)/da);
+
+	imagt_rays[iz0][ix0]=i+3;
+
+	(void) propagate_x_z (1, 2/*iq*/, (float***)0/*t*/, (int***)0/*t_colors*/, s, sx, sz,  dx, dz, da, iz0, ix0, ia0, &ts_color, imagt_rays, i+3);
+
+	ix0 = floor(0.5+(m[i][0]-ox)/dx);
+	iz0 = floor(0.5+(m[i][1]-oz)/dz);
+	ia0 = floor(0.5+(m[i][3]-oa)/da);
+	if (ia0 < 0)
+	    ia0 += na;
+	if (ia0 > na-1)
+	    ia0 -= na;
+
+	(void) propagate_x_z (1, 2/*iq*/, (float***)0/*t*/, (int***)0/*t_colors*/, s, sx, sz,  dx, dz, da, iz0, ix0, ia0, &ts_color, imagt_rays, i+3);
+    }
+    sf_floatwrite(imagt_rays[0],nz*nx,imagt);
+
+    sf_close();
+    exit(0);
+}
