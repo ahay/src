05db4fa4e93e63de1f1f18c0f637393b6bf807b3
Sergey Fomel <sergey.fomel@gmail.com>
2010-08-16 18:27:11 +0000

    modrefl3
    Former-commit-id: c417a5e3218d51fe3851b29b4b2b2c7cbae6a9ff

--- system/seismic/Mmodrefl3.c
+++ system/seismic/Mmodrefl3.c
@@ -29,7 +29,7 @@ The output contains PP and PS tau-p seismograms.
 
 int main(int argc, char* argv[])
 {
-    int nt, n1, i1, i2, n2, ip, np, three;
+    int nt, n1, i1, i2, n2, ns, n1s, ip, np, three;
     float *a=NULL, *b=NULL, *r=NULL, *tpp=NULL, *tps=NULL, *app=NULL, *aps=NULL, *spline=NULL, **pp=NULL, **ps=NULL;
     float dt, tp,ts, a1,a2, b1,b2, r1,r2, eps, rc[4], ang[4];
     float d1, p0, dp, p, as, bs, ad1, bd1; 
@@ -46,6 +46,9 @@ int main(int argc, char* argv[])
     n2 = sf_leftsize(in,2);
 
     if (!sf_histfloat(in,"d1",&d1)) sf_error("No d1= in input");
+
+    if (!sf_getint("sparse",&ns)) ns=10;
+    /* sparseness of reflectivity */
 
     if (!sf_getint("nt",&nt)) sf_error("Need nt=");
     /* time samples */
@@ -65,27 +68,33 @@ int main(int argc, char* argv[])
     sf_putint(out,"n1",nt);
     sf_putfloat(out,"d1",dt);
     sf_putfloat(out,"o1",0.);
+    sf_putstring(out,"label1","Time");
+    sf_putstring(out,"unit1","s");
 
     sf_putint(out,"n2",np);
     sf_putfloat(out,"d2",dp);
     sf_putfloat(out,"o2",p0);
 
     sf_putint(out,"n3",2);
-    sf_putint(out,"n4",n2);
+    sf_putint(out,"n4",n2);    
 
     a = sf_floatalloc(n1);
     b = sf_floatalloc(n1);
     r = sf_floatalloc(n1);
-    tpp = sf_floatalloc(n1);
-    tps = sf_floatalloc(n1);
-    app = sf_floatalloc(n1);
-    aps = sf_floatalloc(n1);
+
+    n1s = (n1-1)*ns;
+    d1 /= ns;
+
+    tpp = sf_floatalloc(n1s);
+    tps = sf_floatalloc(n1s);
+    app = sf_floatalloc(n1s);
+    aps = sf_floatalloc(n1s);
 
     spline = sf_floatalloc(nt);
     pp = sf_floatalloc2(nt,np);
     ps = sf_floatalloc2(nt,np);
 
-    map = stretch4_init (nt, 0., dt, n1, eps);
+    map = stretch4_init (nt, 0., dt, n1s, eps);
 
     for (i2=0; i2 < n2; i2++) {
 	sf_warning("CMP %d of %d",i2+1,n2);
@@ -101,7 +110,7 @@ int main(int argc, char* argv[])
 	    a2 = a[0];
 	    b2 = b[0];
 	    r2 = r[0];
-	    for (i1=1; i1 < n1; i1++) {
+	    for (i1=1; i1 <= n1s; i1++) {
 		as = a2*p;
 		if (fabsf(as) > 1.) 
 		    sf_error("p=%g is postcritical (vp=%g)",p,a2);
@@ -112,23 +121,28 @@ int main(int argc, char* argv[])
 		
 		ad1 = d1*sqrtf(1.-as*as)/a2;
 		bd1 = d1*sqrtf(1.-bs*bs)/b2;
-		
-		tpp[i1] = tp;
-		tps[i1] = ts;
 
 		tp += 2.*ad1;
 		ts += ad1 + bd1;
+
+		tpp[i1-1] = tp;
+		tps[i1-1] = ts;
 	
 		a1 = a2;
-		a2 = a[i1];
 		b1 = b2;
-		b2 = b[i1];
 		r1 = r2;
-		r2 = r[i1];
 
-		zoeppritz (1,a1,a2,b1,b2,r1,r2,true,p,rc,ang);
-		app[i1] = rc[0];
-		aps[i1] = rc[1];
+		if (0==i1%ns) {
+		    a2 = a[i1/ns];
+		    b2 = b[i1/ns];
+		    r2 = r[i1/ns];
+		    zoeppritz (1,a1,a2,b1,b2,r1,r2,true,p,rc,ang);
+		} else {
+		    rc[0] = rc[1] = 0.;
+		}
+
+		app[i1-1] = rc[0];
+		aps[i1-1] = rc[1];
 	    }
 
 	    stretch4_define (map,tpp);
