47da1c41d2ac5cbf93f1c2baf5f12c9070882dda
Jeff Godwin <godwin.jeffrey@gmail.com>
2010-08-17 19:05:31 +0000

    Fixed JAVA_HOME support
    Former-commit-id: 05970c7f34536a919636d21d7de2076ef75cae1c

--- framework/configure.py
+++ framework/configure.py
@@ -1,5 +1,4 @@
-# Copyright (C) 2005-2010 University of Texas at Austin
-#
+# Copyright (C) 2005-2010 University of Texas at Austin #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -1539,7 +1538,8 @@ def java(context):
         need_pkg('java-devel')
     
     context.Message("checking for JAVA_HOME ... ")
-    JAVA_HOME = context.env.get('JAVA_HOME')
+    JAVA_HOME = context.env.get('JAVA_HOME',os.environ.get('JAVA_HOME'))
+    #JAVA_HOME = context.env.get('JAVA_HOME')
     if JAVA_HOME:
         context.Result(JAVA_HOME)
         context.env['JAVA_HOME'] = JAVA_HOME
