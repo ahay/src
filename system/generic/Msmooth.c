0a0fffdba920e7b5dbf4686874f254e33476d808
Sergey Fomel <sergey.fomel@gmail.com>
2010-10-09 03:04:05 +0000

    noise in sfsmooth
    Former-commit-id: 83c7f0b4916ff471413d914c99e29e34168e7cbd

--- system/generic/Msmooth.c
+++ system/generic/Msmooth.c
@@ -74,7 +74,7 @@ int main (int argc, char* argv[])
 	    for (j=0; j < n1/n[i]; j++) {
 		i0 = sf_first_index (i,j,dim1+1,n,s);
 		for (irep=0; irep < nrep; irep++) {
-		    sf_smooth (tr,i0,s[i],diff[i],box[i],data);
+		    sf_smooth2 (tr,i0,s[i],diff[i],box[i],data);
 		}
 	    }
 	    sf_triangle_close(tr);
