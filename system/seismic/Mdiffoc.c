04b789204cfb2dc78736e633a837df7eb07c9d94
Sergey Fomel <sergey.fomel@gmail.com>
2010-12-16 04:53:58 +0000

    update
    Former-commit-id: 7bb3ba6fadba8097ec1584c7cacc5458f1aea2f6

--- system/seismic/Mdiffoc.c
+++ system/seismic/Mdiffoc.c
@@ -23,20 +23,25 @@
 
 int main(int argc, char* argv[]) 
 {
-    int it, nt, ix, nx, i2, n2, n3, nw, next;
-    float v0, v1, dt, t0, o2, d2, t;
+    int it, nt, ix, nx, i2, n2, n3, iw, nw, next;
+    float v0, v1, dt, t0, o2, d2, t, dx, k, w, dw;
     float **data, *trace, *strace;
-    sf_complex *ctrace;
+    sf_complex *ctrace, shift;
     fint1 str, istr;
     kiss_fftr_cfg forw, invs;
     sf_file inp, out;
 
+    sf_init(argc,argv);
     inp = sf_input("in");
+    out = sf_output("out");
+
     if (SF_FLOAT != sf_gettype(inp)) sf_error("Need float input");
     if (!sf_histint(inp,"n1",&nt)) sf_error("No n1= in input");
     if (!sf_histfloat(inp,"d1",&dt)) sf_error("No d1= in input");
     if (!sf_histfloat(inp,"o1",&t0)) t0=0.;  
+
     if (!sf_histint(inp,"n2",&nx)) sf_error("No n2= in input");
+    if (!sf_histfloat(inp,"d2",&dx)) sf_error("No d2= in input");
 
     if (!sf_getfloat("v0",&v0)) v0=SF_EPS;
     /* initial velocity */
@@ -58,6 +63,8 @@ int main(int argc, char* argv[])
     istr = fint1_init(next,n2,0);
 
     nw = n3/2+1;
+    dw = 2*SF_PI/(n3*d2);
+
     forw = kiss_fftr_alloc(n3,0,NULL,NULL);
     invs = kiss_fftr_alloc(n3,1,NULL,NULL);
     if (NULL == forw || NULL == invs) 
@@ -71,6 +78,7 @@ int main(int argc, char* argv[])
 
     /* cosine FT in space */
     sf_cosft_init(nx);
+    dx = SF_PI/(kiss_fft_next_fast_size(nx-1)*dx);
 
     for (it=0; it < nt; it++) {
 	sf_cosft_frw (data[0], it, nt);
@@ -78,6 +86,8 @@ int main(int argc, char* argv[])
 
     for (ix=0; ix < nx; ix++) {
 	/* loop over wavenumbers */
+	k = ix*dx;
+	k *= k*(v0*v0 - v1*v1);
 
 	trace = data[ix];
 
@@ -104,8 +114,22 @@ int main(int argc, char* argv[])
 	/* FFT */
 
 	kiss_fftr(forw,strace, (kiss_fft_cpx *) ctrace);
+
+	/* velocity continuation itself */
+
 	ctrace[0]=sf_cmplx(0.,0.); /* dc */
-	
+
+	for (iw=1; iw < nw; iw++) {
+	    w = iw*dw;
+	    w = k/w;
+	    shift = sf_cmplx(cosf(w)/n3,sinf(w)/n3);
+
+#ifdef SF_HAS_COMPLEX_H
+	    ctrace[iw] *= shift;
+#else
+	    ctrace[iw] = sf_cmul(ctrace[iw],shift);
+#endif
+	}
 
 	/* Inverse FFT */
 
