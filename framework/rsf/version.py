d77bab7cb4da061586963243e8fa192684b9a239
Ioan Vlad <i.vlad@yahoo.com>
2010-12-31 20:14:52 +0000

    a little documentation
    Former-commit-id: 29284589095c2ed4e4a2d60d790a7d2e1ccbc140

--- framework/rsf/version.py
+++ framework/rsf/version.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+# Variable specifying whether this is the ongoing development version,
+# or a stable release. Usage example: For the development version, HTML
+# self-doc will print svn version nr, while for stable releases, it will
+# print the release nr.
 development_version = True
 
+# Version
 label = '1.1alpha'
