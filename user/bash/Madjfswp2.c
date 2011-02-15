4250ed9d6555898a825bb8b3916efd5cebead233
Vladimir Bashkardin <bash@mail.utexas.edu>
2011-02-15 01:39:00 +0000

    Adjoint-state reflection
    Former-commit-id: 6dbe13dcd1308be98d28a0d310b38281b432016a

--- user/bash/Madjfswp2.c
+++ user/bash/Madjfswp2.c
@@ -22,12 +22,17 @@
 
 #include "adjfswp2.h"
 
+static void sf_compute_grad_t (sf_eno2 gradt, float **grtz, float **grtx,
+                               int n1, int n2, float d1, float d2);
+
 int main (int argc,char* argv[]) {
     int n1, n2, nshot, is, niter, nh;
     float d1, d2, o1, o2, oh, dh;
     float **t1, **t2 = NULL, *dt, **l, *h = NULL;
+    float **grtz, **grtx;
     char *sfile = NULL;
     sf_eno horiz = NULL;
+    sf_eno2 gradt;
     sf_file time, lambda, horizon, deltat;
 
     sf_init (argc, argv);
@@ -58,6 +63,8 @@ int main (int argc,char* argv[]) {
     t1  = sf_floatalloc2 (n1, n2);
     t2  = sf_floatalloc2 (n1, n2);
     l  = sf_floatalloc2 (n1, n2);
+    grtz  = sf_floatalloc2 (n1, n2);
+    grtx  = sf_floatalloc2 (n1, n2);
     dt = sf_floatalloc (n2);
 
     sfile = sf_getstring ("horizon");
@@ -77,12 +84,14 @@ int main (int argc,char* argv[]) {
        horiz = sf_eno_init (5, nh);
        sf_eno_set (horiz, h);
        /* Collapse extra dimension */
-       sf_putint (lambda, "n3", nshot);
-       sf_putint (lambda, "n4", 1);
+//       sf_putint (lambda, "n3", nshot);
+//       sf_putint (lambda, "n4", 1);
 
        free (sfile); sfile = NULL;
     } else
        if (!sf_histint (time, "n3", &nshot)) sf_error ("No n3= in input");
+
+    gradt = sf_eno2_init (3, n1, n2);
 
     /* loop over shots */
     for (is = 0; is < nshot; is++) {
@@ -90,21 +99,43 @@ int main (int argc,char* argv[]) {
             sf_warning ("Calculating shot %d of %d", is + 1, nshot);
 
         sf_floatread (t1[0], n1*n2, time);
-        sf_floatread (t2[0], n1*n2, time);
         sf_floatread (dt, n2, deltat);
 
-        sf_init_lambda_2d_sweep (dt, l, t2, n2, n1, d2, d1);
-
-        sf_run_lambda_2d_sweep2 (t2, l, niter, n2, n1, d2, d1);
-/*
-        if (horiz) {
+        if (horiz) { /* Upgoing part */
+            sf_floatread (t2[0], n1*n2, time);
+            sf_eno2_set (gradt, t2);
+            sf_compute_grad_t (gradt, grtz, grtx, n1, n2, d1, d2);
+            sf_init_lambda_2d_sweep (dt, l, grtz, grtx, n1, n2, d1, d2);
+            sf_run_lambda_2d_sweep (l, grtz, grtx, niter, n1, n2, o1, o2, d1, d1,
+                                    NULL, oh, dh, nh);
+            sf_floatwrite (l[0], n1*n2, lambda);
         }
-*/
+        /* Downgoing part */
+        sf_eno2_set (gradt, t1);
+        sf_compute_grad_t (gradt, grtz, grtx, n1, n2, d1, d2);
+        sf_run_lambda_2d_sweep (l, grtz, grtx, niter, n1, n2, o1, o2, d1, d1,
+                                horiz, oh, dh, nh);
         sf_floatwrite (l[0], n1*n2, lambda);
     }
     if (horiz)
         sf_eno_close (horiz);
 
+    sf_eno2_close (gradt);
+
     return 0;
+}
+
+static void sf_compute_grad_t (sf_eno2 gradt, float **grtz, float **grtx,
+                               int n1, int n2, float d1, float d2) {
+    int i, j;
+    float grad[2];
+
+    for (j = 0; j < n2; j++) {
+        for (i = 0; i < n1; i++) {
+            sf_eno2_apply (gradt, i, j, 0., 0., NULL, grad, DER);
+            grtz[j][i] = grad[0]/d1;
+            grtx[j][i] = grad[1]/d2;
+        }
+    }
 }
 
--- user/bash/adjfswp2.c
+++ user/bash/adjfswp2.c
@@ -20,16 +20,18 @@
 #include <math.h>
 #include <rsf.h>
 
-void sf_init_lambda_2d_sweep (float *dt, float **l, float **t,
-                              int n2, int n1, float d2, float d1)
+void sf_init_lambda_2d_sweep (float *dt, float **l, float **grtz, float **grtx,
+                              int n1, int n2, float d1, float d2)
 /*< Initialize lambda on the top of the model >*/
 {
     int i, j;
+    float grz;
 
     for (i = 0; i < n2; i++) {
-        l[i][0] = dt[i] /
-                  -(-t[i][2] + 4.0*t[i][1] - 3.0*t[i][0])*d1*2.0;
-        if (isinf (l[i][0]) || isnan (l[i][0]))
+        grz = grtz[i][0];
+        if (grz != 0.0)
+            l[i][0] = -dt[i] / grz;
+        else
             l[i][0] = 0.0;
     }
 
@@ -40,22 +42,20 @@ void sf_init_lambda_2d_sweep (float *dt, float **l, float **t,
     }
 }
 
-static void sf_fast_lsweep_2d_stencil2 (float **l, sf_eno2 dt,
-                                        int i, int j, int n1, int n2,
-                                        float d1, float d2) {
-    float gr[2], gr2[2];
-    float fz, fx, dd;
+static void sf_fast_lsweep_2d_stencil (float **l, float **grtz, float **grtx,
+                                       int i, int j, int n1, int n2,
+                                       float d1, float d2) {
+    float fz, fx, dd, grz, grx;
     float fzmax, fzmin, fxmax, fxmin;
     int im, jm, ip, jp;
 
-    sf_eno2_apply (dt, i, j, 0., 0., NULL, gr, DER);
+    grz = grtz[j][i];
+    grx = grtx[j][i];
 
-    fz = gr[0]/d1/d1;
-    fx = gr[1]/d2/d2;
-
+    fz = grz/d1;
+    fx = grx/d2;
     fzmin = SF_MAX(-fz,0.);
     fzmax = SF_MAX(fz,0.);
-
     fxmin = SF_MAX(-fx,0.);
     fxmax = SF_MAX(fx,0.);
 
@@ -78,217 +78,98 @@ static void sf_fast_lsweep_2d_stencil2 (float **l, sf_eno2 dt,
     /* Choose between left-sided and right-sided second
        derivative depending on the coefficients */
     if (fzmin != 0.0 && i != 0) {
-        sf_eno2_apply (dt, i - 1, j, 0., 0., NULL, gr2, DER);
-        dd += (gr[0] - gr2[0])/d1/d1;
+        dd += (grz - grtz[j][im])/d1;
     } else if (fzmax != 0.0 && i != (n1 - 1)) {
-        sf_eno2_apply (dt, i + 1, j, 0., 0., NULL, gr2, DER);
-        dd += (gr2[0] - gr[0])/d1/d1;
+        dd += (grtz[j][ip] - grz)/d1;
     }
     if (fxmin != 0.0 && j != 0) {
-        sf_eno2_apply (dt, i, j - 1, 0., 0., NULL, gr2, DER);
-        dd += (gr[1] - gr2[1])/d2/d2;
+        dd += (grx - grtx[jm][i])/d2;
     } else if (fxmax != 0.0 && j != (n1 - 1)) {
-        sf_eno2_apply (dt, i, j + 1, 0., 0., NULL, gr2, DER);
-        dd += (gr2[1] - gr[1])/d2/d2;
+        dd += (grtx[jp][i] - grx)/d2;
     }
 
     l[j][i] = (fzmax*l[j][ip] + fzmin*l[j][im] +
                fxmax*l[jp][i] + fxmin*l[jm][i])/dd;
 }
 
-/* Apply lambda calculation to one point */
-static void sf_fast_lsweep_2d_stencil (float **t, float **l,
-                                       int i, int j, int n1, int n2,
-                                       float d1, float d2) {
-    float ap, am, bp, bm, app, apm, amp, amm, bpp, bpm, bmp, bmm;
-    float tij, tim, tip, tjm, tjp, lij, lim, lip, ljm, ljp;
-
-    tij = t[j][i];
-    tim = t[j][i - 1];
-    tip = t[j][i + 1];
-    tjm = t[j - 1][i];
-    tjp = t[j + 1][i];
-
-    lij = l[j][i];
-    lim = l[j][i - 1];
-    lip = l[j][i + 1];
-    ljm = l[j - 1][i];
-    ljp = l[j + 1][i];
-
-    ap = -(tip - tij) / d1;
-    am = -(tij - tim) / d1;
-    bp = -(tjp - tij) / d2;
-    bm = -(tij - tjm) / d2;
-
-    app = 0.5*(ap + (ap < 0 ? -ap : ap));
-    apm = 0.5*(am + (am < 0 ? -am : am));
-    amp = 0.5*(ap - (ap < 0 ? -ap : ap));
-    amm = 0.5*(am - (am < 0 ? -am : am));
-    bpp = 0.5*(bp + (bp < 0 ? -bp : bp));
-    bpm = 0.5*(bm + (bm < 0 ? -bm : bm));
-    bmp = 0.5*(bp - (bp < 0 ? -bp : bp));
-    bmm = 0.5*(bm - (bm < 0 ? -bm : bm));
-
-    lij = ((apm*lim - amp*lip)/d1 + (bpm*ljm - bmp*ljp)/d2)  /
-          ((app - amm)/d1 + (bpp - bmm)/d2);
-    l[j][i] = lij;
+static int sf_get_horiz2d_iz (sf_eno horiz, float oh, float dh, int nh, int ix,
+                              int n1, int n2, float o1, float o2, float d1, float d2) {
+    int iz;
+    float x;
+    x = o2 + ix*d2;
+    if (x < oh) x = oh;
+    if (x > (oh + (nh - 1)*dh)) x = oh + (nh - 1)*dh;
+    x = (x - oh)/dh;
+    /* Find horizon location in z for this x */
+    sf_eno_apply (horiz, (int)x, x - (int)x, &x, NULL, FUNC);
+    iz = (x - o1)/d1;
+    iz -= 2;
+    return iz < 0 ? 0 : iz;
 }
 
-void sf_run_lambda_2d_sweep (float **t, float **l,
-                             int niter, int n2, int n1,
-                             float d2, float d1)
+void sf_run_lambda_2d_sweep (float **l, float **grtz, float **grtx, int niter,
+                             int n1, int n2, float o1, float o2, float d1, float d2,
+                             sf_eno horiz, float oh, float dh, int nh)
 /*< Run lambda calculation over whole domain >*/
 {
-    int i, j, k = 0, is = -1, js = -1;
-    float sl = 0.0;
+    int i, j, k;
+    int imin, imax, jmin, jmax;
+    float *hor = NULL;
 
-    /* Find shot location */
-    for (j = 0; j < n2; j++) {
-        for (i = 0; i < n1; i++) {
-            if (t[j][i] == 0.0) {
-                js = j;
-                is = i;
-                j = n2 - 1;
-                break;
-            }
-        }
-    }
-
-    /* Sweeps */
-    for (k = 0; k < niter; k++) {
-        for (j = 1; j < (n2 - 1); j++) {
-            for (i = 1; i < (n1 - 1); i++) {
-                if (is != i || js != j)
-                    sf_fast_lsweep_2d_stencil (t, l, i, j, n1, n2,
-                                               d1, d2);
-            }
-        }
-        for (j = n2 - 2; j > 0; j--) {
-            for (i = 1; i < (n1 - 1); i++) {
-                if (is != i || js != j)
-                    sf_fast_lsweep_2d_stencil (t, l, i, j, n1, n2,
-                                               d1, d2);
-            }
-        }
-        for (j = n2 - 2; j > 0; j--) {
-            for (i = n1 - 2; i > 0; i--) {
-                if (is != i || js != j)
-                    sf_fast_lsweep_2d_stencil (t, l, i, j, n1, n2,
-                                               d1, d2);
-            }
-        }
-        for (j = 1; j < (n2 - 1); j++) {
-            for (i = n1 - 2; i > 0; i--) {
-                if (is != i || js != j)
-                    sf_fast_lsweep_2d_stencil (t, l, i, j, n1, n2,
-                                               d1, d2);
-            }
-        }
-    }
-
-    /* Fill the shot location */
-    if (is != -1 && js != -1) {
-        i = 0;
-        if (is > 0) {
-            sl = l[js][is - 1];
-            i++;
-        }
-        if (is < (n1 - 1)) {
-            sl = l[js][is + 1];
-            i++;
-        }
-        if (js > 0) {
-            sl = l[js - 1][is];
-            i++;
-        }
-        if (js < (n2 - 1)) {
-            sl = l[js + 1][is];
-            i++;
-        }
-        sl /= (float)i;
-        l[js][is] = sl;
-    }
-}
-
-void sf_run_lambda_2d_sweep2 (float **t, float **l,
-                              int niter, int n2, int n1,
-                              float d2, float d1)
-/*< Run lambda calculation over whole domain >*/
-{
-    int i, j, k = 0, is = -1, js = -1;
-    float sl = 0.0;
-    sf_eno2 dt;
-
-    dt = sf_eno2_init (3, n1, n2);
-    sf_eno2_set (dt, t);
-
-    /* Find shot location */
-    for (j = 0; j < n2; j++) {
-        for (i = 0; i < n1; i++) {
-            if (t[j][i] == 0.0) {
-                js = j;
-                is = i;
-                j = n2 - 1;
-                break;
-            }
-        }
-    }
-
-    /* Sweeps */
-    for (k = 0; k < niter; k++) {
-        for (j = 1; j < n2; j++) {
-            for (i = 1; i < n1; i++) {
-                if (is != i || js != j)
-                    sf_fast_lsweep_2d_stencil2 (l, dt, i, j, n1, n2,
-                                               d1, d2);
-            }
-        }
-        for (j = n2 - 1; j >= 0; j--) {
-            for (i = 1; i < n1; i++) {
-                if (is != i || js != j)
-                    sf_fast_lsweep_2d_stencil2 (l, dt, i, j, n1, n2,
-                                               d1, d2);
-            }
-        }
-        for (j = n2 - 1; j >= 0; j--) {
-            for (i = n1 - 1; i > 0; i--) {
-                if (is != i || js != j)
-                    sf_fast_lsweep_2d_stencil2 (l, dt, i, j, n1, n2,
-                                               d1, d2);
-            }
-        }
+    if (horiz) {
+        hor = sf_floatalloc (n2);
         for (j = 0; j < n2; j++) {
-            for (i = n1 - 1; i > 0; i--) {
-                if (is != i || js != j)
-                    sf_fast_lsweep_2d_stencil2 (l, dt, i, j, n1, n2,
-                                               d1, d2);
+            hor[j] = sf_get_horiz2d_iz (horiz, oh, dh, nh, j, n1, n2,
+                                        o1, o2, d1, d2);
+            imax = hor[j];
+            for (i = 0; i < imax; i++)
+                l[j][i] = 0.0;
+        }
+    }
+
+    /* Sweeps */
+    for (k = 0; k < niter; k++) {
+        jmin = 0; jmax = n2 - 1;
+        if (horiz) {
+            imin = 0;
+        } else {
+            imin = 1; imax = n1 - 1;
+        }
+        for (j = jmin; j <= jmax; j++) {
+            if (horiz)
+                imax = hor[j];
+            for (i = imin; i <= imax; i++) {
+                sf_fast_lsweep_2d_stencil (l, grtz, grtx, i, j,
+                                           n1, n2, d1, d2);
+            }
+        }
+        for (j = jmax; j >= jmin; j--) {
+            if (horiz)
+                imax = hor[j];
+            for (i = imin; i <= imax; i++) {
+                sf_fast_lsweep_2d_stencil (l, grtz, grtx, i, j,
+                                           n1, n2, d1, d2);
+            }
+        }
+        for (j = jmax; j >= jmin; j--) {
+            if (horiz)
+                imax = hor[j];
+            for (i = imax; i >= imin; i--) {
+                sf_fast_lsweep_2d_stencil (l, grtz, grtx, i, j,
+                                           n1, n2, d1, d2);
+            }
+        }
+        for (j = jmin; j <= jmax; j++) {
+            if (horiz)
+                imax = hor[j];
+            for (i = imax; i >= imin; i--) {
+                sf_fast_lsweep_2d_stencil (l, grtz, grtx, i, j,
+                                           n1, n2, d1, d2);
             }
         }
     }
 
-    sf_eno2_close (dt);
-
-    /* Fill the shot location */
-    if (is != -1 && js != -1) {
-        i = 0;
-        if (is > 0) {
-            sl = l[js][is - 1];
-            i++;
-        }
-        if (is < (n1 - 1)) {
-            sl = l[js][is + 1];
-            i++;
-        }
-        if (js > 0) {
-            sl = l[js - 1][is];
-            i++;
-        }
-        if (js < (n2 - 1)) {
-            sl = l[js + 1][is];
-            i++;
-        }
-        sl /= (float)i;
-        l[js][is] = sl;
-    }
+    if (horiz)
+        free (hor);
 }
 
