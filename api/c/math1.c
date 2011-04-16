8332a4a5ae0368577904c641c46377e3a78e535d
Sergey Fomel <sergey.fomel@gmail.com>
2011-04-16 15:32:58 +0000

    sign in sfmath
    Former-commit-id: 0e548ea0c80d411dbd455a8cb539f5f58a0e8ac0

--- api/c/math1.c
+++ api/c/math1.c
@@ -34,6 +34,12 @@
 #include "file.h"
 /*^*/
 
+static float mysign(float f)
+{
+    f = (f >= 0  ?  1.0f  :  -1.0f );
+    return f;
+}
+
 typedef float (*func)(float);
 static func functable[] = {
     cosf,
@@ -53,7 +59,8 @@ static func functable[] = {
     sqrtf,
     fabsf,
     erff,
-    erfcf
+    erfcf,
+    mysign
 };
 
 static sf_complex myabs(sf_complex c)
@@ -421,6 +428,7 @@ size_t sf_math_parse (char*       output /* expression */,
 		} else if (0==strcmp(key,"abs"))   { *indx = 15;
 		} else if (0==strcmp(key,"erf")  && SF_FLOAT==datatype)   { *indx = 16;
 		} else if (0==strcmp(key,"erfc") && SF_FLOAT==datatype)   { *indx = 17;
+		} else if (0==strcmp(key,"sign") && SF_FLOAT==datatype)   { *indx = 18;   
 		} else if (0==strcmp(key,"arg") && SF_COMPLEX==datatype) { *indx = 16;
 		} else if (0==strcmp(key,"conj") && SF_COMPLEX==datatype) { *indx = 17;
 		} else if (0==strcmp(key,"real") && SF_COMPLEX==datatype) { *indx = 18;
