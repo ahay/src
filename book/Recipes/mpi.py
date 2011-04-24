f7b67b0f209202e436b0d3c829ebf32755851b8a
Jeff Godwin <godwin.jeffrey@gmail.com>
2011-04-24 00:08:55 +0000

    Fixed to reflect new state of cluster.py
    Former-commit-id: aa083e6ceca2cf816fafcccb2fa0554071b9e0b5

--- book/Recipes/mpi.py
+++ book/Recipes/mpi.py
@@ -1,10 +1,11 @@
+cluster = False
 try:
     from rsf.cluster import *
-    cluster = True
+    if CSCONS:
+        cluster = True
 except:
     from rsf.proj import *
-    cluster = False
-import os
+import subprocess
 
 
 def processes(nodes=None,single=None):
@@ -27,13 +28,19 @@ def processes(nodes=None,single=None):
         else:
             return numThreads
 
+def WhereIs(command):
+    p = subprocess.Popen('which %s' % command, shell=True, stdout=subprocess.PIPE)
+    stdout,stderr = p.communicate()
+    if len(stdout) == 0: raise Exception('could not find %s' % command)
+    return stdout.strip('\n')
+
 def _find(np,command,custom=''):
     ''' Find the mpiexec command, and the command to execute '''
     return '%s -np %d %s %s' % (WhereIs('mpiexec'),np,custom,WhereIs(command))
 
 
 def encode(encodings,shotGathers,encoding,
-           np, eprefix,dprefix,mpi=None,time=None,nodes=None,ppn=None,mpiopts=None):
+           np, eprefix,dprefix,mpiopts='--bynode',**kw):
     ''' 
     encode using sfmpiencode.
 
@@ -45,33 +52,29 @@ def encode(encodings,shotGathers,encoding,
 
     shotGathers.insert(0,encoding)
 
-    if mpi:
-        Flow(encodings,shotGathers,
-            '''
-            sfmpiencode
-            ''' + 
-            '''eprefix='''+eprefix+''' dprefix=''' + dprefix + 
-            '''
-            encode=${SOURCES[0]}
-            verb=y
-            ''',mpi=True,np=np,nodes=nodes,time=time,ppn=ppn,mpiopts=mpiopts)
-    else:
-        Flow(encodings,shotGathers,
-            '''
-            %s
-            ''' % _find(np,'sfmpiencode','--bynode') + 
-            '''eprefix='''+eprefix+''' dprefix=''' + dprefix + 
-            '''
-            encode=${SOURCES[0]}
-            verb=y
-            ''')
+    command = _find(np,'sfmpiencode',mpiopts)
+
+    global cluster
+    if not cluster:
+        kw = {}
+
+    Flow(encodings,shotGathers,
+        '''
+        %s
+        ''' % (command) + 
+        '''eprefix='''+eprefix+''' dprefix=''' + dprefix + 
+        '''
+        encode=${SOURCES[0]}
+        verb=y
+        ''',stdin=0, stdout=-1,**kw)
 
 def gridandencode(encodings, shotGathers, encoding, 
            np, 
            eprefix, dprefix,
-           nx,ox,dx,ny,oy,dy,custom,
-           mpi=None,time=None,nodes=None,ppn=None,mpiopts=None):
-    ''' encode using sfbigmpiencode
+           nx,ox,dx,ny,oy,dy,custom=None,
+           mpiopts='--bynode',**kw):
+    ''' 
+    encode using sfbigmpiencode
     encodings - list of produced encoding files
     shotGathers - lsit of shotgathers to encode
     encoding - encoding produced by sfencodemaker
@@ -86,30 +89,24 @@ def gridandencode(encodings, shotGathers, encoding,
     if not '.rsf' in dprefix:
         dprefix +='.rsf'
     shotGathers.insert(0,encoding) 
-    if mpi :
-        Flow(encodings, shotGathers,
-            '''
-            sfbigmpiencode
-            ''' + 
-            ''' eprefix=''' + eprefix + 
-            ''' dprefix=''' + dprefix + 
-            '''
-            encode=${SOURCES[0]}
-            nx=%d ox=%f dx=%f
-            ny=%d oy=%f dy=%f
-            ''' % (nx,ox,dx,ny,oy,dy), mpi=True,time=time,nodes=nodes,ppn=ppn,mpiopts=mpiopts)
-    else: 
-        Flow(encodings, shotGathers,
-            '''
-            %s
-            ''' % (_find(np,'sfbigmpiencode',custom)) + 
-            ''' eprefix=''' + eprefix + 
-            ''' dprefix=''' + dprefix + 
-            '''
-            encode=${SOURCES[0]}
-            nx=%d ox=%f dx=%f
-            ny=%d oy=%f dy=%f
-            ''' % (nx,ox,dx,ny,oy,dy) ,stdin=0, stdout=-1)
+
+    command = _find(np,'sfbigmpiencode',mpiopts)
+
+    global cluster
+    if not cluster:
+        kw = {}
+
+    Flow(encodings, shotGathers,
+        '''
+        %s
+        ''' % (command) + 
+        ''' eprefix=''' + eprefix + 
+        ''' dprefix=''' + dprefix + 
+        '''
+        encode=${SOURCES[0]}
+        nx=%d ox=%f dx=%f
+        ny=%d oy=%f dy=%f
+        ''' % (nx,ox,dx,ny,oy,dy), stdin=0,stdout=-1,**kw)
             
 def gridandstack(stack,files,np,
                 fprefix,
@@ -118,7 +115,7 @@ def gridandstack(stack,files,np,
                 nz,oz,dz,
                 nf=None,of=None,jf=None,
                 shots=None,
-                mpi=None,time=None,nodes=None,ppn=None,mpiopts=None):
+                mpiopts='--bynode', **kw):
     ''' stack files using sfbigencode, does not require files
     to be on the same cube, will relocate them in the cube
     
@@ -141,32 +138,19 @@ def gridandstack(stack,files,np,
     if not '.rsf' in oprefix:
         oprefix += '.rsf'
 
+    global cluster
+    if not cluster:
+        kw = {}
     if shots:
         shotfile = stack+'-shots'
         Flow(shotfile,None,'points type=i out=${TARGETS[0]} x=%s' % reduce(lambda x,y: str(x)+','+str(y),shots))
        
         shotfile += '.rsf'
         files.append(shotfile)
-        if mpi:
-            Flow(stack,files,
-                    '''
-                    sfbigmpistack
-                    ''' + 
-                    '''
-                    nx=%d ny=%d nz=%d
-                    ox=%f oy=%f oz=%d
-                    dx=%f dy=%f dz=%f
-                    shots="%s"
-                    verb=y
-                    ''' % (nx,ny,nz,ox,oy,oz,dx,dy,dz,shotfile) + 
-                    '''
-                    prefix="'''+fprefix+'''" oname="'''+oprefix+'''"''',
-                    mpi=True,nodes=nodes,ppn=ppn,np=np,mpiopts=mpiopts,time=time)
-        else:
-            Flow(stack,files,
+        Flow(stack,files,
                 '''
                 %s 
-                ''' % (_find(np,'sfbigmpistack')) +
+                ''' % (_find(np,'sfbigmpistack',mpiopts)) + 
                 '''
                 nx=%d ny=%d nz=%d
                 ox=%f oy=%f oz=%d
@@ -175,41 +159,27 @@ def gridandstack(stack,files,np,
                 verb=y
                 ''' % (nx,ny,nz,ox,oy,oz,dx,dy,dz,shotfile) + 
                 '''
-                prefix="'''+fprefix+'''" oname="'''+oprefix+'''"''',stdin=0,stdout=-1)
-
+                prefix="'''+fprefix+'''" oname="'''+oprefix+'''"''',
+                stdin=0, stdout=-1, **kw)
+                    
     else:
-        if (not of) or (not jf) or (not nf):
+        if of == None or jf == None or nf == None:
             raise Exception('must specify either shots or nf,of,jf')
 
-        if not mpi:
-            Flow(stack,files,
-                '''
-                %s 
-                ''' % (_find(np,'sfbigmpistack')) +
-                '''
-                nx=%d ny=%d nz=%d
-                ox=%f oy=%f oz=%d
-                dx=%f dy=%f dz=%f
-                nf=%d of=%d jf=%d
-                ''' % (nx,ny,nz,ox,oy,oz,dx,dy,dz,nf,of,jf) + 
-                '''
-                prefix="'''+fprefix+'''" oname="'''+oprefix+'''"''',stdin=0,stdout=-1)
-        else:
-             Flow(stack,files,
-                '''
-                %s 
-                ''' % ('sfbigmpistack') +
-                '''
-                nx=%d ny=%d nz=%d
-                ox=%f oy=%f oz=%d
-                dx=%f dy=%f dz=%f
-                nf=%d of=%d jf=%d
-                ''' % (nx,ny,nz,ox,oy,oz,dx,dy,dz,nf,of,jf) + 
-                '''
-                prefix="'''+fprefix+'''" oname="'''+oprefix+'''"''',
-                mpi=True,ppn=ppn,nodes=nodes,mpiopts=mpiopts,time=time,np=np)
-        
-def stack(stack,np,fprefix,files=None,nf=None,of=None,jf=None,shots=None,mpi=None,time=None,ppn=None,nodes=None,mpiopts=None):
+        Flow(stack,files,
+            '''
+            %s 
+            ''' % (_find(np,'sfbigmpistack',mpiopts)) +
+            '''
+            nx=%d ny=%d nz=%d
+            ox=%f oy=%f oz=%d
+            dx=%f dy=%f dz=%f
+            nf=%d of=%d jf=%d
+            ''' % (nx,ny,nz,ox,oy,oz,dx,dy,dz,nf,of,jf) + 
+            '''
+            prefix="'''+fprefix+'''" oname="'''+oprefix+'''"''',stdin=0,stdout=-1,**kw)
+    
+def stack(stack,np,fprefix=None,files=None,nf=None,of=None,jf=None,shots=None,mpiopts='--bynode',**kw):
     ''' stack files using sfmpistack
     
     stack - output file
@@ -220,11 +190,18 @@ def stack(stack,np,fprefix,files=None,nf=None,of=None,jf=None,shots=None,mpi=Non
     jf - delta of files
     see self-doc for more info
     '''
-    
-
    
-    if not '.rsf' in fprefix:
-        fprefix +='.rsf'
+    if not files:
+        assert fprefix != None
+
+        if not '.rsf' in fprefix:
+            fprefix +='.rsf'
+
+        if shots:
+            files = [fprefix % x for x in shots]
+        else:
+            files = [fprefix % x for x in range(of,of+nf*jf,jf)]
+
     oname = stack
     if not '.rsf' in oname:
         oname += '.rsf'
@@ -232,51 +209,25 @@ def stack(stack,np,fprefix,files=None,nf=None,of=None,jf=None,shots=None,mpi=Non
     if shots:
         Flow(stack+'-shots',None,
             '''
-            points type=i verb=y x=%s 
-            out=${TARGETS[0]}
-            ''' % (','.join([str(shot) for shot in shots])))
+            spike n1=%d nsp=%d mag=%s k1=%s | transp | sfdd type=int
+            ''' % (len(shots),len(shots),','.join([str(shot) for shot in shots]),
+                ','.join([str(i) for i in range(1,len(shots)+1)])
+                    ))
 
-    else:
-        filerange = range(of,of+nf*jf,jf)
-        
-        files = [ fprefix % f for f in filerange]
-    
-    if mpi:
-        if shots:
-            files.insert(0,stack+'-shots')
-            Flow(stack,files,
-                '''
-                sfmpistack shots=${SOURCES[0]}
-                ''' + 
-                ''' prefix="'''+fprefix + 
-                '''" oname="'''+oname+'''"''',
-                mpi=True,np=np,time=time,
-                nodes=nodes,ppn=ppn,mpiopts=mpiopts)
+    global cluster
+    if not cluster:
+        kw = {}
 
-        elif nf and of and jf:
-            Flow(stack,files,
-                '''
-                sfmpistack
-                nf=%d
-                of=%d
-                jf=%d
-                seq=y
-                ''' % (nf,of,jf) + 
-                ''' prefix="'''+fprefix + 
-                '''" oname="'''+oname+'''"''',
-                mpi=True,np=np,time=time,
-                nodes=nodes,ppn=ppn,mpiopts=mpiopts)
-        elif files:
-            Flow(stack,files,
-                '''
-                sfmpistack
-                ''' + ' '.join(files) + 
-                ''' prefix="'''+fprefix + 
-                '''" oname="'''+oname+'''"''',
-                mpi=True,np=np,time=time,
-                nodes=nodes,ppn=ppn,mpiopts=mpiopts)
+    if shots:
+        files.insert(0,stack+'-shots')
+        Flow(stack,files,
+            '''
+            %s shots=${SOURCES[0]}
+            ''' % (_find(np,'sfmpistack',mpiopts)) + 
+            ''' prefix="'''+fprefix + 
+            '''" oname="'''+oname+'''"''',stdin=0,stdout=-1,**kw)
 
-    else:
+    elif nf != None and of != None and jf != None:
         Flow(stack,files,
             '''
             %s
@@ -284,6 +235,14 @@ def stack(stack,np,fprefix,files=None,nf=None,of=None,jf=None,shots=None,mpi=Non
             of=%d
             jf=%d
             seq=y
-            ''' % (_find(np,'sfmpistack'),nf,of,jf) + 
-            ''' prefix="'''+fprefix + '''" oname="'''+oname+'''"''',stdin=0, stdout=-1)
-
+            ''' % (_find(np,'sfmpistack',mpiopts),nf,of,jf) + 
+            ''' prefix="'''+fprefix + 
+            '''" oname="'''+oname+'''"''',stdin=0,stdout=-1,**kw)
+    elif files:
+        Flow(stack,files,
+            '''
+            %s ${SOURCES[:]}
+            ''' % (_find(np,'sfmpistack',mpiopts)) + 
+            ''' oname="'''+oname+'''"''',stdin=0,stdout=-1,**kw)
+    else:
+        raise Exception('must specify either files, nf of and jf or shots')
