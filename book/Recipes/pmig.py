269f076445363cfb880824df59d6610b22f0727b
Sergey Fomel <sergey.fomel@gmail.com>
2011-04-25 03:52:41 +0000

    sfbar
    Former-commit-id: 15c2cbfd7776df83d3553a20d594cf15e51ba22f

--- book/Recipes/pmig.py
+++ book/Recipes/pmig.py
@@ -9,19 +9,25 @@ def pmig(data,hdip,ydip,frame1=1,frame2=1,frame3=1):
 
     if version.old_version():
         return # think how to do it better
-    def grey3(title,color='I'):
-        return '''byte bar=bar.rsf | transp plane=23 | grey3 title="%s"
-        frame1=%d frame2=%d frame3=%d color=%s flat=y point1=0.75 point2=0.75
-        ''' % (title,frame1,frame2,frame3,color)
 
-    Result(data,grey3('Data'))
-    Result(hdip,grey3('Offset Slope','j')   + ' scalebar=y bartype=v')
-    Result(ydip,grey3('Midpoint Slope','j') + ' scalebar=y bartype=v')
+    def Grey3(name,title,color='I',extra=''):
+        Flow('bar'+name,name,'bar')
+        Result(name,[name,'bar'+name],
+               '''
+               byte | transp plane=23 | 
+               grey3 title="%s" bar=${SOURCES[1]}
+               frame1=%d frame2=%d frame3=%d color=%s 
+               flat=y point1=0.75 point2=0.75 %s
+               ''' % (title,frame1,frame2,frame3,color,extra))
+
+    Grey3(data,'Data')
+    Grey3(hdip,'Offset Slope',  'j','scalebar=y bartype=v')
+    Grey3(ydip,'Midpoint Slope','j','scalebar=y bartype=v')
 
     nmo = data+'-nmo'
     vnmo = data+'-vnmo'
     Flow([nmo,vnmo],[data,hdip],'pnmo dip=${SOURCES[1]} vel=${TARGETS[1]}')
-    Result(nmo,grey3('Oriented NMO'))
+    Grey3(nmo,'Oriented NMO')
 
     stk = data+'-stk'
     Flow(stk,nmo,'stack')
@@ -36,9 +42,13 @@ def pmig(data,hdip,ydip,frame1=1,frame2=1,frame3=1):
          pmig hdip=${SOURCES[1]} xdip=${SOURCES[2]} |
          transp plane=23 memsize=500
          ''')
-    Result(mig,grey3('Oriented Prestack Time Migration'))
+    Grey3(mig,'Oriented Prestack Time Migration')
     Result(mig+'2',mig,
-           'stack | grey title="Oriented Prestack Time Migration" wheretitle=t wherexlabel=b')
+           '''
+           stack | 
+           grey title="Oriented Prestack Time Migration" 
+           wheretitle=t wherexlabel=b
+           ''')
     
     mzo = data+'-mzo'
     Flow(mzo,[data+'2',hdip+'2',ydip+'2'],
@@ -46,7 +56,7 @@ def pmig(data,hdip,ydip,frame1=1,frame2=1,frame3=1):
          pmig hdip=${SOURCES[1]} xdip=${SOURCES[2]} mzo=y |
          transp plane=23 memsize=500
          ''')
-    Result(mzo,grey3('Oriented Migration to Zero Offset'))
+    Grey3(mzo,'Oriented Migration to Zero Offset')
     Result(mzo+'2',mzo,
            'stack | grey title="Oriented DMO Stack" wheretitle=t wherexlabel=b')
     
