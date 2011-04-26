9b271ba8fc59540bbd06cf5470a3bdf132ae0e98
Sergey Fomel <sergey.fomel@gmail.com>
2011-04-26 23:50:09 +0000

    update
    Former-commit-id: ca9cd0366a59468852ad67568527938b5f34d9db

--- system/seismic/Mdiffoc.c
+++ system/seismic/Mdiffoc.c
@@ -19,16 +19,14 @@
 
 #include <rsf.h>
 
-#include "fint1.h"
+#include "velcon.h"
+#include "kurtosis.h"
 
 int main(int argc, char* argv[]) 
 {
-    int it, nt, ix, nx, i2, n2, n3, iw, nw, next;
-    float v0, v1, dt, t0, o2, d2, t, dx, k, w, dw;
-    float **data, *trace, *strace;
-    sf_complex *ctrace, shift;
-    fint1 str, istr;
-    kiss_fftr_cfg forw, invs;
+    int nt, nx, ntx, n2, n3, next;
+    float v0, v1, dt, dx, t0, kur;
+    float **data;
     sf_file inp, out;
 
     sf_init(argc,argv);
@@ -43,6 +41,8 @@ int main(int argc, char* argv[])
     if (!sf_histint(inp,"n2",&nx)) sf_error("No n2= in input");
     if (!sf_histfloat(inp,"d2",&dx)) sf_error("No d2= in input");
 
+    ntx = nt*nx;
+
     if (!sf_getfloat("v0",&v0)) v0=SF_EPS;
     /* initial velocity */
 
@@ -53,115 +53,24 @@ int main(int argc, char* argv[])
     if (!sf_getint("pad2",&n3)) n3=2*kiss_fft_next_fast_size((n2+1)/2);
     /* padding for FFT */
 
-    o2 = t0*t0;
-    d2 = t0+(nt-1)*dt;
-    d2 = (d2*d2 - o2)/(n2-1);
-
     if (!sf_getint("extend",&next)) next=4;
     /* trace extension */
-    str = fint1_init(next,nt,0);
-    istr = fint1_init(next,n2,0);
 
-    nw = n3/2+1;
-    dw = 2*SF_PI/(n3*d2);
-
-    forw = kiss_fftr_alloc(n3,0,NULL,NULL);
-    invs = kiss_fftr_alloc(n3,1,NULL,NULL);
-    if (NULL == forw || NULL == invs) 
-	sf_error("KISS FFT allocation error");
+    velcon_init(nt,nx,dt,dx,t0,n2,n3,next);
 
     data = sf_floatalloc2(nt,nx);
-    strace = sf_floatalloc(n3);
-    ctrace = sf_complexalloc(nw);
 
-    sf_floatread(data[0],nt*nx,inp);
+    sf_floatread(data[0],ntx,inp);
 
-    /* cosine FT in space */
-    sf_cosft_init(nx);
-    dx = SF_PI/(kiss_fft_next_fast_size(nx-1)*dx);
+    kur = kurtosis(ntx,data[0]);
+    sf_warning("kurtosis before: %g",kur);
 
-    for (it=0; it < nt; it++) {
-	sf_cosft_frw (data[0], it, nt);
-    }
+    velcon(data,v0,v1);
 
-    for (ix=0; ix < nx; ix++) {
-	/* loop over wavenumbers */
-	k = ix*dx;
-	k *= k*(v0*v0 - v1*v1)/16;
+    kur = kurtosis(ntx,data[0]);
+    sf_warning("kurtosis after: %g",kur);
 
-	trace = data[ix];
-
-	/* stretch t -> t^2 */
-
-	for (it=0; it < nt; it++) {
-	    trace[it] /= nt;
-	}
-
-	fint1_set(str,trace);
-
-	for (i2=0; i2 < n2; i2++) {
-	    t = o2+i2*d2;
-	    t = sqrtf(t);
-	    t = (t-t0)/dt;
-	    it = t;
-	    if (it >= 0 && it < nt) {
-		strace[i2] = fint1_apply(str,it,t-it,false);
-	    } else {
-		strace[i2] = 0.;
-	    }
-	}
-
-	for (i2=n2; i2 < n3; i2++) {
-	    strace[i2] = 0.;
-	}
-
-	/* FFT */
-
-	kiss_fftr(forw,strace, (kiss_fft_cpx *) ctrace);
-
-	/* velocity continuation */
-
-	ctrace[0]=sf_cmplx(0.,0.); /* dc */
-
-	for (iw=1; iw < nw; iw++) {
-	    w = iw*dw;
-	    w = k/w;
-	    shift = sf_cmplx(cosf(w)/n3,sinf(w)/n3);
-
-#ifdef SF_HAS_COMPLEX_H
-	    ctrace[iw] *= shift;
-#else
-	    ctrace[iw] = sf_cmul(ctrace[iw],shift);
-#endif
-	}
-
-	/* Inverse FFT */
-
-	kiss_fftri(invs,(const kiss_fft_cpx *) ctrace, strace);
-
-	/* inverse stretch t^2->t */
-
-	fint1_set(istr,strace);
-
-	for (it=0; it < nt; it++) {
-	    t = t0+it*dt;
-	    t = t*t;
-	    t = (t-o2)/d2;
-	    i2 = t;
-	    if (i2 >= 0 && i2 < n2) {
-		trace[it] = fint1_apply(istr,i2,t-i2,false);
-	    } else {
-		trace[it] = 0.;
-	    }
-	}	
-    } /* ix */
-
-    /* inverse cosine FT in space */
-    for (it=0; it < nt; it++) {
-	sf_cosft_inv (data[0], it, nt);
-    }
-
-    sf_floatwrite(data[0],nt*nx,out);
+    sf_floatwrite(data[0],ntx,out);
 
     exit(0);
 }
