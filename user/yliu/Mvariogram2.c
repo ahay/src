53826dbea678cb36e2829fccac99f5cafb135b45
Yang Liu <yangliu1979@gmail.com>
2011-05-13 05:07:39 +0000

    update
    Former-commit-id: bc4953f4a946d02638681d82de733fab620944d4

--- user/yliu/Mvariogram2.c
+++ user/yliu/Mvariogram2.c
@@ -20,7 +20,7 @@
 
 int main(int argc, char* argv[]) 
 {
-    int i, j, n, i1, n1, i2, n2, i3, n3, dh1, nh1, oh1, dh2, nh2, oh2;
+    int i, j, n, i1, n1, i2, n2, i3, n3, dh1, nh1, dh2, nh2;
     float o1, d1, o2, d2, *fbuf, *vari;
     bool semi, verb;
     sf_file in=NULL, out=NULL;
@@ -42,68 +42,63 @@ int main(int argc, char* argv[])
     if (!sf_getbool("verb",&verb)) verb=false;
     /* verbosity */
     if (!sf_getint("dh1",&dh1)) dh1=1;
-    /* interval (number) of variogram lag in first axis */
-    if (!sf_getint("oh1",&oh1)) oh1=0;
-    /* origin (number) of variogram lag in first axis */
-    if (oh1 < 0 || oh1 >= n1/dh1) oh1=0;
-    if (!sf_getint("nh1",&nh1)) nh1=n1/dh1-oh1;
+    /* interval (jump) of variogram lag in first axis */
+    if (!sf_getint("nh1",&nh1)) nh1=n1/dh1;
     /* number of variogram lag in first axis */
 
     if (!sf_getint("dh2",&dh2)) dh2=1;
-    /* interval (number) of variogram lag in second axis */
-    if (!sf_getint("oh2",&oh2)) oh2=0;
-    /* origin (number) of variogram lag in second axis */
-    if (oh2 < 0 || oh2 >= n2/dh2) oh2=0;
-    if (!sf_getint("nh2",&nh2)) nh2=n2/dh2-oh2;
+    /* interval (jump) of variogram lag in second axis */
+    if (!sf_getint("nh2",&nh2)) nh2=n2/dh2;
     /* number of variogram lag in second axis */
 
     if (!sf_getbool("semi",&semi)) semi=true;
     /* if y, output semivariogram */
 
     sf_settype(out,SF_FLOAT);
-    sf_putint(out,"n1",nh1);
-    sf_putfloat(out,"o1",oh1*dh1*d1);
+    sf_putint(out,"n1",2*nh1-1);
+    sf_putfloat(out,"o1",(-nh1+1)*dh1*d1);
     sf_putfloat(out,"d1",dh1*d1);
-    sf_putstring(out,"label1","X Lag");
+    sf_putstring(out,"label1","Inline Lag");
 
-    sf_putint(out,"n2",nh2);
-    sf_putfloat(out,"o2",oh2*dh2*d2);
+    sf_putint(out,"n2",2*nh2-1);
+    sf_putfloat(out,"o2",(-nh2+1)*dh2*d2);
     sf_putfloat(out,"d2",dh2*d2);
-    sf_putstring(out,"label2","Y Lag");
+    sf_putstring(out,"label2","Crossline Lag");
 
     fbuf = sf_floatalloc(n1*n2);
-    vari = sf_floatalloc(nh1*nh2);
+    vari = sf_floatalloc((2*nh1-1)*(2*nh2-1));
 
     for (i3 = 0; i3 < n3; i3++) {
 	sf_floatread(fbuf,n1*n2,in);
-	for (i1=0; i1 < nh1*nh2; i1++) {
+	for (i1=0; i1 < (2*nh1-1)*(2*nh2-1); i1++) {
 	    vari[i1]=0.;
 	}	
 
-      	for (i2=0; i2 < nh2; i2++) {
+      	for (i2=(-nh2+1); i2 < nh2; i2++) {
 	    if (verb)
-		sf_warning("Slice %d of %d;",i2+1,nh2);
-	    for (i1=0; i1 < nh1; i1++) {
+		sf_warning("Slice %d of %d;",i2+nh2,2*nh2);
+	    for (i1=(-nh1+1); i1 < nh1; i1++) {
 		n = 0;
 		for (j=0; j < n2; j++) {
 		    for (i=0; i < n1; i++) {
 
-			if (i+(i1+oh1)*dh1 < n1 && j+(i2+oh2)*dh2 < n2) {
-			    vari[i2*nh1+i1] += (fbuf[(j+(i2+oh2)*dh2)
-*n1+(i+(i1+oh1)*dh1)] - fbuf[j*n1+i])
-				*(fbuf[(j+(i2+oh2)*dh2)*n1+(i+(i1+oh1)*dh1)] - fbuf[j*n1+i]);
+			if (i+i1*dh1 < n1 && i+i1*dh1 >=0 && 
+			    j+i2*dh2 < n2 && j+i2*dh2 >=0) {
+			    vari[(i2+nh2-1)*(2*nh1-1)+i1+nh1-1] += 
+				(fbuf[(j+i2*dh2)*n1+(i+i1*dh1)] - fbuf[j*n1+i])
+				*(fbuf[(j+i2*dh2)*n1+(i+i1*dh1)] - fbuf[j*n1+i]);
 			    n++;
 			}
 		    }
 		}
 		if (semi) {
-		    vari[i2*nh1+i1] /= (n*2.+FLT_EPSILON);
+		    vari[(i2+nh2-1)*(2*nh1-1)+i1+nh1-1] /= (n*2.+FLT_EPSILON);
 		} else {
-		    vari[i2*nh1+i1] /= (n*1.+FLT_EPSILON);
+		    vari[(i2+nh2-1)*(2*nh1-1)+i1+nh1-1] /= (n*1.+FLT_EPSILON);
 		}
 	    }
 	}
-	sf_floatwrite(vari,nh1*nh2,out);
+	sf_floatwrite(vari,(2*nh1-1)*(2*nh2-1),out);
     }
     
     exit(0);
