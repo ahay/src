4de76b0952c56390223831b0e3d7364426519d7b
Sergey Fomel <sergey.fomel@gmail.com>
2011-07-12 02:10:14 +0000

    error message bug in sfsegyheader
    Former-commit-id: 60704cc2a69a01f86e2843e0b93bb87e9d74c921

--- user/xuxin/fft2.c
+++ user/xuxin/fft2.c
@@ -16,7 +16,9 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+
 #include <rsf.h>
+/*^*/
 
 static bool cmplx;
 static int n1, n2, nk;
