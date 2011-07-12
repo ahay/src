e01cfc59d783e8db6001d9d5547551a409b6b05b
Sergey Fomel <sergey.fomel@gmail.com>
2011-07-12 22:52:29 +0000

    update
    Former-commit-id: cc751f44401e267704f396d93455aebbe1d9d191

--- system/generic/Mboxsmooth.c
+++ system/generic/Mboxsmooth.c
@@ -1,9 +1,4 @@
-/* Multi-dimensional smoothing with boxes.
-
-Takes: rect1=1 rect2=1 ...
-
-rectN defines the size of the smoothing stencil in N-th dimension.
-*/
+/* Multi-dimensional smoothing with boxes. */
 /*
   Copyright (C) 2004 University of Texas at Austin
 
@@ -46,6 +41,7 @@ int main (int argc, char* argv[])
     for (i=0; i < dim; i++) {
 	snprintf(key,6,"rect%d",i+1);
 	if (!sf_getint(key,rect+i)) rect[i]=1;
+        /*( rect#=(1,1,...) smoothing radius on #-th axis )*/ 
 	if (rect[i] > 1) {
 	    dim1 = i;
 	    if (rect[i]+n[i] > rmax) rmax = rect[i]+n[i];
