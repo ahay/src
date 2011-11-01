a38593c8f391b010720b8171a342b98a3a8b8909
Paul Sava <psava@mines.edu>
2011-11-01 03:14:39 +0000

    Former-commit-id: 3a2a3d8f472712057fe2ca9ae5f831b8649fcf04

---
+++ api/c/omptools.c
@@ -0,0 +1,34 @@
+#include "_defs.h"
+#include "getpar.h"
+#include "omptools.h"
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+/*------------------------------------------------------------*/
+int omp_init()
+/*< init OMP parameters >*/
+{
+    int ompnth=1;
+    int ompath=1;
+    
+#ifdef _OPENMP
+    /* OMP allowed threads */
+    if(! sf_getint("ompnth",&ompnth)) ompnth=0;
+
+#pragma omp parallel
+    {	
+	if(omp_get_thread_num()==0) {
+	    ompath=omp_get_num_threads();
+	    if(ompnth!=0)
+		ompnth=SF_MIN(ompnth,ompath);
+	    else
+		ompnth=ompath;
+	    sf_warning("using %d of %d threads",ompnth,ompath);
+	}
+    }
+#endif 
+
+    return ompnth;
+}
