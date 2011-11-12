f23c9c061a98cdf47496327b64c61b2882e9b5fc
Zhonghuan Chen <isaacthu@gmail.com>
2011-11-12 15:41:17 +0000

    Former-commit-id: 50ffb4e9f6a195296de49399dea3c048ff6ba590

---
+++ user/chen/Mfastpwd.c
@@ -0,0 +1,85 @@
+/* 2-D dip estimation using analytical equation. */
+/*
+  Copyright (C) 2011 University of Texas at Austin
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#include <rsf.h>
+
+#include "fastpwd.h"
+#include "divn.h"
+
+int main(int argc, char* argv[])
+{
+    bool verb;
+    int n1, n2, n12, id, i3, n3, rect[2], niter, n[2];
+    float **wav, **dip, **num, **den, norm;
+    sf_file inp, out;
+
+    sf_init(argc,argv);
+    inp = sf_input("in");
+    out = sf_output("out");
+
+    if (SF_FLOAT != sf_gettype(inp)) sf_error("Need float input");
+    
+    if (!sf_histint(inp,"n1",&n1)) sf_error("No n1= in input");
+    if (!sf_histint(inp,"n2",&n2)) sf_error("No n2= in input");
+    n3 = sf_leftsize(inp,2);
+    n12 = n1*n2;
+    n[0] = n1;
+    n[1] = n2;
+
+    if (!sf_getint("rect1",&rect[0])) rect[0]=1;
+    if (!sf_getint("rect2",&rect[1])) rect[1]=1;
+    /* smoothing radius */
+
+    if (!sf_getint("niter",&niter)) niter=100;
+    /* number of iterations */
+
+    if (!sf_getbool("verb",&verb)) verb=true;
+    /* verbosity */
+
+    wav = sf_floatalloc2(n1,n2);
+    dip = sf_floatalloc2(n1,n2);
+    num = sf_floatalloc2(n1,n2);
+    den = sf_floatalloc2(n1,n2);
+
+    fastpwd_init(n1,n2);
+    divn_init(2, n12, n, rect, niter, verb);
+
+    for (i3=0; i3 < n3; i3++) {
+	sf_floatread(wav[0],n12,inp);
+
+	fastpwd(wav,num,den);
+
+	/* smooth division */
+	norm = 0.;
+	for (id=0; id < n12; id++) {
+	    norm += den[0][id]*den[0][id];
+	}
+	norm = sqrtf(n12/norm);
+
+	for (id=0; id < n12; id++) {
+	    num[0][id] *= norm;
+	    den[0][id] *= norm;
+	}
+	divn (num[0], den[0], dip[0]);
+
+	sf_floatwrite(dip[0],n12,out);
+    }
+
+    exit(0);
+}
+    
