c404339d3b8b8a384c3bdf5761a2833f615f64e1
Yihua Cai <caiyihua@gmail.com>
2011-12-08 17:38:59 +0000

    Former-commit-id: 213ee4d8356ead04ac6b87fbf8cda9748dae5134

--- user/tsai/Mnlle.c
+++ user/tsai/Mnlle.c
@@ -29,8 +29,8 @@ m1f, m2f: peak frequencies to estimate; r1s, r2s: ricker spectrum
 int main(int argc, char* argv[])
 {
     int n2, i2, na, ia, niter, iter;
-    float f0, df, f, f2, m, m0, m2, m3, *data, *r, *rp, m1f0, m1f, m1f2, m1f3, m2f0, m2f, m2f2, m2f3, dm1f, dm2f;
-    float rd, r2, rpd, rp2, rpr, ap, num, den, a, e, dm, eps, di, d2, a1, a2, *r1s, r1s2, *r2s, r2s2;
+    float f0, df, f, f2, *data, m1f0, m1f, m1f2, m1f3, m2f0, m2f, m2f2, m2f3, dm1f, dm2f;
+    float eps, di, d2, a1, a2, *r1s, r1s2, *r2s, r2s2;
     float r1sd, r2sd, r1spd, r2spd, r1sp2, r1spr1s, r2sp2, r2spr2s, e1, e2, *r1sp, *r2sp, r1sr2s, r1sr2s2;
     float denpm1, denpm2, r1spr2s, r1sr2sp, r1s2p, r2s2p, pa1m1, pa2m2, pa1m2, pa2m1, allden;
     float a1m1, a2m2, a1m2, a2m1, numm1, numm2, denm1, denm2;
@@ -49,11 +49,9 @@ int main(int argc, char* argv[])
     if (!sf_histfloat(in,"d1",&df)) sf_error("No d1= in input");
     if (!sf_histfloat(in,"o1",&f0)) sf_error("No o1= in input");
 
-/*the following line removed later*/
-    if (!sf_getfloat("m",&m0)) m0 = f0+0.25*(na-1)*df;
 /*the following two lines added*/
-    if (!sf_getfloat("m",&m1f0)) m1f0 = f0+0.25*(na-1)*df;
-    if (!sf_getfloat("m",&m2f0)) m2f0 = f0+0.25*(na-1)*df;
+    if (!sf_getfloat("m",&m1f0)) m1f0 = f0+0.15*(na-1)*df;
+    if (!sf_getfloat("m",&m2f0)) m2f0 = f0+0.35*(na-1)*df;
     /* initial frequency */
     if (!sf_getint("niter",&niter)) niter = 100;
     /* number of iterations */
@@ -67,16 +65,11 @@ int main(int argc, char* argv[])
     sf_fileflush(ma,in);
 
     data = sf_floatalloc(na);
-/*the following two lines removed later*/
-    r = sf_floatalloc(na);
-    rp = sf_floatalloc(na);
 /*added*/
     r1s = sf_floatalloc(na);
     r1sp = sf_floatalloc(na);
     r2s = sf_floatalloc(na);
     r2sp = sf_floatalloc(na);
-
-    m0 = m1f0 = m2f0;
 
     eps = 10.*FLT_EPSILON;
     eps *= eps;
@@ -91,52 +84,28 @@ int main(int argc, char* argv[])
 	    if (di < 0.) data[ia] = -di;
 	    d2 += di*di;
 	}
-
-/*removed later*/
-	m = m0;
-
 /*added*/
 	m1f = m1f0;
 	m2f = m2f0;
-
 	for (iter = 0; iter < niter; iter++) {
-/*removed later*/
-	    m2 = m*m;
-	    m3 = m2*m;
 /*added*/	    
 	    m1f2 = m1f*m1f;
 	    m1f3 = m1f2*m1f;
-
 	    m2f2 = m2f*m2f;
 	    m2f3 = m2f2*m2f;
-
-/*removed later*/	    
-	    rd = r2 = rpd = rp2 = rpr = 0.;
 /*added*/
 	    r1sd = r1s2 = r2sd = r2s2 = r1spd = r2spd = r1sp2 = r1spr1s = r2sp2 = r2spr2s = 0.;
-
 	    for (ia = 0; ia < na; ia++) {
 		f = f0 + ia*df;
 		f2 = f*f;
-/*removed later*/
-		e = exp(-f2/m2);
 /*added*/
 		e1 = exp(-f2/m1f2);
 		e2 = exp(-f2/m2f2);
-		/*removed later*/
-		r[ia] = e*f2/m2;
-		rp[ia] = 2.*e*f2*(f2-m2)/(m3*m2);
 		/*added*/
 		r1s[ia] = e1*f2/m1f2;
 		r2s[ia] = e2*f2/m2f2;
 		r1sp[ia] = 2.*e1*f2*(f2-m1f2)/(m1f3*m1f2);
 		r2sp[ia] = 2.*e2*f2*(f2-m2f2)/(m2f3*m2f2);
-		/*removed later*/
-		rd += r[ia]*data[ia];
-		r2 += r[ia]*r[ia];
-		rpd += rp[ia]*data[ia];
-		rp2 += rp[ia]*rp[ia];
-		rpr += rp[ia]*r[ia];
 		/*added*/
 		r1sd += r1s[ia]*data[ia];
 		r1s2 += r1s[ia]*r1s[ia];
@@ -156,8 +125,6 @@ int main(int argc, char* argv[])
 		r2spr2s += r2sp[ia]*r2s[ia];
 	    }
             r1sr2s2 = r1sr2s*r1sr2s;  
-/*removed later*/
-	    a = rd/(r2 + eps);
 /*added*/
 	    a1 = (r1sd*(r2s2+eps)-r1sr2s*r2sd)/((r1s2+eps)*(r2s2+eps)-r1sr2s2);
 	    a2 = (r2sd*(r1s2+eps)-r1sr2s*r1sd)/((r1s2+eps)*(r2s2+eps)-r1sr2s2);
@@ -177,13 +144,8 @@ int main(int argc, char* argv[])
 	    a2m2 = pa2m2/allden;
 	    a1m2 = pa1m2/allden;
 	    a2m1 = pa2m1/allden;
-/*removed later*/
-	    ap = (rpd-2.*rpr*a)/(r2 + eps);
-	    num =  a*(rpd-rpr*a)+ap*(rd-r2*a);     
-	    den = a*a*rp2 + 2.*a*ap*rpr + ap*ap*(r2+eps) + eps;
-	    dm = num/den;
 /*added*/
-	    numm1 = pa1m1*r1sd+a1*r1spd+pa1m2*r1sd-a*pa1m1*r1s2-a1*a1*r1spr1s-a1*pa1m2*r1s2;
+	    numm1 = pa1m1*r1sd+a1*r1spd+pa1m2*r1sd-a1*pa1m1*r1s2-a1*a1*r1spr1s-a1*pa1m2*r1s2;
 	    denm1 = pa1m1*pa1m1*r1s2+a1*a1*r1sp2+pa1m2*pa1m2*r1s2+2.*pa1m1*a1*r1spr1s+2.*pa1m1*pa1m2*r1s2+2.*a1*pa1m2*r1spr1s;
 	    dm1f = numm1/denm1;
 
@@ -191,23 +153,13 @@ int main(int argc, char* argv[])
 	    denm2 = pa2m1*pa2m1*r2s2+pa2m2*pa2m2*r2s2+a2*a2*r2sp2+2.*pa2m1*pa2m2*r2s2+2.*pa2m1*a2*r2spr2s+2.*pa2m2*a2*r2spr2s;
 
 	    dm2f = numm2/denm2;
-/*removed later*/
-	    r2 = d2 - 2.*rd*a + r2*a*a;
-	    rp2 = dm*dm;
-
 /*added*/
-	    if (verb && 5000 > n2) sf_warning("iter=%d r2=%g rp2=%g m=%g a=%g",
-					      iter,r2,rp2,m,a);
-	    m += dm;
+	    if (verb && 5000 > n2) sf_warning("iter=%d r1s2=%g r2s2=%g r1sp2=%g r2sp2=%g m1f=%g m2f=%g a1=%g a2=%g",
+					      iter,r1s2,r2s2,r1sp2,r2sp2,m1f,m2f,a1,a2);
 	    m1f += dm1f;
             m2f += dm2f;
-	
-	    if (r2 < eps || rp2 < eps) break;
-	}
-        
-	m = fabsf(m);
-	m2 = m*m;
-
+	    if (r1s2 < eps || r2s2 < eps || r1sp2 < eps || r2sp2 < eps) break;
+	}     
 	m1f = fabsf(dm1f);
 	m1f2 = m1f*m1f;
 	m2f = fabsf(dm2f);
@@ -218,12 +170,11 @@ int main(int argc, char* argv[])
 
 	sf_floatwrite(&m2f2,1,ma);
 	sf_floatwrite(&a2,1,ma);
-
         
 	for (ia = 0; ia < na; ia++) {
 	    f = f0 + ia*df;
 	    f2 = f*f;
-	    /* data[ia] = a*exp(-f2/m2)*f2/m2;*/
+
 	    data[ia] = a1*exp(-f2/m1f2)*f2/m1f2+a2*exp(-f2/m2f2)*f2/m2f2;
 	}
         
@@ -238,4 +189,4 @@ int main(int argc, char* argv[])
     exit (0);
 }
 
-/* 	tsai $	 */
+/*tsai $*/
