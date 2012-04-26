c7a1a0efa7939c654f720e4f126c46a3fbcad677
Zhonghuan Chen <isaacthu@gmail.com>
2012-04-26 01:54:13 +0000

    Former-commit-id: 294889314f5a2901f0e2f76e9c48f0e0c4dd2779

---
+++ user/chen/Modip.c
@@ -0,0 +1,57 @@
+/* omnidirectional dip estimation  */
+
+#include <rsf.h>
+#include "odip.h"
+
+int main(int argc, char*argv[])
+{
+	sf_file in, out;
+	int nf, n1, n2, n3, rect[2], niter, liter;
+	int i3;
+	bool verb, opwd;
+	float **wav, **dip;
+
+	sf_init(argc, argv);
+
+	in  = sf_input("in");
+	out = sf_output("out");
+
+	if (SF_FLOAT != sf_gettype(in)) sf_error("Need float type");
+
+    if (!sf_histint(in, "n1", &n1)) sf_error("No n1= in input");
+    if (!sf_histint(in, "n2", &n2)) sf_error("No n2= in input");
+    n3 = sf_leftsize(in, 2);
+
+    if (!sf_getint("nf",&nf)) nf=1;
+    /* PWD filter order */
+    if (!sf_getint("rect1",&rect[0])) rect[0]=1;
+    /* dip smoothness on 1st axis */
+    if (!sf_getint("rect2",&rect[1])) rect[1]=1;
+    /* dip smoothness on 2nd axis */
+	
+    if (!sf_getint("niter",&niter)) niter=5;
+    /* number of iterations */
+    if (!sf_getint("liter",&liter)) liter=20;
+    /* number of linear iterations */
+
+    if (!sf_getbool("opwd", &opwd)) verb = true;
+    /* y: opwd;  n: lpwd */
+    if (!sf_getbool("verb",&verb)) verb = false;
+    /* verbosity flag */
+
+    wav = sf_floatalloc2(n1,n2);
+    dip = sf_floatalloc2(n1,n2);
+
+    /* initialize dip estimation */
+    odip_init(nf, n1, n2, rect, liter, verb);
+	for(i3=0; i3<n3; i3++)
+	{
+		sf_floatread(wav[0], n1*n2, in);
+		odip(wav, dip, niter, opwd);
+		sf_floatwrite(dip[0], n1*n2, out);
+	}
+
+	odip_close();
+	return 0;
+}
+
---
+++ user/chen/odip.c
@@ -0,0 +1,83 @@
+/* omnidirectional dip estimation by PWD */
+
+#include <rsf.h>
+#include "pwd.h"
+
+static int n1, n2, nf;
+static float **u1, **u2, **u3;
+
+void odip_init(int mf, int m1, int m2, int *rect, int niter, bool verb)
+/*< initialize >*/
+{
+	int n, nn[4];
+	nf = mf;
+	n1 = m1;
+	n2 = m2;
+
+	n = n1*n2;
+	nn[0] = n1;
+	nn[1] = n2;
+
+	u1 = sf_floatalloc2(n1,n2);
+	u2 = sf_floatalloc2(n1,n2);
+	u3 = sf_floatalloc2(n1,n2);
+
+	pwd_init(nf, n1, n2);
+	sf_divn_init (2, n, nn, rect, niter, verb);
+}
+
+void odip_close()
+/*< release memory >*/
+{
+	free(u1[0]);
+	free(u1);
+	free(u2[0]);
+	free(u2);
+	free(u3[0]);
+	free(u3);
+	pwd_release();
+	sf_divn_close();
+}
+
+void odip(float **in, float **dip, int nit, bool od)
+/*< omnidirectional dip estimation >*/
+{
+	int it, i1;
+	double norm;
+
+	for(i1=0; i1<n1*n2; i1++) 
+		dip[0][i1] = 0.0;
+
+	for (it=0; it<nit; it++)
+	{	
+		if(od)
+		{
+			opwd(in, u1, dip);
+			opwd_der(in, u2, dip);
+		}else{
+			lpwd(in, u1, dip);
+			lpwd_der(in, u2, dip);
+		}
+
+		norm = 0.0;
+		for(i1=0; i1<n1*n2; i1++) 
+			norm += u2[0][i1]*u2[0][i1];
+		if(norm == 0.0) return;
+		norm = sqrtf(norm/n1/n2);
+
+		for(i1=0; i1<n1*n2; i1++) 
+		{
+			u1[0][i1] /= norm;
+			u2[0][i1] /= norm;
+		}
+
+		sf_divn(u1[0], u2[0], u3[0]);
+		for(i1=0; i1<n1*n2; i1++)
+		{
+			dip[0][i1] -= u3[0][i1];
+		}
+	}
+}
+
+
+
