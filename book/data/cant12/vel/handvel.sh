104dc1bf02477b731b41957df9062e238a454526
Salah Al-Hadab <salah.alhadab@gmail.com>
2012-08-02 22:38:44 +0000

    cant12
    Former-commit-id: bd3d9694e3553255f70cb965e8c82e1960668b94

--- book/data/cant12/vel/handvel.sh
+++ book/data/cant12/vel/handvel.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-awk 'Begin {flag=0;cmp="";done=0} 
+awk 'BEGIN {flag=0;cmp="";done=0} 
 {
 if ($1 ~ /HANDVEL/ ) {
     if (done==0) {cmp=$2; flag=1 ; done=1 }
     else {print "-1 0 " cmp; cmp=cmp+100; flag=1} 
-}else { 
+} else { 
   for( i=1; i<NF; i++) {
      if (flag==1) {print "0.0 " $(i+1)" " cmp; flag=0}
      print $i " " $++i" " cmp
   }
 }
-}' $1
+}
+END {print "-1 0 " cmp}' $1
 
