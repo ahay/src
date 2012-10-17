039a5bd123065ff1263c57f8a31ad51c44382d1b
Vladimir Bashkardin <bash@mail.utexas.edu>
2012-10-17 15:22:18 +0000

    linear interpolation for velocity and gradients in sfrays2 with order=0
    Former-commit-id: a8e3c529dd7fb47bbadbfa4f85c857456d9f8d35

--- system/seismic/grid2.c
+++ system/seismic/grid2.c
@@ -33,6 +33,7 @@ typedef struct Grid2* grid2;
 
 struct Grid2 {
     sf_eno2 pnt;
+    float ***vel;
     int n1, n2;
     float o1, d1, o2, d2;
 };
@@ -44,15 +45,30 @@ grid2 grid2_init (int n1, float o1, float d1 /* first axis */,
 		  int order                  /* interpolation order */)
 /*< Initialize grid object >*/
 {
+    int i, j;
+    float v, vgrad[2];
     grid2 grd;
-    
+
     grd = (grid2) sf_alloc(1,sizeof(*grd));
 
     grd->n1 = n1; grd->o1 = o1; grd->d1 = d1; 
     grd->n2 = n2; grd->o2 = o2; grd->d2 = d2;
-    
-    grd->pnt = sf_eno2_init (order, n1, n2);
+
+    grd->pnt = sf_eno2_init (order != 0 ? order : 3, n1, n2);
     sf_eno2_set1 (grd->pnt, slow2);
+    grd->vel = NULL;
+    if (order <= 0) {
+        grd->vel = sf_floatalloc3 (3, n1, n2);
+        for (j = 0; j < n2; j++) {
+            for (i = 0; i < n1; i++) {
+                sf_eno2_apply (grd->pnt, i, j, 0., 0., &v, vgrad, BOTH);
+                grd->vel[j][i][0] = slow2[j*n1 + i];
+                grd->vel[j][i][1] = 0.5*vgrad[0]/d1;
+                grd->vel[j][i][2] = 0.5*vgrad[1]/d2;
+            }
+        }
+        sf_eno2_close (grd->pnt);
+    }
 
     return grd;
 }
@@ -68,8 +84,26 @@ float grid2_vel(void* par /* grid */,
     grd = (grid2) par;
     x = (xy[0]-grd->o1)/grd->d1; i = x; x -= i;
     y = (xy[1]-grd->o2)/grd->d2; j = y; y -= j;
-    
-    sf_eno2_apply(grd->pnt, i, j, x, y, &f, f1, FUNC);
+
+    if (grd->vel) {
+        if (i < 0) {
+            i = 0; x = 0.0;
+        }
+        if (i >= (grd->n1 - 1)) {
+            i = grd->n1 - 2; x = 1.0;
+        }
+        if (j < 0) {
+            j = 0; y = 0.0;
+        }
+        if (j >= (grd->n2 - 1)) {
+            j = grd->n2 - 2; y = 1.0;
+        }
+        f = grd->vel[j][i][0]*(1.0 - y)*(1.0 - x) +
+            grd->vel[j][i + 1][0]*(1.0 - y)*x +
+            grd->vel[j + 1][i][0]*y*(1.0 - x) +
+            grd->vel[j + 1][i + 1][0]*y*x;
+    } else
+        sf_eno2_apply(grd->pnt, i, j, x, y, &f, f1, FUNC);
     return f;
 }
 
@@ -85,11 +119,33 @@ void grid2_vgrad(void* par   /* grid */,
     grd = (grid2) par;
     x = (xy[0]-grd->o1)/grd->d1; i = floor(x); x -= i;
     y = (xy[1]-grd->o2)/grd->d2; j = floor(y); y -= j;
-    
-    sf_eno2_apply(grd->pnt, i, j, x, y, &f, f1, DER);
-    
-    grad[0] = 0.5*f1[0]/grd->d1;
-    grad[1] = 0.5*f1[1]/grd->d2;
+
+    if (grd->vel) {
+        if (i < 0) {
+            i = 0; x = 0.0;
+        }
+        if (i >= (grd->n1 - 1)) {
+            i = grd->n1 - 2; x = 1.0;
+        }
+        if (j < 0) {
+            j = 0; y = 0.0;
+        }
+        if (j >= (grd->n2 - 1)) {
+            j = grd->n2 - 2; y = 1.0;
+        }
+        grad[0] = grd->vel[j][i][1]*(1.0 - y)*(1.0 - x) +
+                  grd->vel[j][i + 1][1]*(1.0 - y)*x +
+                  grd->vel[j + 1][i][1]*y*(1.0 - x) +
+                  grd->vel[j + 1][i + 1][1]*y*x;
+        grad[1] = grd->vel[j][i][2]*(1.0 - y)*(1.0 - x) +
+                  grd->vel[j][i + 1][2]*(1.0 - y)*x +
+                  grd->vel[j + 1][i][2]*y*(1.0 - x) +
+                  grd->vel[j + 1][i + 1][2]*y*x;
+    } else {
+        sf_eno2_apply(grd->pnt, i, j, x, y, &f, f1, DER);
+        grad[0] = 0.5*f1[0]/grd->d1;
+        grad[1] = 0.5*f1[1]/grd->d2;
+    }
 }
 
 int grid2_term (void* par /* grid */, 
@@ -107,7 +163,10 @@ int grid2_term (void* par /* grid */,
 void grid2_close(grid2 grd)
 /*< Free internal storage >*/
 {
-    sf_eno2_close (grd->pnt);
+    if (grd->vel) {
+        free (grd->vel[0][0]); free (grd->vel[0]); free (grd->vel);
+    } else
+        sf_eno2_close (grd->pnt);
     free (grd);
 }
 
