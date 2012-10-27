1f38f89522fa9fd9043ffed7ff264ef374f5093a
Yang Liu <yangliu1979@gmail.com>
2012-10-27 03:08:21 +0000

    update
    Former-commit-id: 0aaafe762558bebe067529c2c66d4180f5a03faa

--- user/yliu/Mfagrad.c
+++ user/yliu/Mfagrad.c
@@ -29,6 +29,7 @@ int main(int argc, char* argv[])
     float d1, dw, w0, etotal, f1=0., f2=0., e1=0., e2=0., ecum, *fdg, *e;
 /*    float cum1, cum2, cum3, cum4, numer, denom; */
     float *sol=NULL, *dat=NULL, **func=NULL;
+    char *type;
     sf_complex *inp=NULL;
     sf_file in, out;
    
@@ -57,6 +58,9 @@ int main(int argc, char* argv[])
 
     if (lperc >= hperc || lperc < 0. || hperc > 100.) 
 	sf_error("Need 0 <= lperc <= hperc <= 100.");
+
+    if (NULL == (type=sf_getstring("type"))) type="attenuation";
+    /* [low,full,ratio,attenuation] attribute type, the default is attenuation  */
 
     
     if (SF_COMPLEX == sf_gettype(in)) {
@@ -87,96 +91,145 @@ int main(int argc, char* argv[])
 	    }
 	}
 	for (i1=0; i1 < n1; i1++) {
-	    if (grad) {
-		etotal = 0.;
-		for (iw=0; iw < nw; iw++) {
-		    etotal += e[iw*n1+i1];
-		}
-		ecum = 0.;
-		sign = false;
-		for (iw=0; iw < nw; iw++) {
-		    ecum += e[iw*n1+i1];
-		    if (!sign && ecum >= lperc*etotal/100.) {
-			f1 = w0+iw*dw;
-			e1 = e[iw*n1+i1];
-			sign = true;
-		    }
-		    if (ecum >= hperc*etotal/100.) {
-			f2 = w0+iw*dw;
-			e2 = e[iw*n1+i1];
-			break;
-		    }
-		}
-		if (f1 == f2) {
-		    fdg[i1] = 0.;
-		} else {
-		    fdg[i1] = (e2-e1)/(f2-f1);
-		}
-	    } else {
-/*		numer = 0.;
-		denom = 0.;
-		
-		for (iw=0; iw < nw; iw++) {
-		    numer += e[iw*n1+i1]*(w0+iw*dw);
-		    denom += e[iw*n1+i1];
-		}
-
-		index = (int)((numer/denom-w0)/dw);
-		etotal = 0.;
-		for (iw=0; iw < nw; iw++) {
-		    etotal += e[iw*n1+i1];
-		}
-		ecum = 0.;
-		for (iw=0; iw < nw; iw++) {
-		    ecum += e[iw*n1+i1];
-		    if (ecum >= hperc*etotal/100.) {
-			wend = iw;
-			break;
-		    }
-		}
-
-		cum1 = 0.;
-		cum2 = 0.;
-		cum3 = 0.;
-		cum4 = 0.;
-		for (iw=index; iw < wend; iw++) {
-		    cum1 += w0+iw*dw;
-		    cum2 += logf(e[iw*n1+i1]);
-		    cum3 += (w0+iw*dw)*logf(e[iw*n1+i1]);
-		    cum4 += (w0+iw*dw)*(w0+iw*dw);
-		}
-		fdg[i1] = ((nw-index)*cum3-cum1*cum2)/
-		    ((nw-index)*cum4-cum1*cum1);
+	    switch (type[0]) {
+		case 'a':
+		    if (grad) {
+			etotal = 0.;
+			for (iw=0; iw < nw; iw++) {
+			    etotal += e[iw*n1+i1];
+			}
+			ecum = 0.;
+			sign = false;
+			for (iw=0; iw < nw; iw++) {
+			    ecum += e[iw*n1+i1];
+			    if (!sign && ecum >= lperc*etotal/100.) {
+				f1 = w0+iw*dw;
+				e1 = e[iw*n1+i1];
+				sign = true;
+			    }
+			    if (ecum >= hperc*etotal/100.) {
+				f2 = w0+iw*dw;
+				e2 = e[iw*n1+i1];
+				break;
+			    }
+			}
+			if (f1 == f2) {
+			    fdg[i1] = 0.;
+			} else {
+			    fdg[i1] = (e2-e1)/(f2-f1);
+			}
+		    } else {
+/*			numer = 0.;
+			denom = 0.;
+			
+			for (iw=0; iw < nw; iw++) {
+			    numer += e[iw*n1+i1]*(w0+iw*dw);
+			    denom += e[iw*n1+i1];
+			}
+			
+			index = (int)((numer/denom-w0)/dw);
+			etotal = 0.;
+			for (iw=0; iw < nw; iw++) {
+			    etotal += e[iw*n1+i1];
+			}
+			ecum = 0.;
+			for (iw=0; iw < nw; iw++) {
+			    ecum += e[iw*n1+i1];
+			    if (ecum >= hperc*etotal/100.) {
+				wend = iw;
+				break;
+			    }
+			}
+			
+			cum1 = 0.;
+			cum2 = 0.;
+			cum3 = 0.;
+			cum4 = 0.;
+			for (iw=index; iw < wend; iw++) {
+			    cum1 += w0+iw*dw;
+			    cum2 += logf(e[iw*n1+i1]);
+			    cum3 += (w0+iw*dw)*logf(e[iw*n1+i1]);
+			    cum4 += (w0+iw*dw)*(w0+iw*dw);
+			}
+			fdg[i1] = ((nw-index)*cum3-cum1*cum2)/
+			    ((nw-index)*cum4-cum1*cum1);
 */
-
-		/*	index = 0; */
-		emax = e[i1];
-		for (iw=0; iw < nw-1; iw++) {
-		    if (emax < e[(iw+1)*n1+i1]) {
-			emax = e[(iw+1)*n1+i1];
-			wbeg = iw+1;
+/*                      index = 0;                */
+			emax = e[i1];
+			for (iw=0; iw < nw-1; iw++) {
+			    if (emax < e[(iw+1)*n1+i1]) {
+				emax = e[(iw+1)*n1+i1];
+				wbeg = iw+1;
+			    }
+			}
+			
+			wend = nw;
+			
+			if (wbeg == wend-1) wbeg = 0;
+			
+			nd = wend-wbeg+1;
+			sol = sf_floatalloc(2);		
+			dat = sf_floatalloc(nd);
+			func = sf_floatalloc2(nd,2);
+			
+			for (iw=wbeg; iw < wend; iw++) {
+			    dat[iw-wbeg] = logf(e[iw*n1+i1]);
+			    func[0][iw-wbeg] = 1.;
+			    func[1][iw-wbeg] = w0+iw*dw;
+			}
+			
+			linearfit (nd,dat,func,sol);
+			
+			fdg[i1] = sol[1];
+			
 		    }
-		}
-		
-		wend = nw;
-
-		if (wbeg == wend-1) wbeg = 0;
-
-		nd = wend-wbeg+1;
-		sol = sf_floatalloc(2);		
-		dat = sf_floatalloc(nd);
-		func = sf_floatalloc2(nd,2);
-
-		for (iw=wbeg; iw < wend; iw++) {
-		    dat[iw-wbeg] = logf(e[iw*n1+i1]);
-		    func[0][iw-wbeg] = 1.;
-		    func[1][iw-wbeg] = w0+iw*dw;
-		}
-		
-		linearfit (nd,dat,func,sol);
-
-		fdg[i1] = sol[1];
-
+		    break;
+		case 'l':
+		    etotal = 0.;
+		    for (iw=0; iw < nw; iw++) {
+			etotal += e[iw*n1+i1];
+		    }
+		    ecum = 0.;
+		    for (iw=0; iw < nw; iw++) {
+			ecum += e[iw*n1+i1];
+			if (ecum >= lperc*etotal/100.) {
+			    f1 = w0+iw*dw;
+			    break;
+			}
+		    }
+		    fdg[i1] = f1;
+		    break;
+		case 'f':
+		    etotal = 0.;
+		    for (iw=0; iw < nw; iw++) {
+			etotal += e[iw*n1+i1];
+		    }
+		    ecum = 0.;
+		    for (iw=0; iw < nw; iw++) {
+			ecum += e[iw*n1+i1];
+			if (ecum >= hperc*etotal/100.) {
+			    f2 = w0+iw*dw;
+			    break;
+			}
+		    }
+		    fdg[i1] = f2;
+		    break;
+		case 'r':
+		    etotal = 0.;
+		    for (iw=0; iw < nw; iw++) {
+			etotal += e[iw*n1+i1];
+		    }
+		    ecum = 0.;
+		    for (iw=0; iw < nw; iw++) {
+			ecum += e[iw*n1+i1];
+			if (ecum >= lperc*etotal/100.) {
+			    e1 = e[iw*n1+i1];
+			}
+		    }
+		    fdg[i1] = e1/(etotal+FLT_EPSILON);
+		    break;
+		default:
+		    sf_error("Unknown operator \"%s\"",type);
 	    }
 	}
 	sf_floatwrite(fdg,n1,out);
