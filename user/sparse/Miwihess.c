66b953b952c66595e2e0ec1791b8df3b206da3e3
Siwei Li <llisiw@hotmail.com>
2012-11-02 01:35:53 +0000

    backup
    Former-commit-id: 0b532d06c90f42e55774935b0b71d7b766e0c44e

--- user/sparse/Miwihess.c
+++ user/sparse/Miwihess.c
@@ -25,19 +25,31 @@
 
 int main(int argc, char* argv[])
 {
+    bool verb;
     int n1, n2, nh, ns, nw;
-    int ih, is, iw, **pos, i, j, ipos;
+    int ih, is, iw, **pp, i, ip;
+    int zz, xx;
     double omega;
-    float dw, ow, ***wght, ****ahess;
-    sf_complex ****swave, ****rwave, *f;
-    sf_complex **green, temps, tempr;
+    float dw, ow, ***wght;
+    float *ahess, ***ahesss, ***ahessr;
+    sf_complex *f, ***swave, ***rwave;
+    sf_complex ***stemp, ***rtemp;
     sf_file in, out, list, us, ur, wvlt;
     sf_file weight;
     int uts, mts, its;
+    sf_timer timer;
 
     sf_init(argc,argv);
     in  = sf_input("in");
     out = sf_output("out");
+
+    if (!sf_getbool("verb",&verb)) verb=false;
+    /* verbosity flag */
+
+    if (verb)
+	timer = sf_timer_init();
+    else
+	timer = NULL;
 
     if (!sf_getint("nh",&nh)) nh=0;
     /* horizontal space-lag */
@@ -65,21 +77,13 @@ int main(int argc, char* argv[])
     if (!sf_histint(us,"n3",&ns)) sf_error("No ns=.");
     if (!sf_histint(us,"n4",&nw)) sf_error("No nw=.");
     if (!sf_histfloat(us,"d4",&dw)) sf_error("No dw=.");
-    if (!sf_histfloat(us,"o4",&ow)) sf_error("No ow=.");    
-    
-    swave = sf_complexalloc4(n1,n2,ns,nw);
-    sf_complexread(swave[0][0][0],n1*n2*ns*nw,us);
-    sf_fileclose(us);
+    if (!sf_histfloat(us,"o4",&ow)) sf_error("No ow=.");
 
     /* read receiver wavefield */	
     if (NULL == sf_getstring("ur"))
 	sf_error("Need receiver wavefield ur=");
     ur = sf_input("ur");
-
-    rwave = sf_complexalloc4(n1,n2,ns,nw);
-    sf_complexread(rwave[0][0][0],n1*n2*ns*nw,ur);
-    sf_fileclose(ur);
-
+    
     /* read image weight */
     if (NULL == sf_getstring("weight")) {
 	weight = NULL;
@@ -105,90 +109,95 @@ int main(int argc, char* argv[])
 	sf_error("Need list=");
     list = sf_input("list");
 
-    pos = sf_intalloc2(2,ns);
-    sf_intread(pos[0],2*ns,list);
+    pp = sf_intalloc2(2,ns);
+    sf_intread(pp[0],2*ns,list);
     sf_fileclose(list);
 
     /* allocate memory */
-    green = sf_complexalloc2(ns,uts);
-    ahess = sf_floatalloc4(n1,n2,2*nh+1,uts);
+    swave = sf_complexalloc3(n1,n2,ns);
+    rwave = sf_complexalloc3(n1,n2,ns);
+    stemp = sf_complexalloc3(2*nh+1,ns,ns);
+    rtemp = sf_complexalloc3(2*nh+1,ns,ns);
+
+    ahess = sf_floatalloc(n1*n2);
+    ahesss = sf_floatalloc3(2*nh+1,ns,n1*n2); /* NOTE: we cannot afford this! */
+    ahessr = sf_floatalloc3(2*nh+1,ns,n1*n2); /* NOTE: we cannot afford this! */
 
     /* loop over frequency */
+    for (iw=0; iw < nw; iw++) {
+	omega = (double) 2.*SF_PI*(ow+iw*dw);
+
+	if (verb) {
+	    sf_warning("Frequency %d of %d.",iw+1,nw);
+	    sf_timer_start(timer);
+	}
+
+	/* read wavefields */
+	sf_complexread(swave[0][0],n1*n2*ns,us);
+	sf_complexread(rwave[0][0],n1*n2*ns,ur);
+
 #ifdef _OPENMP
-#pragma omp parallel num_threads(uts) private(its,omega,j,i,ipos,is,ih,temps,tempr)
+#pragma omp parallel num_threads(uts) private(its,is,ip,ih,zz,xx,i)
 #endif
-    {
+	{
 #ifdef _OPENMP
-	its = omp_get_thread_num();
+	    its = omp_get_thread_num();
 #else
-	its = 0;
+	    its = 0;
 #endif
 
+	    /* temparary wavefields */
+	    /* NOTE: negletible cost */
 #ifdef _OPENMP
 #pragma omp for
 #endif
-	for (iw=0; iw < nw; iw++) {
-	    omega = (double) 2.*SF_PI*(ow+iw*dw);	    
-	    
-	    /* loop over model */
-	    for (j=0; j < n2; j++) {
-		for (i=0; i < n1; i++) {
-		    
-		    /* green's function */
-		    for (ipos=0; ipos < ns; ipos++) {
-			green[its][ipos] = swave[iw][ipos][j][i]/f[iw];
+	    for (is=0; is < ns; is++) {
+		for (ip=0; ip < ns; ip++) {
+		    for (ih=-nh; ih < nh+1; ih++) {
+			zz = pp[ip][0];
+
+			/* source */
+			xx = pp[ip][1]+ih;
+			if (xx+ih >= 0 && xx+ih < n2)
+			    stemp[is][ip][ih+nh] = -omega*omega/conjf(f[iw])
+				*(wght==NULL? 1.: wght[ih+nh][xx][zz])*rwave[is][xx+ih][zz];
+			
+			/* receiver */
+			xx = pp[ip][1]-ih;
+			if (xx-ih >= 0 && xx-ih < n2)
+			    rtemp[is][ip][ih+nh] = -omega*omega/conjf(f[iw])
+				*(wght==NULL? 1.: wght[ih+nh][xx][zz])*conjf(swave[is][xx-ih][zz]);
 		    }
+		}
+	    }
 
-		    /* loop over shots */
+	    /* loop over model */
+	    /* NOTE: unacceptable cost */
+#ifdef _OPENMP
+#pragma omp for
+#endif
+	    for (i=0; i < n1*n2; i++) {
+		for (ih=-nh; ih < nh+1; ih++) {
 		    for (is=0; is < ns; is++) {
-			/* loop over space-lags */
-			for (ih=-nh; ih < nh+1; ih++) {
-			    /* loop over green's functions */
-			    for (ipos=0; ipos < ns; ipos++) {
-
-				if (pos[ipos][1]+2*ih >= 0 && pos[ipos][1]+2*ih < n2) {
-				    temps = conjf(swave[iw][is][j][i]*green[its][ipos])
-					*rwave[iw][is][pos[ipos][1]+2*ih][pos[ipos][0]];
-				} else {
-				    temps = sf_cmplx(0.,0.);
-				}
-
-				if (pos[ipos][1]-2*ih >= 0 && pos[ipos][1]-2*ih < n2) {
-				    tempr = conjf(swave[iw][is][pos[ipos][1]-2*ih][pos[ipos][0]])
-					*rwave[iw][is][j][i]*conjf(green[its][ipos]);
-				} else {
-				    tempr = sf_cmplx(0.,0.);
-				}
-
-				if (wght != NULL) {
-				    temps *= wght[ih+nh][pos[ipos][1]+ih][pos[ipos][0]];
-				    tempr *= wght[ih+nh][pos[ipos][1]-ih][pos[ipos][0]];
-				}
-				
-				ahess[its][ih+nh][j][i] += -omega*omega*creal(temps+tempr);
-			    }
+			for (ip=0; ip < ns; ip++) {
+			    ahesss[i][ip][ih+nh] += 
+				crealf(conjf(swave[ip][0][i]*swave[is][0][i])*stemp[is][ip][ih+nh]);
+			    ahessr[i][ip][ih+nh] += 
+				crealf(conjf(swave[ip][0][i])*rwave[is][0][i]*rtemp[is][ip][ih+nh]);
 			}
 		    }
 		}
 	    }
 	}
 
-#ifdef _OPENMP
-#pragma omp for
-#endif
-	for (ih=-nh; ih < nh+1; ih++) {
-	    for (j=0; j < n2; j++) {
-		for (i=0; i < n1; i++) {
-		    for (its=1; its < uts; its++) {
-			ahess[0][ih+nh][j][i] += ahess[its][ih+nh][j][i];
-		    }
-		}
-	    }
+	if (verb) {
+	    sf_timer_stop (timer);
+	    sf_warning("Finished in %g seconds.",sf_timer_get_diff_time(timer)/1.e3);
 	}
     }
-    
-    /* write output */
-    sf_floatwrite(ahess[0][0][0],n1*n2*(2*nh+1),out);
+
+    /* assemble approximate Hessian */
+    /* NOTE: to be finished */
 
     exit(0);
 }
