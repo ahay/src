dcf334210cea9d55232b744410965ee3a0b36043
Sergey Fomel <sergey.fomel@gmail.com>
2012-11-04 03:23:57 +0000

    compiling chengjb
    Former-commit-id: c656b1407581b6bb9a4d6515789f6779f9d96adc

--- user/chengjb/kykxkztaper.c
+++ user/chengjb/kykxkztaper.c
@@ -23,9 +23,8 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-
-#include "_cjb.h"
 #include <rsf.h>
+#include "_cjb.h"
 
 /* The tapering operators is borrowed from J. Yan */
 
