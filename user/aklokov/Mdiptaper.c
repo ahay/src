17e05c29047bca148f5788b4dd7ab9a4ebc8fd11
Sergey Fomel <sergey.fomel@gmail.com>
2013-01-25 19:33:44 +0000

    compiling with icc
    Former-commit-id: ad6a13b8f95a1228e77e91397a1bd03b87cacfbc

--- user/aklokov/Mdiptaper.c
+++ user/aklokov/Mdiptaper.c
@@ -1,14 +1,15 @@
-/* Aperture optimization for migrated gathers in the dip-angle domain
-Estimates a constructive imaging part of a reflection event in the dip-angle domain.
-Basing on the estimation defines a stacking weight for every migrated sample.
+/* Aperture optimization for migrated gathers in the dip-angle domain.
 
-Input:
-	dipFile.rsf - dips esitimated in constant-dip subimages. The dips are in degree (!).
-			A positive dip corresponds to an ascending boundary, a negative dip - to a descending boundary.
-			A constant-dip subimage consists of migrated traces correspondig to the same dip-angle.
+   Estimates a constructive imaging part of a reflection event in the dip-angle domain.
+   Basing on the estimation defines a stacking weight for every migrated sample.
 
-Output:
-	taperFile.rsf - optimal weights for the migrated samples
+   Input:
+   dipFile.rsf - dips esitimated in constant-dip subimages. The dips are in degree (!).
+   A positive dip corresponds to an ascending boundary, a negative dip - to a descending boundary.
+   A constant-dip subimage consists of migrated traces correspondig to the same dip-angle.
+
+   Output:
+   taperFile.rsf - optimal weights for the migrated samples
 */
 
 /*
@@ -31,23 +32,32 @@ Output:
 
 #include <rsf.h>
 
-int main (int argc, char* argv[]) {
-   
+int main (int argc, char* argv[]) 
+{
+// dip-angle gathers dimensions
+    int zNum_;   float zStart_;   float zStep_;
+    int dipNum_; float dipStart_; float dipStep_;
+    int xNum_;	 float xStart_;   float xStep_;   
+    sf_file dipFile, taperFile;
+    float dz = 0.f;
+    float greyarea = 0.f;
+    int panelSize, id, startPos, iz, ix, ind;
+    float *dipPanel, *taperPanel;
+    float CONVRATIO, curDip, curDipRad, curDipSin, curDipCos;
+    float curZ, ksi, ksicos2, a, b, c, sqrtD, sin1, sin2, dip1, dip2, dipLeft, dipRight;
+    float secondLeft, secondRight, temp, dip, taper;
+
 // Initialize RSF 
     sf_init (argc,argv);
 // Input files
-    sf_file dipFile = sf_input ("in");
+    dipFile = sf_input ("in");
 // check that the input is float 
     if ( SF_FLOAT != sf_gettype (dipFile) ) sf_error ("Need float input: dip-angle gathers");
     /* dip-angle gathers - stacks in the scattering-angle direction */
 
 // Output file
-    sf_file taperFile = sf_output("out");
+    taperFile = sf_output("out");
 
-// dip-angle gathers dimensions
-	int zNum_;   float zStart_;   float zStep_;
-	int dipNum_; float dipStart_; float dipStep_;
-	int xNum_;	 float xStart_;   float xStep_;
 // Depth/time axis 
     if ( !sf_histint   (dipFile, "n1", &zNum_) )   sf_error ("Need n1= in input");
     if ( !sf_histfloat (dipFile, "d1", &zStep_) )  sf_error ("Need d1= in input");
@@ -62,96 +72,94 @@ int main (int argc, char* argv[]) {
     if ( !sf_histfloat (dipFile, "o3", &dipStart_) )   sf_error ("Need o3= in input");
 
 // tapering paremeters
-	float dz = 0.f;
     if ( !sf_getfloat ("dz", &dz) ) dz = 20.f;
     /* half of a migrated wave length */
-	if (dz < 0) {sf_warning ("dz value is changed to 20"); dz = 20.f;}
+    if (dz < 0) {sf_warning ("dz value is changed to 20"); dz = 20.f;}
 
-	float greyarea = 0.f;
     if ( !sf_getfloat ("greyarea", &greyarea) ) greyarea = 10.f;
     /* width of event tail taper (in degree) */
-	if ( greyarea < 0 ) {sf_warning ("greyarea value is changed to 10"); greyarea = 10.f;}
+    if ( greyarea < 0 ) {sf_warning ("greyarea value is changed to 10"); greyarea = 10.f;}
 
-	const int panelSize = xNum_ * zNum_;
-	float* dipPanel   = sf_floatalloc (panelSize);
-	float* taperPanel = sf_floatalloc (panelSize);
+    panelSize = xNum_ * zNum_;
+    dipPanel   = sf_floatalloc (panelSize);
+    taperPanel = sf_floatalloc (panelSize);
 
-	const float CONVRATIO = M_PI / 180.f;
+    CONVRATIO = SF_PI / 180.f;
 
-	for (int id = 0; id < dipNum_; ++id) {
+    for (id = 0; id < dipNum_; ++id) {
 
-		sf_warning ("dip %d of %d;", id + 1, dipNum_);	
+	sf_warning ("dip %d of %d;", id + 1, dipNum_);	
 
-		const float curDip = dipStart_ + id * dipStep_;
-		const float curDipRad = curDip * CONVRATIO; 
-		const float curDipSin = sin (curDipRad);
-		const float curDipCos = cos (curDipRad);		
+	curDip = dipStart_ + id * dipStep_;
+	curDipRad = curDip * CONVRATIO; 
+	curDipSin = sin (curDipRad);
+	curDipCos = cos (curDipRad);		
 
-		memset (dipPanel,   0, panelSize * sizeof (float));
-		memset (taperPanel, 0, panelSize * sizeof (float));
+	memset (dipPanel,   0, panelSize * sizeof (float));
+	memset (taperPanel, 0, panelSize * sizeof (float));
 
-		const int startPos = id * panelSize * sizeof (float);
-		sf_seek (dipFile, startPos, SEEK_SET);		
-		sf_floatread (dipPanel, panelSize, dipFile);
+	startPos = id * panelSize * sizeof (float);
+	sf_seek (dipFile, startPos, SEEK_SET);		
+	sf_floatread (dipPanel, panelSize, dipFile);
 
-		// taper values are constant for every depth level 
-		for (int iz = 0; iz < zNum_; ++iz) {
+	// taper values are constant for every depth level 
+	for (iz = 0; iz < zNum_; ++iz) {
 
-			const float curZ = zStart_ + iz * zStep_;
-			if (! (curZ - dz > 0) ) continue; // out from data
+	    curZ = zStart_ + iz * zStep_;
+	    if (! (curZ - dz > 0) ) continue; // out from data
 
-			const float ksi = curZ / (curZ - dz); 			
-			const float ksicos2 = pow (ksi * curDipCos, 2);
+	    ksi = curZ / (curZ - dz); 			
+	    ksicos2 = pow (ksi * curDipCos, 2);
 
-			const float a = pow (curDipSin, 2) + ksicos2;
-			const float b = -2 * curDipSin;
-			const float c = 1 - ksicos2;
+	    a = pow (curDipSin, 2) + ksicos2;
+	    b = -2 * curDipSin;
+	    c = 1 - ksicos2;
 
-			const float sqrtD = sqrt (b*b - 4*a*c);
+	    sqrtD = sqrt (b*b - 4*a*c);
 	
-			const float sin1 = (-b + sqrtD) / (2 * a); 
-			const float sin2 = (-b - sqrtD) / (2 * a); 
+	    sin1 = (-b + sqrtD) / (2 * a); 
+	    sin2 = (-b - sqrtD) / (2 * a); 
 	
-			const float dip1 = asin (sin1) / CONVRATIO; 
-			const float dip2 = asin (sin2) / CONVRATIO; 
+	    dip1 = asin (sin1) / CONVRATIO; 
+	    dip2 = asin (sin2) / CONVRATIO; 
 	
-			const float dipLeft  = dip1 < dip2 ? dip1 : dip2;
-			const float dipRight = dip1 < dip2 ? dip2 : dip1;
+	    dipLeft  = dip1 < dip2 ? dip1 : dip2;
+	    dipRight = dip1 < dip2 ? dip2 : dip1;
 
-			// define tails tapering intervals
-			const float secondLeft  = dipLeft  - greyarea;
-			const float secondRight = dipRight + greyarea;
+	    // define tails tapering intervals
+	    secondLeft  = dipLeft  - greyarea;
+	    secondRight = dipRight + greyarea;
 
-			float temp = 0.f;
+	    temp = 0.f;
 
-			for (int ix = 0; ix < xNum_; ++ix) {
-				const int ind = ix * zNum_ + iz;
-				const float dip = dipPanel [ind]; // local slope in degree
-				float taper = 1;
-				if (dip > dipLeft && dip < dipRight) // the slope is in a constructive zone
-					taper = 1;
-				else {	
-					if (dip < secondLeft || dip > secondRight) { // the slope is out from the tapering zone
-						taper = 0;
-					} else {
-						if (dip < dipLeft) temp = -M_PI + M_PI * (dip - secondLeft) * 1.f / (dipLeft - secondLeft); 
-						else 		       temp = M_PI * (dip - dipRight) * 1.f / (secondRight - dipRight); 
-				    	taper = 0.5 + 0.5 * cos (temp);
-					}
-				}
-				taperPanel[ind] = taper;
-			}
-		}	
-		sf_floatwrite (taperPanel, panelSize, taperFile);
+	    for (ix = 0; ix < xNum_; ++ix) {
+		ind = ix * zNum_ + iz;
+		dip = dipPanel [ind]; // local slope in degree
+		taper = 1;
+		if (dip > dipLeft && dip < dipRight) // the slope is in a constructive zone
+		    taper = 1;
+		else {	
+		    if (dip < secondLeft || dip > secondRight) { // the slope is out from the tapering zone
+			taper = 0;
+		    } else {
+			if (dip < dipLeft) temp = -SF_PI + SF_PI * (dip - secondLeft) * 1.f / (dipLeft - secondLeft); 
+			else 		       temp = SF_PI * (dip - dipRight) * 1.f / (secondRight - dipRight); 
+			taper = 0.5 + 0.5 * cos (temp);
+		    }
+		}
+		taperPanel[ind] = taper;
+	    }
 	}	
+	sf_floatwrite (taperPanel, panelSize, taperFile);
+    }	
 
-	sf_warning (".");
+    sf_warning (".");
 
-	sf_fileclose (dipFile);
-	sf_fileclose (taperFile);
+    sf_fileclose (dipFile);
+    sf_fileclose (taperFile);
 
-	free (dipPanel);
-	free (taperPanel);
+    free (dipPanel);
+    free (taperPanel);
 
-	return 0;
+    return 0;
 }
