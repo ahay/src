adec7a1b763379bb6d56c96d635f37b05cdee55e
Sergey Fomel <sergey.fomel@gmail.com>
2013-01-25 19:47:12 +0000

    compiling with icc
    Former-commit-id: 2af364249eee4a2feacf08d41c495bbbdb887e18

--- user/aklokov/ditime2d.c
+++ user/aklokov/ditime2d.c
@@ -36,78 +36,83 @@ static int invMod_;
 static int tLim_;
 
 void ditime2d_init (float dipo,  float dipd,  int dipn,  // dip angle axis 
-				    float xio,   float xid,   int xin,   // xi axis
-			  	    float dip0o, float dip0d, int dip0n, // refl dip axis
-		  		    float to,    float td,    int tn,    // time axis 
-		  			bool isAA,                           // antialiasing
-					int invMod) 						 
+		    float xio,   float xid,   int xin,   // xi axis
+		    float dip0o, float dip0d, int dip0n, // refl dip axis
+		    float to,    float td,    int tn,    // time axis 
+		    bool isAA,                           // antialiasing
+		    int invMod) 						 
 /*< initialize >*/
 {
+    int reflSize, diffSize;
+    float CONVPARAM;
+    float *pTableR, *pTableD;
+    int id, ixi, id0, curDip0, it;
+    float curDip, a, cos_a, sin_a, curXi,aux_diff, curTime, a0, cos_a0, sin_a0,  aux_refl;
 
-	to_   = to;   td_   = td;   tn_   = tn;  
+    to_   = to;   td_   = td;   tn_   = tn;  
     dipo_ = dipo; dipd_ = dipd; dipn_ = dipn;  
 
     dip0o_ = dip0o; dip0d_ = dip0d; dip0n_ = dip0n;  
     xio_  = xio; xid_  = xid;  xin_  = xin;  
 
-	tLim_ = to_ + td_ * (tn_ - 1);
+    tLim_ = to_ + td_ * (tn_ - 1);
 
-	invMod_ = invMod;
-	isAA_   = isAA;
+    invMod_ = invMod;
+    isAA_   = isAA;
 
-	if (isAA_) {
-		aastretch_init  (false, tn_, to_, td_, tn_);
-		sf_halfint_init (true, 2 * tn_, 1.f - 1.f / tn_);
-	} else    
-		stretch_init  (tn_, to_, td_, tn_);
+    if (isAA_) {
+	aastretch_init  (false, tn_, to_, td_, tn_);
+	sf_halfint_init (true, 2 * tn_, 1.f - 1.f / tn_);
+    } else    
+	stretch_init  (tn_, to_, td_, tn_);
 
     amp = sf_floatalloc (tn_);
     str = sf_floatalloc (tn_);
     tx  = sf_floatalloc (tn_);
     tmp = sf_floatalloc (tn_);
 
-	// shift tables
+    // shift tables
 
-	const int reflSize = tn_ * dip0n_ * dipn_;
-	const int diffSize = tn_ * xin_ * dipn_;
+    reflSize = tn_ * dip0n_ * dipn_;
+    diffSize = tn_ * xin_ * dipn_;
 
-	tableRefl_ = sf_floatalloc (reflSize);
-	tableDiff_ = sf_floatalloc (diffSize);
+    tableRefl_ = sf_floatalloc (reflSize);
+    tableDiff_ = sf_floatalloc (diffSize);
 
-	const float CONVPARAM = SF_PI / 180.f;
+    CONVPARAM = SF_PI / 180.f;
 
-	float* pTableR = tableRefl_;
-	float* pTableD = tableDiff_;
+    pTableR = tableRefl_;
+    pTableD = tableDiff_;
 
-    for (int id = 0; id < dipn_; ++id) { 
-		const float curDip = dipo_ + id * dipd_;
-		const float a      = curDip * CONVPARAM;
-		const float cos_a  = cos (a);
-		const float sin_a  = sin (a);
-	    for (int ixi = 0; ixi < xin_; ++ixi) { 
-			const float curXi = xio_ + ixi * xid_;
-			const float aux_diff = (curXi * sin_a + sqrt ( curXi * curXi + cos_a * cos_a ) ) / cos_a;
-			for (int it = 0; it < tn_; ++it, ++pTableD) { 
-			    const float curTime = to_ + it * td_;
-			    *pTableD = curTime * aux_diff;			    
-			}
-		}
-	    for (int id0 = 0; id0 < dip0n_; ++id0) { 
-			const int   curDip0 = dip0o_ + id0 * dip0d_;
-			const float a0 = curDip0 * CONVPARAM;
-			const float cos_a0 = cos (a0);	
-			const float sin_a0 = sin (a0);	
-		
-			const float aux_refl = cos_a * cos_a0 / (1 - sin_a * sin_a0);
-
-			for (int it = 0; it < tn_; ++it, ++pTableR) {		
-			    const float curTime = to_ + it * td_;
-			    *pTableR = curTime * aux_refl;		
-			}
-		}
+    for (id = 0; id < dipn_; ++id) { 
+	curDip = dipo_ + id * dipd_;
+	a      = curDip * CONVPARAM;
+	cos_a  = cos (a);
+	sin_a  = sin (a);
+	for (ixi = 0; ixi < xin_; ++ixi) { 
+	    curXi = xio_ + ixi * xid_;
+	    aux_diff = (curXi * sin_a + sqrt ( curXi * curXi + cos_a * cos_a ) ) / cos_a;
+	    for (it = 0; it < tn_; ++it, ++pTableD) { 
+		curTime = to_ + it * td_;
+		*pTableD = curTime * aux_diff;			    
+	    }
 	}
+	for (id0 = 0; id0 < dip0n_; ++id0) { 
+	    curDip0 = dip0o_ + id0 * dip0d_;
+	    a0 = curDip0 * CONVPARAM;
+	    cos_a0 = cos (a0);	
+	    sin_a0 = sin (a0);	
+		
+	    aux_refl = cos_a * cos_a0 / (1 - sin_a * sin_a0);
 
-	return;
+	    for (it = 0; it < tn_; ++it, ++pTableR) {		
+		curTime = to_ + it * td_;
+		*pTableR = curTime * aux_refl;		
+	    }
+	}
+    }
+
+    return;
 }
 
 void ditime2d_close (void)
@@ -118,105 +123,109 @@ void ditime2d_close (void)
     free(tx);
     free(tmp);
 
-	if (isAA_) {
-	    aastretch_close();
-		sf_halfint_close();
-	} else
-	    stretch_close();
+    if (isAA_) {
+	aastretch_close();
+	sf_halfint_close();
+    } else
+	stretch_close();
 
-	free (tableRefl_);
-	free (tableDiff_);
+    free (tableRefl_);
+    free (tableDiff_);
 
-	return;
+    return;
 }
 
 void ditime2d_lop (bool adj, bool add, int modelSize, int dataSize, 
-		  		   float* modl, float* data) 
+		   float* modl, float* data) 
 /*< operator >*/
 {
-	switch (invMod_) {
-		case 0:
-		    if (modelSize != tn_ * xin_ || dataSize != tn_ * dipn_) 
-			sf_error ("%s: wrong dimensions modelSize=%d dataSize=%d",__FILE__, modelSize, dataSize);
-		    break;
-		case 1:
-		    if (modelSize != tn_ * dip0n_ || dataSize != tn_ * dipn_) 
-			sf_error ("%s: wrong dimensions modelSize=%d dataSize=%d",__FILE__, modelSize, dataSize);
-		    break;
-		case 2:
-		    if (modelSize != tn_ * (xin_ + dip0n_) || dataSize != tn_ * dipn_) 
-			sf_error ("%s: wrong dimensions modelSize=%d dataSize=%d",__FILE__, modelSize, dataSize);
-		    break;
+    float *pTableR, *pTableD;
+    int id, ixi, it, offset, id0;
+    float curTime, t;
+
+    switch (invMod_) {
+	case 0:
+	    if (modelSize != tn_ * xin_ || dataSize != tn_ * dipn_) 
+		sf_error ("%s: wrong dimensions modelSize=%d dataSize=%d",__FILE__, modelSize, dataSize);
+	    break;
+	case 1:
+	    if (modelSize != tn_ * dip0n_ || dataSize != tn_ * dipn_) 
+		sf_error ("%s: wrong dimensions modelSize=%d dataSize=%d",__FILE__, modelSize, dataSize);
+	    break;
+	case 2:
+	    if (modelSize != tn_ * (xin_ + dip0n_) || dataSize != tn_ * dipn_) 
+		sf_error ("%s: wrong dimensions modelSize=%d dataSize=%d",__FILE__, modelSize, dataSize);
+	    break;
     }
 
     sf_adjnull (adj, add, modelSize, dataSize, modl, data);
 
-	float* pTableR = tableRefl_;
-	float* pTableD = tableDiff_;
+    pTableR = tableRefl_;
+    pTableD = tableDiff_;
 
-    for (int id = 0; id < dipn_; ++id) { 
-		// diffracion part
+    for (id = 0; id < dipn_; ++id) { 
+	// diffracion part
 
-		if (1 != invMod_) {	
-		    for (int ixi = 0; ixi < xin_; ++ixi) { 
+	if (1 != invMod_) {	
+	    for (ixi = 0; ixi < xin_; ++ixi) { 
 
-				for (int it = 0; it < tn_; ++it, ++pTableD) { 
-					const float curTime = to_ + it * td_;
-				    const float t = *pTableD;		    
-				    if (t > 0. && t < tLim_) {
-					    str [it] = t;
-						tx[it] = 0.f;  // not sure if it is reasonable to calculate tx - may be too expensive
+		for (it = 0; it < tn_; ++it, ++pTableD) { 
+		    curTime = to_ + it * td_;
+		    t = *pTableD;		    
+		    if (t > 0. && t < tLim_) {
+			str [it] = t;
+			tx[it] = 0.f;  // not sure if it is reasonable to calculate tx - may be too expensive
 //					    tx [it] = anti * fabsf (p - p1) * dx;
-					    amp [it] = 1.;
-					} else {
-						str[it] = curTime - 2.f * td_;
-						tx[it]  = 0.f;
-						amp[it] = 0.f;
-					}
-				}	
-
-				if (isAA_) {
-					aastretch_define (str, tx, amp);
-				    sf_chain (sf_halfint_lop, aastretch_lop,
-						      adj, true, tn_, tn_, tn_, modl + ixi * tn_, data + id * tn_, tmp);
-				} else {
-					stretch_define (str);
-					stretch_lop (adj, true, tn_, tn_, modl + ixi * tn_, data + id * tn_);
-				}
+			amp [it] = 1.;
+		    } else {
+			str[it] = curTime - 2.f * td_;
+			tx[it]  = 0.f;
+			amp[it] = 0.f;
 		    }
-		}
+		}	
 
-		// reflection part
-		if (0 != invMod_) {
-
-		    const int offset = (1 == invMod_) ? 0 : xin_;
-		    for (int id0 = 0; id0 < dip0n_; ++id0) { 
-				for (int it = 0; it < tn_; ++it, ++pTableR) {		
-					const float curTime = to_ + it * td_;
-				    const float t = *pTableR;
-				    if (t > 0. && t < tLim_) {
-						str[it] = t;
-						tx[it] = 0.f;  // not sure if it is reasonable to calculate tx - may be too expensive
-//						tx[it] = fabsf(anti*sx)/t;
-						amp[it] = 1.f;
-				    } else {
-						str[it] = curTime - 2.f * td_;
-						tx[it]  = 0.f;
-						amp[it] = 0.f;
-				    }		
-				}
-		
-				if (isAA_) {
-					aastretch_define (str, tx, amp);
-				    sf_chain (sf_halfint_lop, aastretch_lop,
-						      adj, true, tn_, tn_, tn_, modl + (offset + id0) * tn_, data + id * tn_, tmp);
-				} else {
-					stretch_define (str);
-					stretch_lop (adj, true, tn_, tn_, modl + (offset + id0) * tn_, data + id * tn_);
-				}
-		    }
+		if (isAA_) {
+		    aastretch_define (str, tx, amp);
+		    sf_chain (sf_halfint_lop, aastretch_lop,
+			      adj, true, tn_, tn_, tn_, modl + ixi * tn_, data + id * tn_, tmp);
+		} else {
+		    stretch_define (str);
+		    stretch_lop (adj, true, tn_, tn_, modl + ixi * tn_, data + id * tn_);
 		}
+	    }
 	}
 
-	return;
+	// reflection part
+	if (0 != invMod_) {
+
+	    offset = (1 == invMod_) ? 0 : xin_;
+	    for (id0 = 0; id0 < dip0n_; ++id0) { 
+		for (it = 0; it < tn_; ++it, ++pTableR) {		
+		    curTime = to_ + it * td_;
+		    t = *pTableR;
+		    if (t > 0. && t < tLim_) {
+			str[it] = t;
+			tx[it] = 0.f;  // not sure if it is reasonable to calculate tx - may be too expensive
+//						tx[it] = fabsf(anti*sx)/t;
+			amp[it] = 1.f;
+		    } else {
+			str[it] = curTime - 2.f * td_;
+			tx[it]  = 0.f;
+			amp[it] = 0.f;
+		    }		
+		}
+		
+		if (isAA_) {
+		    aastretch_define (str, tx, amp);
+		    sf_chain (sf_halfint_lop, aastretch_lop,
+			      adj, true, tn_, tn_, tn_, modl + (offset + id0) * tn_, data + id * tn_, tmp);
+		} else {
+		    stretch_define (str);
+		    stretch_lop (adj, true, tn_, tn_, modl + (offset + id0) * tn_, data + id * tn_);
+		}
+	    }
+	}
+    }
+
+    return;
 }
