b64e02b0255c6789b2bfce30baedc77db6df7481
Alexander Klokov <alexander.klokov@gmail.com>
2013-01-30 17:19:14 +0000

    Former-commit-id: e2732cc67d01d900254b02ebf5e6a66fdbfafc7d

--- user/aklokov/depthBackfitMigrator2D.cc
+++ user/aklokov/depthBackfitMigrator2D.cc
@@ -1,6 +1,7 @@
 #include "depthBackfitMigrator2D.hh"
 #include <rsf.hh>
 #include "support.hh"
+#include "iTracer2D.hh"
 
 DepthBackfitMigrator2D::DepthBackfitMigrator2D () {
 }
@@ -8,7 +9,51 @@ DepthBackfitMigrator2D::DepthBackfitMigrator2D () {
 DepthBackfitMigrator2D::~DepthBackfitMigrator2D () {
 }
 
+void DepthBackfitMigrator2D::init (int zNum, float zStart, float zStep, 
+			 					   int pNum, float pStart, float pStep,
+							 	   int xNum, float xStart, float xStep) {
+	
+	zNum_   = zNum;
+	zStep_  = zStep;
+	zStart_ = zStart;
+
+	pNum_   = pNum;
+	pStep_  = pStep;
+	pStart_ = pStart;
+
+	xNum_   = xNum;
+	xStep_  = xStep;
+	xStart_ = xStart;	
+
+	return;
+}
+
 void DepthBackfitMigrator2D::processParialImage (float* piData, float curP, float* xVol, float* tVol, float* piImage) {
+
+	ITracer2D iTracer;
+	iTracer.init (zNum_, zStart_, zStep_, 
+  			      pNum_, pStart_, pStep_,
+			      xNum_, xStart_, xStep_);
+
+	float* xRes = sf_floatalloc (pNum_);
+	float* zRes = sf_floatalloc (pNum_);
+
+	for (int ix = 0; ix < xNum_; ++ix) {
+		const float curX = xStart_ + ix * xStep_;
+		for (int iz = 0; iz < zNum_; ++iz) {
+			const float curZ = zStart_ + iz * zStep_;
+	
+			memset ( xRes, 0, pNum_ * sizeof (float) );
+			memset ( zRes, 0, pNum_ * sizeof (float) );
+
+			iTracer.traceImage (xVol, tVol, curX, curZ, curP, xRes, zRes);
+		}
+	}
+
+	// FINISH
+
+	free (xRes);
+	free (zRes);
 
 	return;
 }
--- user/aklokov/depthBackfitMigrator2D.hh
+++ user/aklokov/depthBackfitMigrator2D.hh
@@ -8,7 +8,25 @@ public:
 	 DepthBackfitMigrator2D ();
 	~DepthBackfitMigrator2D ();
 
+	void init (int zNum, float zStart, float zStep, 
+  			   int pNum, float pStart, float pStep,
+			   int xNum, float xStart, float xStep);
+
 	void processParialImage (float* piData, float curP, float* xVol, float* tVol, float* piImage);
+
+private:
+
+	int   zNum_;
+	float zStep_;
+	float zStart_;
+
+	int   pNum_;
+	float pStep_;
+	float pStart_;
+
+	int   xNum_;
+	float xStep_;
+	float xStart_;
 
 };
 #endif
