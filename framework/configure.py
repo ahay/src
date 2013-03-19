7b9cd904bb73815f39afaa9d218d2388566edcbb
Sergey Fomel <sergey.fomel@gmail.com>
2013-03-19 21:38:32 +0000

    libpath
    Former-commit-id: b95b9e3cf8a4869f749b5da86284965e45ca941d

--- framework/configure.py
+++ framework/configure.py
@@ -216,6 +216,7 @@ def cc(context):
     else:
         context.Result(context_failure)
         need_pkg('gcc')
+    context.env['LIBPATH'] = path_get(context,'LIBPATH')
     context.env['LIBS'] = path_get(context,'LIBS')
     text = '''
     int main(int argc,char* argv[]) {
