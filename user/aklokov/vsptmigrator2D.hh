3fc541f78923003028287877f342980ad1374be9
Alexander Klokov <alexander.klokov@gmail.com>
2013-04-17 16:12:54 +0000

    sfvsptmigda
    Former-commit-id: b1f35a9ffdef5c9abe208f1380c7118f57b7d0b5

---
+++ user/aklokov/vsptmigrator2D.hh
@@ -0,0 +1,44 @@
+#ifndef VSP_TIME_MIGRATOR_2D_H
+#define VSP_TIME_MIGRATOR_2D_H
+
+#include "dmigratorBase.hh"
+
+class VSPTimeMigrator2D : public DepthMigratorBase {
+
+public:
+
+	    VSPTimeMigrator2D ();
+	   ~VSPTimeMigrator2D ();
+
+	void  processGather (Point2D& curGatherCoords, const float* const data, float* image, float* dag, float* aCig);
+
+	void   setVelModel (float** velField) { velField_ = velField; } 	
+
+private:
+
+	bool   getSampleFromData (const float geoY, const float geoX, const float t, const float p, float& sample);
+
+
+
+	bool   getSampleByBeam   (float curX, float curZ, float vel, float curScatAngle, float curDipAngle, float& sample);
+
+	float getSampleByRay (const float yCIG, const float xCIG, const float curZeroTime, 
+					      const float curDip, const float curAz, const float migVel, const bool isAzDip,
+
+					      const float yEmAngle, const float xEmAngle);
+
+	int getReceiverByDirection (float curX, float curZ, float dir2, float& rec);
+	int getSourceByDirection (float curX, float curZ, float dir2, float& src);
+
+	void processTimeSample (const float curX, const float curZ, const float* const data, 
+											float* curImage, float* curDag, float* curCig);
+
+	float getVel (float curZ, float xCIG);
+
+
+
+	// velocity model
+	float**  velField_;
+
+};
+#endif
