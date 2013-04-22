57fb48fb6abf22ea68cd777d2610dac021ba1646
Esteban Diaz <ediazpan@mines.edu>
2013-04-22 17:35:13 +0000

    Former-commit-id: f78c861ed1a2a98386992b489e3a3c9938163f0f

--- user/ediazp/Mcorrelate.py
+++ user/ediazp/Mcorrelate.py
@@ -12,32 +12,49 @@ b [file] : is taken from  signal
 '''
 import rsf.api as rsf
 import numpy as np
-
+import sys
 
 par = rsf.Par()
 
-b = par.string("signal",None) # File name with other signal
-
-ntau = par.int("ntau",100) # number of tau lags 
-
-
 Fa = rsf.Input()
-Fb = rsf.Input(b)
+Fb = rsf.Input("signal")
 Fout = rsf.Output()
 
+# pars from command line
+ntau = par.int("ntau",100) # number of tau lags 
 
+# parameters from input file
+n1a = Fa.int("n1")    # number of samples from input
+n1b = Fb.int("n1")    # number of samples from signal
 
-n1 = Fa.int("n1")    # number of samples
 d1 = Fa.float("d1")  # dt
 
-if ntau > n1 : ntau=n1-1
+if ntau > n1a or ntau > n1b : ntau = min(n1a-1,n1b-1)
 
-ntraces = Fa.size(1) # number of traces
+ntraces = Fa.size(1) # number of traces of input file
 
-a = np.zeros(n1,'f')
-b = np.zeros(n1,'f')
 
-center = (n1*2-1)/2 # center sample of the correlation
+
+#######################################################################
+# Check that files have the same sampling interval and geometry
+if not ntraces == Fb.size(1):
+  sys.stderr.write('error! :input and signal do not have the same number\
+                    of traces\n')
+  sys.exit(1)
+
+if not d1 == Fb.float("d1"):
+  sys.stderr.write('error! :input and signal do not have the same \
+                    sampling interval\n')
+  sys.exit(1)
+#######################################################################
+  
+
+  
+
+a = np.zeros(n1a,'f')
+b = np.zeros(n1b,'f')
+
+center = (n1a+n1b-1)/2 # center sample of the correlation
 l1 = center-ntau
 l2 = center+ntau+1
 
