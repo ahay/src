33ed67758a5eff71133b133e2e2e80f5319f339e
Esteban Diaz <ediazpan@mines.edu>
2013-04-23 20:17:52 +0000

    Former-commit-id: 1014fac3387f7240912735cd3e408e7208f1e100

--- user/ediazp/Mcorrelate.py
+++ user/ediazp/Mcorrelate.py
@@ -9,10 +9,23 @@ It implements corr(a,b) along the fast axis
 a [file] : is taken from stdin
 b [file] : is taken from  signal
 
+As for now, it requires both files to have the 
+same sampling interval
 '''
+
+
 import rsf.api as rsf
 import numpy as np
 import sys
+
+def normalize(a,b):
+  a0 = np.sqrt((a**2).sum())
+  b0 = np.sqrt((b**2).sum())
+  s = 1./(a0*b0)
+
+  return s
+
+
 
 par = rsf.Par()
 
@@ -22,51 +35,45 @@ Fout = rsf.Output()
 
 # pars from command line
 ntau = par.int("ntau",100) # number of tau lags 
+norm = par.bool("norm",False) # if True scale correlation by 1/(sqrt(corr(a,a)[0])*sqrt(corr(b,b)[0])) so the correlation goes from -1,1
+print norm
+
+
 
 # parameters from input file
-n1 = Fa.int("n1")    # number of samples from input
+n1a = Fa.int("n1")    # number of samples from input
+n1b = Fb.int("n1")    # number of samples from input
+d1a = Fa.float("d1")  # dt
+d1b = Fb.float("d1")  # dt
 
-d1 = Fa.float("d1")  # dt
+if not d1a == d1b:
+  print  >> sys.stderr, 'both files are required to have the same sampling interval'
+  sys.exit(1)
 
-if ntau > n1a or ntau > n1b : ntau = min(n1a-1,n1b-1)
+
+if ntau > n1a or ntau > n1b: ntau = min(n1a-1,n1b-1) 
 
 ntraces = Fa.size(1) # number of traces of input file
 
-
-
-#######################################################################
-# Check that files have the same sampling interval and geometry
-if not ntraces == Fb.size(1):
-  sys.stderr.write('error! :input and signal do not have the same number\
-                    of traces\n')
-  sys.exit(1)
-
-if not d1 == Fb.float("d1"):
-  sys.stderr.write('error! :input and signal do not have the same \
-                    sampling interval\n')
-  sys.exit(1)
-#######################################################################
-  
-
-  
-
-a = np.zeros(n1,'f')
-b = np.zeros(n1,'f')
-
-center = (n1*n1-1)/2 # center sample of the correlation
+a = np.zeros(n1a,'f')
+b = np.zeros(n1b,'f')
+center = (n1a+n1b-1)/2 -(n1a-n1b)*0.5 # center sample of the correlation
 l1 = center-ntau
 l2 = center+ntau+1
 
 Fout.put("n1",ntau*2+1)
-Fout.put("o1",-ntau*d1)
-
+Fout.put("o1",-ntau*d1a)
 
 for i2 in range(ntraces):
   Fa.read(a)
   Fb.read(b)
   corr = np.correlate(a,b,mode='full')
   out = corr[l1:l2]
-  Fout.write(out)
+  if norm:
+    s = normalize(a,b)
+    Fout.write(out*s)
+  else:
+    Fout.write(out)
 
 Fa.close()
 Fb.close()
