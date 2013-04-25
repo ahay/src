3c370eda9dc928fbd6b29cc551ed9e7169c388e4
Karl Schleicher <k_schleicher@hotmail.com>
2013-04-25 12:40:44 +0000

    compute the index of each min and max header value.  Print in table
    Former-commit-id: 44cc13b7e5d123df6804be77054ca5215b93aeb1

--- system/seismic/Mheaderattr.c
+++ system/seismic/Mheaderattr.c
@@ -27,7 +27,8 @@ Only nonzero values are reported.
 
 int main(int argc, char* argv[])
 {
-    int i1, i2, n1, n2, *max=NULL, *min=NULL, *inp=NULL, imax, imin;
+    int i1, i2, n1, n2;
+    int *max=NULL, *min=NULL, *inp=NULL, *imax=NULL, *imin=NULL;
     double *mean=NULL;
     char pad[] = "                    ", out[21];
     sf_file head=NULL;
@@ -39,18 +40,27 @@ int main(int argc, char* argv[])
     if (!sf_histint(head,"n1",&n1)) sf_error("No n1= in input");
     n2 = sf_leftsize(head,1);
 
-    printf("******************************************* \n");
-    printf("%d headers, %d traces\n",n1,n2);
+    fprintf(stdout,"******************************************* \n");
+    fprintf(stdout,"%d headers, %d traces\n",n1,n2);
  
     inp = sf_intalloc(n1);
-    max = sf_intalloc(n1);
+
     min = sf_intalloc(n1);
+    max = sf_intalloc(n1);
+
+    imin = sf_intalloc(n1);
+    imax = sf_intalloc(n1);
+
     mean = (double*) sf_alloc(n1,sizeof(double));
 
     sf_intread(inp,n1,head);
-    imax = imin = 0;
     for (i1=0; i1 < n1; i1++) {
-	min[i1] = max[i1] = inp[i1];
+	min [i1] = inp[i1];
+	max [i1] = inp[i1];
+	
+	imin[i1] = 0;
+	imax[i1] = 0;
+
 	mean[i1] = inp[i1];
     }
 
@@ -58,32 +68,36 @@ int main(int argc, char* argv[])
 	sf_intread(inp,n1,head);
 	for (i1=0; i1 < n1; i1++) {
 	    if (min[i1] > inp[i1]) {
-		min[i1] = inp[i1];
-		imin = i2;
+		min [i1] = inp[i1];
+		imin[i1] = i2;
 	    }
 	    if (max[i1] < inp[i1]) {
-		max[i1] = inp[i1];
-		imax = i2;
+		max [i1] = inp[i1];
+		imax[i1] = i2;
 	    }
 	    mean[i1] += inp[i1];
 	}
     }
 
     segy_init(n1,head);
-
+    
+    /* put headers on table of numbers */
+    fprintf(stdout,"\n");
+    fprintf(stdout,"indx     key        indx         min       ");
+    fprintf(stdout,"indx         max           mean\n");
+    fprintf(stdout," key    name         min       value       ");
+    fprintf(stdout," max       value          value\n");
     for (i1=0; i1 < n1; i1++) {
 	if (min[i1] != 0 || max[i1] != 0) {
-	    snprintf(out,21,"key[%d]=\"%s\"",i1,segykeyword(i1));
-	    printf("%s%s",out,pad+strlen(out));
-	    snprintf(out,21,"min[%d]=%d",imin,min[i1]);
-	    printf("%s%s",out,pad+strlen(out));
-	    snprintf(out,21,"max[%d]=%d",imax,max[i1]);
-	    printf("%s%s",out,pad+strlen(out));
-	    printf("mean=%lg\n",mean[i1]/n2);
+	  fprintf(stdout,"%4d %8s %10d %11d %10d %11d %14e\n",
+		  i1,segykeyword(i1),
+		  imin[i1],min[i1],
+		  imax[i1],max[i1],
+		  mean[i1]/n2);
 	}
     }
 
-    printf("******************************************* \n");
+    fprintf(stdout,"******************************************* \n");
 
     exit(0);
 }
