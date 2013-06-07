015941a7e973791b9bb39141b002f7cc5004343b
Alexander Klokov <alexander.klokov@gmail.com>
2013-06-07 22:56:23 +0000

    Former-commit-id: 72ddc1fab8b90ed9b81b885e227914c5366c56d7

--- user/aklokov/Mrotvol.c
+++ user/aklokov/Mrotvol.c
@@ -20,22 +20,43 @@
 
 #include <rsf.h>
 
+int n1, n2, n3;
+float d1, d2, d3, o1, o2, o3;
+float* trace;
+sf_file inFile, outFile;
+/*void getTriangle (float x1, float y1, 	
+				  float* px1, float* py1,
+				  float* px2, float* py2,
+				  float* px3, float* py3) {
+	
+
+}*/
+
+void getTrace (int i2r, int i3r) {
+
+	const size_t posr = (i3r * n2 + i2r) * n1 * sizeof (float);
+	sf_seek (inFile, posr, SEEK_SET);		
+    sf_floatread (trace, n1, inFile);	
+	
+	return;
+}
+
+
 int main (int argc, char* argv[]) {
 
 // Initialize RSF 
     sf_init (argc, argv);
 // Input files
-    sf_file inFile = sf_input ("in");
+    inFile = sf_input ("in");
 // check that the input is float 
     if ( SF_FLOAT != sf_gettype (inFile) ) sf_error ("Need float input");
     /* input volume */
 // Output file
-    sf_file outFile = sf_output("out");
+    outFile = sf_output("out");
     /* output volume */
 
 // Depth/time axis 
-	int n1, n2, n3;
-	float d1, d2, d3, o1, o2, o3;
+
     if ( !sf_histint   (inFile, "n1", &n1) ) sf_error ("Need n1= in input");
     if ( !sf_histfloat (inFile, "d1", &d1) ) sf_error ("Need d1= in input");
     if ( !sf_histfloat (inFile, "o1", &o1) ) sf_error ("Need o1= in input");
@@ -47,11 +68,11 @@ int main (int argc, char* argv[]) {
     if ( !sf_histfloat (inFile, "o3", &o3) ) sf_error ("Need o3= in input");
 
 
-	float xf, zf, theta;			
+	float xf, yf, theta;			
     /* x-coord of the fixed point */
     if ( !sf_getfloat ("xf", &xf) ) xf = 0.f;
     /* z-coord of the fixed point */
-    if ( !sf_getfloat ("zf", &zf) ) zf = 0.f;
+    if ( !sf_getfloat ("yf", &yf) ) yf = 0.f;
     /* rotation angle */	
 	if ( !sf_getfloat ("theta", &theta) ) theta = 0.f;	
 
@@ -59,45 +80,33 @@ int main (int argc, char* argv[]) {
 	const float sint = sin (-thetaRad);
 	const float cost = cos (-thetaRad);
 
-	const float zLim = n1 - 1;
+	const float yLim = n3 - 1;
 	const float xLim = n2 - 1;
 
-	const int sliceSize = n1 * n2;
-    float* sliceIn  = sf_floatalloc (sliceSize);	
-    float* sliceOut = sf_floatalloc (sliceSize);	
+    trace  = sf_floatalloc (n1);	
 
 	for (int i3 = 0; i3 < n3; ++i3) {
-		// read data
-	    sf_floatread (sliceIn, sliceSize, inFile);					
-	
+		float y = o3 + i3 * d3 - yf;
 		for (int i2 = 0; i2 < n2; ++i2) {
 			float x = o2 + i2 * d2 - xf;
-			for (int i1 = 0; i1 < n1; ++i1) {
-				float z = o1 + i1 * d1 - zf;
 
-				float zr = cost * z - sint * x + zf;
-				const int i1r = (zr - o1) / d1;
-				if (i1r < 0 || i1r > zLim) continue;
+			for (int it = 0; it < n1; ++it)
+				trace[it] = 0.f;
 
-				float xr = sint * z + cost * x + xf;
-				const int i2r = (xr - o2) / d2;
-				if (i1r < 0 || i1r > xLim) continue;
+			float xr = cost * x - sint * y + xf;
+			const int i2r = (xr - o2) / d2;
+			float yr = sint * x + cost * y + yf;
+			const int i3r = (yr - o3) / d3;
 
-				const int indIn  = i1r + i2r * n1;			
-				const int indOut = i1 + i2 * n1;			
-				
-				sliceOut [indOut] = sliceIn [indIn];
-			}
+			if (i3r > 0 && i3r < yLim && i2r > 0 && i2r < xLim) 
+				getTrace (i2r, i3r);
+			
+			sf_floatwrite (trace, n1, outFile);					
 		}
-	    
-		sf_floatwrite (sliceOut, sliceSize, outFile);					
 	}
 
 	sf_fileclose (inFile);
     sf_fileclose (outFile);
-
-    free (sliceIn);
-    free (sliceOut);
 
     return 0;
 }
