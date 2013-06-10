9781259abfbb9b98fa828307b86eeaf65d8d764b
Alexander Klokov <alexander.klokov@gmail.com>
2013-06-10 14:33:25 +0000

    Former-commit-id: 4d520356b35aae2de5f5a6134acc3d780abeed83

--- user/aklokov/Mrotvol.c
+++ user/aklokov/Mrotvol.c
@@ -24,23 +24,112 @@ int n1, n2, n3;
 float d1, d2, d3, o1, o2, o3;
 float* trace;
 sf_file inFile, outFile;
-/*void getTriangle (float x1, float y1, 	
-				  float* px1, float* py1,
-				  float* px2, float* py2,
-				  float* px3, float* py3) {
+
+
+bool isPointInsideTriangle (float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3) {
+
+	double x01 = x0 - x1;
+	double y01 = y0 - y1;
+	if (fabs (x01) < 1e-6 && fabs (y01) < 1e-6) return true;
+
+	double x02 = x0 - x2;
+	double y02 = y0 - y2;
+	if (fabs (x02) < 1e-6 && fabs (y02) < 1e-6) return true;
+
+	double x03 = x0 - x3;
+	double y03 = y0 - y3;
+	if (fabs (x03) < 1e-6 && fabs (y03) < 1e-6) return true;
+
+	//
+		
+	double x31 = x3 - x1; 
+	double y31 = y3 - y1;
+
+	double x21 = x2 - x1;
+	double y21 = y2 - y1;
+
+	// dot products
+
+	double dot00 = x31*x31 + y31*y31;
+	double dot01 = x31*x21 + y31*y21;
+	double dot02 = x31*x01 + y31*y01;
+	double dot11 = x21*x21 + y21*y21;
+	double dot12 = x21*x01 + y21*y01;
+
+	// Compute barycentric coordinates
+	double invDenom = 1.0 / (dot00 * dot11 - dot01 * dot01);
+	double u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+	double v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+	// Check if point is in triangle
+	return (u >= 0) && (v >= 0) && (u + v < 1);
+}
+
+void getTrace (float xr, float yr, float* restrace) {
+
+	int ind1x = (xr - o2) / d2;
+	int ind1y = (yr - o3) / d3;
+	int ind2x = ind1x + 1;
+	int ind2y = ind1y;
+	int ind3x = ind1x;
+	int ind3y = ind1y + 1;
+
+	float x1 = ind1x * d2;
+	float y1 = ind1y * d3;
+	float x2 = (ind1x + 1) * d2;
+	float y2 = ind1y * d3;
+	float x3 = ind1x * d2;
+	float y3 = (ind1y + 1) * d3;
+
+	// check triangle
+	bool res = isPointInsideTriangle (xr, yr, x1, y1, x2, y2, x3, y3);
+	if (!res) {
+		ind1x += 1;
+		ind1y += 1;
+		x1 = ind1x * d2;
+		y1 = ind1y * d3;
+	}
+
+	const float x21 = x2 - x1;
+	const float x31 = x3 - x1;
+	const float y21 = y2 - y1;
+	const float y31 = y3 - y1;
+
+	const float denom3 = y31*x21 - x31*y21;
+	if (fabs (denom3) < 1e-9) return;
+	const float denom2 = y21*x31 - x21*y31;
+	if (fabs (denom2) < 1e-9) return;
 	
+	const float m3 = ( (yr - y1)*x21 - (xr - x1)*y21 ) / denom3;
+	const float m2 = ( (yr - y1)*x31 - (xr - x1)*y31 ) / denom2;
+	const float m1 = 1.f - m2 - m3;
+	
+	//
+	float* trace1 = sf_floatalloc (n1);	
+	float* trace2 = sf_floatalloc (n1);	
+	float* trace3 = sf_floatalloc (n1);	
 
-}*/
-
-void getTrace (float xr, float yr) {
-
-	const int i2r = (xr - o2) / d2;
-	const int i3r = (yr - o3) / d3;
-
-	const size_t posr = (i3r * n2 + i2r) * n1 * sizeof (float);
+	// trace1 
+	size_t posr = (ind1y * n2 + ind1x) * n1 * sizeof (float);
 	sf_seek (inFile, posr, SEEK_SET);		
-    sf_floatread (trace, n1, inFile);	
+	sf_floatread (trace1, n1, inFile);	
 	
+	posr = (ind2y * n2 + ind2x) * n1 * sizeof (float);
+	sf_seek (inFile, posr, SEEK_SET);		
+	sf_floatread (trace2, n1, inFile);	
+
+	posr = (ind3y * n2 + ind3x) * n1 * sizeof (float);
+	sf_seek (inFile, posr, SEEK_SET);		
+	sf_floatread (trace3, n1, inFile);	
+
+	for (int i = 0; i < n1; ++i) {
+		restrace[i] = m1 * trace1[i] + m2 * trace2[i] + m3 * trace3[i];
+	}
+
+	free (trace1);
+	free (trace2);
+	free (trace3);
+
 	return;
 }
 
@@ -103,7 +192,7 @@ int main (int argc, char* argv[]) {
 			float yr = sint * x + cost * y + yf;
 
 			if (yr > yMin && yr < yMax && xr > xMin && xr < xMax) 
-				getTrace (xr, yr);
+				getTrace (xr, yr, trace);
 			
 			sf_floatwrite (trace, n1, outFile);					
 		}
