eac532ae28ed04e26768233ab581be5066d4066b
Sergey Fomel <sergey.fomel@gmail.com>
2013-06-15 15:26:09 +0000

    compiling on icc
    Former-commit-id: 0b1aa18e93d383933a5cdc6cf5e3aa8496db5852

--- user/aklokov/Mrotvol.c
+++ user/aklokov/Mrotvol.c
@@ -26,126 +26,129 @@ sf_file inFile, outFile;
 
 bool isPointInsideTriangle (float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3) {
 
-	double x01 = x0 - x1;
-	double y01 = y0 - y1;
-	if (fabs (x01) < 1e-6 && fabs (y01) < 1e-6) return true;
+    double x01 = x0 - x1;
+    double y01 = y0 - y1;
+    if (fabs (x01) < 1e-6 && fabs (y01) < 1e-6) return true;
 
-	double x02 = x0 - x2;
-	double y02 = y0 - y2;
-	if (fabs (x02) < 1e-6 && fabs (y02) < 1e-6) return true;
+    double x02 = x0 - x2;
+    double y02 = y0 - y2;
+    if (fabs (x02) < 1e-6 && fabs (y02) < 1e-6) return true;
 
-	double x03 = x0 - x3;
-	double y03 = y0 - y3;
-	if (fabs (x03) < 1e-6 && fabs (y03) < 1e-6) return true;
+    double x03 = x0 - x3;
+    double y03 = y0 - y3;
+    if (fabs (x03) < 1e-6 && fabs (y03) < 1e-6) return true;
 
-	//
+    //
 		
-	double x31 = x3 - x1; 
-	double y31 = y3 - y1;
+    double x31 = x3 - x1; 
+    double y31 = y3 - y1;
 
-	double x21 = x2 - x1;
-	double y21 = y2 - y1;
+    double x21 = x2 - x1;
+    double y21 = y2 - y1;
 
-	// dot products
+    // dot products
 
-	double dot00 = x31*x31 + y31*y31;
-	double dot01 = x31*x21 + y31*y21;
-	double dot02 = x31*x01 + y31*y01;
-	double dot11 = x21*x21 + y21*y21;
-	double dot12 = x21*x01 + y21*y01;
+    double dot00 = x31*x31 + y31*y31;
+    double dot01 = x31*x21 + y31*y21;
+    double dot02 = x31*x01 + y31*y01;
+    double dot11 = x21*x21 + y21*y21;
+    double dot12 = x21*x01 + y21*y01;
 
-	// Compute barycentric coordinates
-	double invDenom = 1.0 / (dot00 * dot11 - dot01 * dot01);
-	double u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-	double v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+    // Compute barycentric coordinates
+    double invDenom = 1.0 / (dot00 * dot11 - dot01 * dot01);
+    double u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+    double v = (dot00 * dot12 - dot01 * dot02) * invDenom;
 
-	// Check if point is in triangle
-	return (u >= 0) && (v >= 0) && (u + v < 1);
+    // Check if point is in triangle
+    return (u >= 0) && (v >= 0) && (u + v < 1);
 }
 
 void getTrace (float xr, float yr, float* restrace) {
+    int i;
 
-	int ind1x = xr / d2;
-	int ind1y = yr / d3;
-	int ind2x = ind1x + 1;
-	int ind2y = ind1y;
-	int ind3x = ind1x;
-	int ind3y = ind1y + 1;
+    int ind1x = xr / d2;
+    int ind1y = yr / d3;
+    int ind2x = ind1x + 1;
+    int ind2y = ind1y;
+    int ind3x = ind1x;
+    int ind3y = ind1y + 1;
 
-	float x1 = ind1x * d2;
-	float y1 = ind1y * d3;
-	float x2 = (ind1x + 1) * d2;
-	float y2 = ind1y * d3;
-	float x3 = ind1x * d2;
-	float y3 = (ind1y + 1) * d3;
+    float x1 = ind1x * d2;
+    float y1 = ind1y * d3;
+    float x2 = (ind1x + 1) * d2;
+    float y2 = ind1y * d3;
+    float x3 = ind1x * d2;
+    float y3 = (ind1y + 1) * d3;
 
-	// check triangle
-	bool res = isPointInsideTriangle (xr, yr, x1, y1, x2, y2, x3, y3);
-	if (!res) {
-		ind1x += 1;
-		ind1y += 1;
-		x1 = ind1x * d2;
-		y1 = ind1y * d3;
-	}
+    // check triangle
+    bool res = isPointInsideTriangle (xr, yr, x1, y1, x2, y2, x3, y3);
+    if (!res) {
+	ind1x += 1;
+	ind1y += 1;
+	x1 = ind1x * d2;
+	y1 = ind1y * d3;
+    }
 
-	const float x21 = x2 - x1;
-	const float x31 = x3 - x1;
-	const float y21 = y2 - y1;
-	const float y31 = y3 - y1;
+    const float x21 = x2 - x1;
+    const float x31 = x3 - x1;
+    const float y21 = y2 - y1;
+    const float y31 = y3 - y1;
 
-	const float denom3 = y31*x21 - x31*y21;
-	if (fabs (denom3) < 1e-9) return;
-	const float denom2 = y21*x31 - x21*y31;
-	if (fabs (denom2) < 1e-9) return;
+    const float denom3 = y31*x21 - x31*y21;
+    if (fabs (denom3) < 1e-9) return;
+    const float denom2 = y21*x31 - x21*y31;
+    if (fabs (denom2) < 1e-9) return;
 	
-	const float m3 = ( (yr - y1)*x21 - (xr - x1)*y21 ) / denom3;
-	const float m2 = ( (yr - y1)*x31 - (xr - x1)*y31 ) / denom2;
-	const float m1 = 1.f - m2 - m3;
+    const float m3 = ( (yr - y1)*x21 - (xr - x1)*y21 ) / denom3;
+    const float m2 = ( (yr - y1)*x31 - (xr - x1)*y31 ) / denom2;
+    const float m1 = 1.f - m2 - m3;
 	
-	// interpolation
-	float* trace1 = sf_floatalloc (n1);	
-	float* trace2 = sf_floatalloc (n1);	
-	float* trace3 = sf_floatalloc (n1);	
+    // interpolation
+    float* trace1 = sf_floatalloc (n1);	
+    float* trace2 = sf_floatalloc (n1);	
+    float* trace3 = sf_floatalloc (n1);	
 
-	int xshift = o2 / d2 + 0.5;
-	int yshift = o3 / d3 + 0.5;
+    int xshift = o2 / d2 + 0.5;
+    int yshift = o3 / d3 + 0.5;
 
-	ind1x -= xshift;
-	ind2x -= xshift;
-	ind3x -= xshift;
-	ind1y -= yshift;
-	ind2y -= yshift;
-	ind3y -= yshift;
+    ind1x -= xshift;
+    ind2x -= xshift;
+    ind3x -= xshift;
+    ind1y -= yshift;
+    ind2y -= yshift;
+    ind3y -= yshift;
 
-	// trace1 
-	size_t posr = (ind1y * n2 + ind1x) * n1 * sizeof (float);
-	sf_seek (inFile, posr, SEEK_SET);		
-	sf_floatread (trace1, n1, inFile);	
-	// trace2
-	posr = (ind2y * n2 + ind2x) * n1 * sizeof (float);
-	sf_seek (inFile, posr, SEEK_SET);		
-	sf_floatread (trace2, n1, inFile);	
-	// trace3 
-	posr = (ind3y * n2 + ind3x) * n1 * sizeof (float);
-	sf_seek (inFile, posr, SEEK_SET);		
-	sf_floatread (trace3, n1, inFile);	
+    // trace1 
+    size_t posr = (ind1y * n2 + ind1x) * n1 * sizeof (float);
+    sf_seek (inFile, posr, SEEK_SET);		
+    sf_floatread (trace1, n1, inFile);	
+    // trace2
+    posr = (ind2y * n2 + ind2x) * n1 * sizeof (float);
+    sf_seek (inFile, posr, SEEK_SET);		
+    sf_floatread (trace2, n1, inFile);	
+    // trace3 
+    posr = (ind3y * n2 + ind3x) * n1 * sizeof (float);
+    sf_seek (inFile, posr, SEEK_SET);		
+    sf_floatread (trace3, n1, inFile);	
 
-	float m = m1 + m2 + m3;
+    float m = m1 + m2 + m3;
 
-	for (int i = 0; i < n1; ++i) {
-		restrace[i] = m1 * trace1[i] + m2 * trace2[i] + m3 * trace3[i];
+    for (i = 0; i < n1; ++i) {
+	restrace[i] = m1 * trace1[i] + m2 * trace2[i] + m3 * trace3[i];
 		
-	}
+    }
 
-	free (trace1);
-	free (trace2);
-	free (trace3);
+    free (trace1);
+    free (trace2);
+    free (trace3);
 
-	return;
+    return;
 }
 
 
-int main (int argc, char* argv[]) {
+int main (int argc, char* argv[]) 
+{
+    int i3, i2, it;
 
 // Initialize RSF 
     sf_init (argc, argv);
@@ -171,47 +174,47 @@ int main (int argc, char* argv[]) {
     if ( !sf_histfloat (inFile, "o3", &o3) ) sf_error ("Need o3= in input");
 
 
-	float xf, yf, theta;			
+    float xf, yf, theta;			
     if ( !sf_getfloat ("xf", &xf) ) xf = 0.f;
     /* inline-coord of the vertical axes */
     if ( !sf_getfloat ("yf", &yf) ) yf = 0.f;
     /* xline-coord of the vertical axes */
-	if ( !sf_getfloat ("theta", &theta) ) theta = 0.f;	
+    if ( !sf_getfloat ("theta", &theta) ) theta = 0.f;	
     /* rotation angle */	
 
-	const float thetaRad = theta * M_PI / 180.f;
-	const float sint = sin (-thetaRad);
-	const float cost = cos (-thetaRad);
+    const float thetaRad = theta * M_PI / 180.f;
+    const float sint = sin (-thetaRad);
+    const float cost = cos (-thetaRad);
 
-	// limits
-	const float yMin = o3;
-	const float yMax = o3 + (n3-1)*d3;
-	const float xMin = o2;
-	const float xMax = o2 + (n2-1)*d2;
+    // limits
+    const float yMin = o3;
+    const float yMax = o3 + (n3-1)*d3;
+    const float xMin = o2;
+    const float xMax = o2 + (n2-1)*d2;
 
     trace  = sf_floatalloc (n1);	
 
-	for (int i3 = 0; i3 < n3; ++i3) {
-		float y = o3 + i3 * d3 - yf;
-		for (int i2 = 0; i2 < n2; ++i2) {
-			float x = o2 + i2 * d2 - xf;
+    for (i3 = 0; i3 < n3; ++i3) {
+	float y = o3 + i3 * d3 - yf;
+	for (i2 = 0; i2 < n2; ++i2) {
+	    float x = o2 + i2 * d2 - xf;
 
-			for (int it = 0; it < n1; ++it)
-				trace[it] = 0.f;
+	    for (it = 0; it < n1; ++it)
+		trace[it] = 0.f;
 
-			float xr = cost * x - sint * y + xf;
-			float yr = sint * x + cost * y + yf;
+	    float xr = cost * x - sint * y + xf;
+	    float yr = sint * x + cost * y + yf;
 
-			if (yr > yMin && yr < yMax && xr > xMin && xr < xMax) 
-				getTrace (xr, yr, trace);
+	    if (yr > yMin && yr < yMax && xr > xMin && xr < xMax) 
+		getTrace (xr, yr, trace);
 			
-			sf_floatwrite (trace, n1, outFile);					
-		}
+	    sf_floatwrite (trace, n1, outFile);					
 	}
+    }
 
-	free (trace);
+    free (trace);
 
-	sf_fileclose (inFile);
+    sf_fileclose (inFile);
     sf_fileclose (outFile);
 
     return 0;
