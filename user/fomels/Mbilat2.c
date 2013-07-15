57a47cf4dcb21dfb40dbd714fb6cd5bd0317174a
Sergey Fomel <sergey.fomel@gmail.com>
2013-07-15 21:17:29 +0000

    pfd
    Former-commit-id: adead50270c39e3a83859fc57d6915064a2cd8e4

--- user/fomels/Mbilat2.c
+++ user/fomels/Mbilat2.c
@@ -21,7 +21,7 @@
 
 int main(int argc, char* argv[])
 {
-    int n1, n2, n12, i3, n3, i1, i2, r1, r2, k1, k2, j1, j2;
+    int n1, n2, n12, i3, n3, i1, i2, r1, r2, k1, k2, j1, j2, repeat, ir;
     float **dinp, **dout, **norm, a1, a2, a3, d1, d2, d0, d, delta1, delta2, delta3, gauss;
     sf_file inp, out;
     
@@ -50,43 +50,53 @@ int main(int argc, char* argv[])
     if (!sf_getfloat("a2",&a2)) a2=a1;   /* horizontal attenuation factor */
     if (!sf_getfloat("a3",&a3)) a3=0.0f; /* data attenuation factor */ 
 
+    if (!sf_getint("repeat",&repeat)) repeat=1; /* repeat the operation */
+
     for (i3=0; i3 < n3; i3++) {
 	sf_floatread(dinp[0],n12,inp);
 
-	for (i2=0; i2 < n2; i2++) {
-	    for (i1=0; i1 < n1; i1++) {
-		dout[i2][i1] = 0.0f;
-		norm[i2][i1] = 0.0f;
-		d0 = dinp[i2][i1];
+	for (ir=0; ir < repeat; ir++) {
 
-		for (k2=-r2; k2 <= r2; k2++) {
-		    j2 = i2+k2;
-		    if (j2 < 0 || j2 >= n2) continue;
-		    delta2 = (j2-i2)*d2;
-		    delta2 *= delta2;
+	    for (i2=0; i2 < n2; i2++) {
+		for (i1=0; i1 < n1; i1++) {
+		    dout[i2][i1] = 0.0f;
+		    norm[i2][i1] = 0.0f;
+		    d0 = dinp[i2][i1];
 
-		    for (k1=-r1; k1 <= r1; k1++) {
-			j1 = i1+k1;
-			if (j1 < 0 || j1 >= n1) continue;
-			delta1 = (j1-i1)*d1;
-			delta1 *= delta1;
+		    for (k2=-r2; k2 <= r2; k2++) {
+			j2 = i2+k2;
+			if (j2 < 0 || j2 >= n2) continue;
+			delta2 = (j2-i2)*d2;
+			delta2 *= delta2;
 
-			d = dinp[j2][j1];
-			delta3 = d-d0;
-			delta3 *= delta3;
+			for (k1=-r1; k1 <= r1; k1++) {
+			    j1 = i1+k1;
+			    if (j1 < 0 || j1 >= n1) continue;
+			    delta1 = (j1-i1)*d1;
+			    delta1 *= delta1;
 
-			gauss = expf(-a1*delta1-a2*delta2-a3*delta3);
+			    d = dinp[j2][j1];
+			    delta3 = d-d0;
+			    delta3 *= delta3;
 
-			dout[i2][i1] += d*gauss;
-			norm[i2][i1] += gauss;
+			    gauss = expf(-a1*delta1-a2*delta2-a3*delta3);
+
+			    dout[i2][i1] += d*gauss;
+			    norm[i2][i1] += gauss;
+			}
+		    }
+
+		    if (norm[i2][i1] > 0.0f) {
+			dinp[i2][i1] = dout[i2][i1]/norm[i2][i1]; 
+		    } else {
+			dinp[i2][i1] = 0.0f;
 		    }
 		}
-
-		if (norm[i2][i1] > 0.0f) dout[i2][i1] /= norm[i2][i1];
 	    }
+
 	}
 	
-	sf_floatwrite(dout[0],n12,out);
+	sf_floatwrite(dinp[0],n12,out);
     }
 
     exit(0);
