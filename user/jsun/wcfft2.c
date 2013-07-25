8d2bf03dbd491f52a53e67c20c6683df4552d09c
Junzhe Sun <sunjzhe@gmail.com>
2013-07-25 20:04:05 +0000

    1
    Former-commit-id: 8753250604a1934f3726ad45b26c4e0be65e7cff

--- user/jsun/wcfft2.c
+++ user/jsun/wcfft2.c
@@ -20,64 +20,34 @@
 #ifdef _OPENMP
 #include <omp.h>
 #endif
-#ifdef SF_HAS_FFTW
+
 #include <fftw3.h>
-#endif
+
 
 static int n1, n2, nk;
 static float wt;
 
 static sf_complex **cc;
 
-#ifdef SF_HAS_FFTW
-static fftwf_plan cfg=NULL, icfg=NULL;
-#else
-static kiss_fft_cfg cfg1, icfg1, cfg2, icfg2;
-static kiss_fft_cpx **tmp, *ctrace2;
-static sf_complex *trace2;
-#endif
 
-int cfft2w_init(int pad1           /* padding on the first axis */,
+static fftwf_plan cfg=NULL, icfg=NULL;
+
+int wcfft2_init(int pad1           /* padding on the first axis */,
 	       int nx,   int ny   /* input data size */, 
 	       int *nx2, int *ny2 /* padded data size */)
 /*< initialize >*/
 {
-#ifdef SF_HAS_FFTW
 #ifdef _OPENMP
     fftw_init_threads();
-    sf_warning("Using threaded FFTW3! \n");
-#endif
-#endif
-
-#ifndef SF_HAS_FFTW
-    int i2;
+    //sf_warning("Using threaded FFTW3! \n");
 #endif
 
     nk = n1 = kiss_fft_next_fast_size(nx*pad1);
     
-#ifndef SF_HAS_FFTW
-    cfg1  = kiss_fft_alloc(n1,0,NULL,NULL);
-    icfg1 = kiss_fft_alloc(n1,1,NULL,NULL);
-#endif
-  
     n2 = kiss_fft_next_fast_size(ny);
 
     cc = sf_complexalloc2(n1,n2);
     
-#ifndef SF_HAS_FFTW
-    cfg2  = kiss_fft_alloc(n2,0,NULL,NULL);
-    icfg2 = kiss_fft_alloc(n2,1,NULL,NULL);
- 	
-    tmp =    (kiss_fft_cpx **) sf_alloc(n2,sizeof(*tmp));
-    tmp[0] = (kiss_fft_cpx *)  sf_alloc(nk*n2,sizeof(kiss_fft_cpx));
-    for (i2=0; i2 < n2; i2++) {
-	tmp[i2] = tmp[0]+i2*nk;
-    }
-	
-    trace2 = sf_complexalloc(n2);
-    ctrace2 = (kiss_fft_cpx *) trace2;
-#endif
-
     *nx2 = n1;
     *ny2 = n2;
 	
@@ -86,13 +56,12 @@ int cfft2w_init(int pad1           /* padding on the first axis */,
     return (nk*n2);
 }
 
-void cfft2w(sf_complex *inp /* [n1*n2] */, 
+void wcfft2(sf_complex *inp /* [n1*n2] */, 
 	   sf_complex *out /* [nk*n2] */)
 /*< 2-D FFT >*/
 {
     int i1, i2;
 
-#ifdef SF_HAS_FFTW
 #ifdef _OPENMP
     fftw_plan_with_nthreads(omp_get_max_threads());
 #endif
@@ -103,71 +72,36 @@ void cfft2w(sf_complex *inp /* [n1*n2] */,
 			      FFTW_FORWARD, FFTW_MEASURE);
       if (NULL == cfg) sf_error("FFTW failure.");
     }
-#endif
 
     /* FFT centering */
     for (i2=0; i2<n2; i2++) {
 	for (i1=0; i1<n1; i1++) {
 		cc[i2][i1] = ((i2%2==0)==(i1%2==0))? inp[i2*n1+i1]:-inp[i2*n1+i1];
-	  /*
-#ifdef SF_HAS_COMPLEX_H
-		cc[i2][i1] = ((i2%2==0)==(i1%2==0))? inp[i2*n1+i1]:(-1*inp[i2*n1+i1]);
-#else
-		cc[i2][i1] = ((i2%2==0)==(i1%2==0))? inp[i2*n1+i1]:sf_cneg(inp[i2*n1+i1]);
-#endif
-	  */
 	}
     }
 
-#ifdef SF_HAS_FFTW
     fftwf_execute(cfg);
-#else	
-    for (i2=0; i2 < n2; i2++) {
-	kiss_fft_stride(cfg1,(kiss_fft_cpx *) cc[i2],tmp[i2],1);
-    }
 
-    for (i1=0; i1 < nk; i1++) {
-	kiss_fft_stride(cfg2,tmp[0]+i1,ctrace2,nk);
-	for (i2=0; i2<n2; i2++) {
-	    out[i2*nk+i1] = trace2[i2];
-	}
-    }
-#endif
 }
 
-void icfft2w_allocate(sf_complex *inp /* [nk*n2] */)
+void iwcfft2_allocate(sf_complex *inp /* [nk*n2] */)
 /*< allocate inverse transform >*/
 {
-#ifdef SF_HAS_FFTW
     icfg = fftwf_plan_dft_2d(n2,n1,
 			     (fftwf_complex *) inp, 
 			     (fftwf_complex *) cc[0],
 			     FFTW_BACKWARD, FFTW_MEASURE);
     if (NULL == icfg) sf_error("FFTW failure.");
-#endif
 }
 
-void icfft2w(sf_complex *out /* [n1*n2] */, 
+void iwcfft2(sf_complex *out /* [n1*n2] */, 
 	    sf_complex *inp /* [nk*n2] */)
 /*< 2-D inverse FFT >*/
 {
     int i1, i2;
 
-#ifdef SF_HAS_FFTW
     fftwf_execute(icfg);
-#else
-    for (i1=0; i1 < nk; i1++) {
-	kiss_fft_stride(icfg2,(kiss_fft_cpx *) (inp+i1),ctrace2,nk);
-		
-	for (i2=0; i2<n2; i2++) {
-	    tmp[i2][i1] = ctrace2[i2];
-	}
-    }
-    for (i2=0; i2 < n2; i2++) {
-	kiss_fft_stride(icfg1,tmp[i2],(kiss_fft_cpx *) cc[i2],1);
-    }
-#endif
-    
+
     /* FFT centering and normalization*/
     for (i2=0; i2<n2; i2++) {
 	for (i1=0; i1<n1; i1++) {
@@ -180,12 +114,10 @@ void icfft2w(sf_complex *out /* [n1*n2] */,
     }
 }
 
-void cfft2w_finalize()
+void wcfft2_finalize()
 /*< clean up fftw >*/
 {
-#ifdef SF_HAS_FFTW
 #ifdef _OPENMP
     fftw_cleanup_threads();
-#endif
 #endif
 }
