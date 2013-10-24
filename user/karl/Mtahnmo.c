23318d17dd9885d72bab808ff499c3cde13d07cb
Karl Schleicher <k_schleicher@hotmail.com>
2013-10-24 21:49:15 +0000

    initial version.  Like Mtahgethw.c
    Former-commit-id: 3e78ccffc7a3a1a5f3bbd6f5f1599d38fff32a49

---
+++ user/karl/Mtahnmo.c
@@ -0,0 +1,157 @@
+/* 
+   tahnmo: Trace And Header normal moveout.
+
+   tah is the abbreviation of Trace And Header.  It identifies a group of
+   programs designed to:
+   1- read trace and headers from separate rsf files and write them to 
+      standard output
+   2- filter programs that read and write standard input/output and process 
+      the tah data
+   3- read tah data from standard input and write separate rsf files for the
+      trace and headers data
+
+   These programs allow Seismic Unix (su) like processing in Madagascar.  
+   Some programs have su like names.
+
+   Some programs in this suite are sf_tahread, sf_tahgethw, f_tahhdrmath, 
+   and sf_tahwrite.
+*/
+/*
+   Program change history:
+   date       Who             What
+   10/23/2012 Karl Schleicher Original program
+*/
+#include <string.h>
+#include <rsf.h>
+#include <rsfsegy.h>
+
+#include "tahsub.c"
+
+/* very sparingly make some global variables. */
+int verbose;
+
+int main(int argc, char* argv[])
+{
+  int verbose;
+  sf_file in=NULL, out=NULL;
+  int n1_traces;
+  int n1_headers;
+
+  char* header_format=NULL;
+  sf_datatype typehead;
+  /* kls do I need to add this?  sf_datatype typein; */
+  float* fheader=NULL;
+  float* intrace=NULL;
+  int numkeys;
+  int ikey;
+  char** list_of_keys;
+  int *indx_of_keys;
+  int n_traces;
+
+  
+  sf_init (argc,argv);
+
+   /*****************************/
+  /* initialize verbose switch */
+  /*****************************/
+  /* verbose flag controls ammount of print */
+  /*( verbose=1 0 terse, 1 informative, 2 chatty, 3 debug ) */
+  /* fprintf(stderr,"read verbose switch.  getint reads command line.\n"); */
+  if(!sf_getint("verbose",&verbose))verbose=1;
+  fprintf(stderr,"verbose=%d\n",verbose);
+ 
+  /******************************************/
+  /* input and output data are stdin/stdout */
+  /******************************************/
+
+  if(verbose>0)fprintf(stderr,"read in file name\n");  
+  in = sf_input ("in");
+
+  if(verbose>0)fprintf(stderr,"read out file name\n");
+  out = sf_output ("out");
+
+  if (!sf_histint(in,"n1_traces",&n1_traces))
+    sf_error("input data not define n1_traces");
+  if (!sf_histint(in,"n1_headers",&n1_headers)) 
+    sf_error("input data does not define n1_headers");
+
+  header_format=sf_histstring(in,"header_format");
+  if(strcmp (header_format,"native_int")==0) typehead=SF_INT;
+  else                                       typehead=SF_FLOAT;
+
+  if(verbose>0)fprintf(stderr,"allocate headers.  n1_headers=%d\n",n1_headers);
+  fheader = sf_floatalloc(n1_headers);
+ 
+  if(verbose>0)fprintf(stderr,"allocate intrace.  n1_traces=%d\n",n1_traces);
+  intrace= sf_floatalloc(n1_traces);
+
+  if(verbose>0)fprintf(stderr,"call list of keys\n");
+ 
+  list_of_keys=sf_getnstring("key",&numkeys);
+  if(list_of_keys==NULL)
+    sf_error("The required parameter \"key\" was not found.");
+  /* I wanted to use sf_getstrings, but it seems to want keys seperated
+     with :'s instenad of ,'s */
+  /*
+  numkeys=sf_getnumpars("key");
+  if(numkeys==0)
+    sf_error("The required parameter \"key\" was not found.");
+  fprintf(stderr,"alloc list_of_keys numkeys=%d\n",numkeys);
+  list_of_keys=(char**)sf_alloc(numkeys,sizeof(char*)); 
+  sf_getstrings("key",list_of_keys,numkeys);
+  */
+  /* print the list of keys */
+  if(verbose>1){
+    fprintf(stderr,"numkeys=%d\n",numkeys);
+    for(ikey=0; ikey<numkeys; ikey++){
+      fprintf(stderr,"list_of_keys[%d]=%s\n",ikey,list_of_keys[ikey]);
+    }
+  }
+  
+/* maybe I should add some validation that n1== n1_traces+n1_headers+2
+     and the record length read in the second word is consistent with 
+     n1.  */
+
+  /* put the history from the input file to the output */
+  sf_fileflush(out,in);
+
+  /* segy_init gets the list header keys required by segykey function  */
+  segy_init(n1_headers,in);
+  indx_of_keys=sf_intalloc(numkeys);
+  for (ikey=0; ikey<numkeys; ikey++){
+    /* kls need to check each of these key names are in the segy header and
+       make error message for invalid keys.  Of does segykey do this? NO, just
+       segmentation fault. */
+    indx_of_keys[ikey]=segykey(list_of_keys[ikey]);
+  }
+
+
+
+  /***************************/
+  /* start trace loop        */
+  /***************************/
+  if(verbose>0)fprintf(stderr,"start trace loop\n");
+  while (0==sf_get_tah(intrace, fheader, n1_traces, n1_headers, in)){
+    if(verbose>1)fprintf(stderr,"process the tah in sftahgethw\n");
+    /* process the tah. */
+    /* this program prints selected header keys */
+      for(ikey=0; ikey<numkeys; ikey++){
+	fprintf(stderr," %s=",list_of_keys[ikey]);
+	if(typehead == SF_INT){
+	  /* just cast the header to int so the print works */
+	  fprintf(stderr,"%d",((int*)fheader)[indx_of_keys[ikey]]);
+	} else {
+	  fprintf(stderr,"%f",fheader[indx_of_keys[ikey]]);
+	}
+      } /* end of the for(ikey..) loop */
+      fprintf(stderr,"\n");
+      /***************************/
+      /* write trace and headers */
+      /***************************/
+      sf_put_tah(intrace, fheader, n1_traces, n1_headers, out);
+  }
+
+  exit(0);
+}
+
+  
