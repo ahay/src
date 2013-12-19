cfeeb85b8e11ca4bfea84b805e597615d79d9599
Pengliang Yang <ypl.2100@gmail.com>
2013-12-19 20:34:12 +0000

    MWNI with shaping
    Former-commit-id: 9d3088b3776fe356391a2bf047593cf79f64147c

--- user/pyang/Mmwni3d.c
+++ user/pyang/Mmwni3d.c
@@ -1,8 +1,9 @@
-/* bandlimited minimum weighted-norm interpolation (MWNI) 
- implemented with conjugate gradient least squares (CGLS) method
+/* Minimum weighted-norm interpolation (MWNI) with shaping,
+ implemented with conjugate gradient least squares (CGLS) method using
+ shaping regularization
 */
 /*
-  Copyright (C) 2013  Xi'an Jiaotong University, UT Austin (Pengliang Yang)
+  Copyright (C) 2013 Pengliang Yang, Xi'an Jiaotong University, UT Austin
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -20,54 +21,21 @@
 */
 #include <rsf.h>
 #include <math.h>
-#include <complex.h>
-#include <fftw3.h>
 
 #ifdef _OPENMP
 #include <omp.h>
 #endif
 
-
-void apply_mask(float *mask, sf_complex *before, sf_complex *after, int n1, int n2, int n3)
-/*< apply the mask >*/
-{
-	for(int i3=0;i3<n3;i3++)	
-	for(int i2=0;i2<n2;i2++)
-	{
-		if (mask[i2+i3*n2]){			
-		    for(int i1=0; i1<n1; i1++) after[i1+n1*(i2+n2*i3)]=before[i1+n1*(i2+n2*i3)];
-		}else{		
-		    for(int i1=0; i1<n1; i1++) after[i1+n1*(i2+n2*i3)]=0.0;			
-		}
-	}
-}
-
-void lowpass_filter(sf_complex *tmp, int n1, int n2, int n3)
-/*< only low-wavenumber preserved >*/
-{
-	for(int i3=0;i3<n3;i3++){
-		for(int i2=0;i2<n2;i2++){			
-	    	    if( (i3>SF_NINT(n3*0.2) && i3<SF_NINT(n3*0.8)) ||
-			(i2>SF_NINT(n2*0.2) && i2<SF_NINT(n2*0.8)) ) {	
-			for(int i1=0; i1<n1; i1++) 
-				tmp[i1+n1*(i2+n2*i3)]=0.0;
-		    }	
-		}
-	}
-}
-
+#include "mwni3d.h"
 
 int main(int argc, char* argv[])
 {
     /* input and output variables */
     bool verb;
-    int niter; 
-    float tol;
+    int niter,dim, n1, n2, n3, rect[3], ndat[3];
+    char key[6];
+    float tol, *din, *mask;
     sf_file Fin, Fout, Fmask;/* mask and I/O files*/ 
-
-    /* define temporary variables */
-    int n1,n2,n3;
-    float *din, *mask;
 
     sf_init(argc,argv);	/* Madagascar initialization */
 #ifdef _OPENMP
@@ -90,87 +58,24 @@ int main(int argc, char* argv[])
     if (!sf_histint(Fin,"n1",&n1)) sf_error("No n1= in input");
     if (!sf_histint(Fin,"n2",&n2)) sf_error("No n2= in input");
     if (!sf_histint(Fin,"n3",&n3)) sf_error("No n3= in input");
+    ndat[0]=n1; ndat[1]=n2; ndat[2]=n3;   dim=3;
+    for (int i=0; i < dim; i++) {
+	snprintf(key,6,"rect%d",i+1);
+	if (!sf_getint(key,rect+i)) rect[i]=1;/*( rect#=(1,1,...) smoothing radius on #-th axis )*/ 
+    }
 
     /* allocate data and mask arrays */
     din=sf_floatalloc(n1*n2*n3); sf_floatread(din,n1*n2*n3,Fin);
     if (NULL != sf_getstring("mask")){
 	mask=sf_floatalloc(n2*n3);
-	sf_floatread(mask,n2*n3,Fmask);
+	sf_floatread(mask, n2*n3, Fmask);
     }
 
-    /*=========================== conjugate gradient iterations ================= */
-    float rho0, rho_old, rho_new, beta, alpha;
-    fftwf_complex *m, *r, *sm, *sr, *gm, *gr;
-    fftwf_plan fft3, ifft3;/* execute plan for FFT and IFFT */
+    mwni3d_init(dim, ndat, rect, mask, tol, verb);
+    mwni3d_inversion(din, din, niter);
+    mwni3d_close();
 
-    m=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*n1*n2*n3);
-    r=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*n1*n2*n3);
-    gm=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*n1*n2*n3);
-    gr=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*n1*n2*n3);
-    sm=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*n1*n2*n3);
-    sr=(fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*n1*n2*n3);
-    fft3=fftwf_plan_dft_3d(n1,n2,n3,gm,gm,FFTW_FORWARD,FFTW_MEASURE);	
-    ifft3=fftwf_plan_dft_3d(n1,n2,n3,gm,gm,FFTW_BACKWARD,FFTW_MEASURE);
-
-    // initialization
-    for(int i=0; i<n1*n2*n3; i++) 
-    {
-	r[i]=-din[i];
-	m[i]=0.0;
-	sm[i]=0.0;
-	sr[i]=0.0;
-	gm[i]=0.0;
-	gr[i]=0.0;
-    }
-
-    // conjugate gradient loop
-    for(int iter=0;iter<niter;iter++)
-    {
-	apply_mask(mask, r, gm, n1, n2, n3); 		// gm=M.*r
-	fftwf_execute(ifft3);				// gm=ifft3(M.*r);
-	for(int i=0;i<n1*n2*n3;i++) gm[i]/=sqrtf(n1*n2*n3);
-	lowpass_filter(gm, n1, n2, n3);			// gm=p.*fft3(M.*r);
-	fftwf_execute(fft3);				// gm=fft3(gm);
-	for(int i=0;i<n1*n2*n3;i++) gm[i]/=sqrtf(n1*n2*n3);
-	apply_mask(mask, gm, gr, n1, n2, n3); 		// gr=M.*fft3(gm);
-	rho_new=cblas_scnrm2 (n1*n2*n3, gm, 1);		// rho_new=sum(abs(gm(:)).^2);
-
-	if (iter == 0) {
-	    beta=0.0;
-	    rho0=rho_new;
-	} else {
-	    beta=rho_new/rho_old;
-	    if(beta<tol ||rho_new/rho0<tol) break;
-	    for(int i=0;i<n1*n2*n3;i++){ 
-		sm[i]*=beta; sm[i]+=gm[i];		// sm=gm+beta*sm;
-		sr[i]*=beta; sr[i]+=gr[i];		// sr=gr+beta*sr;
-	    }		
-	    float sr2=cblas_scnrm2 (n1*n2*n3, sr, 1);	// sr2=sum(abs(sr(:)).^2);
-	    alpha=-rho_new/sr2; 			// alpha=-rho_new/sum(abs(sr(:)).^2);
-	    for(int i=0;i<n1*n2*n3;i++){ 
-		m[i]+=alpha*sm[i];			// m=m+alpha*sm;
-		r[i]+=alpha*sr[i];			// r=r+alpha*sr;
-	    }	
-	}
-	float res=cblas_scnrm2 (n1*n2*n3, r, 1);	// sr2=sum(abs(sr(:)).^2);
-	if (verb) sf_warning("iteration %d of %d res %g", iter+1, niter, res);
-	rho_old=rho_new;
-    }
-    for(int i=0; i<n1*n2*n3; i++) gm[i]=m[i];    	// copy m(:) to g(:)
-    fftwf_execute(fft3);			 	// gm=fft3(m);
-    for(int i=0; i<n1*n2*n3; i++) din[i]=crealf(gm[i])/sqrt(n1*n2*n3);	// drec=real(ifft2(m)*n1*n2);
-
-    fftwf_free(m);
-    fftwf_free(r);
-    fftwf_free(gm);
-    fftwf_free(gr);
-    fftwf_free(sm);
-    fftwf_free(sr);
-    fftwf_destroy_plan(fft3);
-    fftwf_destroy_plan(ifft3);
-    /* ============================================================================= */
-
-    sf_floatwrite(din,n1*n2*n3,Fout); /* output reconstructed seismograms */
+    sf_floatwrite(din, n1*n2*n3, Fout); /* output reconstructed seismograms */
 
     sf_close();
     exit(0);
