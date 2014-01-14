2804256cdcd8708efa92495959663c75230cd8e2
Karl Schleicher <k_schleicher@hotmail.com>
2014-01-14 05:18:50 +0000

    update documentation
    Former-commit-id: 1db404432667adfad7da7ef5e771ce5e35affab0

--- user/karl/Mtahstack.c
+++ user/karl/Mtahstack.c
@@ -1,20 +1,63 @@
-/* tahmakeskey: Trace And Header MAKE Secondary KEY.
+/* Read Trace And Header (tah) from STDIN, stack by mathing header key
 
-   tah is the abbreviation of Trace And Header.  It identifies a group of
-   programs designed to:
-   1- read trace and headers from separate rsf files and write them to 
-      standard output
-   2- filter programs that read and write standard input/output and process 
-      the tah data
-   3- read tah data from standard input and write separate rsf files for the
-      trace and headers data
+tah is the abbreviation of Trace And Header.  Madagascar programs 
+that begin with sftah are a designed to:
+1- read trace and headers from separate rsf files and write them to 
+   standard output (ie sftahread)
+2- filter programs that read and write standard input/output and 
+   process the tah data (eg sftahnmo, sftahstack)
+3- read tah data from standard input and write separate rsf files for 
+   the trace and headers data (ie sftahwrite)
 
-   These programs allow Seismic Unix (su) like processing in Madagascar.  
-   Some programs have su like names.
+These programs allow Seismic Unix (su) like processing in Madagascar.  
+Some programs have su like names.
 
-   Some programs in this suite are sf_tahread, sf_tahgethw, f_tahhdrmath, 
-   and sf_tahwrite.
- */
+Some programs in this suite are sftahread, sftahgethw, ftahhdrmath, 
+and sftahwrite.
+
+The sftahstack program is designed to stack sorted data. Trace and 
+header data (tah) are read from from standard input (usually a pipe).
+Gathers to be stacked are defined by the input parameter \'key\'.
+The key parameter is a list of header keys to watch.  A new gather 
+starts when one of the header keys change.  On each gather the stack
+trace is initialized to zero, traces in the gather are summed, and
+the stack trace is divided by the time variant fold, and the stacked
+trace written to output.  The output trace header is taken from the
+first trace in the input gather.    
+
+EXAMPLE:
+
+sftahread \\
+   verbose=1 \\
+   input=npr3_gathers.rsf \\
+| sftahnmo \\
+   verbose=1  \\
+   tnmo=0,.373,.619,.826,.909,1.017,1.132,1.222,1.716,3.010 \\
+   vnmo=9086,10244,11085,10803,10969,11578,12252,12669,14590,17116 \\
+| sftahstack key=iline,xline verbose=1 \\
+| sftahwrite \\
+   verbose=1                           \\
+   label2="xline" o2=1 n2=188 d2=1   \\
+   label3="iline" o3=1 n3=345 d3=1   \\
+   output=mappedstack.rsf \\
+>/dev/null
+
+sfgrey <mappedstack.rsf | sfpen
+
+In this example the cmp sorted prestack data, npr3_gathers.rsf,  are 
+read by sftahread.  The headers are in the file npr3_gathers_hdr.rsf, 
+the headers parameter default.  The headers are merged with the trace 
+amplitudes and the tah data sent down the pipe for nmo and stack.  The
+sftahstack program uses both the iline and xline keys to determine
+which traces blong to a gather.  Using both keys avoids a problem on 
+edges of a survey when uising xline along may merge gathers across 
+ilines (a special case that does sometimes happen). sftahwrite writes
+the trace data to mappedstack.rsf and the headers are written to the
+file mappedstack_hdr.rsf.  The order of the data in the output file
+is defined by the iline and xline trace headers, so the  data order
+is (time,xline,iline).  Finally, the output volume is displayed using
+sfgrey.
+*/
 /*
    Program change history:
    date       Who             What
