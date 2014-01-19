db9369bbbbacbfa377b6d0d55cc49f0d79415b74
Pengliang Yang <ypl.2100@gmail.com>
2014-01-19 15:25:24 +0000

    staggered grid fd coefficients calculation file
    Former-commit-id: cb4aea51d340103013fa9cf171374e2b423b2d27

---
+++ user/pyang/staggered_fdcoeff.m
@@ -0,0 +1,35 @@
+function c=staggered_fdcoeff(NJ)
+% compute 2*N-order staggered-grid finite difference coefficients (NJ=2N)
+% Example:
+%     format long
+%     NJ=10;
+%     c=FindCoeff(NJ)
+% 
+% Note: This code is devoted alongside the manuscript of P.L. Yang et al, 
+% Using the effective boundary saving strategy in GPU-based RTM programming
+
+N=NJ/2;
+x=zeros(N,1);
+b=zeros(N,1);   b(1)=1;
+c=b;
+for k=1:N
+    x(k)=(2*k-1)^2;
+end
+
+for k=1:N-1
+    for i=N:-1:k+1
+        b(i)=b(i)-x(k)*b(i-1);
+    end
+end
+
+for k=N-1:-1:1
+    for i=k+1:N
+        b(i)=b(i)/(x(i)-x(i-k));
+    end
+    for i=k:N-1
+        b(i)=b(i)-b(i+1);
+    end
+end
+for k=1:N
+    c(k)=b(k)/(2*k-1);
+end
