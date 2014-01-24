133e016dc7eb15ceea8d2be8dd98235984447c1f
Sergey Fomel <sergey.fomel@gmail.com>
2014-01-24 16:51:01 +0000

    compilation
    Former-commit-id: d61d34461b192a5d9abf1f842dd667efc7b019fa

--- user/chengjb/kykxkztaper.c
+++ user/chengjb/kykxkztaper.c
@@ -22,10 +22,9 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-
+#include <rsf.h>
 
 #include "_cjb.h"
-#include <rsf.h>
 
 /* The tapering operators is borrowed from J. Yan */
 
