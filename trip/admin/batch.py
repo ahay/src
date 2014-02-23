bbd026faf8ef2c242ae4719e817960f0853c5f34
William Symes <symes@caam.rice.edu>
2014-02-23 13:54:43 +0000

    more
    Former-commit-id: b0f2edc0ca4a77bafe1e4727605f2dc081e6add0

--- trip/admin/batch.py
+++ trip/admin/batch.py
@@ -300,7 +300,9 @@ def getBatchAttributes(batch):
 
 def getThreads(dict):
     nthread=1
-    if ('ppn' in dict.keys()) nthread = nthread * int(dict['ppn'])
-    if ('nodes' in dict.keys()) nthread = nthread * int(dict['nodes])
+    if ('ppn' in dict.keys()):
+        nthread = nthread * int(dict['ppn'])
+    if ('nodes' in dict.keys()):
+        nthread = nthread * int(dict['nodes'])
     return str(nthread)
                                                        
