a80f7a2e70f35c938792628cf9f3283748841ff2
Yin Huang <maggiehuangyin@gmail.com>
2014-06-27 20:23:17 +0000

    add helm as a grid operator
    Former-commit-id: cfc9689c48633df78f8922d8eda633a27757c4fb

---
+++ trip/iwave/grid/include/helmop.hh
@@ -0,0 +1,329 @@
+#ifndef __TSOPT_HELMOP__
+#define __TSOPT_HELMOP__
+
+#include "linop_base.hh"
+#include "space.hh"
+#include "rnmat.hh"
+
+#ifdef IWAVE_USE_MPI
+#include "mpigridpp.hh"
+#else
+#include "gridpp.hh"
+#endif
+
+#include <gridio.h>
+#include <f2c.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+/* helmholtz power function */
+extern "C" void helm_(int,         /* bc */
+                      integer *,   /* n1 */
+                      integer *,   /* n2 */
+                      float *,     /* d1 */
+                      float *,     /* d2 */
+                      float *,     /* w1 */
+                      float *,     /* w2 */
+                      float *,     /* p */
+                      float *,     /* datum */
+                      float *,     /* data in */
+                      float *,     /* data out */
+                      float *,     /* workspace */
+                      integer *,   /* length of workspace */
+                      integer *    /* error flag */
+);
+
+/* lenwork must be > 6*n1*n2+3*max(n2,2*n1)+21 */
+
+
+namespace TSOpt {
+    
+    using TSOpt::GridSpace;
+
+    using namespace RVL;
+    using namespace RVLAlg;
+    using namespace RVLUmin;
+    using RVL::ContentPackage;
+    using RVL::LocalDataContainer;
+    
+#ifdef IWAVE_USE_MPI
+    typedef MPIGridSpace myGridSpace;
+#else
+    typedef GridSpace myGridSpace;
+#endif
+    
+    class HelmFO: public BinaryLocalFunctionObject<ireal> {
+        
+    private:
+        grid const & gdom;
+        ireal scale1, scale2;
+        ireal power, datum;
+        int DirichletSides;
+
+        HelmFO();
+        
+    public:
+        HelmFO(grid const & _gdom,
+               ireal _scale1=1.0f,
+               ireal _scale2=1.0f,
+               ireal _power=0.0f,
+               ireal _datum=0.0f,
+               int _DirichletSides=0)
+        : gdom(_gdom),scale1(_scale1),scale2(_scale2),power(_power), datum(_datum), DirichletSides(_DirichletSides){
+        }
+        
+        HelmFO(HelmFO const & f)
+        : gdom(f.gdom), scale1(f.scale1), scale2(f.scale2), power(f.power), datum(f.datum), DirichletSides(f.DirichletSides){
+        }
+        
+        using RVL::LocalEvaluation<ireal>::operator();
+        void operator()(LocalDataContainer<ireal> & x,
+                        LocalDataContainer<ireal> const & y);
+        
+        string getName() const { string tmp = "HelmFO"; return tmp; }
+        
+    };
+    void HelmFO::operator()(LocalDataContainer<ireal> & x,
+                            LocalDataContainer<ireal> const & y){
+        float *indata=NULL;
+        float *outdata=NULL;
+        float *work=NULL;
+        IPNT n_arr;
+        RPNT d_arr;
+        integer f2c_n1;
+        integer f2c_n2;
+        integer lenwork;
+        
+        ContentPackage<ireal, RARR>  & gx =
+        dynamic_cast<ContentPackage <ireal, RARR>  &> (x);
+        ContentPackage<ireal, RARR> const & gy =
+        dynamic_cast<ContentPackage <ireal, RARR> const &> (y);
+        
+        // precondition - metadata are same dimn
+        RARR  & rax = gx.getMetadata();
+        RARR const & ray = gy.getMetadata();
+        int dimx; int dimy;
+        ra_ndim(&rax,&dimx);
+        ra_ndim(&ray,&dimy);
+        //cerr << "\n xdim=" << dimx << endl;
+        //cerr << "\n ydim=" << dimy << endl;
+        if (dimx != dimy) {
+            RVLException e;
+            e<<"Error: HelmFO::operator()\n";
+            e<<"arguments have different dims:\n";
+            e<<"dimx="<<dimx<<" dimy="<<dimy<<"\n";
+            throw e;
+        }
+        
+        // compute grid params
+        IPNT gsx; IPNT gex;
+        IPNT gsy; IPNT gey;
+        IPNT s; IPNT e;
+        ra_a_gse(&rax,gsx,gex);
+        ra_a_gse(&ray,gsy,gey);
+//        cerr << "\n===========================\n";
+//        cerr << "\n gsx[0]=" << gsx[0] << endl;
+//        cerr << "\n gex[0]=" << gex[0] << endl;
+//        cerr << "\n gsx[1]=" << gsx[1] << endl;
+//        cerr << "\n gex[1]=" << gex[1] << endl;
+//        cerr << "\n===========================\n";
+//        cerr << "\n gsy[0]=" << gsy[0] << endl;
+//        cerr << "\n gey[0]=" << gey[0] << endl;
+//        cerr << "\n gsy[1]=" << gsy[1] << endl;
+//        cerr << "\n gey[1]=" << gey[1] << endl;
+//        cerr << "\n===========================\n";
+        // calculate grid overlap
+        for (int ii=0;ii<dimx;ii++)  {
+            s[ii]=max(gsy[ii],gsx[ii]);
+            e[ii]=min(gey[ii],gex[ii]);
+        }
+        
+        
+        get_d(d_arr,gdom);
+        get_n(n_arr,gdom);
+        // initialize f2c ints
+        f2c_n1 = n_arr[0];
+        f2c_n2 = n_arr[1];
+        float _scale1=scale1;
+        float _scale2=scale2;
+        float _power=power;
+        float _datum=datum;
+        integer iter=0;
+        
+        // initialize workspace
+        lenwork = 6*n_arr[1]*n_arr[0]+3*iwave_max(n_arr[1],2*n_arr[0])+21;
+//        cerr << "\n lenwork=" << lenwork << endl;
+//        cerr << "\n length of data = " << get_datasize_grid(gdom) << endl;
+//        cerr << "\n n_arr[0] = " << n_arr[0] << endl;
+//        cerr << "\n n_arr[1] = " << n_arr[1] << endl;
+        
+        if (!(work = (float *)malloc(lenwork*sizeof(float)))) {
+            RVLException e;
+            e<<"Error: HelmOp::apply - failed to allocate " << lenwork << " floats for work buffer\n";
+            throw e;
+        }
+        // allocate data arrays
+        if (!(indata = (float *)malloc(get_datasize_grid(gdom)*sizeof(float)))) {
+            RVLException e;
+            e<<"Error: HelmOp::apply - failed to allocate " << get_datasize_grid(gdom) << " floats for input data\n";
+            throw e;
+        }
+        if (!(outdata = (float *)malloc(get_datasize_grid(gdom)*sizeof(float)))) {
+            RVLException e;
+            e<<"Error: HelmOp::apply - failed to allocate " << get_datasize_grid(gdom) << " floats for output data\n";
+            throw e;
+        }
+        IPNT i;
+        integer idx;
+#if RARR_MAX_NDIM > 0
+        if (dimx==1) {
+            for (i[0]=s[0];i[0]<=e[0];i[0]++) {
+                indata[i[0]-s[0]]=ray._s1[i[0]];
+            }
+        }
+#endif
+#if RARR_MAX_NDIM > 1
+        if (dimx==2) {
+            for (i[1]=s[1];i[1]<=e[1];i[1]++) {
+                for (i[0]=s[0];i[0]<=e[0];i[0]++) {
+                    idx = (i[1]-s[1])*n_arr[0] + i[0]-s[0];
+                    indata[idx]=ray._s2[i[1]][i[0]];
+                }
+            }
+        }
+#endif
+        if (dimx<1 || dimx>2) {
+            RVLException e;
+            e<<"Error: HelmFO::operator()\n";
+            e<<"dim = "<<dimx<<" outside of admissible set {1, 2}\n";
+            throw e;
+        }
+        
+        helm_(DirichletSides,&f2c_n1,&f2c_n2,
+              &(d_arr[0]),&(d_arr[1]),
+              &(scale1),&(scale2),
+              &power,&datum,
+              indata,
+              outdata,
+              work,
+              &lenwork,
+              &iter);
+        fprintf(stderr, "\n indata [100] = %f\n", indata[100]);
+        fprintf(stderr, "\n outdata [100] = %f\n", outdata[100]);
+        // copy data back
+#if RARR_MAX_NDIM > 0
+        if (dimx==1) {
+            for (i[0]=s[0];i[0]<=e[0];i[0]++) {
+                rax._s1[i[0]]=outdata[i[0]-s[0]];
+            }
+        }
+#endif
+#if RARR_MAX_NDIM > 1
+        if (dimx==2) {
+            for (i[1]=s[1];i[1]<=e[1];i[1]++) {
+                for (i[0]=s[0];i[0]<=e[0];i[0]++) {
+                    idx = (i[1]-s[1])*n_arr[0] + i[0]-s[0];
+                    rax._s2[i[1]][i[0]]=outdata[idx];
+                }
+            }
+        }
+#endif
+    }
+    
+    
+    
+    template<typename Scalar>
+    class HelmOp: public LinearOp<Scalar> {
+    private:
+        
+        //GridSpace const & dom;
+        StdProductSpace<Scalar> const & dom;
+        //grid const & gdom;
+        
+        RPNT w_arr;
+        Scalar power, datum;
+        int DirichletSides;
+        
+        FILE * str;
+        
+        // default construction disabled
+        HelmOp();
+        
+    protected:
+        
+        void apply(const Vector<Scalar> & x,
+                   Vector<Scalar> & y) const {
+            try {
+                myGridSpace const & gsp = dynamic_cast<myGridSpace const &> (dom[0]);
+                grid const & gdom=gsp.getGrid();
+                HelmFO fo(gdom,w_arr[0],w_arr[1],power,datum,DirichletSides);
+                MPISerialFunctionObject<Scalar> mpifo(fo);
+                y.eval(mpifo,x);
+            }
+            catch (RVLException & e) {
+                e<<"\ncalled in HelmOp::apply\n";
+                throw e;
+            }
+            
+        }
+        
+        
+        void applyAdj(const Vector<Scalar> & x,
+                      Vector<Scalar> & y) const {
+            try {
+                RVLException e;
+                e<<"Error: HelmOp::applyAdj\n";
+                e<<"  method is not implemented!\n";
+                throw e;
+                //Helm^T;
+            }
+            catch (RVLException & e) {
+                e<<"\ncalled in HelmOp::applyAdj\n";
+                throw e;
+            }
+        }
+        
+    public:
+        
+        HelmOp(HelmOp const & A):
+        dom(A.dom), str(A.str),
+        power(A.power), datum(A.datum), DirichletSides(A.DirichletSides){
+            RASN(w_arr,A.w_arr);
+        }
+        
+        HelmOp(StdProductSpace<Scalar> const & _dom,
+               RPNT _w_arr,
+               Scalar _power=0.0f,
+               Scalar _datum=0.0f,
+               int _DirichletSides=0):
+        dom(_dom),
+        power(_power), datum(_datum), DirichletSides(_DirichletSides){
+            RASN(w_arr,_w_arr);
+            // check dimension
+/*            if (gdom.dim != 2) {
+                RVLException e;
+                e<<"Error: HelmOp::HelmOp - input grid is not 2D\n";
+                throw e;
+           }
+*/
+        }
+        
+        ~HelmOp() {}
+        
+        // this class is considered terminal, with no overrides foreseen,
+        // so clone method is not virtual
+        LinearOp<Scalar> * clone() const { return new HelmOp(*this); }
+        
+        // access to domain, range
+        const Space<float> & getDomain() const { return dom; }
+        const Space<float> & getRange() const { return dom; }
+        
+        ostream & write(ostream & str) const {
+            str<<"HelmOp\n";
+            return str;
+        }
+        
+    };
+}
+#endif
