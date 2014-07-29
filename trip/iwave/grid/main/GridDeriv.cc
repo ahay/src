749bf06be194bfde8b2b26b1acf2e4f13e7917a2
William Symes <symes@caam.rice.edu>
2014-07-29 03:56:57 +0000

    new
    Former-commit-id: 7961f23601c7996f37817e8f7c7b7a41f48222a4

---
+++ trip/iwave/grid/main/GridDeriv.cc
@@ -0,0 +1,83 @@
+#include "parser.h"
+#ifdef IWAVE_USE_MPI
+#include "mpigridpp.hh"
+#else
+#include "gridpp.hh"
+#endif
+#include "gridops.hh"
+
+using RVL::valparse;
+using RVL::RVLException;
+using RVL::Vector;
+using RVL::LinearOp;
+using RVL::AssignFilename;
+
+using TSOpt::GridDerivOp;
+#ifdef IWAVE_USE_MPI
+using TSOpt::MPIGridSpace;
+typedef TSOpt::MPIGridSpace gsp;
+#else
+using TSOpt::GridSpace;
+typedef TSOpt::GridSpace gsp;
+#endif
+
+int xargc;
+char **xargv;
+
+int main(int argc, char ** argv) {
+
+  try {
+
+#ifdef IWAVE_USE_MPI
+    int ts=0;
+    MPI_Init_thread(&argc,&argv,MPI_THREAD_FUNNELED,&ts);    
+#endif
+
+    PARARRAY * pars = ps_new();
+    
+    if (ps_createargs(pars,argc-1,&(argv[1]))) {
+      RVLException e;
+      e<<"ERROR: GridDerivOp from ps_creatargs \n";
+      e<<"  called with args:\n";
+      e<<"  argc = "<<argc-1<<"\n";
+      for (int i=0;i<argc-1;i++) 
+	e<<"  argv["<<i<<"] = "<<argv[i+1]<<"\n";
+      throw e;
+    }
+    // since the product of grid spaces is not really an 
+    // out-of-core structure, this driver operates on single
+    // grid spaces
+    string inp = valparse<string>(*pars,"input");
+    string outp = valparse<string>(*pars,"output");
+    int axis = valparse<int>(*pars,"axis");
+
+    ps_delete(&pars);
+
+    gsp sp(inp,"notype",true);
+    
+    GridDerivOp op(sp,axis);
+
+    Vector<float> invec(sp);
+    Vector<float> outvec(sp);
+    AssignFilename afin(inp);
+    AssignFilename afout(outp);
+    invec.eval(afin);
+    outvec.eval(afout);
+
+    op.applyOp(invec,outvec);
+
+#ifdef IWAVE_USE_MPI
+    MPI_Finalize();
+#endif
+  }
+  catch (RVLException & e) {
+    e.write(cerr);
+#ifdef IWAVE_USE_MPI
+    MPI_Abort(MPI_COMM_WORLD,0);
+#endif
+    exit(1);
+  }
+  
+}
+
+
