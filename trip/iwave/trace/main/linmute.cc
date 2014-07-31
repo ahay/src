5164d77b64bbc49bd665553daa0ed1045077eb55
William Symes <symes@caam.rice.edu>
2014-07-31 23:18:32 +0000

    interim
    Former-commit-id: 2f2a52edb57d09bc733dcf8a18eb7fe07908ca33

--- trip/iwave/trace/main/linmute.cc
+++ trip/iwave/trace/main/linmute.cc
@@ -1,10 +1,5 @@
 #include "parser.h"
-#ifdef IWAVE_USE_MPI
-#include "mpisegypp.hh"
-#include "mpiserialfo.hh"
-#else
 #include "segypp.hh"
-#endif
 #include "segyops.hh"
 #include "op.hh"
 
@@ -16,15 +11,8 @@ using RVL::LinearOpFO;
 using RVL::AssignFilename;
 
 using TSOpt::SEGYLinMute;
-
-#ifdef IWAVE_USE_MPI
-using RVL::MPISerialFunctionObject;
-using TSOpt::MPISEGYSpace;
-typedef TSOpt::MPISEGYSpace tsp;
-#else
 using TSOpt::SEGYSpace;
 typedef TSOpt::SEGYSpace tsp;
-#endif
 
 int xargc;
 char **xargv;
@@ -32,11 +20,6 @@ char **xargv;
 int main(int argc, char ** argv) {
 
   try {
-
-#ifdef IWAVE_USE_MPI
-    int ts=0;
-    MPI_Init_thread(&argc,&argv,MPI_THREAD_FUNNELED,&ts);    
-#endif
 
     PARARRAY * pars = ps_new();
     
@@ -71,21 +54,12 @@ int main(int argc, char ** argv) {
     SEGYLinMute mute(valparse<float>(*pars,"mute_slope",0.0f),
 		     valparse<float>(*pars,"mute_zotime",0.0f),
 		     valparse<float>(*pars,"mute_width",0.0f));
-#ifdef IWAVE_USE_MPI
-    MPISerialFunctionObject<float> mpimute(mute);
-    LinearOpFO<float> muteop(dom,dom,mpimute,mpimute);
-    muteop.applyOp(ddin,ddout);
-    MPI_Finalize();
-#else
+
     LinearOpFO<float> muteop(dom,dom,mute,mute);
     muteop.applyOp(ddin,ddout);
-#endif
-    }
-    catch (RVLException & e) {
-        e.write(cerr);
-#ifdef IWAVE_USE_MPI
-        MPI_Abort(MPI_COMM_WORLD,0);
-#endif
-        exit(1);
-    }
+  }
+  catch (RVLException & e) {
+    e.write(cerr);
+    exit(1);
+  }
 }
