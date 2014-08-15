4fa3e81d8ae320013e5fea58c22c379d395cda49
Karl Schleicher <k_schleicher@hotmail.com>
2014-08-15 17:56:28 +0000

    improve parallel/slurm usage
    Former-commit-id: 098b29dbb5fa807102343416d97b20a2563e0394

--- book/data/seam-phase1-2d/sfmshots/host.sh
+++ book/data/seam-phase1-2d/sfmshots/host.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-
+    # scontrol is a slurm command. in the next list it produces a list
+    # of the individual nodes eg c557-[204,301] changes to:
+    # c557-204
+    # c557-301
     declare -a hostlist=(`scontrol show hostname $SLURM_NODELIST `)
     declare -a node_clusters=(`echo $SLURM_TASKS_PER_NODE | sed -e's/,/ /g'`)
 
@@ -8,7 +11,7 @@
         echo "TACC: Error -> slurm host list unavailable"
         exit 1
     fi
-#    echo "DEBUG: ${hostlist[@]} "
+    echo "DEBUG: ${hostlist[@]} "
 
     #Initialize the hostlist index
     host_id=0
@@ -19,6 +22,7 @@
     export output=""
     #Build the hostlist using the SLURM_TASKS_PER_NODE syntax
     for nodes in ${node_clusters[@]}; do
+      echo nodes=$nodes
       #Get the task count and node count for each node cluster
       task_count=`echo $nodes | awk -F '(' '{print $1}'`
       if [[ `echo $nodes | grep x` ]]; then
@@ -26,12 +30,13 @@
       else
         node_count=1
       fi
-#     echo ${hostlist[${host_id}]} $task_count
-#     echo $host_id
-#      echo "DEBUG: nodes=$nodes task_count=$task_count  node_count=$node_count"
+     echo ${hostlist[${host_id}]} $task_count
+     echo $host_id
+      echo "DEBUG: nodes=$nodes task_count=$task_count  node_count=$node_count"
 
       #Build the host list to match tasks per node
       # and set up PE env variable for each task
+      echo node_count=$((node_count-1))
       for i in `seq 0 $((node_count-1))`; do
          output="${output} ${hostlist[${host_id}]} $task_count" 
       ((host_id++))
