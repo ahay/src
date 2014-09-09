1d75ef2f55f429e9191a7f955e18665526b170e5
Sergey Fomel <sergey.fomel@gmail.com>
2014-09-09 01:51:24 +0000

    hw1
    Former-commit-id: ab1db9fc2a219fdaeb222fe97522db12876599e0

--- trip/admin/linux.mpi.icc.py
+++
@@ -1,4 +0,0 @@
-CC = 'mpicc' 
-CCFLAGS = '-O3 -xSSE4.2 -falign-functions=16 -restrict -vec-report3 -opt-report 3 -opt-report-phase=hlo -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DIWAVE_USE_MPI'
-CFLAGS = '-std=c99'
-CXX = 'mpicxx'
