a06b80050611a34bf48b0c64a21f2bb5a8936e4c
Yin Huang <maggiehuangyin@gmail.com>
2014-09-17 02:44:46 +0000

    add fwi
    Former-commit-id: 33926b7488dbe84df7aad8060f9e3af28ad273f1

--- trip/iwave/acd/main/acdefwi.cc
+++ trip/iwave/acd/main/acdefwi.cc
@@ -6,13 +6,15 @@
 #include "iwop.hh"
 #include "functions.hh"
 #include "op.hh"
+#include "ls.hh"
 #include "blockop.hh"
 #include "cgnealg.hh"
 #include "LBFGSBT.hh"
 #include "LinFitLS.hh"
-#include "acdds_scan_selfdoc.hh"
+#include "LinFitLSSM.hh"
+#include "acdiva_selfdoc.h"
 #include <par.h>
-#include "scantest.hh"
+#include "adjtest.hh"
 #include "segyops.hh"
 
 //#define GTEST_VERBOSE
@@ -33,17 +35,19 @@ using RVL::OperatorEvaluation;
 using RVL::LinearOp;
 using RVL::LinearOpFO;
 using RVL::OpComp;
+using RVL::RegLeastSquaresFcnlGN;
 using RVL::SymmetricBilinearOp;
 using RVL::AssignFilename;
 using RVL::AssignParams;
 using RVL::RVLRandomize;
 using RVL::ScaleOpFwd;
 using RVL::TensorOp;
-using RVL::Scan;
+using RVL::AdjointTest;
 using RVLUmin::CGNEPolicy;
 using RVLUmin::CGNEPolicyData;
 using RVLUmin::LBFGSBT;
 using RVLUmin::LinFitLS;
+using RVLUmin::LinFitLSSM;
 using RVLUmin::CGNEAlg;
 using TSOpt::IWaveEnvironment;
 using TSOpt::IWaveTree;
@@ -53,6 +57,7 @@ using TSOpt::TASK_RELN;
 using TSOpt::IOTask;
 using TSOpt::IWaveOp;
 using TSOpt::SEGYTaperMute;
+using TSOpt::GridMaskOp;
 #ifdef IWAVE_USE_MPI
 using TSOpt::MPIGridSpace;
 using TSOpt::MPISEGYSpace;
@@ -62,6 +67,7 @@ using TSOpt::SEGYSpace;
 #endif
 using TSOpt::GridExtendOp;
 using TSOpt::GridDerivOp;
+using TSOpt::GridHelmOp;
 
 int xargc;
 char **xargv;
@@ -108,20 +114,20 @@ int main(int argc, char ** argv) {
     else {
 #endif
 
-      // the Op
+      /* the Op */
       IWaveOp iwop(*pars,stream);
         
-        SEGYTaperMute mute(valparse<float>(*pars,"mute_slope",0.0f),
-                         valparse<float>(*pars,"mute_zotime",0.0f),
-                         valparse<float>(*pars,"mute_width",0.0f),0,
-                         valparse<float>(*pars,"min",0.0f),
-                         valparse<float>(*pars,"max",numeric_limits<float>::max()),
-                         valparse<float>(*pars,"taper_width",0.0f),
-                         valparse<int>(*pars,"taper_type",0),
-                         valparse<float>(*pars,"time_width",0.0f));        
-
-      LinearOpFO<float> muteop(iwop.getRange(),iwop.getRange(),mute,mute);
-      OpComp<float> op(iwop,muteop);
+      SEGYTaperMute tnm(valparse<float>(*pars,"mute_slope",0.0f),
+                        valparse<float>(*pars,"mute_zotime",0.0f),
+                        valparse<float>(*pars,"mute_width",0.0f),0,
+                        valparse<float>(*pars,"min",0.0f),
+                        valparse<float>(*pars,"max",numeric_limits<float>::max()),
+                        valparse<float>(*pars,"taper_width",0.0f),
+                        valparse<int>(*pars,"taper_type",0),
+                        valparse<float>(*pars,"time_width",0.0f));
+        
+      LinearOpFO<float> tnmop(iwop.getRange(),iwop.getRange(),tnm,tnm);
+      OpComp<float> op(iwop,tnmop);
     
       /* generate physical model space */
 #ifdef IWAVE_USE_MPI
@@ -131,56 +137,40 @@ int main(int argc, char ** argv) {
 #endif
       // make it a product, so it's compatible with domain of op
       StdProductSpace<ireal> dom(csqsp);
+      
+
 
       // vel-squared model!
-      Vector<ireal> m_beg(dom);
-      Vector<ireal> m_end(dom);
-      Vector<ireal> dm(op.getDomain());
+      Vector<ireal> m0(op.getDomain());
+      Vector<ireal> m(op.getDomain());
 
-      // read in start point and end point
-      AssignFilename mfn_beg(valparse<std::string>(*pars,"csq_beg"));
-      Components<ireal> cm_beg(m_beg);
-      cm_beg[0].eval(mfn_beg);
+      AssignFilename mf0n(valparse<std::string>(*pars,"init_csq"));
+      Components<ireal> cm0(m0);
+      cm0[0].eval(mf0n);
 
-      AssignFilename mfn_end(valparse<std::string>(*pars,"csq_end"));
-      Components<ireal> cm_end(m_end);
-      cm_end[0].eval(mfn_end);
-        
-      Vector<ireal> m_diff(dom);
-      Components<ireal> cm_diff(m_diff);
-      m_diff.copy(m_end);
-      m_diff.linComb(-1.0f,m_beg);
-      
-      
-//      AssignFilename dmfn(valparse<std::string>(*pars,"reflectivity"));
-//      Components<ireal> cdm(dm);
-//      cdm[0].eval(dmfn);
-//      dm.zero();
+      AssignFilename mfn(valparse<std::string>(*pars,"final_csq"));
+      Components<ireal> cm(m);
+      cm[0].eval(mfn);
+      m.copy(m0);
+    
+      // muted data
       Vector<ireal> mdd(op.getRange());
       std::string mddnm = valparse<std::string>(*pars,"datamut","");
       if (mddnm.size()>0) {
         AssignFilename mddfn(mddnm);
         mdd.eval(mddfn);
       }
-      {
+      { // read data - only needed to define muted data
         Vector<ireal> dd(op.getRange());
         AssignFilename ddfn(valparse<std::string>(*pars,"data"));
         Components<ireal> cdd(dd);
         cdd[0].eval(ddfn);
-        muteop.applyOp(dd,mdd);
-      } 
+        tnmop.applyOp(dd,mdd);
+      }
 
-
-      CGNEPolicyData<float> pd(
-            valparse<float>(*pars,"ResidualTol",100.0*numeric_limits<float>::epsilon()),
-            valparse<float>(*pars,"GradientTol",100.0*numeric_limits<float>::epsilon()),
-            valparse<float>(*pars,"MaxStep",numeric_limits<float>::max()),
-            valparse<int>(*pars,"MaxIter",10),true);
-    
       /* output stream */
-      std::stringstream res,strscan;
+      std::stringstream res;
       res<<scientific;
-      strscan<<scientific;
     
       // choice of GridDerivOp for semblance op is placeholder - extended axis is dim-th, with id=dim+1
       // note default weight of zero!!!
@@ -198,30 +188,38 @@ int main(int argc, char ** argv) {
 	throw e;
       }
 #endif
-      GridDerivOp dsop(op.getDomain(),dsdir,valparse<float>(*pars,"DSWeight",0.0f));
-      TensorOp<float> top(op,dsop);
-      // create RHS of block system
-      Vector<float> td(top.getRange());
-      Components<float> ctd(td);
-      ctd[0].copy(mdd);
-      ctd[1].zero();
+    // assign window widths - default = 0;
+    RPNT swind,ewind;
+    RASN(swind,RPNT_0);
+    RASN(ewind,RPNT_0);
+    swind[0]=valparse<float>(*pars,"sww1",0.0f);
+    swind[1]=valparse<float>(*pars,"sww2",0.0f);
+    swind[2]=valparse<float>(*pars,"sww3",0.0f);
+    ewind[0]=valparse<float>(*pars,"eww1",0.0f);
+    ewind[1]=valparse<float>(*pars,"eww2",0.0f);
+    ewind[2]=valparse<float>(*pars,"eww3",0.0f);
+
+     GridDerivOp dsop(op.getDomain(),dsdir,1.0f);//valparse<float>(*pars,"DSWeight",0.0f));
+     // need to read in model space for bg input to GridWindowOp
+     Vector<ireal> m_in(op.getDomain());
+     AssignFilename minfn(valparse<std::string>(*pars,"csqext"));
+     Components<ireal> cmin(m_in);
+     cmin[0].eval(minfn);
+     GridMaskOp mop(op.getDomain(),m_in,swind,ewind);
+     OperatorEvaluation<float> mopeval(mop,m_in);
+     LinearOp<float> const & lmop=mopeval.getDeriv();
+     OpComp<float> cop(mop,op);
+     OpComp<float> cdsop(mop,dsop);
 
       // choice of preop is placeholder
-      ScaleOpFwd<float> preop(top.getDomain(),1.0f);
-            Vector<float> dm0(op.getDomain());
-            string refname = valparse<std::string>(*pars,"ref0","");
-            if (refname.size()>0){
-                AssignFilename dmfn(refname);
-                Components<float> cdm0(dm0);
-                cdm0[0].eval(dmfn);
-            } else { dm0.zero(); }
-      LinFitLS<float, CGNEPolicy<float>, CGNEPolicyData<float> > f(top,preop,td,dm0,pd,false,res);
-      GridExtendOp g(dom,op.getDomain());
-      FcnlOpComp<float> gf(f,g);
+      // ScaleOpFwd<float> preop(cop.getDomain(),1.0f);
+      Vector<ireal> zm(op.getDomain());
+      zm.zero();
+      RegLeastSquaresFcnlGN<float> f(cop, dsop, mdd, zm, valparse<float>(*pars,"DSWeight",0.0f));
 
       // lower, upper bds for csq
-      Vector<float> lb(gf.getDomain());
-      Vector<float> ub(gf.getDomain());
+      Vector<float> lb(f.getDomain());
+      Vector<float> ub(f.getDomain());
       float lbcsq=valparse<float>(*pars,"cmin");
       lbcsq=lbcsq*lbcsq;
       RVLAssignConst<float> asl(lbcsq);
@@ -238,29 +236,60 @@ int main(int argc, char ** argv) {
       ULBoundsTest<float> ultest(lb,ub,mn);
 #endif
 
-      FunctionalBd<float> fbd(gf,ultest);
+      FunctionalBd<float> fbd(f,ultest);
 
-      // read in scan test parameters
-      int  nscan=valparse<int>(*pars,"NScan",11);
-      float hmin=valparse<float>(*pars,"HMin",-1.0f);
-      float hmax=valparse<float>(*pars,"HMax",1.0f);
-        
-      /* scan */
-      Scan(fbd,m_beg,m_diff,nscan,hmin,hmax,strscan);
+      LBFGSBT<float> alg(fbd,m,
+			 valparse<float>(*pars,"InvHessianScale",1.0f),
+			 valparse<int>(*pars,"MaxInvHessianUpdates",5),
+			 valparse<int>(*pars,"MaxLineSrchSteps",10),
+			 valparse<bool>(*pars,"VerboseDisplay",true), 
+			 valparse<float>(*pars,"FirstStepLength",1.0f), 
+			 valparse<float>(*pars,"GAStepAcceptThresh",0.1f),
+			 valparse<float>(*pars,"GAStepDoubleThresh",0.9f), 
+			 valparse<float>(*pars,"LSBackTrackFac",0.5f), 
+			 valparse<float>(*pars,"LSDoubleFac",1.8f),
+			 valparse<float>(*pars,"MaxFracDistToBdry",1.0), 
+			 valparse<float>(*pars,"LSMinStepFrac",1.e-06),
+			 valparse<int>(*pars,"MaxLBFGSIter",3), 
+			 valparse<float>(*pars,"AbsGradThresh",0.0), 
+			 valparse<float>(*pars,"RelGradThresh",1.e-2), 
+			 res);
+      if (valparse<int>(*pars,"MaxLBFGSIter",3) <= 0) {
+	float val = alg.getFunctionalEvaluation().getValue();
+	res<<"=========================== LINFITLS ==========================\n";
+	res<<"value of IVA functional = "<<val<<"\n";    
+	res<<"=========================== LINFITLS ==========================\n";
+      }
+      else {
+	alg.run();
+      }
+
+      std::string dataest = valparse<std::string>(*pars,"dataest","");
+      std::string datares = valparse<std::string>(*pars,"datares","");
+      std::string normalres = valparse<std::string>(*pars,"normalres","");
+      if (dataest.size()>0) {
+          OperatorEvaluation<float> opeval(op,m);
+          Vector<float> est(op.getRange());
+          AssignFilename estfn(dataest);
+          est.eval(estfn);
+          est.copy(opeval.getValue());
+          if (datares.size()>0) {
+              Vector<float> res(op.getRange());
+              AssignFilename resfn(datares);
+              res.eval(resfn);
+              res.copy(est);
+              res.linComb(-1.0f,mdd);
+          }
+      }
 
       if (retrieveRank() == 0) {
 	std::string outfile = valparse<std::string>(*pars,"outfile","");
 	if (outfile.size()>0) {
 	  ofstream outf(outfile.c_str());
-          outf<<"\n === h ========= functional value ================ \n";
-          outf<<strscan.str();
-          outf<<"\n ================================================= \n";
-          outf<<res.str();
-          outf.close();
+	  outf<<res.str();
+	  outf.close();
 	}
 	else {
-          cout<<strscan.str();
-          cout<<"\n ================================================= \n";
 	  cout<<res.str();
 	}
       }
