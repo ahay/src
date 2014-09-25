a34d7a51f77449aa048ce1cdf80702242c2cab0d
Yangkang Chen <chenyk1990@gmail.com>
2014-09-25 05:07:26 +0000

    snr3
    Former-commit-id: ef5ab3ef1e191d7017619a621bf0056e4792ec7e

--- user/chenyk/Msnr3.c
+++ user/chenyk/Msnr3.c
@@ -1,7 +1,7 @@
 /* Compute signal-noise-ratio.
 SNR=10 log10(sum(clean)/sum(noise))*/
 /*
-  Copyright (C) 2013 University of Texas at Austin
+  Copyright (C) 2014 University of Texas at Austin
   
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -23,8 +23,8 @@ SNR=10 log10(sum(clean)/sum(noise))*/
 
 int main (int argc, char* argv[]) 
 {
-	int n1,nn1,n2,nn2,n3,nn3; /*n1 is trace length, n2 is the number of traces, n3 is the number of 3th axis*/
-	int i,j,k;
+	int n1,nn1,n2,nn2; /*n1 is trace length, n2 is the number of traces*/
+	int i,j;
 
 	float *trace1, *trace2;
         float en, es, snr; /*en is noise energy, es is signal energy, snr is signal-noise-ratio*/
@@ -36,27 +36,24 @@ int main (int argc, char* argv[])
         snrf = sf_output("out");
     
 	if (!sf_histint(signal,"n1",&n1)) sf_error("No n1= in input");
-	if (!sf_histint(signal,"n2",&n2)) sf_error("No n2= in input");
-	n3 = sf_leftsize(signal,2);
-	/* get the trace length (n1) and the number of traces (n2) and n3 from signal*/
+	n2 = sf_leftsize(signal,1);
+	/* get the trace length (n1) and the number of traces (n2) from signal*/
 
 	if (!sf_histint(noise,"n1",&nn1)) sf_error("No n1= in noise");
-	if (!sf_histint(noise,"n2",&nn2)) sf_error("No n2= in noise");
-	nn3 = sf_leftsize(noise,2);
-	/* get the trace length (n1) and the number of traces (n2) and n3 from noise*/	
+	nn2 = sf_leftsize(noise,1);
+	/* get the trace length (n1) and the number of traces (n2) from noise*/	
 
-	if(n1 !=nn1 || n2 !=nn2 || n3 !=nn3) sf_error("size doesn't match");
+	if(n1 !=nn1 || n2 !=nn2) sf_error("size doesn't match");
 
-        sf_putint(snrf,"n1",n3);
+        sf_putint(snrf,"n1",1);
 	sf_putfloat(snrf,"d1",1);
         sf_putint(snrf,"n2",1);
-
+        sf_putint(snrf,"n3",1);
 
 	/*set the data space*/
 	trace1 = sf_floatalloc(n1*n2);
 	trace2 = sf_floatalloc(n1*n2);
 
-                for(k=0;k<n3;k++) {
                     sf_floatread(trace1,n1*n2,signal);
 		    sf_floatread(trace2,n1*n2,noise);
 
@@ -76,15 +73,8 @@ int main (int argc, char* argv[])
                         }
                     }
                     snr=10*log10(es/en);
-                            
-                    printf("***************************************\n");
-                    printf("signal energy at n3=%d      = %f \n", (k+1),es);
-                    printf("noise  energy at n3=%d      = %f \n", (k+1),en);
-                    printf("the SNR       at n3=%d      = %f \n", (k+1),snr);
-                    printf("***************************************\n");
 
 		    sf_floatwrite(&snr,1,snrf);
-                }
 
     exit (0);
 }
