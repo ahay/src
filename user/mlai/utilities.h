ebb9fc7468ca09d2f014fac8094f9e9b60c2d1bd
Mark Lai <yenming.mark.lai@gmail.com>
2015-02-04 22:19:01 +0000

    adding utility files in my user directory (mlai) for the first time
    Former-commit-id: bd07eeee28d6858f14fa7746538915da502cc663

---
+++ user/mlai/utilities.h
@@ -0,0 +1,29 @@
+#include <sys/stat.h>
+#define SECONDS_TO_SLEEP 30
+#define DO_DEBUG_FILE_NAME "/home/mlai/debug/DO_DEBUG"
+
+static inline int doesFileExist(const char* filename)
+/*< Checks if a file exist using stat() function. Returns 1 
+ * if the file exist otherwise returns 0.
+ * >*/
+{
+    struct stat buffer;
+    int exist = stat(filename,&buffer);
+    if(exist == 0)
+        return 1;
+    else // -1
+        return 0;
+}
+
+static inline void sleepIfFileExist(int seconds_to_sleep, const char* file_name)
+/*<Sleeps if a file exists>*/
+{	
+    if (doesFileExist(file_name)){
+    	
+		printf("The file %s exists.  Waiting %d seconds for someone to attach ...\n", 
+				file_name, 
+				seconds_to_sleep);
+		sleep(seconds_to_sleep);
+		printf("Done waiting!\n");
+    }
+}
