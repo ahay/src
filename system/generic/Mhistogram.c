6b8c4f62e29ce88a3359dca5fd6e623479d78328
Sergey Fomel <sergey.fomel@gmail.com>
2015-03-04 21:43:15 +0000

    program of the month
    Former-commit-id: 7a83ada09549e83963779c5ed5fad86a6a7acf8e

--- system/generic/Mhistogram.c
+++ system/generic/Mhistogram.c
@@ -1,7 +1,11 @@
 /* Compute a histogram of integer- or float-valued input data.
 
 The output grid is not centered on the bins; it marks their "left edge".
-I.e., the first sample holds the number of values between o1 and o1+d1. */
+I.e., the first sample holds the number of values between o1 and o1+d1. 
+
+February 2015 program of the month:
+http://ahay.org/rsflog/index.php?/archives/427-Program-of-the-month-sfhistogram.html
+*/
 /*
   Copyright (C) 2004 University of Texas at Austin
 
