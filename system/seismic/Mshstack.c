e17d00a300f21f7de139a68229069d6b30928403
Sergey Fomel <sergey.fomel@gmail.com>
2015-03-31 22:38:50 +0000

    shstack
    Former-commit-id: e180d7e74924eb9df3eda599a24337fd19c82c3d

---
+++ system/seismic/Mshstack.c
@@ -0,0 +1,142 @@
+/* Shaping stack. */
+/*
+  Copyright (C) 2015 University of Texas at Austin
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#include <rsf.h>
+
+#include "inmo.h"
+
+int main (int argc, char* argv[])
+{
+    bool half, slow;
+    int ix,ih, nd, nt,nx, nh, CDPtype, jump, niter, restart;
+    float dt, t0, h0, dh, eps, dy, tol;
+    float *trace, *trace2, *vel, *off, **gather, **dense;
+    sf_file cmp, stack, velocity, offset;
+
+    sf_init (argc,argv);
+    cmp = sf_input("in");
+    velocity = sf_input("velocity");
+    stack = sf_output("out");
+
+    if (SF_FLOAT != sf_gettype(cmp)) sf_error("Need float input");
+    if (!sf_histint(cmp,"n1",&nt)) sf_error("No n1= in input");
+    if (!sf_histfloat(cmp,"d1",&dt)) sf_error("No d1= in input");
+    if (!sf_histfloat(cmp,"o1",&t0)) sf_error("No o1= in input");
+
+    if (!sf_histint(cmp,"n2",&nh)) sf_error("No n2= in input");
+    
+    sf_putint(stack,"n2",1);
+
+    off = sf_floatalloc(nh);
+
+    if (!sf_getbool("half",&half)) half=true;
+    /* if y, the second axis is half-offset instead of full offset */
+
+    CDPtype=1;
+    if (NULL != sf_getstring("offset")) {
+	offset = sf_input("offset");
+	sf_floatread (off,nh,offset);
+	sf_fileclose(offset);
+    } else {
+	if (!sf_histfloat(cmp,"d2",&dh)) sf_error("No d2= in input");
+	if (!sf_histfloat(cmp,"o2",&h0)) sf_error("No o2= in input");
+	
+	if (sf_histfloat(cmp,"d3",&dy)) {
+	    CDPtype=half? 0.5+dh/dy : 0.5+0.5*dh/dy;
+	    if (CDPtype < 1) {
+		CDPtype=1;
+	    } else if (1 != CDPtype) {
+		sf_histint(cmp,"CDPtype",&CDPtype);
+	    	sf_warning("CDPtype=%d",CDPtype);
+	    }
+	} 
+
+	for (ih = 0; ih < nh; ih++) {
+	    off[ih] = h0 + ih*dh; 
+	}
+    }
+
+    if (!sf_getbool("slowness",&slow)) slow=false;
+    /* if y, use slowness instead of velocity */
+
+    nx = sf_leftsize(cmp,2);
+
+    if (!sf_getfloat ("h0",&h0)) h0=0.;
+    /* reference offset */
+    if (half) h0 *= 2.;
+    if (!sf_getfloat("eps",&eps)) eps=0.01;
+    /* stretch regularization */
+
+    if (!sf_getint("jump",&jump)) jump=1;
+    /* subsampling */
+
+    if (!sf_getint("niter",&niter)) niter=10;
+    /* number of iterations */
+
+    if (!sf_getint("restart",&restart)) restart=niter;
+    /* GMRES memory */
+
+    if (!sf_getfloat("tol",&tol)) tol=0.0;
+    /* GMRES tolerance */
+
+    nd = (nt-1)*jump+1;
+    
+    sf_putint(stack,"n1",nd);
+    sf_putfloat(stack,"d1",dt/jump);
+
+    trace = sf_floatalloc(nd);
+    trace2 = sf_floatalloc(nd);
+    gather = sf_floatalloc2(nt,nh);
+    dense = sf_floatalloc2(nd,nh);
+
+    vel = sf_floatalloc(nd);
+
+    for (ix = 0; ix < nx; ix++) { /* loop over midpoint */
+	sf_floatread (vel,nd,velocity);	
+	
+	inmo_init(vel, off, nh, 
+		  h0, dh, CDPtype, ix,
+		  nt, slow,
+		  t0, dt, eps, half,
+		  jump);
+
+	sf_floatread (gather[0],nt*nh,cmp);
+
+	/* apply backward operator */
+	interpolate(gather, dense);
+	nmostack(dense,trace);
+
+#ifdef sdfsdfg
+	sf_gmres_init(nd,restart);
+
+	/* run GMRES */
+	sf_gmres(trace,trace2,inmo_oper,NULL,niter,tol,true);
+
+	sf_gmres_close();
+#endif
+
+	sf_floatwrite (trace2,nd,stack);
+
+	inmo_close();
+    }
+
+
+    exit (0);
+}
+
+
---
+++ system/seismic/inmo.c
@@ -0,0 +1,184 @@
+/* Inverse normal moveout. */
+/*
+  Copyright (C) 2015 University of Texas at Austin
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <math.h>
+
+#include <rsf.h>
+
+#include "stretch4.h"
+
+static int nh, nt, ns, jump;
+static float *stretch; /* **dense2, **sparse2; */
+static map4* nmo;
+static sf_bands spl;
+
+void inmo_init(const float* vel /* velocity */,
+	       const float* off /* offset */,
+	       int nh1 /* number of offsets */,
+	       float h0, float dh, int CDPtype, int ix,
+	       int nt1 /* time samples */, bool slow,
+	       float t0, float dt, float eps, bool half,
+	       int jump1 /* subsampling */)
+/*< initialization >*/
+{
+    int it, ih;
+    float f, h, *coord;
+    const int nw=4;
+
+    nh = nh1;
+    ns = nt1;
+    jump = jump1;
+    nt = (ns-1)*jump+1;
+    dt = dt/jump;
+
+    nmo = (map4*) sf_alloc(nh,sizeof(map4));
+    stretch = sf_floatalloc(nt);
+
+    for (ih=0; ih < nh; ih++) {
+	nmo[ih] = stretch4_init (nt, t0, dt, nt, eps);
+	
+	h = off[ih] + (dh/CDPtype)*(ix%CDPtype); 
+	if (half) h *= 2;
+	h = h*h - h0*h0;
+	    
+	for (it=0; it < nt; it++) {
+	    f = t0 + it*dt;
+	    if (slow) {
+		f = f*f + h*vel[it]*vel[it];
+	    } else {
+		f = f*f + h/(vel[it]*vel[it]);
+	    }
+	    if (f < 0.) {
+		stretch[it]=t0-10.*dt;
+	    } else {
+		stretch[it] = sqrtf(f);
+	    }
+	}
+
+	stretch4_define (nmo[ih],stretch);
+    }
+
+    coord = sf_floatalloc(nt);
+    for (it=0; it < nt; it++) {
+	coord[it] = it;
+    }
+
+    spl = sf_spline_init(nw,ns);
+    sf_int1_init (coord, 0.0, jump, ns, sf_spline_int, nw, nt, 0.0);
+    free(coord);
+    
+/*
+    dense2  = sf_floatalloc2(nt,nh);
+    sparse2 = sf_floatalloc2(ns,nh);
+*/
+}
+
+void inmo_close(void)
+/*< free allocated storage >*/
+{
+    int ih;
+
+    for (ih=0; ih < nh; ih++) {
+	stretch4_close(nmo[ih]);
+    }
+    free(nmo);
+    free(stretch);
+     
+    sf_int1_close();
+    sf_banded_close(spl);
+/*
+    free(*dense2);
+    free(dense2);
+    free(*sparse2);
+    free(sparse2);
+*/
+}
+
+void inmo(const float *trace /* input trace [nt] */,
+	  float **gather     /* output CMP gather [nt][nh] */)
+/*< apply inverse nmo >*/
+{
+    int ih;
+
+    for (ih = 0; ih < nh; ih++) {
+	stretch4_apply (nmo[ih],trace,gather[ih]);
+    }
+}
+
+
+void nmostack(float **gather /* input CMP gather */,
+	      float *trace /* output trace */)
+/*< apply NMO and stack >*/
+{
+    int ih, it;
+
+    for (it=0; it < nt; it++) {
+	trace[it] = 0.0f;
+    }
+
+    for (ih = 0; ih < nh; ih++) {
+	stretch4_invert (nmo[ih],stretch,gather[ih]);
+	for (it=0; it < nt; it++) {
+	    trace[it] += stretch[it];
+	}
+    }
+
+    /* normalize */
+    for (it=0; it < nt; it++) {
+	trace[it] /= nh;
+    }
+}
+
+
+void subsample(float **dense, float **sparse) 
+/*< subsample a gather >*/
+{
+    int ih, it, is;
+
+    for (ih = 0; ih < nh; ih++) {
+	for (it=is=0; it < nt; it+=jump, is++) {
+	    sparse[ih][is] = dense[ih][it];
+	}
+    }
+}
+
+
+void interpolate(float **sparse, float **dense)
+/*< interpolate to a denser sampling >*/
+{
+    int ih;
+
+    for (ih = 0; ih < nh; ih++) {
+	sf_banded_solve(spl,sparse[ih]);
+	sf_int1_lop (false,false,ns,nt,sparse[ih],dense[ih]);
+    }
+
+}
+
+/*
+void inmo_oper(int nx, const float *trace, float *trace2, void* user_data)
+/< operator to invert by GMRES >/
+{
+    inmo(trace,dense2);
+    subsample(dense2,sparse2);
+    interpolate(sparse2,dense2);
+    nmostack(dense2,trace2);
+}
+*/
+	      
