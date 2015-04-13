4e9016e39411dd7a0616e22a918f89eb6ce8e4c1
Sergey Fomel <sergey.fomel@gmail.com>
2015-04-13 21:30:54 +0000

    cleanup
    Former-commit-id: 4520478c0b45e015bf1d0d4af4410080cc9bef31

--- admin/cleanup.py
+++ admin/cleanup.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 # Prepare a list of programs without examples for removing them from the stable version
-
+import os
 import rsf.doc
 import rsf.prog
 
@@ -8,5 +8,8 @@ progs = rsf.doc.progs
 for prog in progs.keys():
     sfprog = rsf.doc.progs[prog]
     if not sfprog.uses:
-        print sfprog.file
+        if not os.path.dirname(rsfprog.file) in ('system/main','plot/test','plot/main',
+                                                 'su/plot','pens/main',
+                                                 'user/ivlad','user/jennings'):
+            print sfprog.file
 
