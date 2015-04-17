e15a0825d5d6c8f826f88f20eed2fcc088475ab4
Sergey Fomel <sergey.fomel@gmail.com>
2015-04-17 22:12:29 +0000

    conjgrad_adj
    Former-commit-id: 18beef5010041d94db343391cfd67ff17269bf7c

--- api/c/conjgrad.c
+++ api/c/conjgrad.c
@@ -202,4 +202,130 @@ void sf_conjgrad(sf_operator prec  /* data preconditioning */,
 
 }
 
+void sf_conjgrad_adj(bool adj /* adjoint flag */,
+		     sf_operator oper  /* linear operator */, 
+		     sf_operator shape /* shaping operator */, 
+		     float* p          /* preconditioned model */, 
+		     float* x          /* estimated model */, 
+		     float* dat        /* data */, 
+		     int niter         /* number of iterations */) 
+/*< Conjugate gradient solver with shaping and its adjoint. >*/
+{
+    double gn, gnp, alpha, beta, g0, dg, r0;
+    float *q, *sq, *gq, *y;
+    int i, iter;
+
+    q = sf_floatalloc(nx);
+    y = sf_floatalloc(nx);
+    sq = sf_floatalloc(nx);
+    gq = sf_floatalloc(nx);
+    
+    if (adj) { /* dat -> x */
+	for (i=0; i < nr; i++) {
+	    r[i] = - dat[i];
+	}
+	oper(true,false,nx,nr,q,r);
+    } else { /* x -> dat */
+	for (i=0; i < nx; i++) {
+	    q[i] = -x[i];
+	}
+    }
+    
+    for (i=0; i < np; i++) {
+	p[i] = 0.;
+    }
+    for (i=0; i < nx; i++) {
+	y[i] = 0.;
+    }
+    
+    dg = g0 = gnp = 0.;
+    r0 = cblas_snrm2(nx,q,1);
+    if (r0 == 0.) {
+	if (verb) sf_warning("zero residual: r0=%g",r0);
+	return;
+    }
+
+    for (iter=0; iter < niter; iter++) {
+	for (i=0; i < np; i++) {
+	    gp[i] = eps*p[i];
+	}
+	for (i=0; i < nx; i++) {
+	    gx[i] = q[i]-eps*y[i];
+	}
+
+	shape(true,true,np,nx,gp,gx);
+	shape(false,false,np,nx,gp,gx);
+
+	oper(false,false,nx,nr,gx,gr);
+	oper(true,false, nx,nr,gq,gr);
+
+	gn = cblas_dsdot(np,gp,1,gp,1);
+
+	if (iter==0) {
+	    g0 = gn;
+
+	    for (i=0; i < np; i++) {
+		sp[i] = gp[i];
+	    }
+	    for (i=0; i < nx; i++) {
+		sx[i] = gx[i];
+		sq[i] = gq[i];
+	    }
+	    for (i=0; i < nr; i++) {
+		sr[i] = gr[i];
+	    }
+	} else {
+	    alpha = gn / gnp;
+	    dg = gn / g0;
+
+	    if (alpha < tol || dg < tol) {
+		if (verb) 
+		    sf_warning(
+			"convergence in %d iterations, alpha=%g, gd=%g",
+			iter,alpha,dg);
+		break;
+	    }
+
+	    cblas_saxpy(np,alpha,sp,1,gp,1);
+	    cblas_sswap(np,sp,1,gp,1);
+
+	    cblas_saxpy(nx,alpha,sx,1,gx,1);
+	    cblas_sswap(nx,sx,1,gx,1);
+
+	    cblas_saxpy(nx,alpha,sq,1,gq,1);
+	    cblas_sswap(nx,sq,1,gq,1);
+
+	    cblas_saxpy(nr,alpha,sr,1,gr,1);
+	    cblas_sswap(nr,sr,1,gr,1);
+	}
+
+	beta = cblas_dsdot(nr,sr,1,sr,1) + eps*(cblas_dsdot(np,sp,1,sp,1) - cblas_dsdot(nx,sx,1,sx,1));
+	
+	if (verb) sf_warning("iteration %d res: %f grad: %f",
+			     iter,cblas_snrm2(nx,q,1)/r0,dg);
+
+	alpha = - gn / beta;
+	
+	cblas_saxpy(np,alpha,sp,1,p,1);
+	cblas_saxpy(nx,alpha,sx,1,y,1);
+	cblas_saxpy(nx,alpha,sq,1,q,1);
+	
+	gnp = gn;
+    }
+
+    if (adj) { /* dat -> x */
+	for (i=0; i < nr; i++) {
+	    x[i] = y[i];
+	}
+    } else { /* x -> dat */
+	oper(false,false,nx,nd,y,dat);
+    }   
+
+    free(q);
+    free(y);
+    free(sq);
+    free(gq);
+}
+
+
 /* 	$Id$	 */
