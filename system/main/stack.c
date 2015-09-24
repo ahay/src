9322d47fd97376ee70f65e8a8a1bb2fd8ffc7ef5
huidotwang <hui.wang@kaust.edu.sa>
2015-09-24 14:47:49 -0600

    fix bug with gcc optimization flag -O2

--- system/main/stack.c
+++ system/main/stack.c
@@ -29,7 +29,7 @@ This operation is adjoint to sfspray.
 
 int main(int argc, char* argv[])
 {
-    int j, ni, *fold = NULL, axis, ndims[SF_MAX_DIM],dim,lim;
+    int j, ni, n2_tmp, *fold = NULL, axis, ndims[SF_MAX_DIM],dim,lim;
     off_t i, n, i2, i3, n2, n3;
     sf_file in, out;
     char key1[7], *prog;
@@ -84,7 +84,8 @@ int main(int argc, char* argv[])
       }
     }else{
       sprintf(key1,"n%d",axis);
-      if (!sf_histint(in,key1,(int *)&n2)) sf_error("No %s= in input",key1);
+      if (!sf_histint(in,key1,&n2_tmp)) sf_error("No %s= in input",key1);
+      n2 = n2_tmp;
       n3 = sf_unshiftdim(in,out,axis);
     }
     if (n2 < 100) {
