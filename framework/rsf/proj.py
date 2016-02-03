e2ed0b80d5082447e0537d7cd6f898512030a6a0
Karl Schleicher <k_schleicher@hotmail.com>
2016-02-03 17:03:04 -0600

    resolve the conflict

--- framework/rsf/proj.py
+++ framework/rsf/proj.py
@@ -375,6 +375,7 @@ class Project(Environment):
         # the command wil be different and scons wilL think it needs to be 
         # rerun.
 
+        # resolve conflict here
         if len(self.nodes) > 1:
             self.hosts = self.cwd + '/hosts.txt'
             print "using %s to keep track of nodes in use by pscons."%self.hosts
@@ -393,6 +394,7 @@ class Project(Environment):
                 hosts_fd.write(string.ljust(host,40)+
                                string.ljust("notrunning",10)+"\n")
             hosts_fd.close()
+
         for key in self['ENV'].keys():
             # quote the env values because stampede has env variable 
             # SLURM_NODE_ALIASES=(null)  This makes problems in the ssh to nodes.
