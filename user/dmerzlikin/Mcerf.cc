77b19200e5a9543d5cf7fb281e1032f0247f2ebd
Sergey Fomel <sergey.fomel@gmail.com>
2016-02-15 09:06:25 -0600

    moved sfcerf to user/dmerzlikin

--- user/dmerzlikin/Mcerf.cc
+++ user/dmerzlikin/Mcerf.cc
@@ -17,7 +17,7 @@
 //   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <rsf.hh>
 
-#include "faddeeva.hh"
+#include "Faddeeva3.hh"
 
 int main(int argc, char** argv)
 {   
