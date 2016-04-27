e1392e9d7ee7cc176e84aaa505a17ff1a451d55e
Sergey Fomel <sergey.fomel@gmail.com>
2016-04-27 15:53:09 -0500

    gcc on a Mac

--- framework/configure.py
+++ framework/configure.py
@@ -268,13 +268,13 @@ def cc(context):
     if plat['OS'] == 'darwin':
         context.env['LINKFLAGS'] = context.env.get('LINKFLAGS','') + \
             ' -framework Accelerate '
-        context.env['CPPDEFINES'] = path_get(context,'CPPDEFINES',
-                                             '__ACCELERATE__')
         if os.path.isdir('/opt'):   # paths for MacPorts
             context.env['CPPPATH'] = path_get(context,'CPPPATH',
                                               '/opt/local/include')
             context.env['LIBPATH'] = path_get(context,'LIBPATH',
                                               '/opt/local/lib')
+            context.env['CPPDEFINES'] = path_get(context,'CPPDEFINES',
+                                                '__ACCELERATE__')
         if os.path.isdir('/sw'):    # paths for Fink
             context.env['CPPPATH'] = path_get(context,'CPPPATH',
                                               '/sw/include')
