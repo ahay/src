b84d0bc80f7226646120958d49d148e609e20f5f
Sergey Fomel <sergey.fomel@gmail.com>
2017-07-10 15:58:39 +0800

    cleanup

--- book/eeg/README.txt
+++
@@ -1,4 +0,0 @@
-This directory contains research from
-the EEG group at ETH Zuerich.
-
--Filippo Broggini
--- book/sep/rwe/rwe.py
+++
@@ -1,6 +0,0 @@
-from rsf.proj import *
-
-def slow(name,par):
-    Flow('slow_'+name,'vel_'+name,
-         'window n2=%(nx)d min2=%(ox)g | math output=1/input ' % par)
-    
