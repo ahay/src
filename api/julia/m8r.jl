6c66f9c68ec4745a3fcef5061421bf82148f99e5
cdacosta <c.dacostaf@gmail.com>
2017-11-24 11:44:47 +0000

    Uint8 -> UInt8

--- api/julia/m8r.jl
+++ api/julia/m8r.jl
@@ -4,47 +4,47 @@ export sf_init
 
 immutable File
      tag::String
-     rsf::Ptr{Uint8}
+     rsf::Ptr{UInt8}
 end
 
 function init()
 	 argv = [basename(Base.source_path())]
 	 append!(argv,ARGS)
-	 ccall((:sf_init,"libdrsf"),Void,(Int32,Ptr{Ptr{Uint8}}),length(argv),argv)
+	 ccall((:sf_init,"libdrsf"),Void,(Int32,Ptr{Ptr{UInt8}}),length(argv),argv)
 end
 
 function input(tag::String)
-	 rsf = ccall((:sf_input,"libdrsf"),Ptr{Uint8},(Ptr{Uint8},),tag)
+	 rsf = ccall((:sf_input,"libdrsf"),Ptr{UInt8},(Ptr{UInt8},),tag)
 	 File(tag,rsf)
 end
 
 function output(tag::String)
-	 rsf = ccall((:sf_output,"libdrsf"),Ptr{Uint8},(Ptr{Uint8},),tag)
+	 rsf = ccall((:sf_output,"libdrsf"),Ptr{UInt8},(Ptr{UInt8},),tag)
 	 File(tag,rsf)
 end
 
 function histint(file::File,name::String)
 	 val = Cint[0]
-	 ccall((:sf_histint,"libdrsf"),Bool,(Ptr{Uint8},Ptr{Uint8},Ptr{Cint}),file.rsf,name,val)
+	 ccall((:sf_histint,"libdrsf"),Bool,(Ptr{UInt8},Ptr{UInt8},Ptr{Cint}),file.rsf,name,val)
 	 return val[]
 end
 
 function leftsize(file::File,dim::Int)
-	 ccall((:sf_leftsize,"libdrsf"),Culonglong,(Ptr{Uint8},Cint),file.rsf,dim)
+	 ccall((:sf_leftsize,"libdrsf"),Culonglong,(Ptr{UInt8},Cint),file.rsf,dim)
 end
 
 function getfloat(name::String)
 	 val = Cfloat[0]
-	 ccall((:sf_getfloat,"libdrsf"),Bool,(Ptr{Uint8},Ptr{Cfloat}),name,val)
+	 ccall((:sf_getfloat,"libdrsf"),Bool,(Ptr{UInt8},Ptr{Cfloat}),name,val)
 	 return val[]
 end
 
 function floatread(arr::Array{Float32,1},size::Int32,file::File)
-	 ccall((:sf_floatread,"libdrsf"),Void,(Ptr{Cfloat},Csize_t,Ptr{Uint8}),arr,size,file.rsf)
+	 ccall((:sf_floatread,"libdrsf"),Void,(Ptr{Cfloat},Csize_t,Ptr{UInt8}),arr,size,file.rsf)
 end
 
 function floatwrite(arr::Array{Float32,1},size::Int32,file::File)
-	 ccall((:sf_floatwrite,"libdrsf"),Void,(Ptr{Cfloat},Csize_t,Ptr{Uint8}),arr,size,file.rsf)
+	 ccall((:sf_floatwrite,"libdrsf"),Void,(Ptr{Cfloat},Csize_t,Ptr{UInt8}),arr,size,file.rsf)
 end
 
 end
