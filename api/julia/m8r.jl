f0b72050e2d9aa8033c0fac2c194f89e45a99e54
cdacosta <c.dacostaf@gmail.com>
2017-11-24 14:02:40 +0000

    whitespace

--- api/julia/m8r.jl
+++ api/julia/m8r.jl
@@ -3,36 +3,36 @@ module m8r
 export sf_init
 
 immutable File
-     tag::String
-     rsf::Ptr{UInt8}
+    tag::String
+    rsf::Ptr{UInt8}
 end
 
 function init()
-	 argv = [basename(Base.source_path())]
-	 append!(argv,ARGS)
-	 ccall((:sf_init,"libdrsf"),Void,(Int32,Ptr{Ptr{UInt8}}),length(argv),argv)
+    argv = [basename(Base.source_path())]
+    append!(argv,ARGS)
+    ccall((:sf_init,"libdrsf"),Void,(Int32,Ptr{Ptr{UInt8}}),length(argv),argv)
 end
 
 function input(tag::String)
-	 rsf = ccall((:sf_input,"libdrsf"),Ptr{UInt8},(Ptr{UInt8},),tag)
-	 File(tag,rsf)
+    rsf = ccall((:sf_input,"libdrsf"),Ptr{UInt8},(Ptr{UInt8},),tag)
+    File(tag,rsf)
 end
 
 function output(tag::String)
-	 rsf = ccall((:sf_output,"libdrsf"),Ptr{UInt8},(Ptr{UInt8},),tag)
-	 File(tag,rsf)
+    rsf = ccall((:sf_output,"libdrsf"),Ptr{UInt8},(Ptr{UInt8},),tag)
+    File(tag,rsf)
 end
 
 function histint(file::File,name::String)
-	 val = Cint[0]
-	 ccall((:sf_histint,"libdrsf"),Bool,(Ptr{UInt8},Ptr{UInt8},Ptr{Cint}),file.rsf,name,val)
-	 return val[]
+    val = Cint[0]
+    ccall((:sf_histint,"libdrsf"),Bool,(Ptr{UInt8},Ptr{UInt8},Ptr{Cint}),file.rsf,name,val)
+    return val[]
 end
 
 function histfloat(file::File,name::String)
-	 val = Cfloat[0]
-	 ccall((:sf_histfloat,"libdrsf"),Bool,(Ptr{UInt8},Ptr{UInt8},Ptr{Cfloat}),file.rsf,name,val)
-	 return val[]
+    val = Cfloat[0]
+    ccall((:sf_histfloat,"libdrsf"),Bool,(Ptr{UInt8},Ptr{UInt8},Ptr{Cfloat}),file.rsf,name,val)
+    return val[]
 end
 
 function histstring(file::File,name::String)
@@ -44,31 +44,31 @@ function histstring(file::File,name::String)
 end
 
 function leftsize(file::File,dim::Int)
-	 ccall((:sf_leftsize,"libdrsf"),Culonglong,(Ptr{UInt8},Cint),file.rsf,dim)
+    ccall((:sf_leftsize,"libdrsf"),Culonglong,(Ptr{UInt8},Cint),file.rsf,dim)
 end
 
 function getfloat(name::String)
-	 val = Cfloat[0]
-	 ccall((:sf_getfloat,"libdrsf"),Bool,(Ptr{UInt8},Ptr{Cfloat}),name,val)
-	 return val[]
+    val = Cfloat[0]
+    ccall((:sf_getfloat,"libdrsf"),Bool,(Ptr{UInt8},Ptr{Cfloat}),name,val)
+    return val[]
 end
 
 function floatread(arr::Array{Float32,1},size::Int32,file::File)
-	 ccall((:sf_floatread,"libdrsf"),Void,(Ptr{Cfloat},Csize_t,Ptr{UInt8}),arr,size,file.rsf)
+    ccall((:sf_floatread,"libdrsf"),Void,(Ptr{Cfloat},Csize_t,Ptr{UInt8}),arr,size,file.rsf)
 end
 
 function floatwrite(arr::Array{Float32,1},size::Int32,file::File)
-	 ccall((:sf_floatwrite,"libdrsf"),Void,(Ptr{Cfloat},Csize_t,Ptr{UInt8}),arr,size,file.rsf)
+    ccall((:sf_floatwrite,"libdrsf"),Void,(Ptr{Cfloat},Csize_t,Ptr{UInt8}),arr,size,file.rsf)
 end
 
 function putint(file::File,name::String,val::Int)
     val = convert(Cint, val)
-	ccall((:sf_putint,"libdrsf"),Void,(Ptr{UInt8},Ptr{UInt8},Cint),file.rsf,name,val)
+    ccall((:sf_putint,"libdrsf"),Void,(Ptr{UInt8},Ptr{UInt8},Cint),file.rsf,name,val)
 end
 
 function putfloat(file::File,name::String,val::Real)
     val = convert(Cfloat, val)
-	ccall((:sf_putfloat,"libdrsf"),Void,(Ptr{UInt8},Ptr{UInt8},Cfloat),file.rsf,name,val)
+    ccall((:sf_putfloat,"libdrsf"),Void,(Ptr{UInt8},Ptr{UInt8},Cfloat),file.rsf,name,val)
 end
 
 function putstring(file::File,name::String,val::String)
