8b50025656aa7a08988635213dbe42b48f79db24
cdacosta <c.dacostaf@gmail.com>
2017-11-26 12:55:48 +0000

    rearragne tests for float

--- api/julia/test/runtests.jl
+++ api/julia/test/runtests.jl
@@ -1,13 +1,7 @@
 #!/usr/bin/env julia
 using Base.Test
 
-open("test_inp.rsf", "w") do rsf_f
-    Base.run(pipeline(`sfspike n1=2 k1=1,2,2
-                               n2=3 k2=1,2,3
-                               nsp=3 mag=1,4,2 out=stdout`, rsf_f))
-end
-Base.append!(ARGS, ["int1=1", "float1=1e-99", "str1=ḉ", "bool1=n",
-                    "file=test_inp.rsf"])
+Base.append!(ARGS, ["int1=1", "float1=1e-99", "str1=ḉ", "bool1=n"])
 
 using m8r
 m8r.init()
@@ -37,8 +31,13 @@ println("getbool")
 @test m8r.getbool("bool4") == true
 
 
-println("input")
-inp = m8r.input(m8r.getstring("file"))
+println("input float")
+open("test_inp_float.rsf", "w") do rsf_f
+    run(pipeline(`sfspike n1=2 k1=1,2,2
+                               n2=3 k2=1,2,3
+                               nsp=3 mag=1,4,2 out=stdout`, rsf_f))
+end
+inp = m8r.input("test_inp_float.rsf")
 @test m8r.size(inp) == [2, 3]
 @test m8r.gettype(inp) == 4
 dat = m8r.read(inp)
@@ -54,9 +53,10 @@ dat = m8r.read(inp)
 @test m8r.histstring(inp, "label2") == "Distance"
 @test m8r.histstring(inp, "unit1") == "s"
 @test m8r.histstring(inp, "unit2") == "km"
+run(`sfrm test_inp_float.rsf`)
 
-println("output")
-out = m8r.output("test_out.rsf")
+println("output float")
+out = m8r.output("test_out_float.rsf")
 m8r.putint(out, "n1", 1)
 m8r.putint(out, "n2", 2)
 m8r.putfloat(out, "d1", 3)
@@ -68,7 +68,33 @@ m8r.putstring(out, "label2", "é")
 m8r.putstring(out, "unit1", "普通话")
 m8r.putstring(out, "unit2", "µm")
 
-m8r.floatwrite(Float32[1; 2], Int32[m8r.leftsize(out, 0)][1], out)
+m8r.floatwrite(Float32[1.5; 2.5], Int32[m8r.leftsize(out, 0)][], out)
+
+@test m8r.histint(out, "n1") == 1
+@test m8r.histint(out, "n2") == 2
+@test m8r.histfloat(out, "d1") ≈ 3
+@test m8r.histfloat(out, "d2") ≈ 4
+@test m8r.histfloat(out, "o1") ≈ 5
+@test m8r.histfloat(out, "o2") ≈ 6
+@test m8r.histstring(out, "label1") == "a"
+@test m8r.histstring(out, "label2") == "é"
+@test m8r.histstring(out, "unit1") == "普通话"
+@test m8r.histstring(out, "unit2") == "µm"
+run(`sfrm test_out_float.rsf`)
+
+
+
+m8r.putint(out, "n1", 1)
+m8r.putint(out, "n2", 2)
+m8r.putfloat(out, "d1", 3)
+m8r.putfloat(out, "d2", 4)
+m8r.putfloat(out, "o1", 5)
+m8r.putfloat(out, "o2", 6)
+m8r.putstring(out, "label1", "a")
+m8r.putstring(out, "label2", "é")
+m8r.putstring(out, "unit1", "普通话")
+m8r.putstring(out, "unit2", "µm")
+
 
 @test m8r.histint(out, "n1") == 1
 @test m8r.histint(out, "n2") == 2
@@ -81,6 +107,4 @@ m8r.floatwrite(Float32[1; 2], Int32[m8r.leftsize(out, 0)][1], out)
 @test m8r.histstring(out, "unit1") == "普通话"
 @test m8r.histstring(out, "unit2") == "µm"
 
-Base.run(`sfrm test_inp.rsf`)
-Base.run(`sfrm test_out.rsf`)
 println("all good!")
