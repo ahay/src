b86f50617ca1f258e24d32bfb52bb490acfad805
cdacosta <c.dacostaf@gmail.com>
2017-12-19 12:09:39 -0500

    Improve docs for rsf_write

--- api/julia/m8r.jl
+++ api/julia/m8r.jl
@@ -380,13 +380,13 @@ Finally, one may write from a pipe to a file with:
     Do *not* supply the file as an `m8r.File` type unless you know exactly what
     you are doing. Because of how Madagascar is set up, calling `m8r.output`
     automatically sets the filetype to whatever was read in the previous
-    `m8r.input("in")` call. If this function has not yet been called, defaults
-    to `float`. Therefore, it is *impossible* to create a `complex` file
-    immediately after reading a `float`-type file. We overcome this limitation
-    with other `rsf_write` methods which write dummy files to dummy pipes to
-    "trick" Madagascar into switching file types.
+    `m8r.input("in")` call. If this function has not yet been called, it
+    defaults to `float`. Therefore, it is *impossible* to create a `complex`
+    file immediately after reading a `float`-type file. We overcome this
+    limitation with other `rsf_write` methods by writing dummy files to dummy
+    pipes to "trick" Madagascar into switching file types.
 
-    In addition, do *not* try to write to a filehandle which has already been
+    In addition, do *not* try to write to a filer handle which has already been
     written to with `rsf_write`, as `rsf_write` closes the file. Doing so will
     cause a segfault.
 
