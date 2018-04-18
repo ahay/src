1275b7dd39a3a7d00bf5c14093015579c4c4a3c6
Kit Chambers <kitchambers@nanometrics.ca>
2018-04-18 20:44:54 +0100

    Revert "Minor fix to alow compile"
    This reverts commit aac96e43c2ce3a43307676ec5adb40df53d9f4f4.

--- user/jsun/lowrank.cc
+++ user/jsun/lowrank.cc
@@ -15,8 +15,8 @@
 //   along with this program; if not, write to the Free Software
 //   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <rsf.hh>
-#include "../lexing/vecmatop.hh"
-#include "../lexing/serialize.hh"
+#include "vecmatop.hh"
+#include "serialize.hh"
 /* head files aumatically produced from C programs */
 extern "C"{
 #include "rtmutil.h"
