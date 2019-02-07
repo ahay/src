1eedf72dbfd99e5e5cb948ac631fe782e1384dc5
Luke <deckerla@gmail.com>
2019-02-07 13:20:19 -0600

    dtw

--- user/luke/Mdtw.c
+++ user/luke/Mdtw.c
@@ -27,7 +27,10 @@ int main (int argc, char* argv[])
 	if (!sf_histfloat(_in,"o1",&o1))   sf_error("No o1=");
     /* maximum shift */
     /* Get smoothing radius */
-    if (!sf_getint("maxshift",&maxshift))   maxshift=20;
+    if (!sf_getint("maxshift",&maxshift)){   
+		sf_warning("maxshift set to 20");
+		maxshift=20;
+	}
     /* maximum shift */
     if (!sf_getfloat("exp",&ex))   ex = 2;
     /* error exponent (g-f)^exp */
