36d751ea9cb54ba66ebe5067d84ae7e577e3774e
Sergey Fomel <sergey.fomel@gmail.com>
2019-03-01 08:21:32 -0600

    cascade

--- user/fomels/Mboxcascade.c
+++ user/fomels/Mboxcascade.c
@@ -20,10 +20,8 @@
 
 int main(int argc, char* argv[])
 {
-    bool tri;
     int n1, n2, i1, i2, n, i, m;
-    float r;
-    sf_complex *trace, c, new, pre;
+    float r, *trace, *trace2;
     sf_file inp, out;
 
     sf_init(argc,argv);
@@ -33,42 +31,31 @@ int main(int argc, char* argv[])
     if (!sf_histint(inp,"n1",&n1)) sf_error("No n1= in input");
     n2 = sf_leftsize(inp,1);
 
-    trace = sf_complexalloc(n1);
-
-    if (!sf_getint("rect",&n)) n=1;
+    trace = sf_floatalloc(n1);
+    trace2 = sf_floatalloc(n1);
+    
+    if (!sf_getint("rect",&n)) n=0;
     /* smoothing radius */
 
     if (!sf_getint("inter",&m)) m=n;
     /* interrupt */
 
-    if (!sf_getbool("triangle",&tri)) tri=false;
-    /* if triangle filter */
-
     for (i2=0; i2 < n2; i2++) {
-	sf_complexread(trace,n1,inp);
+	sf_floatread(trace,n1,inp);
 	for (i=1; i < n; i++) {
-	    r = 2*SF_PI*i/n;
-	    c = -1.0f/sf_cmplx(cosf(r),sinf(r));
-	    pre = 0.0f;
-	    for (i1=0; i1 < n1; i1++) {
-		new = trace[i1];
-		trace[i1] += c*pre;
-		pre = new;
-		if (i==n-1) trace[i1] /= n;
+	    r = -2*cosf(2*SF_PI*i/n);
+	    trace2[0] = r*trace[0]+trace[1];
+	    for (i1=1; i1 < n1-1; i1++) {
+		trace2[i1] = r*trace[i1]+trace[i1-1]+trace[i1+1];
 	    }
-	    if (tri) {
-		pre = 0.0f;
-		for (i1=n1-1; i1 >= 0; i1--) {
-		    new = trace[i1];
-		    trace[i1] += c*pre;
-		    pre = new;
-		    if (i==n-1) trace[i1] /= n;
-		}
+	    trace2[n1-1] = r*trace[n1-1]+trace[n1-2];
+	    for (i1=0; i1 < n1; i1++) {
+		trace[i1] = (i < n-1)? trace2[i1]: trace2[i1]/(n*n);
 	    }
 	    if (i==m) break;	
 	}
 	
-	sf_complexwrite(trace,n1,out);
+	sf_floatwrite(trace,n1,out);
     }
 
     exit(0);
