e086e11b32679c51352dcc4f0ba47d2a9c0f858e
Zhicheng Geng <zhichenggeng@utexas.edu>
2019-03-20 17:41:38 -0500

    Compatibility with python 2/3

--- user/godwinj/Mgui.py
+++ user/godwinj/Mgui.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-''' 
+'''
 Open tkMadagascar GUI
 '''
+from __future__ import print_function
 import sys
 try:
     from Tkinter import *
-except Exception, e:
-    print 'Could not find Tkinter installation: ', e
+except Exception as e:
+    print('Could not find Tkinter installation: ', e)
     sys.exit(1)
-    
+
 try:
     import rsf.gui.Gui as gui
     gui.launch()
-except Exception, e:
-    print 'Failed to start tkMadagascar: ', e
+except Exception as e:
+    print('Failed to start tkMadagascar: ', e)
