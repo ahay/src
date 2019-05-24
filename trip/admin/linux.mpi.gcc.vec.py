f5b1dd7c6320a41169e02962340604ac7c7e18ab
Zhicheng Geng <zhichenggeng@utexas.edu>
2019-05-24 11:09:08 -0500

    Clean core

--- trip/admin/linux.mpi.gcc.vec.py
+++
@@ -1,4 +0,0 @@
-CC = 'mpicc' 
-CCFLAGS = '-O3 -mfpmath=sse -ffast-math -falign-functions=16 -fstrict-aliasing -ftree-vectorize -ftree-vectorizer-verbose=5 -fprefetch-loop-arrays --param vect-max-version-for-alias-checks=200 -pedantic -Wunused -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DIWAVE_USE_MPI'
-CFLAGS = '-std=c99 -Wimplicit'
-CXX = 'mpicxx'
