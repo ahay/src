7789ca472f81f73d0cf10d2ed512edfffacfa47d
Zhicheng Geng <zhichenggeng@utexas.edu>
2019-09-09 17:13:52 -0500

    Update admin/release and change version to 3.0

--- admin/release/printmains.py
+++ admin/release/printmains.py
@@ -8,32 +8,32 @@ for main in glob.glob('M*.c'):
     mains.append(main[1:-2])
 if mains:
     mains.sort()
-    print "C:", ' '.join(mains)
+    print("C:", ' '.join(mains))
 
 mains = []
 for main in glob.glob('M*.cc'):
     mains.append(main[1:-3])
 if mains:
     mains.sort()
-    print "C++:", ' '.join(mains)
+    print("C++:", ' '.join(mains))
 
 mains = []
 for main in glob.glob('M*.cu'):
     mains.append(main[1:-3])
 if mains:
     mains.sort()
-    print "CUDA:", ' '.join(mains)
+    print("CUDA:", ' '.join(mains))
 
 mains = []
 for main in glob.glob('M*.f90'):
     mains.append(main[1:-4])
 if mains:
     mains.sort()
-    print "F90:", ' '.join(mains)
+    print("F90:", ' '.join(mains))
 
 mains = []
 for main in glob.glob('M*.py'):
     mains.append(main[1:-3])
 if mains:
     mains.sort()
-    print "Python:", ' '.join(mains)
+    print("Python:", ' '.join(mains))
