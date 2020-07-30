1fb993dba5a377e43f46133adba0b11747e8f4a6
chenyk1990 <chenyk1990@gmail.com>
2020-07-30 15:50:00 -0500

    northo

---
+++ user/chenyk/ntriangle.c
@@ -0,0 +1,227 @@
+/* Non-stationary triangle smoothing (fixed version) 
+Note Fomel's ntriangle.c may contain bugs in user/fomel/ntriangle.c
+t[i] -> t[o+i*d]; s[i]->s[o+i*d] in triple */
+/*
+  Copyright (C) 2020 University of Texas at Austin
+  Corrected by Yangkang Chen, Jan, 2020
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <rsf.h>
+
+#include "ntriangle.h"
+
+#ifndef _ntriangle_h
+
+typedef struct Ntriangle *ntriangle;
+/* abstract data type */
+/*^*/
+#endif
+
+struct Ntriangle {
+    float *tmp;
+    int np, nb, nx;
+};
+
+static void fold (int o, int d, int nx, int nb, int np, 
+		  const float *x, float* tmp);
+static void fold2 (int o, int d, int nx, int nb, int np, 
+		   float *x, const float* tmp);
+static void doubint (int nx, float *x, bool der);
+static void triple (int o, int d, int nx, int nb, 
+		    const float* t, const int* s, float* x, const float* tmp);
+static void triple2 (int o, int d, int nx, int nb, 
+		     const float* t, const int* s, const float* x, float* tmp);
+
+ntriangle ntriangle_init (int nbox /* maximum triangle length */, 
+			  int ndat /* data length */)
+/*< initialize >*/
+{
+    ntriangle tr;
+
+    tr = (ntriangle) sf_alloc(1,sizeof(*tr));
+
+    tr->nx = ndat;
+    tr->nb = nbox;
+    tr->np = ndat + 2*nbox;
+    
+    tr->tmp = sf_floatalloc(tr->np);
+
+    return tr;
+}
+
+static void fold (int o, int d, int nx, int nb, int np, 
+		  const float *x, float* tmp)
+{
+    int i, j;
+
+    /* copy middle */
+    for (i=0; i < nx; i++) 
+	tmp[i+nb] = x[o+i*d];
+    
+    /* reflections from the right side */
+    for (j=nb+nx; j < np; j += nx) {
+	for (i=0; i < nx && i < np-j; i++)
+	    tmp[j+i] = x[o+(nx-1-i)*d];
+	j += nx;
+	for (i=0; i < nx && i < np-j; i++)
+	    tmp[j+i] = x[o+i*d];
+    }
+    
+    /* reflections from the left side */
+    for (j=nb; j >= 0; j -= nx) {
+	for (i=0; i < nx && i < j; i++)
+	    tmp[j-1-i] = x[o+i*d];
+	j -= nx;
+	for (i=0; i < nx && i < j; i++)
+	    tmp[j-1-i] = x[o+(nx-1-i)*d];
+    }
+}
+
+static void fold2 (int o, int d, int nx, int nb, int np, 
+		   float *x, const float* tmp)
+{
+    int i, j;
+
+    /* copy middle */
+    for (i=0; i < nx; i++) 
+	x[o+i*d] = tmp[i+nb];
+
+    /* reflections from the right side */
+    for (j=nb+nx; j < np; j += nx) {
+	for (i=0; i < nx && i < np-j; i++)
+	    x[o+(nx-1-i)*d] += tmp[j+i];
+	j += nx;
+	for (i=0; i < nx && i < np-j; i++)
+	    x[o+i*d] += tmp[j+i];
+    }
+    
+    /* reflections from the left side */
+    for (j=nb; j >= 0; j -= nx) {
+	for (i=0; i < nx && i < j; i++)
+	    x[o+i*d] += tmp[j-1-i];
+	j -= nx;
+	for (i=0; i < nx && i < j; i++)
+	    x[o+(nx-1-i)*d] += tmp[j-1-i];
+    }
+}
+    
+static void doubint (int nx, float *xx, bool der)
+{
+    int i;
+    float t;
+
+    /* integrate backward */
+    t = 0.;
+    for (i=nx-1; i >= 0; i--) {
+	t += xx[i];
+	xx[i] = t;
+    }
+
+    if (der) return;
+
+    /* integrate forward */
+    t=0.;
+    for (i=0; i < nx; i++) {
+	t += xx[i];
+	xx[i] = t;
+    }
+}
+
+static void triple (int o, int d, int nx, int nb, 
+		    const float* t,
+		    const int* s,
+		    float* x, const float* tmp)
+{
+    int i, nt, nt1, ns;
+    float tt, wt, wt1;
+
+    for (i=0; i < nx; i++) {
+	tt = t[o+i*d];
+	nt = floorf(tt);
+	nt1 = nt+1;
+	ns = nb + s[o+i*d];
+	wt  = (nt1*nt1-tt*tt)/(nt*nt*(nt+nt1));
+	wt1 = (tt*tt-nt*nt)/(nt1*nt1*(nt+nt1));
+	
+// 	sf_warning("ix/nx=%d/%d,tt=%g,nt=%d,ns=%d,wt=%g,wt1=%g",i,nx,tt,nt,ns,wt,wt1);
+	
+	x[o+i*d] = 2*(wt+wt1)*tmp[i+ns] - 
+	    (tmp[i+ns-nt1] + tmp[i+ns+nt1])*wt1 - 
+	    (tmp[i+ns-nt]  + tmp[i+ns+nt ])*wt;
+    }
+}
+
+static void triple2 (int o, int d, int nx, int nb, 
+		     const float* t,
+		     const int* s,
+		     const float* x, float* tmp)
+{
+    int i, nt, nt1, ns;
+    float tt, wt, wt1;
+
+    for (i=0; i < nx + 2*nb; i++) {
+	tmp[i] = 0;
+    }
+
+    for (i=0; i < nx; i++) {
+	tt = t[i];
+	nt = floorf(tt);
+	nt1 = nt+1;
+	ns = nb + s[i];
+	wt  = x[o+i*d]*(nt1*nt1-tt*tt)/(nt*nt*(nt+nt1));
+	wt1 = x[o+i*d]*(tt*tt-nt*nt)/(nt1*nt1*(nt+nt1));
+	tmp[i+ns-nt1] -= wt1; 
+	tmp[i+ns-nt]  -= wt; 
+	tmp[i+ns]     += 2*(wt+wt1);
+	tmp[i+ns+nt]  -= wt;
+	tmp[i+ns+nt1] -= wt1;
+    }
+}
+
+void nsmooth (ntriangle tr /* smoothing object */, 
+	      int o, int d /* sampling. o: starting index, d: stride in samples for 1/2/3rd dimension; all refer to a correct index in a 1D vector  */, 
+	      bool der     /* derivative flag */, 
+	      const float *t /* triangle lengths */, 
+	      const int *s /* triangle shifts */,
+	      float *x     /* data (smoothed in place) */)
+/*< smooth >*/
+{
+    fold (o,d,tr->nx,tr->nb,tr->np,x,tr->tmp); 
+    doubint (tr->np,tr->tmp,der);
+    triple (o,d,tr->nx,tr->nb,t,s,x,tr->tmp);
+}
+
+void nsmooth2 (ntriangle tr /* smoothing object */, 
+	       int o, int d /* sampling */, 
+	       bool der     /* derivative flag */, 
+	       const float *t /* triangle lengths */,
+	       const int *s /* triangle shifts */,
+	       float *x     /* data (smoothed in place) */)
+/*< alternative smooth >*/
+{
+    triple2 (o,d,tr->nx,tr->nb,t,s,x,tr->tmp);
+    doubint (tr->np,tr->tmp,der);
+    fold2 (o,d,tr->nx,tr->nb,tr->np,x,tr->tmp);
+}
+
+void  ntriangle_close(ntriangle tr)
+/*< free allocated storage >*/
+{
+    free (tr->tmp);
+    free (tr);
+}
+
