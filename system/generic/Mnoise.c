8394dee797bcf2a4d96ae82fe195ec39683944dd
psava <psava@mines.edu>
2021-01-10 01:16:28 -0700

    complex noise

--- system/generic/Mnoise.c
+++ system/generic/Mnoise.c
@@ -1,26 +1,3 @@
-/* Add random noise to the data.
-
-July 2011 program of the month:
-http://ahay.org/blog/2011/07/03/program-of-the-month-sfnoise/
-*/
-/*
-  Copyright (C) 2004 University of Texas at Austin
-  
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
 #include <math.h>
 #include <time.h>
 
@@ -28,79 +5,167 @@ http://ahay.org/blog/2011/07/03/program-of-the-month-sfnoise/
 
 int main (int argc, char* argv[])
 {
-    float mean, var, range, a, b, *dat;
-    size_t nbuf, nsiz;
-    int seed;
-    size_t i;
-    bool normal, rep;
-    sf_file in, out;
+  float   mean, var, range, a, b;
+  size_t  nbuf, nsiz;
+  int     seed;
+  size_t  i;
+  bool    normal, rep, isreal;
+  sf_file in, out;
 
-    sf_init (argc,argv);
-    in = sf_input ("in");
-    out = sf_output("out");
+  float      *datR; /*    real data */
+  sf_complex *datC; /* complex data */
+  float      re,im;
 
-    if (SF_FLOAT != sf_gettype(in)) sf_error("Need float input");
+  sf_init (argc,argv);
+  in  = sf_input ( "in");
+  out = sf_output("out");
 
-    if (!sf_getint("seed",&seed)) seed = time(NULL);
-    /* random seed */
-    init_genrand((unsigned long) seed);
+  /* ------------------------------------------------------------ */
+  //if (SF_FLOAT != sf_gettype(in)) sf_error("Need float input");
 
-    if (!sf_getbool("type",&normal)) normal=true;
-    /* noise distribution, y: normal, n: uniform */
-    if (!sf_getfloat("var",&var)) {
+  if (SF_FLOAT == sf_gettype(in))
+    isreal = true;
+  else
+    isreal = false;
+
+  /* ------------------------------------------------------------ */
+  /* random seed */
+  if (!sf_getint("seed",&seed)) seed = time(NULL);
+  init_genrand((unsigned long) seed);
+
+  /* ------------------------------------------------------------ */
+  if (!sf_getbool("type",&normal)) normal=true;
+  /* noise distribution, y: normal, n: uniform */
+
+  if (!sf_getfloat("var",&var)) {
 	/* noise variance */
-	if (!sf_getfloat("range",&range)) {
-	    /* noise range (default=1) */
-	    a = 1.;
-	} else {
-	    a = normal? 2.*range/9. : 2.*range;
-	}
-    } else {
-	a = normal? sqrtf(var): sqrtf(12*var);
-    }
 
-    if (!sf_getfloat("mean",&mean)) mean=0;
-    /* noise mean */
-    b = normal? mean: mean - 0.5*a;
+  	if (!sf_getfloat("range",&range)) {
+  	    /* noise range (default=1) */
+  	    a = 1.;
+  	} else {
+  	    a = normal ? 2.*range/9. : 2.*range;
+  	}
 
-    if (!sf_getbool("rep",&rep)) rep=false;
-    /* if y, replace data with noise */
+  } else {
+	   a = normal ? sqrtf(var) : sqrtf(12*var);
+  }
 
+  /* ------------------------------------------------------------ */
+  if (!sf_getfloat("mean",&mean)) mean=0;
+  /* noise mean */
+  b = normal ? mean : mean - 0.5*a;
+
+  /* ------------------------------------------------------------ */
+  if (!sf_getbool("rep",&rep)) rep=false;
+  /* if y, replace data with noise */
+
+  /* ------------------------------------------------------------ */
+  if(isreal) {
     nbuf = BUFSIZ/sizeof(float);
-    dat = sf_floatalloc (nbuf);
+    datR = sf_floatalloc(nbuf);
+  } else {
+    nbuf = BUFSIZ/sizeof(sf_complex);
+    datC = sf_complexalloc(nbuf);
+  }
 
-    for (nsiz = sf_filesize(in); nsiz > 0; nsiz -= nbuf) {
-	if (nbuf > nsiz) nbuf = nsiz;
+  /* ------------------------------------------------------------ */
+  for (nsiz = sf_filesize(in); nsiz > 0; nsiz -= nbuf) {
+  	if (nbuf > nsiz) nbuf = nsiz;
 
-	if (rep) {
-	    if (normal) {
-		for (i=0; i < nbuf; i++) {
-		    dat[i] = a*sf_randn_one_bm() + b;
-		}
-	    } else {
-		for (i=0; i < nbuf; i++) {
-		    dat[i] = a*genrand_real1() + b;
-		}
-	    }
-	} else {
-	    sf_floatread(dat,nbuf,in);
-	    
-	    if (normal) {
-		for (i=0; i < nbuf; i++) {
-		    dat[i] += a*sf_randn_one_bm() + b;
-		}
-	    } else {
-		for (i=0; i < nbuf; i++) {
-		    dat[i] += a*genrand_real1() + b;
-		}
-	    }
-	}
+    if (rep) {
 
-	sf_floatwrite(dat,nbuf,out);  
+      if(isreal) { /* generate real noise */
+
+        if (normal) {
+          for (i=0; i < nbuf; i++)
+            datR[i] = a * sf_randn_one_bm() + b;
+        } else {
+          for (i=0; i < nbuf; i++)
+            datR[i] = a * genrand_real1() + b;
+        }
+
+      } else { /* generate complex noise */
+
+        if (normal) {
+          for (i=0; i < nbuf; i++) {
+            re = a * sf_randn_one_bm() + b;
+            im = a * sf_randn_one_bm() + b;
+            #ifdef SF_HAS_COMPLEX_H
+              datC[i]   = re+im*I;
+            #else
+              datC[i].r = re;
+              datC[i].i = im;
+            #endif
+          }
+        } else {
+          for (i=0; i < nbuf; i++) {
+            re = a * genrand_real1() + b;
+            im = a * genrand_real1() + b;
+            #ifdef SF_HAS_COMPLEX_H
+              datC[i]   = re+im*I;
+            #else
+              datC[i].r = re;
+              datC[i].i = im;
+            #endif
+          }
+        }
+
+      }
+
+    } else {
+
+      if(isreal) sf_floatread  (datR,nbuf,in);
+      else       sf_complexread(datC,nbuf,in);
+
+      if(isreal) { /* add real noise */
+
+        if (normal) {
+          for (i=0; i < nbuf; i++)
+            datR[i] += a * sf_randn_one_bm() + b;
+        } else {
+          for (i=0; i < nbuf; i++)
+            datR[i] += a * genrand_real1() + b;
+        }
+
+      } else { /* add complex noise */
+
+        if (normal) {
+          for (i=0; i < nbuf; i++) {
+            re = a * sf_randn_one_bm() + b;
+            im = a * sf_randn_one_bm() + b;
+            #ifdef SF_HAS_COMPLEX_H
+              datC[i]   += re+im*I;
+            #else
+              datC[i].r += re;
+              datC[i].i += im;
+            #endif
+          }
+        } else {
+          for (i=0; i < nbuf; i++) {
+            re = a * genrand_real1() + b;
+            im = a * genrand_real1() + b;
+            #ifdef SF_HAS_COMPLEX_H
+              datC[i]   += re+im*I;
+            #else
+              datC[i].r += re;
+              datC[i].i += im;
+            #endif
+          }
+        }
+
+      }
+
     }
 
+    if(isreal) sf_floatwrite  (datR,nbuf,out);
+    else       sf_complexwrite(datC,nbuf,out);
+  }
 
-    exit(0);
+  if(isreal) free(datR);
+  else       free(datC);
+
+  exit(0);
 }
 
 /* 	$Id$	 */
