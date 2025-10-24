b44c61697f7a37a107e682211bb6904f5d070ba1
Sergey Fomel <sergey.fomel@gmail.com>
2025-10-24 14:29:59 -0500

    convection

--- user/fomels/Mconvection.c
+++ user/fomels/Mconvection.c
@@ -22,79 +22,92 @@
 
 int main (int argc, char* argv[])
 {
-  bool verb;
-  int niter, n1, n2, n12, i1, i2, rect[2], m[2];
-  float **data, **rhs, ***conv, ***lhs, dif, mean;
-  sf_file in, flt;
+    bool verb;
+    int niter, order, n1, n2, n12, i1, i2, iw, nw, rect[2], m[2];
+    float **data, **rhs, ***conv, ***lhs, dif, mean;
+    sf_file in, flt;
 
-  sf_init(argc, argv);
-  in = sf_input("in");
-  flt = sf_input("out");
+    sf_init(argc, argv);
+    in = sf_input("in");
+    flt = sf_output("out");
 
-  if (SF_FLOAT != sf_gettype(in)) sf_error("Need float input");
-  if (!sf_histint(in,"n1",&n1)) sf_error("Need n1= in input");
-  if (!sf_histint(in,"n2",&n2)) sf_error("Need n2= in input");
+    if (SF_FLOAT != sf_gettype(in)) sf_error("Need float input");
+    if (!sf_histint(in,"n1",&n1)) sf_error("Need n1= in input");
+    if (!sf_histint(in,"n2",&n2)) sf_error("Need n2= in input");
 
-  sf_putint(flt, "n3", 2);
+    if (!sf_getint("order",&order)) order=1;
+    /* accuracy order */
+    nw = 2*order;
+    
+    sf_putint(flt, "n3", nw);
 
-  if (!sf_getint("rect1",&rect[0])) rect[0]=1;
-  if (!sf_getint("rect2",&rect[1])) rect[1]=1;
-  /* smoothing */
+    if (!sf_getint("rect1",&rect[0])) rect[0]=1;
+    if (!sf_getint("rect2",&rect[1])) rect[1]=1;
+    /* smoothing */
 
-  if (!sf_getint("niter",&niter)) niter=100;
-  /* number of iterations */
+    if (!sf_getint("niter",&niter)) niter=100;
+    /* number of iterations */
 
-  if (!sf_getbool("verb",&verb)) verb=true;
-  /* verbosity flag */
+    if (!sf_getbool("verb",&verb)) verb=true;
+    /* verbosity flag */
+
   
-  data = sf_floatalloc2(n1,n2);
-  rhs = sf_floatalloc2(n1,n2);
+    data = sf_floatalloc2(n1,n2);
+    rhs = sf_floatalloc2(n1,n2);
   
-  conv = sf_floatalloc3(n1,n2,2);
-  lhs = sf_floatalloc3(n1,n2,2);
+    conv = sf_floatalloc3(n1,n2,nw);
+    lhs = sf_floatalloc3(n1,n2,nw);
 
-  n12 = n1*n2;
-  m[0] = n1;
-  m[1] = n2;
+    n12 = n1*n2;
+    m[0] = n1;
+    m[1] = n2;
 
-  sf_multidivn_init(2, 2, n12, m, rect, **lhs, NULL, verb); 
+    sf_multidivn_init(nw, 2, n12, m, rect, **lhs, NULL, verb); 
 
-  sf_floatread(data[0],n12,in);
+    sf_floatread(data[0],n12,in);
 
-  mean = 0.;
-  for (i2=0; i2 < n2-1; i2++) {
-    rhs[i2][0] = 0.0f;
-    lhs[0][i2][0] = 0.0f;
-    lhs[1][i2][0] = 0.0f;
-    for (i1=1; i1 < n1-1; i1++) {
-      dif = data[i2+1][i1] - data[i2][i1];
-      rhs[i2][i1] = dif;
-      lhs[0][i2][i1] = dif - data[i2+1][i1+1] + data[i2][i1-1];
-      lhs[1][i2][i1] = dif - data[i2+1][i1-1] + data[i2][i1+1];
-      mean += lhs[0][i2][i1]*lhs[0][i2][i1] + lhs[1][i2][i1]*lhs[1][i2][i1];
+    mean = 0.;
+    for (i2=0; i2 < n2-1; i2++) {
+	rhs[i2][0] = 0.0f;
+	for (iw=0; iw < nw; iw++) {
+	    lhs[iw][i2][0] = 0.0f;
+	}
+	for (i1=1; i1 < n1-1; i1++) {
+	    dif = data[i2+1][i1] - data[i2][i1];
+	    rhs[i2][i1] = dif;
+	    for (iw=0; iw < order; iw++) {
+		lhs[2*iw  ][i2][i1] = dif - data[i2+1][i1+iw] + data[i2][i1-iw];
+		lhs[2*iw+1][i2][i1] = dif - data[i2+1][i1-iw] + data[i2][i1+iw];
+	    }
+	    for (iw=0; iw < nw; iw++) {
+		mean += lhs[iw][i2][i1]*lhs[iw][i2][i1];
+	    }
+	}
+	rhs[i2][n1-1] = 0.0f;
+	for (iw=0; iw < nw; iw++) {
+	    lhs[iw][i2][n1-1] = 0.0f;
+	}
     }
-    rhs[i2][n1-1] = 0.0f;
-    lhs[0][i2][n1-1] = 0.0f;
-    lhs[1][i2][n1-1] = 0.0f;
-  }
-  for (i1=0; i1 < n1; i1++) {
-    rhs[n2-1][i1] = 0.0f;
-    lhs[0][n2-1][i1] = 0.0f;
-    lhs[1][n2-1][i1] = 0.0f;
-  }
-  mean = sqrtf (mean/(n12*2));
-  for (i2=0; i2 < n2-1; i2++) {
-    for (i1=1; i1 < n1-1; i1++) {
-      rhs[i2][i1] /= mean;
-      lhs[0][i2][i1] /= mean;
-      lhs[1][i2][i1] /= mean;
+    for (i1=0; i1 < n1; i1++) {
+	rhs[n2-1][i1] = 0.0f;
+	for (iw=0; iw < nw; iw++) {
+	    lhs[iw][n2-1][i1] = 0.0f;
+	}
     }
-  }
+    mean = sqrtf (mean/(n12*2));
+    for (i2=0; i2 < n2-1; i2++) {
+	for (i1=1; i1 < n1-1; i1++) {
+	    rhs[i2][i1] /= mean;
+	    for (iw=0; iw < nw; iw++) {
+		lhs[iw][i2][i1] /= mean;
+	    }
+	}
+    }
 
-  sf_multidivn (*rhs,**conv,niter);
-  sf_floatwrite(**conv,n12*2,flt);
+    sf_multidivn (*rhs,**conv,niter);
+    sf_floatwrite(**conv,n12*nw,flt);
   
-  exit(0);
+    exit(0);
 }
   
 
